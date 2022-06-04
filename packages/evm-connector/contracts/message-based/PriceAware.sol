a220f7267fbad94e16da5dfba596846e44da5781
hatskier <suvorovalex1997@gmail.com>
2022-06-04 21:37:46 +0200

    Changes

--- packages/evm-connector/contracts/message-based/PriceAware.sol
+++ packages/evm-connector/contracts/message-based/PriceAware.sol
@@ -2,13 +2,24 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+// import "hardhat/console.sol";
+
+// import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+// import "../commons/BytesLib.sol";
 
 abstract contract PriceAware {
-  using ECDSA for bytes32;
+  // using ECDSA for bytes32;
 
   uint256 constant _MAX_DATA_TIMESTAMP_DELAY = 3 * 60; // 3 minutes
   uint256 constant _MAX_BLOCK_TIMESTAMP_DELAY = 15; // 15 seconds
+
+  // Constants for better readablity of the assembly code
+  uint256 constant CALLDATA_SLOT_BYTES_COUNT = 32;
+  uint256 constant NEXT_FREE_MEMORY_POINTER_LOCATION = 0x40;
+  uint256 constant SIGNATURE_BYTES_COUNT = 65;
+  uint256 constant DATAPOINTS_NUMBER_BYTES_COUNT = 2;
+  uint256 constant DATAPOINTS_NUMBER_AND_SIGNATURE_BYTES_COUNT = 67; // 65 + 2
 
   /* ========== VIRTUAL FUNCTIONS (MAY BE OVERRIDEN IN CHILD CONTRACTS) ========== */
 
@@ -65,59 +76,145 @@ abstract contract PriceAware {
     // The data that is signed (symbols, values, timestamp) are inside the {} brackets
     // [origina_call_data| ?]{[[symbol | 32][value | 32] | n times][timestamp | 32]}[size | 1][signature | 65]
 
-    // 1. First we extract dataSize - the number of data items (symbol,value pairs) in the message
-    uint16 dataSize; // Number of data points
+    // 0. Init helpful variables
+    // uint256 calldataBytesCount = msg.data.length;
+
+    // TODO: remove
+    // bytes memory copiedCalldata = copyBytes(msg.data);
+    // console.log("\nCalldata");
+    // console.logBytes(copiedCalldata);
+
+    // 1. Extracting dataPointsCount - the number of data points
+    uint16 dataPointsCount; // Number of data points
     assembly {
       // Calldataload loads slots of 32 bytes
       // The last 65 bytes are for signature
       // We load the previous 32 bytes and automatically take the 2 least significant ones (casting to uint16)
-      dataSize := calldataload(sub(calldatasize(), 97))
-    }
-
-    // 2. We calculate the size of signable message expressed in bytes
-    // ((symbolLen(32) + valueLen(32)) * dataSize + timeStamp length
-    uint16 messageLength = dataSize * 64 + 32; // Length of data message in bytes
-
-    // 3. We extract the signableMessage
-
-    // (That's the high level equivalent 2k gas more expensive)
-    // bytes memory rawData = msg.data.slice(msg.data.length - messageLength - 65, messageLength);
-
-    bytes memory signableMessage;
-    assembly {
-      signableMessage := mload(0x40)
-      mstore(signableMessage, messageLength)
-      // The starting point is callDataSize minus length of data(messageLength), signature(65) and size(2) = 67
-      calldatacopy(
-        add(signableMessage, 0x20),
-        sub(calldatasize(), add(messageLength, 67)),
-        messageLength
+      dataPointsCount := calldataload(
+        sub(
+          calldatasize(),
+          add(SIGNATURE_BYTES_COUNT, CALLDATA_SLOT_BYTES_COUNT)
+        )
       )
-      mstore(0x40, add(signableMessage, 0x20))
     }
+
+    // 2. Calculating the size of signed message expressed in bytes
+    // ((symbolLen(32) + valueLen(32)) * dataSize + timestamp bytes size
+    // uint16 signedMessageBytesCount = dataPointsCount * 64 + 32; // <- previous version
+    uint256 signedMessageBytesCount = dataPointsCount * 64 + 32;
+
+    // 3. We extract the signedMessage
+    // High level equivalent below (1.2k gas more expensive)
+    // TODO: verify gas improvement on the final version
+    // uint256 startIndex = msg.data.length -
+    //   signedMessageBytesCount -
+    //   DATAPOINTS_NUMBER_BYTES_COUNT -
+    //   SIGNATURE_BYTES_COUNT;
+    // uint256 endIndex = msg.data.length -
+    //   DATAPOINTS_NUMBER_BYTES_COUNT -
+    //   SIGNATURE_BYTES_COUNT;
+    // bytes memory signedMessage = msg.data[startIndex:endIndex];
+
+    // Optimised assembly version
+    bytes memory signedMessage;
+    assembly {
+      signedMessage := mload(NEXT_FREE_MEMORY_POINTER_LOCATION)
+      // Bytes arrays have the convention of the first 32 bytes storing the length of the bytes array (improve comment)
+      mstore(signedMessage, signedMessageBytesCount)
+      // The starting point is callDataSize minus length of data(messageLength), signature(65) and size(2) = 67
+      let signedMessageBytesStartPtr := add(
+        signedMessage,
+        CALLDATA_SLOT_BYTES_COUNT
+      )
+      calldatacopy(
+        signedMessageBytesStartPtr,
+        sub(
+          calldatasize(),
+          add(
+            signedMessageBytesCount,
+            DATAPOINTS_NUMBER_AND_SIGNATURE_BYTES_COUNT
+          )
+        ),
+        signedMessageBytesCount
+      )
+
+      // mstore(NEXT_FREE_MEMORY_POINTER_LOCATION, signedMessageBytesPtr) // <- old version (has memory leak)
+
+      // new version
+      mstore(
+        NEXT_FREE_MEMORY_POINTER_LOCATION,
+        add(signedMessageBytesStartPtr, signedMessageBytesCount)
+      )
+    }
+
+    // console.log("\nsigned message part");
+    // console.log("Hehe");
+
+    // // TODO: remove
+    // console.log("\nsigned message");
+    // console.logBytes(signedMessage);
 
     // 4. We first hash the raw message and then hash it again with the prefix
     // Following the https://github.com/ethereum/eips/issues/191 standard
-    bytes32 hash = keccak256(signableMessage);
-    bytes32 hashWithPrefix = keccak256(
-      abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+    bytes32 signedMessageHash = keccak256(signedMessage);
+    // TODO: experiment
+    bytes32 signedHash = keccak256(
+      abi.encodePacked("\x19Ethereum Signed Message:\n32", signedMessageHash)
     );
+    // bytes32 hashWithPrefix = signedMessageHash.toEthSignedMessageHash();
+    // bytes32 hashWithPrefix = signedMessageHash;
+    // console.log("\nHash to verify");
+    // console.log(Strings.toHexString(uint256(hashWithPrefix), 32));
 
     // 5. We extract the off-chain signature from calldata
 
-    // (That's the high level equivalent 2k gas more expensive)
-    // bytes memory signature = msg.data.slice(msg.data.length - 65, 65);
+    // High level equivalent (0.5k gas more expensive)
+    // TODO: verify gas improvement on the final version
+    // uint256 signatureStartIndex = msg.data.length - SIGNATURE_BYTES_COUNT;
+    // uint256 signatureEndIndex = msg.data.length;
+    // bytes memory signature = msg.data[signatureStartIndex:signatureEndIndex];
+
+    // Optimised assembly version
     bytes memory signature;
     assembly {
-      signature := mload(0x40)
-      mstore(signature, 65)
-      calldatacopy(add(signature, 0x20), sub(calldatasize(), 65), 65)
-      mstore(0x40, add(signature, 0x20))
+      signature := mload(NEXT_FREE_MEMORY_POINTER_LOCATION)
+      mstore(signature, SIGNATURE_BYTES_COUNT)
+      let signatureBytesStartPtr := add(signature, 32)
+      calldatacopy(
+        signatureBytesStartPtr,
+        sub(calldatasize(), SIGNATURE_BYTES_COUNT),
+        SIGNATURE_BYTES_COUNT
+      )
+      mstore(
+        NEXT_FREE_MEMORY_POINTER_LOCATION,
+        add(signatureBytesStartPtr, SIGNATURE_BYTES_COUNT)
+      )
     }
+
+    // TODO: remove
+    // console.log("\nSignature");
+    // console.logBytes(signature);
 
     // 6. We verify the off-chain signature against on-chain hashed data
 
-    address signer = hashWithPrefix.recover(signature);
+    // Signature verification using openzeppelin library
+    // address signer = hashWithPrefix.recover(signature);
+
+    // Alternative option for signature verification (without using openzeppelin lbrary)
+    // It's 0.5k gas cheaper
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    assembly {
+      r := mload(add(signature, 32))
+      s := mload(add(signature, 64))
+      v := and(mload(add(signature, 65)), 255)
+    }
+    address signer = ecrecover(signedHash, v, r, s);
+
+    // console.log("\nSigner");
+    // console.log(signer);
+
     require(isSignerAuthorized(signer), "Signer not authorized");
 
     // 7. We extract timestamp from callData
@@ -133,13 +230,18 @@ abstract contract PriceAware {
     // 8. We validate timestamp
     require(isTimestampValid(dataTimestamp), "Data timestamp is invalid");
 
-    return _readFromCallData(symbols, uint256(dataSize), messageLength);
+    return
+      _readFromCallData(
+        symbols,
+        uint256(dataPointsCount),
+        signedMessageBytesCount
+      );
   }
 
   function _readFromCallData(
     bytes32[] memory symbols,
     uint256 dataSize,
-    uint16 messageLength
+    uint256 messageLength
   ) private pure returns (uint256[] memory) {
     uint256[] memory values;
     uint256 i;
@@ -147,13 +249,21 @@ abstract contract PriceAware {
     uint256 readyAssets;
     bytes32 currentSymbol;
 
+    // TODO: remove
+    // values = new uint256[](1);
+    // values[0] = 42 * (10**8);
+    // return values;
+
     // We iterate directly through call data to extract the values for symbols
     assembly {
       let start := sub(calldatasize(), add(messageLength, 67))
 
       values := msize()
       mstore(values, mload(symbols))
-      mstore(0x40, add(add(values, 0x20), mul(mload(symbols), 0x20)))
+      mstore(
+        NEXT_FREE_MEMORY_POINTER_LOCATION,
+        add(add(values, 0x20), mul(mload(symbols), 0x20))
+      )
 
       for {
         i := 0
--- packages/evm-connector/test/sample-price-aware-mock.test.ts
+++ packages/evm-connector/test/sample-price-aware-mock.test.ts
@@ -3,6 +3,10 @@ import { ethers } from "hardhat";
 import { WrapperBuilder } from "../src/index";
 import { SamplePriceAwareMock } from "../typechain-types";
 
+// We lock the timestamp to have deterministic gas consumption
+// for being able to compare gas costs of different implementations
+const TIMESTAMP_FOR_TESTS = 1654353400000;
+
 describe("SamplePriceAwareMock", function () {
   let contract: SamplePriceAwareMock;
 
@@ -14,26 +18,49 @@ describe("SamplePriceAwareMock", function () {
     await contract.deployed();
   });
 
-  it("Should properly read oracle data using contract view function", async () => {
-    const wrappedContract = WrapperBuilder.wrap(contract).usingMockData({
-      timestampMilliseconds: Date.now(),
-      dataPoints: [{ symbol: "ETH", value: 42 }],
-    });
-
-    const result = await wrappedContract.getEthPriceSecurely();
-    expect(result.div(10 ** 8).toNumber()).to.be.equal(42);
-  });
-
   it("Should properly execute transaction on PriceAware contract", async () => {
     const wrappedContract = WrapperBuilder.wrap(contract).usingMockData({
-      timestampMilliseconds: Date.now(),
-      dataPoints: [{ symbol: "ETH", value: 43 }],
+      timestampMilliseconds: TIMESTAMP_FOR_TESTS,
+      dataPoints: [{ symbol: "ETH", value: 42 }],
     });
 
     const tx = await wrappedContract.saveLatestEthPriceInStorage();
     await tx.wait();
 
     const latestEthPriceFromContract = await contract.latestEthPrice();
-    expect(latestEthPriceFromContract.div(10 ** 8).toNumber()).to.be.equal(43);
+    expect(latestEthPriceFromContract.div(10 ** 8).toNumber()).to.be.equal(42);
   });
+
+  // it("Should properly read oracle data using contract view function", async () => {
+  //   const wrappedContract = WrapperBuilder.wrap(contract).usingMockData({
+  //     timestampMilliseconds: TIMESTAMP_FOR_TESTS,
+  //     dataPoints: [{ symbol: "ETH", value: 43 }],
+  //   });
+
+  //   const result = await wrappedContract.getEthPriceSecurely();
+  //   expect(result.div(10 ** 8).toNumber()).to.be.equal(43);
+  // });
+
+  // TODO: fix it for dataPointsCount > 1023
+  // it("Should properly execute transaction with a big dataPackage (2k data points)", async () => {
+  //   // Prepare data points
+  //   const dataPoints = [{ symbol: "ETH", value: 42 }];
+  //   for (let i = 0; i < 2000; i++) {
+  //     dataPoints.push({
+  //       symbol: "SYMBOL_" + i,
+  //       value: i,
+  //     });
+  //   }
+
+  //   const wrappedContract = WrapperBuilder.wrap(contract).usingMockData({
+  //     timestampMilliseconds: TIMESTAMP_FOR_TESTS,
+  //     dataPoints,
+  //   });
+
+  //   const tx = await wrappedContract.saveLatestEthPriceInStorage();
+  //   await tx.wait();
+
+  //   const latestEthPriceFromContract = await contract.latestEthPrice();
+  //   expect(latestEthPriceFromContract.div(10 ** 8).toNumber()).to.be.equal(42);
+  // });
 });
