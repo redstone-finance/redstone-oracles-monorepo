a38fc2bd3a2db5ce422e63ef6247c2b188a31177
hatskier <suvorovalex1997@gmail.com>
2022-06-04 21:41:56 +0200

    PriceAware gas consumption improved

--- packages/evm-connector/contracts/mocks/PriceAwareMock.sol
+++ packages/evm-connector/contracts/mocks/PriceAwareMock.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol"; // TODO: remove
 import "../message-based/PriceAware.sol";
 
 contract PriceAwareMock is PriceAware {
@@ -13,9 +12,6 @@ contract PriceAwareMock is PriceAware {
     override
     returns (bool)
   {
-    // TODO: remove
-    console.log("Received signer", _receviedSigner);
-    console.log("Expected signer", 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
     // Mock signer address
     return _receviedSigner == 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
   }
