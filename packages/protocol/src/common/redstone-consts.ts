5b81c35a6bfbfed23ac63a9fd395b79dbb81714c
hatskier <suvorovalex1997@gmail.com>
2022-06-19 21:52:30 +0200

    test: implemented tests for utils module in redstone protocol

--- packages/protocol/src/common/redstone-consts.ts
+++ packages/protocol/src/common/redstone-consts.ts
@@ -1,8 +1,11 @@
+// Abbreviations:
+// * BS = Byte size
+
 // Number of bytes reserved to store timestamp
 export const TIMESTAMP_BYTE_SIZE = 6;
 
 // Number of bytes reserved to store the number of data points
-export const DATA_POINTS_COUNT_BYTE_SIZE = 3;
+export const DATA_POINTS_COUNT_BS = 3;
 
 // Number of bytes reserved to store default datapoints byte size
 export const DEFAULT_DATA_POINT_VALUE_BYTE_SIZE_BS = 4;
@@ -11,7 +14,7 @@ export const DEFAULT_DATA_POINT_VALUE_BYTE_SIZE_BS = 4;
 export const VALUE_BYTE_SIZE_BS = 8;
 
 // Default value byte size for numeric values
-export const DEFAULT_NUM_VALUE_BYTE_SIZE = 32;
+export const DEFAULT_NUM_VALUE_BS = 32;
 
 // Default precision for numeric values
-export const DEFAULT_NUM_VALUE_PRECISION = 8;
+export const DEFAULT_NUM_VALUE_DECIMALS = 8;
