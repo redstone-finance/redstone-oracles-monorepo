166ce47f809f829c8c629116f749210242de62d1
hatskier <suvorovalex1997@gmail.com>
2022-06-21 22:01:39 +0200

    feat: implemented serialization of many data packages

--- packages/protocol/test/dynamic-data-package.test.ts
+++ packages/protocol/test/dynamic-data-package.test.ts
@@ -1,7 +1,5 @@
-import { Wallet } from "ethers";
-import { computePublicKey, hexlify } from "ethers/lib/utils";
+import { hexlify } from "ethers/lib/utils";
 import { DynamicDataPackage } from "../src/data-package/DynamicDataPackage";
-import { SignedDataPackage } from "../src/data-package/SignedDataPackage";
 import { StringDataPoint } from "../src/data-point/StringDataPoint";
 
 const TIMESTAMP_FOR_TESTS = 1654353400000;
@@ -11,10 +9,10 @@ const EXPECTED_SERIALIZED_UNSIGNED_DATA_PACKAGE =
   "0x" +
   "4554480000000000000000000000000000000000000000000000000000000000" + // bytes32("ETH")
   "457468657265756d" + // toUtf8Bytes("Ethereum")
-  "0000000000000008" + // bytes size of the string "Ethereum" (8)
+  "00000008" + // bytes size of the string "Ethereum" (8)
   "4254430000000000000000000000000000000000000000000000000000000000" + // bytes32("BTC")
   "426974636f696e" + // toUtf8Bytes("Bitcoin")
-  "0000000000000007" + // bytes size of the string "Bitcoin" (7)
+  "00000007" + // bytes size of the string "Bitcoin" (7)
   "01812f2590c0" + // timestamp
   "00000000" + // default data points byte size (0 - indicates that each data point is dynamic)
   "000002"; // data points count
