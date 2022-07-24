116b6504d11307f02d9ec41f1d211d6628bba7bd
hatskier <suvorovalex1997@gmail.com>
2022-07-24 20:43:12 +0200

    refactor: removed scripts from smartweave contracts package

--- packages/oracles-smartweave-contracts/scripts/nice-logger.ts
+++
@@ -1,9 +0,0 @@
-import util from "util";
-
-const log = (val: any) => {
-  console.log(util.inspect(val, { depth: null, colors: true }));
-};
-
-export default {
-  log,
-};
--- packages/oracles-smartweave-contracts/scripts/redstone-oracle-registry/print-redstone-oracle-registry-state.ts
+++
@@ -1,7 +0,0 @@
-import redstoneSDK from "redstone-sdk";
-import niceLogger from "../nice-logger";
-
-export const printRedstoneOracleRegistryState = async () => {
-  const state = await redstoneSDK.getOracleRegistryState();
-  niceLogger.log(state);
-};
