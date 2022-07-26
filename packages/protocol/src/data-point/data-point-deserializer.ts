cb9a94cf825224ba70dd5c3db056ed10d6843b57
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-07-26 09:28:35 +0200

    Changes from PR #9

---
+++ packages/protocol/src/data-point/data-point-deserializer.ts
@@ -0,0 +1,17 @@
+import { base64 } from "ethers/lib/utils";
+import { DataPoint, DataPointPlainObj } from "./DataPoint";
+import { INumericDataPoint, NumericDataPoint } from "./NumericDataPoint";
+
+// This function was moved to a separate file, because it was not
+// possible to make it as a static method in the DataPoint class.
+// It would cause circularly importing classes, which is not supported
+// More info here: https://stackoverflow.com/a/44727578
+export const deserializeDataPointFromObj = (
+  plainObject: DataPointPlainObj
+): DataPoint => {
+  if (typeof plainObject.value == "number") {
+    return new NumericDataPoint(plainObject as INumericDataPoint);
+  } else {
+    return new DataPoint(plainObject.symbol, base64.decode(plainObject.value));
+  }
+};
