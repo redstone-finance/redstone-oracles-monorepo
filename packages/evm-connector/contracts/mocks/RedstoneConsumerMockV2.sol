b8a5975ef4d97570b6b4e2f4bb3acd7a2da4c8e4
hatskier <suvorovalex1997@gmail.com>
2022-07-30 02:14:18 +0200

    refactor: contract folders renamed

--- packages/evm-connector/contracts/mocks/RedstoneConsumerMockV2.sol
+++ packages/evm-connector/contracts/mocks/RedstoneConsumerMockV2.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.4;
 
 // import "hardhat/console.sol";
-import "../message-based/RedstoneConsumerBaseV2.sol";
+import "../core/RedstoneConsumerBaseV2.sol";
 import "./AuthorisedMockSignersBase.sol";
 
 contract RedstoneConsumerMockV2 is RedstoneConsumerBaseV2, AuthorisedMockSignersBase {
