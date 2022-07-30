c088ed97673457b2177f36e0297029bfb1daaae4
hatskier <suvorovalex1997@gmail.com>
2022-07-30 02:11:00 +0200

    refactor: renamed base to old

---
+++ packages/evm-connector/contracts/message-based/to-remove/RedstoneConsumerBaseOwnable.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "./RedstoneConsumerBase.sol";
+
+// contract RedstoneConsumerBaseOwnable is RedstoneConsumerBase, Ownable {
+//   address private trustedSigner;
+
+//   function authorizeSigner(address _trustedSigner) external onlyOwner {
+//     require(_trustedSigner != address(0));
+//     trustedSigner = _trustedSigner;
+
+//     emit TrustedSignerChanged(trustedSigner);
+//   }
+
+//   function isSignerAuthorized(address _receviedSigner) public view virtual override returns (bool) {
+//     return _receviedSigner == trustedSigner;
+//   }
+
+//   /* ========== EVENTS ========== */
+
+//   /**
+//    * @dev emitted after the owner updates trusted signer
+//    * @param newSigner the address of the new signer
+//    **/
+//   event TrustedSignerChanged(address indexed newSigner);
+// }
