27cf715f6b936003d9116c131d0ec67d3f80a1c8
hatskier <suvorovalex1997@gmail.com>
2022-07-30 04:39:14 +0200

    feat: implemented data feed wrapper

--- packages/evm-connector/src/wrappers/MockWrapper.ts
+++ packages/evm-connector/src/wrappers/MockWrapper.ts
@@ -1,17 +1,33 @@
-import { old } from "redstone-protocol";
-import { MOCK_PRIVATE_KEYS } from "../helpers/test-utils";
+import {
+  DataPackage,
+  serializeSignedDataPackages,
+  SignedDataPackage,
+} from "redstone-protocol";
+import {
+  MockSignerAddress,
+  getMockSignerPrivateKey,
+} from "../helpers/test-utils";
 import { BaseWrapper } from "./BaseWrapper";
 
+export interface MockDataPackageConfig {
+  signer: MockSignerAddress;
+  dataPackage: DataPackage;
+}
+
 export class MockWrapper extends BaseWrapper {
-  constructor(private mockDataPackage: old.DataPackage) {
+  constructor(private mockDataPackages: MockDataPackageConfig[]) {
     super();
   }
 
   async getBytesDataForAppending(): Promise<string> {
-    const signedDataPackage = await old.signDataPackage(
-      this.mockDataPackage,
-      MOCK_PRIVATE_KEYS[0]
-    );
-    return old.serializeSignedDataPackageToHexString(signedDataPackage);
+    const signedDataPackages: SignedDataPackage[] = [];
+
+    for (const mockDataPackage of this.mockDataPackages) {
+      const privateKey = getMockSignerPrivateKey(mockDataPackage.signer);
+      const signedDataPackage = mockDataPackage.dataPackage.sign(privateKey);
+      signedDataPackages.push(signedDataPackage);
+    }
+
+    return serializeSignedDataPackages(signedDataPackages);
   }
 }
