8267222e7b3c41c4431f63080532053418094333
hatskier <suvorovalex1997@gmail.com>
2022-08-03 22:05:18 +0200

    feat: redstone payload implementation with unsigned metadata and marker

--- packages/protocol/src/common/redstone-constants.ts
+++ packages/protocol/src/common/redstone-constants.ts
@@ -18,3 +18,13 @@ export const DEFAULT_NUM_VALUE_DECIMALS = 8;
 
 // Number of bytes reserved for data packages count
 export const DATA_PACKAGES_COUNT_BS = 2;
+
+// Number of bytes reserved for unsigned metadata byte size
+export const UNSGINED_METADATA_BYTE_SIZE_BS = 3;
+
+// RedStone marker that will be appended to the end of each transaction
+// Represented as hex string
+export const REDSTONE_MARKER_HEX = "0x0000ff0000";
+
+// Byte size of the RedStone marker
+export const REDSTONE_MARKER_BS = REDSTONE_MARKER_HEX.length / 2;
