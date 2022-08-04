07ca4bc1ec7ef5c5a8fadc37bf88e641ea4e7942
hatskier <suvorovalex1997@gmail.com>
2022-08-05 01:13:42 +0200

    chore: unsigned metadata skipping implemented in evm-connector

--- packages/evm-connector/src/wrappers/DataFeedWrapper.ts
+++ packages/evm-connector/src/wrappers/DataFeedWrapper.ts
@@ -1,16 +1,22 @@
-import { serializeSignedDataPackages } from "redstone-protocol";
+import { RedstonePayload } from "redstone-protocol";
 import { DataPackagesRequestParams, requestDataPackages } from "redstone-sdk";
 import { BaseWrapper } from "./BaseWrapper";
+import { version } from "../../package.json";
 
 export class DataFeedWrapper extends BaseWrapper {
   constructor(private dataPackagesRequestParams: DataPackagesRequestParams) {
     super();
   }
 
+  getUnsignedMetadata(): string {
+    return `${version}#${this.dataPackagesRequestParams.dataFeedId}`;
+  }
+
   async getBytesDataForAppending(): Promise<string> {
+    const unsignedMetadata = this.getUnsignedMetadata();
     const signedDataPackages = await requestDataPackages(
       this.dataPackagesRequestParams
     );
-    return serializeSignedDataPackages(signedDataPackages);
+    return RedstonePayload.prepare(signedDataPackages, unsignedMetadata);
   }
 }
