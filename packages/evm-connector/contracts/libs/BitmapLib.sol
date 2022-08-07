bc8eff92ee679c001242b84f4ab3abf4e0eea3af
hatskier <suvorovalex1997@gmail.com>
2022-08-07 19:26:21 +0200

    added natspec comments to the RedstoneConsumerBase contract

--- packages/evm-connector/contracts/libs/BitmapLib.sol
+++ packages/evm-connector/contracts/libs/BitmapLib.sol
@@ -3,19 +3,13 @@
 pragma solidity ^0.8.4;
 
 library BitmapLib {
-  function setBitInBitmap(uint256 bitmap, uint256 bitIndex)
-    internal
-    pure
-    returns (uint256)
-  {
+  uint256 constant EMPTY_BITMAP = 0;
+
+  function setBitInBitmap(uint256 bitmap, uint256 bitIndex) internal pure returns (uint256) {
     return bitmap | (1 << bitIndex);
   }
 
-  function getBitFromBitmap(uint256 bitmap, uint256 bitIndex)
-    internal
-    pure
-    returns (bool)
-  {
+  function getBitFromBitmap(uint256 bitmap, uint256 bitIndex) internal pure returns (bool) {
     uint256 bitAtIndex = bitmap & (1 << bitIndex);
     return bitAtIndex > 0;
   }
