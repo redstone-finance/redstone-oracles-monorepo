cfe528c9a97a0ae8b64da85b4dd29543f2f114c4
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-10-13 15:22:19 +0200

    feat: implemented advanced logging in cache-service (#81)

--- packages/cache-service/src/app.controller.ts
+++ packages/cache-service/src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Logger } from "@nestjs/common";
 
 const GITHUB_REPO_LINK =
   "https://github.com/redstone-finance/redstone-oracles-monorepo/tree/main/packages/cache-service";
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   @Get()
   getRootRouteResponse(): string {
-    console.log("Received a request to the root route");
+    this.logger.log("Received a request to the root route");
     return (
       `Hello! This is an instance of redstone-cache-service. ` +
       `You can find more information at: ${GITHUB_REPO_LINK}`
