04cdbdae56b9041a2afaf4fd9a067d630a0c972d
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-10-26 19:22:10 +0200

    feat: prepare evm-connector for npm publishing (#94)
    * feat: added StocksDemoConsumerBase contract
    
    * chore: updated timestamp validation in StocksDemoConsumerBase.sol
    
    * Added image with tx payload structure
    
    * chore: package json scripts updated
    
    * choer: new version published
    
    * chore: scripts for npm publishing updated in package.json
    
    * chore: added an optional urls param to the WrapperBuilder
    
    * chore: bumped redstone-sdk version in evm-connector
    
    * chore: added @openzeppelin/contracts to evm-connector deps
    
    * chore: new version published
    
    * feat: added base contracts for data services
    
    * 0.0.2
    
    * feat: timestamp validation adjusted
    
    * version updated
    
    * updated authorised addresses for avalanche-prod data service
    
    * new version published
    
    * chore: experiment with npm publishing
    
    * build: 3.0.13
    
    * build: 3.0.14
    
    * chore: improved npm versionining commits
    
    * build: @redstone-finance/evm-connector@0.0.1
    
    * chore: changed the package name to @redstone-finance/evm-connector
    
    * build: @redstone-finance/evm-connector@0.0.2
    
    * build: @redstone-finance/evm-connector@0.0.3
    
    * build: redstone-oracles-smartweave-contracts@1.0.1
    
    * build: redstone-sdk@1.0.2
    
    * build: redstone-protocol@1.0.1
    
    * build: redstone-sdk@1.0.3
    
    * build: @redstone-finance/evm-connector@0.0.4
    
    * docs: readme updated in evm-connector
    
    * build: @redstone-finance/evm-connector@0.0.5
    
    * set specific versions of deps in evm-connector
    
    * build: @redstone-finance/evm-connector@0.0.6
    
    * build: @redstone-finance/evm-connector@0.0.7

---
+++ packages/evm-connector/contracts/data-services/RapidDemoConsumerBase.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "../core/RedstoneConsumerNumericBase.sol";
+
+contract RapidDemoConsumerBase is RedstoneConsumerNumericBase {
+  constructor() {
+    uniqueSignersThreshold = 1;
+  }
+
+  function getAuthorisedSignerIndex(address _signerAddress)
+    public
+    view
+    virtual
+    override
+    returns (uint256)
+  {
+    if (_signerAddress == 0xf786a909D559F5Dee2dc6706d8e5A81728a39aE9) {
+      return 0;
+    } else {
+      revert("Signer is not authorised");
+    }
+  }
+}
