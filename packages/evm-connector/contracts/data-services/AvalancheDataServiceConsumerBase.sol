da01652d20d4967d3a3877ecddacb2f467df4ce7
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-12-05 16:46:17 +0100

    chore: updated redstone avalanche prod nodes details (#183)

--- packages/evm-connector/contracts/data-services/AvalancheDataServiceConsumerBase.sol
+++ packages/evm-connector/contracts/data-services/AvalancheDataServiceConsumerBase.sol
@@ -6,40 +6,22 @@ import "../core/RedstoneConsumerNumericBase.sol";
 
 contract AvalancheDataServiceConsumerBase is RedstoneConsumerNumericBase {
   function getUniqueSignersThreshold() public view virtual override returns (uint8) {
-    return 10;
+    return 3;
   }
 
-  function getAuthorisedSignerIndex(address signerAddress)
-    public
-    view
-    virtual
-    override
-    returns (uint8)
-  {
-    if (signerAddress == 0x981bdA8276ae93F567922497153de7A5683708d3) {
+  function getAuthorisedSignerIndex(
+    address signerAddress
+  ) public view virtual override returns (uint8) {
+    if (signerAddress == 0x1eA62d73EdF8AC05DfceA1A34b9796E937a29EfF) {
       return 0;
-    } else if (signerAddress == 0x3BEFDd935b50F172e696A5187DBaCfEf0D208e48) {
+    } else if (signerAddress == 0x2c59617248994D12816EE1Fa77CE0a64eEB456BF) {
       return 1;
-    } else if (signerAddress == 0xc1D5b940659e57b7bDF8870CDfC43f41Ca699460) {
+    } else if (signerAddress == 0x12470f7aBA85c8b81D63137DD5925D6EE114952b) {
       return 2;
-    } else if (signerAddress == 0x1Cd8F9627a2838a7DAE6b98CF71c08B9CbF5174a) {
+    } else if (signerAddress == 0x109B4a318A4F5ddcbCA6349B45f881B4137deaFB) {
       return 3;
-    } else if (signerAddress == 0xbC5a06815ee80dE7d20071703C1F1B8fC511c7d4) {
+    } else if (signerAddress == 0x83cbA8c619fb629b81A65C2e67fE15cf3E3C9747) {
       return 4;
-    } else if (signerAddress == 0xe9Fa2869C5f6fC3A0933981825564FD90573A86D) {
-      return 5;
-    } else if (signerAddress == 0xDf6b1cA313beE470D0142279791Fa760ABF5C537) {
-      return 6;
-    } else if (signerAddress == 0xa50abc5D76dAb99d5fe59FD32f239Bd37d55025f) {
-      return 7;
-    } else if (signerAddress == 0x496f4E8aC11076350A59b88D2ad62bc20d410EA3) {
-      return 8;
-    } else if (signerAddress == 0x41FB6b8d0f586E73d575bC57CFD29142B3214A47) {
-      return 9;
-    } else if (signerAddress == 0xC1068312a6333e6601f937c4773065B70D38A5bF) {
-      return 10;
-    } else if (signerAddress == 0xAE9D49Ea64DF38B9fcbC238bc7004a1421f7eeE8) {
-      return 11;
     } else {
       revert SignerNotAuthorised(signerAddress);
     }
