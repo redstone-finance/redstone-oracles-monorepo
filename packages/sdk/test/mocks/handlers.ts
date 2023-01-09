38b8785efd3436caf324d3da8b552cf87e3e0c1d
Alex Suvorov <Suvorovalex1997@gmail.com>
2023-01-09 16:33:05 +0100

    chore: few improvements in redstone-sdk function for data packages requesting (#289)
    * feat: added timestamp sorting and signers number checking in redstone-sdk
    
    * fix: fixed versioning in unsigned metadata in evm-connector
    
    * test: implemented tests for the new features in redstone-sdk
    
    * build: redstone-sdk@1.0.7
    
    * chore: bumped redstone-sdk version in evm-connector
    
    * build: @redstone-finance/evm-connector@0.0.17
    
    * chore: Improve error logging in dry-runs mode
    
    Co-authored-by: cehali <cezary.haliniarz@gmail.com>

--- packages/sdk/test/mocks/handlers.ts
+++ packages/sdk/test/mocks/handlers.ts
@@ -2,8 +2,31 @@ import { rest } from "msw";
 import { DEFAULT_CACHE_SERVICE_URLS } from "../../src";
 import { mockSignedDataPackages } from "./mock-packages";
 
+const getMockResponseObject = (url: string) => {
+  if (url.includes("sorted-asc-only-eth")) {
+    const signedDataPackagesObjects = mockSignedDataPackages.ETH;
+    const [dataPackage1, dataPackage2] = signedDataPackagesObjects;
+    return {
+      ETH: [
+        {
+          ...dataPackage1,
+          timestampMilliseconds: 10,
+        },
+        {
+          ...dataPackage2,
+          timestampMilliseconds: 11,
+        },
+      ],
+    };
+  } else {
+    return mockSignedDataPackages;
+  }
+};
+
 export const handlers = [
   ...DEFAULT_CACHE_SERVICE_URLS,
+  "https://good-url-1.com",
+  "https://good-url-sorted-asc-only-eth.com",
   "https://bad-url-1.com",
   "https://bad-url-2.com",
 ].map((url) =>
@@ -11,7 +34,8 @@ export const handlers = [
     if (req.url.origin.includes("bad-url")) {
       return res(ctx.status(400), ctx.json(null));
     } else {
-      return res(ctx.status(200), ctx.json(mockSignedDataPackages));
+      const responseObject = getMockResponseObject(url);
+      return res(ctx.status(200), ctx.json(responseObject));
     }
   })
 );
