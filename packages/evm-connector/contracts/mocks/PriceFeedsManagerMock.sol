c009cb518d30ea1f22a5b69689fadb0c84606b20
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-02-08 22:22:14 +0100

    feat: add upgradability to PriceFeedsManager (#351)
    * feat: add upgradability to PriceFeedsManager
    
    * Change registry to Ownable, minor refactoring
    
    * Add tests
    
    * Changes after code review

---
+++ packages/evm-connector/contracts/mocks/PriceFeedsManagerMock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./AuthorisedMockSignersBase.sol";
+import "../price-feeds/PriceFeedsManager.sol";
+
+contract PriceFeedsManagerMock is PriceFeedsManager, AuthorisedMockSignersBase {
+  constructor() PriceFeedsManager() {}
+
+  function getUniqueSignersThreshold() public view virtual override returns (uint8) {
+    return 10;
+  }
+
+  function getAuthorisedSignerIndex(address signerAddress)
+    public
+    view
+    virtual
+    override
+    returns (uint8)
+  {
+    return getAuthorisedMockSignerIndex(signerAddress);
+  }
+}
--- packages/evm-connector/contracts/price-feeds/PriceFeedsManager.sol
+++ packages/evm-connector/contracts/price-feeds/PriceFeedsManager.sol
@@ -3,49 +3,27 @@
 pragma solidity ^0.8.4;
 
 import "../data-services/AvalancheDataServiceConsumerBase.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./PriceFeedsRegistry.sol";
 import "./PriceFeed.sol";
 
-contract PriceFeedsManager is AvalancheDataServiceConsumerBase {
+contract PriceFeedsManager is AvalancheDataServiceConsumerBase, Initializable {
   uint256 public lastRound = 0;
-  uint256 private lastUpdateTimestampMilliseconds;
-  mapping(bytes32 => PriceFeed) private priceFeedsContracts;
-
-  bytes32[] dataFeedsIds = [
-    bytes32("BTC"),
-    bytes32("ETH"),
-    bytes32("AVAX"),
-    bytes32("USDT"),
-    bytes32("USDC"),
-    bytes32("BUSD"),
-    bytes32("LINK"),
-    bytes32("GMX"),
-    bytes32("PNG"),
-    bytes32("QI"),
-    bytes32("JOE"),
-    bytes32("YAK"),
-    bytes32("PTP")
-  ];
+  uint256 public lastUpdateTimestampMilliseconds = 0;
+  PriceFeedsRegistry public priceFeedRegistry;
 
   error ProposedTimestampSmallerOrEqualToLastTimestamp(
     uint256 lastUpdateTimestampMilliseconds,
     uint256 blockTimestamp
   );
 
-  error ProposedTimestampDoesNotMatchProposedTimestamp(
+  error ProposedTimestampDoesNotMatchReceivedTimestamp(
     uint256 proposedTimestamp,
     uint256 receivedTimestampMilliseconds
   );
 
-  constructor() {
-    for (uint256 i = 0; i < dataFeedsIds.length; i++) {
-      priceFeedsContracts[dataFeedsIds[i]] = new PriceFeed(
-        address(this),
-        dataFeedsIds[i],
-        string(
-          abi.encodePacked("RedStone price feed for ", string(abi.encodePacked(dataFeedsIds[i])))
-        )
-      );
-    }
+  function initialize(address priceFeedsRegistryAddress) public initializer {
+    priceFeedRegistry = PriceFeedsRegistry(priceFeedsRegistryAddress);
   }
 
   function validateTimestamp(uint256 receivedTimestampMilliseconds) public view override {
@@ -55,7 +33,7 @@ contract PriceFeedsManager is AvalancheDataServiceConsumerBase {
       after validation in valivalidateTimestampFromUser and equal to proposedTimestamp
     */
     if (receivedTimestampMilliseconds != lastUpdateTimestampMilliseconds) {
-      revert ProposedTimestampDoesNotMatchProposedTimestamp(
+      revert ProposedTimestampDoesNotMatchReceivedTimestamp(
         lastUpdateTimestampMilliseconds,
         receivedTimestampMilliseconds
       );
@@ -73,10 +51,6 @@ contract PriceFeedsManager is AvalancheDataServiceConsumerBase {
 
   function isProposedRoundValid(uint256 proposedRound) private view returns (bool) {
     return proposedRound == lastRound + 1;
-  }
-
-  function getPriceFeedContractAddress(bytes32 dataFeedId) public view returns (address) {
-    return address(priceFeedsContracts[dataFeedId]);
   }
 
   function getLastRound() public view returns (uint256) {
@@ -101,9 +75,12 @@ contract PriceFeedsManager is AvalancheDataServiceConsumerBase {
       getOracleNumericValuesFromTxMsg will call validateTimestamp
       for each data package from the redstone payload 
     */
+    bytes32[] memory dataFeedsIds = priceFeedRegistry.getDataFeeds();
     uint256[] memory values = getOracleNumericValuesFromTxMsg(dataFeedsIds);
     for (uint256 i = 0; i < dataFeedsIds.length; i++) {
-      priceFeedsContracts[dataFeedsIds[i]].storeDataFeedValue(values[i]);
+      PriceFeed(priceFeedRegistry.getPriceFeedContractAddress(dataFeedsIds[i])).storeDataFeedValue(
+          values[i]
+        );
     }
   }
 }
