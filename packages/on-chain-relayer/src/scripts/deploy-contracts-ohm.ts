e23f0e254a591297a3885e0c72f4dc15a6afd482
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-02-23 08:31:02 +0100

    feat: prepare on-chain-relayer for OHM (#363)

---
+++ packages/on-chain-relayer/src/scripts/deploy-contracts-ohm.ts
@@ -0,0 +1,50 @@
+import { ContractFactory, Wallet, utils } from "ethers";
+import { getProvider } from "../utils";
+import { config } from "../config";
+import managerAbi from "../config/price-feeds-manager.abi.json";
+import registryAbi from "../config/price-feeds-registry.abi.json";
+import { bytesCodes } from "../config/bytes-codes";
+
+(async () => {
+  const provider = getProvider();
+  const signer = new Wallet(config.privateKey, provider);
+
+  console.log("Deploying manager contract...");
+  const managerFactory = new ContractFactory(
+    managerAbi,
+    bytesCodes.manager,
+    signer
+  );
+  const managerContract = await managerFactory.deploy();
+  await managerContract.deployed();
+  console.log(`Manager contract deployed: ${managerContract.address}`);
+
+  console.log("Deploying registry contract...");
+  const registryFactory = new ContractFactory(
+    registryAbi,
+    bytesCodes.registry,
+    signer
+  );
+  const registryContract = await registryFactory.deploy(
+    managerContract.address
+  );
+  await registryContract.deployed();
+  console.log(`Registry contract deployed: ${registryContract.address}`);
+
+  console.log(
+    "Initializing manager contract with registry contract address..."
+  );
+  const initializeTransaction = await managerContract.initialize(
+    registryContract.address
+  );
+  await initializeTransaction.wait();
+  console.log("Manager contract initialized");
+
+  console.log("Adding OHM data feed to registry...");
+  const ohmDataFeedId = utils.formatBytes32String("OHM");
+  const addDataFeedTransaction = await registryContract.addDataFeed(
+    ohmDataFeedId
+  );
+  await addDataFeedTransaction.wait();
+  console.log("OHM data feed to registry added");
+})();
