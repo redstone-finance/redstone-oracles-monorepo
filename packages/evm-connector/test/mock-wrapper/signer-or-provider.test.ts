6f522f64a2a82ca36c3f42e5eb14bb8b9c8f0163
rucinpk <37451666+rucinpk@users.noreply.github.com>
2023-03-07 14:11:17 +0100

    fix: contracts without signer (#371)
    * replaced signer call with provider call
    
    * replaced signer call with provider call
    
    * fix after review
    
    * fix after review
    
    * fix after review
    
    * fix after review
    
    * fixed build
    
    * fixed after reivew

---
+++ packages/evm-connector/test/mock-wrapper/signer-or-provider.test.ts
@@ -0,0 +1,63 @@
+import chai, { expect } from "chai";
+import { ethers } from "hardhat";
+import { SampleRedstoneConsumerNumericMock } from "../../typechain-types";
+import { WrapperBuilder } from "../../src/index";
+import { utils } from "redstone-protocol";
+import { mockNumericPackages } from "../tests-common";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
+
+const DATA_FEED_ID = "ETH";
+const EXPECTED_DATA_FEED_VALUE = 4200000000;
+
+describe("SignerOrProviderTest", function () {
+  let deployedContract: SampleRedstoneConsumerNumericMock;
+
+  this.beforeEach(async () => {
+    const ContractFactory = await ethers.getContractFactory(
+      "SampleRedstoneConsumerNumericMock"
+    );
+    deployedContract = await ContractFactory.deploy();
+    await deployedContract.deployed();
+  });
+
+  it("Should call static function without signer", async () => {
+    const contract = deployedContract.connect(ethers.provider);
+
+    const wrappedContract =
+      WrapperBuilder.wrap(contract).usingMockDataPackages(mockNumericPackages);
+
+    const response = await wrappedContract.getValueForDataFeedId(
+      utils.convertStringToBytes32(DATA_FEED_ID)
+    );
+    expect(response.toNumber()).to.equal(EXPECTED_DATA_FEED_VALUE);
+  });
+
+  it("Should revert with non-static function without signer", async () => {
+    const contract = deployedContract.connect(ethers.provider);
+
+    const wrappedContract =
+      WrapperBuilder.wrap(contract).usingMockDataPackages(mockNumericPackages);
+
+    await expect(
+      wrappedContract.saveOracleValueInContractStorage(
+        utils.convertStringToBytes32(DATA_FEED_ID)
+      )
+    ).to.be.rejectedWith(
+      "Cannot read properties of null (reading 'sendTransaction')"
+    );
+  });
+
+  it("Should call non-static function with signer", async () => {
+    const wrappedContract =
+      WrapperBuilder.wrap(deployedContract).usingMockDataPackages(
+        mockNumericPackages
+      );
+
+    const tx = await wrappedContract.saveOracleValueInContractStorage(
+      utils.convertStringToBytes32(DATA_FEED_ID)
+    );
+    await tx.wait();
+  });
+});
