51ef79372f322a1947ff74484f1a979194a048ac
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2023-05-24 16:35:57 +0200

    build: @redstone-finance/fuel-connector@0.2.1 (#670)
    * build: @redstone-finance/fuel-connector@0.2.1
    
    * build: @redstone-finance/fuel-connector@0.2.2

--- packages/fuel-connector/src/autogenerated/common.d.ts
+++ packages/fuel-connector/src/autogenerated/common.d.ts
@@ -4,9 +4,9 @@
 /* eslint-disable */
 
 /*
-  Fuels version: 0.31.0
-  Forc version: 0.32.2
-  Fuel-Core version: 0.17.1
+  Fuels version: 0.42.0
+  Forc version: 0.35.5
+  Fuel-Core version: 0.17.3
 */
 
 /*
