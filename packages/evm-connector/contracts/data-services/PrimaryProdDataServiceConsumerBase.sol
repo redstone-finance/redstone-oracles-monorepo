19e78dc0dd02d969afd9836dc70b6ced8e7aa25c
Mike <konopek68@gmail.com>
2023-06-06 18:52:32 +0200

    691 add redstone primary contracts (#705)
    * feat: add redstone primary contracts
    
    * chore: remove data service url for redstone-primary-demo not deployed yet

---
+++ packages/evm-connector/contracts/data-services/PrimaryProdDataServiceConsumerBase.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.4;
+
+import "../core/RedstoneConsumerNumericBase.sol";
+
+contract PrimaryProdDataServiceConsumerBase is RedstoneConsumerNumericBase {
+  function getDataServiceId() public view virtual override returns (string memory) {
+    return "redstone-primary-prod";
+  }
+
+  function getUniqueSignersThreshold() public view virtual override returns (uint8) {
+    return 3;
+  }
+
+  function getAuthorisedSignerIndex(
+    address signerAddress
+  ) public view virtual override returns (uint8) {
+    if (signerAddress == 0x8BB8F32Df04c8b654987DAaeD53D6B6091e3B774) {
+      return 0;
+    } else if (signerAddress == 0xdEB22f54738d54976C4c0fe5ce6d408E40d88499) {
+      return 1;
+    } else if (signerAddress == 0x51Ce04Be4b3E32572C4Ec9135221d0691Ba7d202) {
+      return 2;
+    } else if (signerAddress == 0xDD682daEC5A90dD295d14DA4b0bec9281017b5bE) {
+      return 3;
+    } else if (signerAddress == 0x9c5AE89C4Af6aA32cE58588DBaF90d18a855B6de) {
+      return 4;
+    } else {
+      revert SignerNotAuthorised(signerAddress);
+    }
+  }
+}
