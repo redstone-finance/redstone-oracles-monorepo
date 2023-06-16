be0a7c669c2e6c744ed17813fd7b9fc38b1b3cf7
Mike <konopek68@gmail.com>
2023-06-16 14:28:41 +0200

    feat: Add gas optimized transaction deliver (#734)
    * feat: implement transaction deliver
    
    * feat: add debug logs
    
    * feat: minior tweak to transaction deliver algo
    
    * feat: Add TransactionDeliver to on-chain-relayer
    
    * feat: fix tests
    
    * fix: typo
    
    * feat: changes after hatskier review

--- packages/on-chain-relayer/src/core/contract-interactions/update-prices.ts
+++ packages/on-chain-relayer/src/core/contract-interactions/update-prices.ts
@@ -9,6 +9,7 @@ import {
 } from "../../custom-integrations/mento/mento-utils";
 
 import { getSortedOraclesContractAtAddress } from "./get-contract";
+import { TransactionDeliveryMan } from "redstone-rpc-providers";
 
 interface UpdatePricesArgs {
   adapterContract: Contract;
@@ -17,6 +18,10 @@ interface UpdatePricesArgs {
 }
 
 const TX_CONFIG = { gasLimit: config.gasLimit };
+
+const deliveryMan = new TransactionDeliveryMan({
+  expectedDeliveryTimeMs: config.expectedTxDeliveryTimeInMS,
+});
 
 export const updatePrices = async (
   dataPackages: DataPackagesResponse,
@@ -67,10 +72,16 @@ const updatePricesInPriceFeedsAdapter = async ({
   wrapContract,
   proposedTimestamp,
 }: UpdatePricesArgs): Promise<TransactionResponse> => {
-  return await wrapContract(adapterContract).updateDataFeedsValues(
-    proposedTimestamp,
-    TX_CONFIG
+  const wrappedContract = wrapContract(adapterContract);
+
+  const deliveryResult = await deliveryMan.deliver(
+    wrappedContract,
+    "updateDataFeedsValues",
+    [proposedTimestamp],
+    TX_CONFIG.gasLimit ? Number(TX_CONFIG.gasLimit) : undefined
   );
+
+  return deliveryResult;
 };
 
 const updatePricesInMentoAdapter = async ({
