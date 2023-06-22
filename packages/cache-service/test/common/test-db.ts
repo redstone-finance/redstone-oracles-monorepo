a1b9c3820dbb4bc082e058f75dfa723c8a3720ac
Mike <konopek68@gmail.com>
2023-06-22 12:58:33 +0200

    feat: Add test asserting that cache-service will work with DataPoint (#757)
    * feat: Add test asserting that cache-service will work with DataPoint
    
    * feat: DONT DROP THE DATABASE like me :)

--- packages/cache-service/test/common/test-db.ts
+++ packages/cache-service/test/common/test-db.ts
@@ -4,12 +4,18 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 let mongo: MongoMemoryServer;
 
 export const connectToTestDB = async () => {
-  mongo = await MongoMemoryServer.create();
+  mongo = await MongoMemoryServer.create({});
   const uri = mongo.getUri();
   await mongoose.connect(uri);
 };
 
+const TEST_DB_ALLOWED_HOSTS = ["127.0.0.1", "localhost", undefined];
 export const dropTestDatabase = async () => {
+  if (!TEST_DB_ALLOWED_HOSTS.includes(mongoose.connection.host)) {
+    throw new Error(
+      `You are trying to drop non local database! ${mongoose.connection.host}`
+    );
+  }
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
   await mongo.stop();
