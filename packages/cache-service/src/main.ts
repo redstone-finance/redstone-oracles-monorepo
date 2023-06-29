52e43367bb8052cb77cabf1fcfffc9ff00e95f9b
Mike <konopek68@gmail.com>
2023-06-29 12:50:22 +0200

    fix: configure properly nestjs-pino to log proper error stack (#784)

--- packages/cache-service/src/main.ts
+++ packages/cache-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { urlencoded, json } from "express";
 import { Logger } from "nestjs-pino";
+import { LoggerErrorInterceptor } from "nestjs-pino";
 import config from "./config";
 
 const REQUEST_SIZE_LIMIT = "50mb";
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.use(json({ limit: REQUEST_SIZE_LIMIT }));
   app.use(urlencoded({ extended: true, limit: REQUEST_SIZE_LIMIT }));
   app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.enableCors();
   await app.listen(config.appPort);
 }
