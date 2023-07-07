da9e022ad51afe3220e224841115bba67857435c
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-07-07 17:44:05 +0200

    feat: add script to compare prices from two DBs (#475)
    * feat: add script to compare prices from two DBs
    
    * refactor: script refactoring
    
    * feat: add comparing of data feeds ids in data points
    
    * Changes after code review
    
    * Prepare for multiple tokens which are in one db

---
+++ packages/cache-service/scripts/common.ts
@@ -0,0 +1,58 @@
+import {
+  CachedDataPackage,
+  DataPackage,
+} from "../src/data-packages/data-packages.model";
+
+interface QueryDataPackagesParams {
+  startTimestamp: number;
+  endTimestamp: number;
+  dataFeedId: string;
+  dataServiceId: string;
+}
+
+interface DataPackagesGroupedByField {
+  [signer: string]: CachedDataPackage[];
+}
+
+export async function queryDataPackages({
+  startTimestamp,
+  endTimestamp,
+  dataFeedId,
+  dataServiceId,
+}: QueryDataPackagesParams): Promise<CachedDataPackage[]> {
+  return await DataPackage.find(
+    {
+      timestampMilliseconds: {
+        $gte: startTimestamp,
+        $lte: endTimestamp,
+      },
+      dataFeedId,
+      dataServiceId,
+    },
+    { timestampMilliseconds: 1, signerAddress: 1, dataPoints: 1 }
+  );
+}
+
+export function groupDataPackagesByField(
+  dataPackages: CachedDataPackage[],
+  field: "signerAddress" | "timestampMilliseconds"
+): DataPackagesGroupedByField {
+  const groupedBySigner: DataPackagesGroupedByField = {};
+
+  for (const dataPackage of dataPackages) {
+    if (!groupedBySigner[dataPackage[field]]) {
+      groupedBySigner[dataPackage[field]] = [];
+    }
+    groupedBySigner[dataPackage[field]].push(dataPackage);
+  }
+
+  return groupedBySigner;
+}
+
+export function getDeviationPercentage(a: number, b: number) {
+  return Math.abs((a - b) / Math.min(a, b)) * 100;
+}
+
+export function formatTime(timestamp: number) {
+  return new Date(timestamp).toISOString();
+}
