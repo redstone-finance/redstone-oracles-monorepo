cebdfbcb625d70ed7b9abb5cbc513ed71571ba35
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-08-08 13:06:23 +0200

    feat: add obfuscation to node code (#893)

---
+++ packages/oracles-smartweave-contracts/index.ts
@@ -0,0 +1,2 @@
+export { default as redstoneOraclesInitialState } from "./src/contracts/redstone-oracle-registry/initial-state.json";
+export * from "./src/contracts/redstone-oracle-registry/types";
