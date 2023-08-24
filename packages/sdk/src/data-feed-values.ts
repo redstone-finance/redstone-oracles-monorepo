de170971d9e440925b69c964fb56dc9fede53b64
Mike <konopek68@gmail.com>
2023-08-24 11:54:58 +0200

    feat: add method to fetch prices from gateways (#988)
    * feat: add method to fetch prices from gateways
    
    * feat: improvements
    
    * fix: use existing type
    
    * fix: type
    
    * fix: revert change from differernt commit
    
    * fix: node build by renaming function
    
    * feat: add better types for return value

---
+++ packages/sdk/src/data-feed-values.ts
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { resolveDataServiceUrls } from "./data-services-urls";
+import { MathUtils, RedstoneTypes } from "redstone-utils";
+
+export interface GetDataFeedValuesInput {
+  aggregationAlgorithm?: "median" | "min" | "max"; // median by default
+  dataServiceId?: string; // "redstone-main-demo" by default
+  gatewayUrls?: string[]; // if not specified, use default for dataServiceId
+}
+
+export type GetDataFeedValuesOutput = Record<string, number | undefined>;
+type GatewayResponse = RedstoneTypes.DataPackageFromGatewayResponse;
+
+const DEFAULT_DATA_SERVICE_ID = "redstone-main-demo";
+const DEFAULT_AGGREGATION_ALGORITHM = "median";
+
+export const getDataFeedValues = async (
+  args: GetDataFeedValuesInput = {}
+): Promise<GetDataFeedValuesOutput> => {
+  const dataServiceId = args.dataServiceId ?? DEFAULT_DATA_SERVICE_ID;
+  const aggregationAlgorithm =
+    args.aggregationAlgorithm ?? DEFAULT_AGGREGATION_ALGORITHM;
+  const gatewayUrls = args.gatewayUrls ?? resolveDataServiceUrls(dataServiceId);
+
+  const dataPackagesPerFeed = await Promise.any<GatewayResponse>(
+    gatewayUrls.map((url) => getDataPackagesFromGateway(url, dataServiceId))
+  );
+
+  const result: GetDataFeedValuesOutput = {};
+
+  for (const [dataFeedId, dataPackages] of Object.entries(
+    dataPackagesPerFeed
+  )) {
+    const plainValues = dataPackages
+      .filter((dp) => dp.dataPoints.length === 1)
+      .map((dp) => Number(dp.dataPoints[0].value));
+
+    // case for ___ALL_FEEDS___
+    if (plainValues.length === 0) {
+      continue;
+    }
+
+    result[dataFeedId] = aggregateValues(plainValues, aggregationAlgorithm);
+  }
+
+  return result;
+};
+
+const getDataPackagesFromGateway = async (
+  url: string,
+  dataServiceId: string
+): Promise<GatewayResponse> => {
+  const response = await axios.get<GatewayResponse>(
+    `${url}/data-packages/latest/${dataServiceId}`
+  );
+
+  if (typeof response.data === "string") {
+    throw new Error(
+      `Failed to fetch data package from ${url}. Data service ID responded with: ${response.data}`
+    );
+  }
+
+  return response.data;
+};
+
+export const aggregateValues = (
+  plainValues: number[],
+  aggregationAlgorithm: "median" | "min" | "max"
+) => {
+  switch (aggregationAlgorithm) {
+    case "max":
+      return Math.max(...plainValues);
+    case "min":
+      return Math.min(...plainValues);
+    case "median":
+      return MathUtils.getMedian(plainValues);
+    default:
+      throw new Error(
+        `Unsupported aggregationAlgorithm ${aggregationAlgorithm}`
+      );
+  }
+};
