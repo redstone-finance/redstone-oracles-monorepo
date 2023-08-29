116ce3f6be4e56c84ca9595efd4ea7064260e064
Mike <konopek68@gmail.com>
2023-08-29 17:46:17 +0200

    feat: redstone-sdk@1.8.0 (#1020)

---
+++ packages/sdk/src/fetch-data-packages.ts
@@ -0,0 +1,27 @@
+import { resolveDataServiceUrls } from "./data-services-urls";
+import { RedstoneCommon, RedstoneTypes } from "redstone-utils";
+
+/** Fetches data-packages from gateway */
+type FetchDataPackagesArgs = {
+  dataServiceId: string;
+  gatewayUrls?: string[];
+};
+
+export const fetchDataPackages = async ({
+  dataServiceId,
+  gatewayUrls,
+}: FetchDataPackagesArgs): Promise<RedstoneTypes.DataPackageFromGatewayResponse> => {
+  const resolvedGatewayUrls =
+    gatewayUrls ?? resolveDataServiceUrls(dataServiceId);
+
+  const urls = resolvedGatewayUrls.map(
+    (baseUrl) => `${baseUrl}/data-packages/latest/${dataServiceId}`
+  );
+
+  const responseFromGateway =
+    await RedstoneCommon.fetchWithFallbacks<RedstoneTypes.DataPackageFromGatewayResponse>(
+      { urls }
+    );
+
+  return responseFromGateway.data;
+};
