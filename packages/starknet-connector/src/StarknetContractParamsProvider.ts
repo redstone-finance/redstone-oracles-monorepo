373cef2d228cc3953f3cc4105bfd962662981cca
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2023-09-15 10:29:55 +0200

    chore: update configs and code for other connectors (#1083)
    * chore: update configs and code for other connectors

--- packages/starknet-connector/src/StarknetContractParamsProvider.ts
+++ packages/starknet-connector/src/StarknetContractParamsProvider.ts
@@ -7,7 +7,7 @@ export class StarknetContractParamsProvider extends ContractParamsProvider {
   protected override async requestPayload(
     requestParams: DataPackagesRequestParams
   ): Promise<string> {
-    let changedRequestParams = { ...requestParams };
+    const changedRequestParams = { ...requestParams };
     changedRequestParams["dataFeeds"] = undefined;
 
     return await super.requestPayload(changedRequestParams);
