987c7781c221d9cfa6a0d918ba6aeb0a17703e32
Mike <konopek68@gmail.com>
2023-09-19 12:58:09 +0200

    feat: Add multi node brodcaster (#1084)

--- packages/utils/src/common/errors.ts
+++ packages/utils/src/common/errors.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from "axios";
+
 export const assert = (condition: boolean, errMsg?: string) => {
   if (!condition) {
     const errText = `Assertion failed: ` + (errMsg ? `: ${errMsg}` : "");
@@ -11,3 +13,29 @@ export const assertWithLog = (condition: boolean, errMsg?: string) => {
     console.error(errText);
   }
 };
+
+export function stringifyError(e: unknown): string {
+  const error = e as
+    | AggregateError
+    | AxiosError
+    | undefined
+    | Error
+    | { toJSON: () => string };
+
+  if (error === undefined) {
+    return "undefined";
+  } else if (error instanceof AggregateError) {
+    const errorMessages: string[] = error.errors.map(stringifyError);
+    return `Aggregate error messages: ${errorMessages.join("; ")}`;
+  } else if (axios.isAxiosError(error)) {
+    return JSON.stringify(error.response?.data) + " | " + error.stack;
+  } else {
+    if (error instanceof Error) {
+      return error.stack || String(error);
+    } else if (typeof error.toJSON === "function") {
+      return JSON.stringify(error.toJSON());
+    } else {
+      return "error can't be handle by stringifyError function";
+    }
+  }
+}
