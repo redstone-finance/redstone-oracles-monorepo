dec4193932c4c27ec71e3e7353f3b7a4b2decf35
robal <robert@warp.cc>
2023-09-19 16:34:10 +0200

    chore: unify stringifyError usages (#1122)

--- packages/cache-service/src/utils/utils.ts
+++ packages/cache-service/src/utils/utils.ts
@@ -1,22 +1,5 @@
 import { Logger } from "@nestjs/common";
-
-// TODO: this should be in common package
-/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function stringifyError(e: any) {
-  if (e === undefined) {
-    return "undefined";
-  } else if (e === null) {
-    return "null";
-  } else if (e.response) {
-    return JSON.stringify(e.response.data) + " | " + e.stack;
-  } else if (e.toJSON) {
-    return JSON.stringify(e.toJSON());
-  } else {
-    return e.stack || String(e);
-  }
-}
-/* eslint-enable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return */
+import { RedstoneCommon } from "@redstone-finance/utils";
 
 export function runPromiseWithLogging<T>(
   promise: Promise<T>,
@@ -29,7 +12,9 @@ export function runPromiseWithLogging<T>(
       return result;
     })
     .catch((error) => {
-      logger.error(`Failure: ${message}. ${stringifyError(error)}`);
+      logger.error(
+        `Failure: ${message}. ${RedstoneCommon.stringifyError(error)}`
+      );
       throw error;
     });
 }
