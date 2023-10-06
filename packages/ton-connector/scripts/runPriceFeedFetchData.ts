73e59af50b29e6720a368db59af4ef31cebfcdf0
robal <robert@warp.cc>
2023-10-06 12:37:22 +0200

    Changes from PR #1214

--- packages/ton-connector/scripts/runPriceFeedFetchData.ts
+++ packages/ton-connector/scripts/runPriceFeedFetchData.ts
@@ -1,13 +1,10 @@
 import { NetworkProvider } from "@ton-community/blueprint";
-import * as dotenv from "dotenv";
 import { TonPriceFeedContractConnector } from "../src/price-feed/TonPriceFeedContractConnector";
 import { BlueprintTonNetwork } from "../src";
 import { config } from "../src/config";
 import * as fs from "fs";
 
 export async function run(provider: NetworkProvider) {
-  dotenv.config();
-
   const contract = await new TonPriceFeedContractConnector(
     new BlueprintTonNetwork(provider, config),
     await fs.promises.readFile(`deploy/price_feed.address`, "utf8")
