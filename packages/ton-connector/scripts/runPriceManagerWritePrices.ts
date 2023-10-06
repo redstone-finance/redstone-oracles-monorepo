42d9c0c62c929f35154fc195e5bb996056433538
robal <robert@warp.cc>
2023-10-06 12:37:22 +0200

    chore: unify dotenv usage (#1214)

--- packages/ton-connector/scripts/runPriceManagerWritePrices.ts
+++ packages/ton-connector/scripts/runPriceManagerWritePrices.ts
@@ -1,5 +1,4 @@
 import { NetworkProvider } from "@ton-community/blueprint";
-import * as dotenv from "dotenv";
 import { ContractParamsProvider } from "@redstone-finance/sdk";
 import { TonPriceManagerContractConnector } from "../src/price-manager/TonPriceManagerContractConnector";
 import { BlueprintTonNetwork } from "../src";
@@ -7,8 +6,6 @@ import { config } from "../src/config";
 import * as fs from "fs";
 
 export async function run(provider: NetworkProvider) {
-  dotenv.config();
-
   const contract = await new TonPriceManagerContractConnector(
     new BlueprintTonNetwork(provider, config),
     await fs.promises.readFile(`deploy/price_manager.address`, "utf8")
