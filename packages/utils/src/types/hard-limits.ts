8e1b81ec156f91063722b6d4aaa35e1f80896a86
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-10-06 11:24:56 +0200

    feat: add check for last updated timestamp in hard limits (#1209)

---
+++ packages/utils/src/types/hard-limits.ts
@@ -0,0 +1,15 @@
+export const LAST_UPDATED_TIMESTAMP_SYMBOL = "__lastUpdatedTimestamp" as const;
+
+type TimestampInHardLimits = {
+  [LAST_UPDATED_TIMESTAMP_SYMBOL]: number;
+};
+
+export interface HardLimitsForSymbol {
+  lower: number;
+  upper: number;
+}
+
+export type HardLimits = Partial<Record<string, HardLimitsForSymbol>>;
+
+export type HardLimitsWithTimestamp = HardLimits &
+  Partial<TimestampInHardLimits>;
--- packages/utils/src/types/index.ts
+++ packages/utils/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./redstone-metadata";
 export * from "./cache-response";
+export * from "./hard-limits";
