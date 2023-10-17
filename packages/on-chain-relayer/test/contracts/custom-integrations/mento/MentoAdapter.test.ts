ff8ebb5d8e36b20402bbc1d32e52d1fcc28c4c4e
robal <robert@warp.cc>
2023-10-17 11:52:28 +0200

    feat: do not deliver prices to mento sorted oracles when deviation ex… (#1235)
    * feat: do dot deliver prices to mento sorted oracles when deviation exceeds a defined threshold

--- packages/on-chain-relayer/test/contracts/custom-integrations/mento/MentoAdapter.test.ts
+++ packages/on-chain-relayer/test/contracts/custom-integrations/mento/MentoAdapter.test.ts
@@ -46,8 +46,9 @@ describe("MentoAdapter", () => {
     const { lesserKey, greaterKey } = calculateLinkedListPosition(
       rates,
       normalizeValue(valueToReport),
-      signer.address
-    );
+      signer.address,
+      Number.MAX_SAFE_INTEGER
+    )!;
 
     const tx = await sortedOracles
       .connect(signer)
@@ -64,7 +65,8 @@ describe("MentoAdapter", () => {
     mockToken1Value: number,
     mockToken2Value: number,
     adapter?: MentoAdapterBase,
-    locationsModifierFn: LocationsModifierFn = (l) => l
+    locationsModifierFn: LocationsModifierFn = (l) => l,
+    maxAllowedDeviation = Number.MAX_SAFE_INTEGER
   ) => {
     const adapterToTest = adapter ?? mentoAdapter;
 
@@ -86,18 +88,37 @@ describe("MentoAdapter", () => {
     // Prepare arguments
     const proposedTimestamp = timestampMilliseconds;
     const locationsInSortedLinkedLists =
-      await prepareLinkedListLocationsForMentoAdapterReport({
-        mentoAdapter: adapterToTest,
-        wrapContract,
-        sortedOracles,
-      });
+      await prepareLinkedListLocationsForMentoAdapterReport(
+        {
+          mentoAdapter: adapterToTest,
+          wrapContract,
+          sortedOracles,
+        },
+        maxAllowedDeviation
+      );
 
+    if (!locationsInSortedLinkedLists) {
+      return;
+    }
     // Updating oracle values
     await wrapContract(adapterToTest).updatePriceValues(
       proposedTimestamp,
       locationsModifierFn(locationsInSortedLinkedLists)
     );
   };
+
+  const reportWithAdapterAndDeviation = async (
+    mockToken1Value: number,
+    mockToken2Value: number,
+    maxAllowedDeviation = Number.MAX_SAFE_INTEGER
+  ) =>
+    await reportWithAdapter(
+      mockToken1Value,
+      mockToken2Value,
+      mentoAdapter,
+      (l) => l,
+      maxAllowedDeviation
+    );
 
   const expectOracleValues = async (
     tokenAddress: string,
@@ -246,5 +267,25 @@ describe("MentoAdapter", () => {
       mentoAdapterV2,
       sortedOracles.address
     );
+  });
+
+  it("Should not report oracle values when deviation is too big", async () => {
+    await reportDirectly(mockToken1Address, 40, signers[0]);
+    await reportDirectly(mockToken1Address, 42, signers[1]);
+
+    // acceptable deviation
+    await reportWithAdapterAndDeviation(42, 1000, 10);
+    await expectOracleValues(mockToken1Address, [42, 42, 40]);
+    await expectOracleValues(mockToken2Address, [1000]);
+
+    // we deviate from ourselves (16 vs 1000), should get in
+    await reportWithAdapterAndDeviation(45, 16, 10);
+    await expectOracleValues(mockToken1Address, [45, 42, 40]);
+    await expectOracleValues(mockToken2Address, [16]);
+
+    // deviation too big: 42 vs 50
+    await reportWithAdapterAndDeviation(50, 160, 10);
+    await expectOracleValues(mockToken1Address, [45, 42, 40]);
+    await expectOracleValues(mockToken2Address, [16]);
   });
 });
