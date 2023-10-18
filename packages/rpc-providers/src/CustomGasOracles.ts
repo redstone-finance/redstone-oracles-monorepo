884138111df879c6c94e798988c686d3f70faca2
Mike <konopek68@gmail.com>
2023-10-18 15:43:29 +0200

    feat: add custom gas oracle for kava (#1256)
    feat: add custom gas oracle for kava

---
+++ packages/rpc-providers/src/CustomGasOracles.ts
@@ -0,0 +1,61 @@
+import {
+  GasOracleFn,
+  TransactionDeliveryManOpts,
+} from "./TransactionDeliveryMan";
+import { fetchWithCache } from "./common";
+
+const ONE_GWEI = 1e9;
+const ONE_MICRO_KAVA = 1e6;
+
+const getEthFeeFromGasOracle: GasOracleFn = async (
+  opts: TransactionDeliveryManOpts
+) => {
+  const response = // rate limit is 5 seconds
+    (
+      await fetchWithCache<{
+        result: { suggestBaseFee: number; FastGasPrice: number };
+      }>(
+        `https://api.etherscan.io/api?module=gastracker&action=gasoracle`,
+        6_000
+      )
+    ).data;
+
+  const { suggestBaseFee, FastGasPrice } = response.result;
+
+  if (!suggestBaseFee || !FastGasPrice) {
+    throw new Error("Failed to fetch price from oracle");
+  }
+
+  return {
+    maxFeePerGas: Math.round(FastGasPrice * ONE_GWEI),
+    maxPriorityFeePerGas: Math.round(
+      (FastGasPrice - suggestBaseFee) * ONE_GWEI
+    ),
+    gasLimit: opts.gasLimit,
+  };
+};
+
+const kavaGasOracle: GasOracleFn = async (
+  opts: TransactionDeliveryManOpts,
+  attempt: number
+  // eslint-disable-next-line @typescript-eslint/require-await
+) => {
+  if (attempt <= 3) {
+    throw new Error("Fallback to provider oracle");
+  } else if (attempt <= 7) {
+    return {
+      gasLimit: opts.gasLimit,
+      gasPrice: 0.05 * ONE_MICRO_KAVA,
+    };
+  } else {
+    return {
+      gasLimit: opts.gasLimit,
+      gasPrice: 0.25 * ONE_MICRO_KAVA,
+    };
+  }
+};
+
+export const CHAIN_ID_TO_GAS_ORACLE = {
+  1: getEthFeeFromGasOracle,
+  2222: kavaGasOracle,
+} as Record<number, GasOracleFn | undefined>;
