2a4626ca6b00fbe6643a60846a8ad6001ff9134e
Mike <konopek68@gmail.com>
2023-11-07 15:57:07 +0100

    feat: add multi call provider (#1231)
    * feat: add multi call provider
    
    * fix: linter
    
    * fix: log
    
    * Delete packages/oracle-node/logs.txt
    
    * fix: robert review fixes
    
    * feat: add unit tests
    
    * feat: new way of building providers
    
    * feat: add unit tests
    
    * feat: add custom log function
    
    * refactor: rename
    
    * feat: allow to run deploy images on workflow_dispatch
    
    * Revert "feat: allow to run deploy images on workflow_dispatch"
    
    This reverts commit 97e10b740c3fa9f4204d2d7b9fac9a29cbb5fb2b.
    
    * fix: check-source-conistency test
    
    * fix: reverted changed
    
    * fix: nit pick
    
    * fix: import path
    
    * fix: maybe
    
    * fix: test
    
    * feat: make resolveMulticall sync
    
    * refactor: rename multicallDecorator to multicallWrapper
    
    * fix: cpu increasing over long periods of time
    
    * feat: falback to default execution  model if multicall fails
    
    * revert this commit
    
    * Revert "revert this commit"
    
    This reverts commit 6fc901efc31c026f794ba6271f9855ca9c9d6d57.

--- packages/rpc-providers/src/index.ts
+++ packages/rpc-providers/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./ProviderWithFallback";
 export * from "./ProviderWithAgreement";
 export * from "./TransactionDeliveryMan";
+export * from "./multicall/MulticallWrapper";
---
+++ packages/rpc-providers/test/multicall/MutlicallWrapper.test.ts
@@ -0,0 +1,272 @@
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { BigNumber, Contract, Wallet, ethers } from "ethers";
+import * as hardhat from "hardhat";
+import Sinon from "sinon";
+import {
+  ProviderWithAgreement,
+  ProviderWithFallback,
+  withMulticall,
+} from "../../src";
+import * as multicallUtils from "../../src/multicall/Multicall3Caller";
+import { Counter } from "../../typechain-types";
+import { HardhatProviderMocker, deployCounter } from "../helpers";
+
+chai.use(chaiAsPromised);
+
+const multicallFnSpy = Sinon.spy(multicallUtils.multicall3);
+(multicallUtils as any).multicall3 = multicallFnSpy;
+
+function getProvider(
+  multicallAddress: string,
+  providerFabric: () => ethers.providers.Provider,
+  bufferSize = 2
+) {
+  const multicallProvider = withMulticall(providerFabric, {
+    autoResolveInterval: -1,
+    bufferSizePerBlockId: bufferSize,
+    multicallAddress,
+  });
+  return multicallProvider;
+}
+
+const NOT_MULTICALL_ADDRESS = Wallet.createRandom().address;
+
+const describeMultiWrapperSuite = (
+  providerName: string,
+  providerFabric: () => ethers.providers.Provider
+) => {
+  describe(`${providerName}`, () => {
+    let counter!: Counter;
+    let multicall!: Contract;
+
+    beforeEach(async () => {
+      await hardhat.ethers.provider.send("hardhat_reset", []);
+      counter = await deployCounter(hardhat.ethers.provider);
+      await counter.inc().then((tx) => tx.wait());
+      multicall = await hardhat.ethers.deployContract("Multicall3");
+      multicallFnSpy.resetHistory();
+    });
+
+    it("should group 2 calls to same contract", async () => {
+      const multicallProvider = getProvider(multicall.address, providerFabric);
+      counter = counter.connect(multicallProvider);
+
+      const [count, countPlusOne] = await Promise.all([
+        counter.getCount(),
+        counter.getCountPlusOne(),
+      ]);
+
+      expect(count.toNumber() + 1).to.eq(countPlusOne);
+    });
+
+    it("should group 2 calls to same contract and one to different contract", async () => {
+      const multicallProvider = getProvider(multicall.address, providerFabric);
+      counter = counter.connect(multicallProvider);
+      const counter2 = (await deployCounter(hardhat.ethers.provider)).connect(
+        multicallProvider
+      );
+
+      const [count, count2] = await Promise.all([
+        counter.getCount(),
+        counter2.getCountPlusOne(),
+      ]);
+      expect(count).to.eq(count2);
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("should make call to one contract", async () => {
+      const multicallProvider = getProvider(
+        multicall.address,
+        providerFabric,
+        1
+      );
+      counter = counter.connect(multicallProvider);
+
+      const [count] = await Promise.all([counter.getCount()]);
+
+      expect(count.toNumber()).to.eq(1);
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("should throw error on fail (error should match with ethers error), with same contract", async () => {
+      const [ethersProviderResult] = await Promise.allSettled([
+        counter.connect(hardhat.ethers.provider).fail(),
+      ]);
+
+      const multicallProvider = getProvider(multicall.address, providerFabric);
+      counter = counter.connect(multicallProvider);
+
+      const [resultCounter, resultCounter2] = await Promise.allSettled([
+        counter.getCount(),
+        counter.fail(),
+      ]);
+
+      const resolvedResultCounter =
+        resultCounter as PromiseFulfilledResult<BigNumber>;
+
+      expect(resolvedResultCounter.value.toNumber()).to.eq(1);
+      expect(resolvedResultCounter.status).to.eq("fulfilled");
+
+      expect(resultCounter2).to.deep.eq(ethersProviderResult);
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("should throw error on fail (error should match with ethers error), with two contracts", async () => {
+      const [ethersProviderResult] = await Promise.allSettled([
+        counter.connect(hardhat.ethers.provider).fail(),
+      ]);
+
+      const multicallProvider = getProvider(multicall.address, providerFabric);
+      counter = counter.connect(multicallProvider);
+
+      const counter2 = (await deployCounter(hardhat.ethers.provider)).connect(
+        multicallProvider
+      );
+
+      const [resultCounter, resultCounter2] = await Promise.allSettled([
+        counter2.getCount(),
+        counter.fail(),
+      ]);
+
+      const resolvedResultCounter =
+        resultCounter as PromiseFulfilledResult<BigNumber>;
+
+      expect(resolvedResultCounter.value).to.eq(0);
+      expect(resolvedResultCounter.status).to.eq("fulfilled");
+
+      expect(resultCounter2).to.deep.eq(ethersProviderResult);
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("should reject on all contracts on network error", async () => {
+      // we provide wrong multicall address to simulate rpc error
+      const multicallProvider = getProvider(
+        NOT_MULTICALL_ADDRESS,
+        () => {
+          const provider = providerFabric();
+          provider.call = () => Promise.reject();
+          return provider;
+        },
+        2
+      );
+      counter = counter.connect(multicallProvider);
+
+      const [resultCounter, resultCounter2] = await Promise.allSettled([
+        counter.getCount(),
+        counter.getCount(),
+      ]);
+
+      expect(resultCounter.status).to.eq("rejected");
+      expect(resultCounter2.status).to.eq("rejected");
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("should group by blockTag", async () => {
+      // we provide wrong multicall address to simulate rpc error
+      const multicallProvider = getProvider(
+        multicall.address,
+        providerFabric,
+        1
+      );
+      counter = counter.connect(multicallProvider);
+
+      const blockNumber = await multicallProvider.getBlockNumber();
+
+      const [resultCounter, resultCounter2] = await Promise.all([
+        counter.getCount({ blockTag: blockNumber }),
+        counter.getCount(),
+      ]);
+
+      expect(resultCounter).to.eq(1);
+      expect(resultCounter2).to.eq(1);
+
+      expect(multicallFnSpy.getCalls().length).to.eq(2);
+    });
+
+    it("it should fallback to provider.call when multicall fails", async () => {
+      const multicallProvider = getProvider(
+        NOT_MULTICALL_ADDRESS,
+        providerFabric,
+        2
+      );
+
+      counter = counter.connect(multicallProvider);
+
+      const [count, count2] = await Promise.all([
+        counter.getCount(),
+        counter.getCount(),
+      ]);
+      expect(count).to.eq(count2);
+      expect(multicallFnSpy.getCalls().length).to.eq(1);
+    });
+
+    it("it should work when fallback fails partially", async () => {
+      let stub: Sinon.SinonStub<any[], any>;
+
+      const customProviderFabric = () => {
+        const provider = providerFabric();
+
+        stub = Sinon.stub()
+          .onFirstCall()
+          .rejects(new Error("multi call error"))
+          .onSecondCall()
+          .rejects(new Error("first fallback call error"))
+          .onThirdCall()
+          .callsFake(provider.call.bind(provider));
+
+        provider.call = stub;
+
+        return provider;
+      };
+
+      const multicallProvider = getProvider(
+        NOT_MULTICALL_ADDRESS,
+        customProviderFabric,
+        2
+      );
+
+      counter = counter.connect(multicallProvider);
+
+      const [count, count2] = await Promise.allSettled([
+        counter.getCount(),
+        counter.getCount(),
+      ]);
+
+      expect(stub!.getCalls().length).eq(3);
+      const countCasted = count as PromiseRejectedResult;
+      const count2Casted = count2 as PromiseFulfilledResult<BigNumber>;
+
+      expect(countCasted.status).eq("rejected");
+      expect(countCasted.reason.message).eq("first fallback call error");
+      expect(count2Casted.status).eq("fulfilled");
+      expect(count2Casted.value.toString()).eq("1");
+    });
+  });
+};
+
+describe("Multicall decorator", () => {
+  describeMultiWrapperSuite(
+    "HardhatEthersProvider",
+    () => new HardhatProviderMocker(hardhat.ethers.provider).provider
+  );
+
+  describeMultiWrapperSuite(
+    "ProviderWithAgreement",
+    () =>
+      new ProviderWithAgreement([
+        hardhat.ethers.provider,
+        hardhat.ethers.provider,
+        new ethers.providers.JsonRpcProvider(), // this one will always fail
+      ])
+  );
+
+  describeMultiWrapperSuite(
+    "ProviderWithFallback",
+    () =>
+      new ProviderWithFallback([
+        new ethers.providers.JsonRpcProvider(), // this one will always fail
+        hardhat.ethers.provider,
+      ])
+  );
+});
