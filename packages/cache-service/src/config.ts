acdf05c95e17e3c9227b3d61bc256c01722bba8b
Wojciech Rybakiewicz <98404816+wrybakiewicz@users.noreply.github.com>
2023-11-16 00:49:01 +0100

    feat: mongodb add ttl index (#1461)

--- packages/cache-service/src/config.ts
+++ packages/cache-service/src/config.ts
@@ -6,6 +6,7 @@ interface CacheServiceConfigRequiredFields {
   // empty string means that in-memory DB will be created and used
   // e.g. by e2e test process
   mongoDbUrl: string;
+  mongoDbTTLSeconds: number;
   streamrPrivateKey?: string;
   enableStreamrListening: boolean;
   enableDirectPostingRoutes: boolean;
@@ -70,6 +71,7 @@ const optionalConfig: CacheServiceConfigOptionalFields =
 const config: CacheServiceConfig = {
   appPort: Number(getEnv("APP_PORT", false) || DEFAULT_APP_PORT),
   mongoDbUrl: getEnv("MONGO_DB_URL"),
+  mongoDbTTLSeconds: Number(getEnv("MONGO_DB_TTL_SECONDS")),
   enableStreamrListening: getEnv("ENABLE_STREAMR_LISTENING") === "true",
   streamrPrivateKey: getEnv("STREAMR_PRIVATE_KEY", false),
   enableDirectPostingRoutes: getEnv("ENABLE_DIRECT_POSTING_ROUTES") === "true",
