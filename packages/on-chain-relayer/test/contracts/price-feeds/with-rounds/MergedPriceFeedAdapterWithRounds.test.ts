47217d059da5a7559a10aa3242b9b07a1af4a7fc
Alex Suvorov <Suvorovalex1997@gmail.com>
2023-11-16 14:12:39 +0100

    feat: added events emitting and aggregator function (#1355)
    * feat: added events emitting and aggregator function
    
    * test: implemented great custom tests for merged adapter
    
    * chore: fixes after Mike's review
    
    * chore: added virtual word to functions
    
    * test: added few more tests for geting latest round values
    
    * chore: fixes after Czarek's review
    
    * feat: added check for the size of the values and dataFeedIds arrays
    
    * chore: formattig improved
    
    * chore: removed _emitEventAfterSingleValueUpdate function from common merged adapter contract

--- packages/on-chain-relayer/test/contracts/price-feeds/with-rounds/MergedPriceFeedAdapterWithRounds.test.ts
+++ packages/on-chain-relayer/test/contracts/price-feeds/with-rounds/MergedPriceFeedAdapterWithRounds.test.ts
@@ -2,19 +2,28 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { describeCommonPriceFeedTests } from "../common/price-feed-utils";
 import { describeCommonPriceFeedsAdapterTests } from "../common/price-feeds-adapter-utils";
+import { describeCommonMergedPriceFeedAdapterTests } from "../common/merged-price-feed-adapter-utils";
 
 chai.use(chaiAsPromised);
 
-describe("PriceFeedWithRounds", () => {
+const contractName = "MergedPriceFeedAdapterWithRoundsMock";
+
+describe("MergedPriceFeedAdapterWithRounds", () => {
   describeCommonPriceFeedTests({
-    priceFeedContractName: "MergedPriceFeedAdapterWithRoundsMock",
-    adapterContractName: "MergedPriceFeedAdapterWithRoundsMock",
+    priceFeedContractName: contractName,
+    adapterContractName: contractName,
     expectedRoundIdAfterOneUpdate: 1,
   });
 
   describeCommonPriceFeedsAdapterTests({
-    adapterContractName: "MergedPriceFeedAdapterWithRoundsMock",
+    adapterContractName: contractName,
     hasOnlyOneDataFeed: true,
     skipTestsForPrevDataTimestamp: false,
+  });
+
+  describeCommonMergedPriceFeedAdapterTests({
+    mergedPriceFeedAdapterContractName: contractName,
+    updatedContractName: "MergedPriceFeedAdapterWithRoundsUpdatedMock",
+    roundsEndabled: true,
   });
 });
