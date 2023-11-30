7bd9c2c222269a77ae0a1fc75ad07e1676da083d
Mike <konopek68@gmail.com>
2023-11-30 10:57:09 +0100

    feat: add telemetry to providers (#1547)

---
+++ packages/utils/src/common/telemetry.ts
@@ -0,0 +1,31 @@
+const seralizeToInfluxObj = (
+  obj: Record<string, string | number | boolean | undefined>
+) =>
+  Object.entries(obj)
+    .map(
+      ([k, v]) =>
+        `${fluxEscape(k)}=${fluxEscape(
+          v !== undefined ? v.toString() : "undefined"
+        )}`
+    )
+    .join(",");
+
+function fluxEscape(value: string) {
+  return value
+    .replace(/\\/g, "\\\\")
+    .replace(/"/g, '\\"')
+    .replace(/,/g, "\\,")
+    .replace(/=/g, "\\=")
+    .replace(/ /g, "\\ ");
+}
+
+export const makeInfluxMetric = (
+  name: string,
+  tags: Record<string, string | number | boolean | undefined>,
+  fields: Record<string, string | number | boolean | undefined>
+) => {
+  const parsedTags = seralizeToInfluxObj(tags);
+  const parseFields = seralizeToInfluxObj(fields);
+
+  return `${name},${parsedTags} ${parseFields} ${Date.now()}`;
+};
