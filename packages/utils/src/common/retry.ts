133e0990beae15bdfb7ec1b6c1b28828e268f8d1
Mike <konopek68@gmail.com>
2023-12-13 16:01:06 +0100

    Changes from PR #1553

--- packages/utils/src/common/retry.ts
+++ packages/utils/src/common/retry.ts
@@ -5,37 +5,49 @@ export type RetryConfig<T extends (...args: unknown[]) => Promise<unknown>> = {
   maxRetries: number;
   waitBetweenMs?: number;
   disableLog?: boolean;
+  backOff?: {
+    backOffBase: number;
+  };
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function retry<T extends (...args: any[]) => Promise<unknown>>(
   config: RetryConfig<T>
 ) {
+  if (config.maxRetries === 0) {
+    throw new Error(
+      `Setting ${config.maxRetries} to 0 will never call underlying function`
+    );
+  }
   return async (...args: Parameters<T>): Promise<Awaited<ReturnType<T>>> => {
-    let lastError: unknown;
+    const error = new AggregateError(
+      `Retry failed after ${config.maxRetries} attempts`
+    );
     for (let i = 0; i < config.maxRetries; i++) {
       try {
         return await (config.fn(...args) as Promise<ReturnType<T>>);
       } catch (e) {
-        lastError = e;
+        error.errors.push(e);
+
+        // don't wait in last iteration
+        if (config.waitBetweenMs && i !== config.maxRetries - 1) {
+          const sleepTimeBackOffMultiplier = config.backOff
+            ? Math.pow(config.backOff.backOffBase, i)
+            : 1;
+          const sleepTime = config.waitBetweenMs * sleepTimeBackOffMultiplier;
+
+          await sleep(sleepTime);
+        }
+
         if (!config.disableLog) {
           console.log(
-            `Function ${config.fn.name} failed. Retrying ${i}/${
+            `Function ${config.fn.name} failed. Retrying ${i + 1}/${
               config.maxRetries
-            } after ${config.waitBetweenMs || 0}`
+            }`
           );
-        }
-        // don't wait in last iteration
-        if (config.waitBetweenMs && i === config.maxRetries - 1) {
-          await sleep(config.waitBetweenMs);
         }
       }
     }
-    if (!config.disableLog) {
-      console.log(
-        `Function ${config.fn.name} failed after ${config.maxRetries} attempts`
-      );
-    }
-    throw lastError;
+    throw error;
   };
 }
