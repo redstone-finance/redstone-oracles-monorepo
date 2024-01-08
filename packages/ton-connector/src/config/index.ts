f0cf3161cf505d4a0e71f890276b27a42a4389d2
robal <robert@warp.cc>
2024-01-08 11:58:46 +0100

    chore: common code for parsing env vars (#1715)

--- packages/ton-connector/src/config/index.ts
+++ packages/ton-connector/src/config/index.ts
@@ -1,21 +1,14 @@
+import { RedstoneCommon } from "@redstone-finance/utils";
 import { TonApiV2Config } from "../network/TonNetwork";
 
 const API_ENDPOINT = "https://testnet.toncenter.com/api/v2/jsonRPC";
-
-const getFromEnv = (name: string) => {
-  const envVariable = process.env[name];
-  if (!envVariable) {
-    throw new Error(`Missing environment variable ${name}`);
-  }
-  return envVariable;
-};
 
 export type TonConnectorConfig = TonApiV2Config & {
   mnemonic: string[];
 };
 
 export const config = Object.freeze(<TonConnectorConfig>{
-  mnemonic: getFromEnv("WALLET_MNEMONIC").split(" "),
-  apiKey: getFromEnv("TONCENTER_API_KEY"),
+  mnemonic: RedstoneCommon.getFromEnv("WALLET_MNEMONIC").split(" "),
+  apiKey: RedstoneCommon.getFromEnv("TONCENTER_API_KEY"),
   endpoint: API_ENDPOINT,
 });
