ef0717fce8cf22f61958c22c70488105e812f135
robal <robert@warp.cc>
2024-01-08 11:58:46 +0100

    chore: common code for parsing env vars (#1715)

--- packages/on-chain-relayer/src/FilesystemConfigProvider.ts
+++ packages/on-chain-relayer/src/FilesystemConfigProvider.ts
@@ -1,28 +1,13 @@
+import { RedstoneCommon } from "@redstone-finance/utils";
 import "dotenv/config";
+import fs from "fs";
+import { z } from "zod";
+import { makeConfigProvider } from "./make-config-provider";
 import {
   ConfigProvider,
   OnChainRelayerEnv,
   OnChainRelayerManifestSchema,
 } from "./types";
-import fs from "fs";
-import { makeConfigProvider } from "./make-config-provider";
-
-type GetFromEnvType = {
-  (name: string, optional: true): string | undefined;
-  (name: string, optional: boolean): string | undefined;
-  (name: string, optional?: false): string;
-};
-
-const getFromEnv: GetFromEnvType = (
-  name: string,
-  optional: boolean = false
-) => {
-  const envValue = process.env[name];
-  if (!envValue && !optional) {
-    throw new Error(`Missing environment variable ${name}`);
-  }
-  return envValue!;
-};
 
 // copy of method from oracle-node. Probably should be moved to some common package
 const readJSON = <T>(path: string): T => {
@@ -34,32 +19,12 @@ const readJSON = <T>(path: string): T => {
   }
 };
 
-type GetJSONFromEnvType = {
-  <T>(varName: string, optional: true): T | undefined;
-  <T>(varName: string, optional: boolean): T | undefined;
-  <T>(varName: string, optional?: false): T;
-};
-const getJSONFromEnv: GetJSONFromEnvType = <T>(
-  varName: string,
-  optional = false
-): T | undefined => {
-  const envVal = getFromEnv(varName, optional);
-  if (!envVal) {
-    return;
-  }
-  try {
-    return JSON.parse(envVal) as T;
-  } catch (e) {
-    if (!optional) {
-      throw e;
-    }
-  }
-  return undefined;
-};
-
 const readManifest = () => {
-  const manifestFromEnvAsString = getFromEnv("MANIFEST_OVERRIDE", true);
-  const manifestPath = getFromEnv("MANIFEST_FILE");
+  const manifestFromEnvAsString = RedstoneCommon.getFromEnv(
+    "MANIFEST_OVERRIDE",
+    z.string().optional()
+  );
+  const manifestPath = RedstoneCommon.getFromEnv("MANIFEST_FILE", z.string());
   const manifestObject: unknown = manifestFromEnvAsString
     ? JSON.parse(manifestFromEnvAsString)
     : readJSON(manifestPath);
@@ -68,53 +33,77 @@ const readManifest = () => {
 
 export const fileSystemConfigProvider: ConfigProvider = () => {
   const manifest = readManifest();
-  const mentoMaxDeviationAllowedString = getFromEnv(
-    "MENTO_MAX_DEVIATION_ALLOWED",
-    true
-  );
-
-  const mentoMaxDeviationAllowed = mentoMaxDeviationAllowedString
-    ? Number.parseInt(mentoMaxDeviationAllowedString)
-    : undefined;
 
   const env: OnChainRelayerEnv = {
-    disableCustomGasOracle:
-      getFromEnv("DISABLE_CUSTOM_GAS_ORACLE", true) === "true",
-    relayerIterationInterval: Number(getFromEnv("RELAYER_ITERATION_INTERVAL")),
-    rpcUrls: JSON.parse(getFromEnv("RPC_URLS")) as string[],
-    agreementAcceptableBlocksDiff: Number(
-      getFromEnv("AGREEMENT_ACCEPTABLE_BLOCKS_DIFF", true) ?? -1
+    disableCustomGasOracle: RedstoneCommon.getFromEnv(
+      "DISABLE_CUSTOM_GAS_ORACLE",
+      z.boolean().default(false)
     ),
-    singleProviderOperationTimeout: Number(
-      getFromEnv("SINGLE_PROVIDER_OPERATION_TIMEOUT", true) ?? 5_000
+    relayerIterationInterval: Number(
+      RedstoneCommon.getFromEnv("RELAYER_ITERATION_INTERVAL", z.number())
     ),
-    allProvidersOperationTimeout: Number(
-      getFromEnv("ALL_PROVIDERS_OPERATION_TIMEOUT", true) ?? 20_000
+    rpcUrls: RedstoneCommon.getFromEnv("RPC_URLS", z.array(z.string().url())),
+    agreementAcceptableBlocksDiff: RedstoneCommon.getFromEnv(
+      "AGREEMENT_ACCEPTABLE_BLOCKS_DIFF",
+      z.number().default(-1)
     ),
-    privateKey: getFromEnv("PRIVATE_KEY"),
-    gasLimit: Number.parseInt(getFromEnv("GAS_LIMIT")!),
-    gasMultiplier: Number.parseFloat(
-      getFromEnv("GAS_MULTIPLIER", true) || "1.125"
+    singleProviderOperationTimeout: RedstoneCommon.getFromEnv(
+      "SINGLE_PROVIDER_OPERATION_TIMEOUT",
+      z.number().default(5_000)
     ),
-    healthcheckPingUrl: getFromEnv("HEALTHCHECK_PING_URL", true),
-    expectedTxDeliveryTimeInMS: Number(
-      getFromEnv("EXPECTED_TX_DELIVERY_TIME_IN_MS")
+    allProvidersOperationTimeout: RedstoneCommon.getFromEnv(
+      "ALL_PROVIDERS_OPERATION_TIMEOUT",
+      z.number().default(20_000)
     ),
-    isArbitrumNetwork: getFromEnv("IS_ARBITRUM_NETWORK", true) === "true",
-    fallbackOffsetInMinutes: Number.parseInt(
-      getFromEnv("FALLBACK_OFFSET_IN_MINUTES", true) ?? "0"
+    privateKey: RedstoneCommon.getFromEnv("PRIVATE_KEY", z.string().min(16)),
+    gasLimit: RedstoneCommon.getFromEnv(
+      "GAS_LIMIT",
+      z.number().positive().int()
     ),
-    cacheServiceUrls: getJSONFromEnv("CACHE_SERVICE_URLS", true),
-    historicalPackagesGateways: getJSONFromEnv(
+    gasMultiplier: RedstoneCommon.getFromEnv(
+      "GAS_MULTIPLIER",
+      z.number().default(1.125)
+    ),
+    healthcheckPingUrl: RedstoneCommon.getFromEnv(
+      "HEALTHCHECK_PING_URL",
+      z.string().url().optional()
+    ),
+    expectedTxDeliveryTimeInMS: RedstoneCommon.getFromEnv(
+      "EXPECTED_TX_DELIVERY_TIME_IN_MS",
+      z.number().int().positive()
+    ),
+    isArbitrumNetwork: RedstoneCommon.getFromEnv(
+      "IS_ARBITRUM_NETWORK",
+      z.boolean().default(false)
+    ),
+    fallbackOffsetInMinutes: RedstoneCommon.getFromEnv(
+      "FALLBACK_OFFSET_IN_MINUTES",
+      z.number().int().default(0)
+    ),
+    cacheServiceUrls: RedstoneCommon.getFromEnv(
+      "CACHE_SERVICE_URLS",
+      z.array(z.string().url()).optional()
+    ),
+    historicalPackagesGateways: RedstoneCommon.getFromEnv(
       "HISTORICAL_PACKAGES_GATEWAYS",
-      true
+      z.array(z.string().url()).optional()
     ),
-    isAuctionModel: getFromEnv("IS_AUCTION_MODEL", true) === "true",
-    mentoMaxDeviationAllowed,
-    isNotLazy: getFromEnv("IS_NOT_LAZY", true) === "true",
-    fallbackSkipDeviationBasedFrequentUpdates:
-      getFromEnv("SKIP_TX_SENDING_IF_OFFSET_MINUTES_DID_NOT_PASS", true) ===
-      "true",
+    isAuctionModel: RedstoneCommon.getFromEnv(
+      "IS_AUCTION_MODEL",
+      z.boolean().default(false)
+    ),
+    mentoMaxDeviationAllowed: RedstoneCommon.getFromEnv(
+      "MENTO_MAX_DEVIATION_ALLOWED",
+      z.number().gt(0).optional()
+    ),
+    isNotLazy: RedstoneCommon.getFromEnv(
+      "IS_NOT_LAZY",
+      z.boolean().default(false)
+    ),
+    fallbackSkipDeviationBasedFrequentUpdates: RedstoneCommon.getFromEnv(
+      "SKIP_TX_SENDING_IF_OFFSET_MINUTES_DID_NOT_PASS",
+      z.boolean().default(false)
+    ),
   };
 
   return makeConfigProvider(manifest, env);
