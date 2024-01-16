29c5d6dad9d9b8a1037fd945b4f789885955bfc6
Mike <konopek68@gmail.com>
2024-01-16 11:02:47 +0100

    chore: add script for listing manifests in md (#1795)

---
+++ packages/on-chain-relayer/scripts/list-manifests.ts
@@ -0,0 +1,33 @@
+import { readdirSync, promises } from "fs";
+import { OnChainRelayerManifest } from "../src";
+import path from "path";
+
+async function main() {
+  const dir = readdirSync("./relayer-manifests");
+
+  const manifestsContent = await Promise.all(
+    dir.map((p) =>
+      promises
+        .readFile(path.join("relayer-manifests", p))
+        .then((b) => b.toString())
+    )
+  );
+
+  const manfiests = manifestsContent.map(
+    (c) => JSON.parse(c) as OnChainRelayerManifest
+  );
+
+  const manfiestInfo = manfiests.map((m, i) => {
+    const priceFeeds = Object.keys(m.priceFeeds).join(",");
+    return `
+${dir[i].replaceAll(".json", "")}
+  - priceFeeds: ${priceFeeds}
+  - chain: ${m.chain.name}
+  - dataServiceId: ${m.dataServiceId}`;
+  });
+
+  console.log(manfiestInfo.join("\n"));
+  console.log({ count: manfiestInfo.length });
+}
+
+void main();
