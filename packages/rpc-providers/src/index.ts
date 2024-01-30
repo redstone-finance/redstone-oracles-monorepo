7b840a52bc01ecad19800ee89a1e3adf71123d33
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2024-01-30 11:07:00 +0100

    feat: add LAB.m token (#1865)

--- packages/rpc-providers/src/index.ts
+++ packages/rpc-providers/src/index.ts
@@ -2,5 +2,6 @@ export * from "./ProviderWithFallback";
 export * from "./ProviderWithAgreement";
 export * from "./TransactionDeliveryMan";
 export * from "./multicall/MulticallWrapper";
+export * from "./multicall/Multicall3Caller";
 export * from "./MegaProviderBuilder";
 export * from "./TxBrodcaster";
