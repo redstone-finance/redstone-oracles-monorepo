b75098a7d35a2a2b1523b0893eced90266c5acd3
Mike <konopek68@gmail.com>
2024-02-02 16:27:31 +0100

    feat: Add multi call provider call data size limit (#1904)
    * feat: add multi call provider call data size limit
    
    * fix: linter
    
    * fix: enable timeout in hard-limits
    
    * fix: removing -1 from length
    
    * fix: parameter passing for Cezary
    
    * Update packages/rpc-providers/src/multicall/MulticallWrapper.ts
    
    Co-authored-by: robal <robert@warp.cc>
    
    * fix: in blockhain-providers too
    
    ---------
    
    Co-authored-by: robal <robert@warp.cc>

--- packages/rpc-providers/src/multicall/MulticallWrapper.ts
+++ packages/rpc-providers/src/multicall/MulticallWrapper.ts
@@ -1,8 +1,13 @@
 import { BlockTag, TransactionRequest } from "@ethersproject/abstract-provider";
 import { providers } from "ethers";
 import { Deferrable } from "ethers/lib/utils";
-import { multicall3 } from "./Multicall3Caller";
+import {
+  MULTICALL3_DETERMINISTIC_ADDRESS,
+  multicall3,
+} from "./Multicall3Caller";
 import { MulticallBuffer } from "./MulticallBuffer";
+import { RedstoneCommon } from "@redstone-finance/utils";
+import { z } from "zod";
 
 async function prepareMulticall3Request(tx: Deferrable<TransactionRequest>) {
   return {
@@ -12,40 +17,70 @@ async function prepareMulticall3Request(tx: Deferrable<TransactionRequest>) {
   };
 }
 
-const DEFAULT_LOG = (msg: string) => {
-  console.log(`[MulticallProvider] ${msg}`);
-};
+const DEFAULT_LOG = (msg: string) => console.log(`[MulticallProvider] ${msg}`);
 
 export type MulticallDecoratorOptions = {
-  bufferSizePerBlockId: number;
+  maxCallsCount?: number;
+  maxCallDataSize?: number;
   /** If set to -1 auto resolve is disabled */
-  autoResolveInterval: number;
+  autoResolveInterval?: number;
   multicallAddress?: string;
   log?: (msg: string) => void;
 };
 
+const parseMulticallConfig = (
+  opts: MulticallDecoratorOptions
+): Required<MulticallDecoratorOptions> => {
+  return {
+    multicallAddress:
+      opts.multicallAddress ??
+      RedstoneCommon.getFromEnv(
+        "MULTICALL_CONTRACT_ADDRESS",
+        z.string().default(MULTICALL3_DETERMINISTIC_ADDRESS)
+      ),
+    autoResolveInterval:
+      opts.autoResolveInterval ??
+      RedstoneCommon.getFromEnv(
+        "MULTICALL_AUTO_RESOLVE_INTERVAL",
+        z.number().default(100)
+      ),
+    log: opts.log ?? DEFAULT_LOG,
+    maxCallsCount:
+      opts.maxCallsCount ??
+      RedstoneCommon.getFromEnv(
+        "MULTICALL_MAX_CALLS_COUNT",
+        z.number().default(30)
+      ),
+    maxCallDataSize:
+      opts.maxCallDataSize ??
+      RedstoneCommon.getFromEnv(
+        "MULTICALL_MAX_CALL_DATA_SIZE",
+        z.number().default(50_000)
+      ), // 0.05MB
+  };
+};
+
 export function withMulticall<T extends providers.Provider>(
   buildProvider: () => T,
-  options: MulticallDecoratorOptions
+  options: MulticallDecoratorOptions = {}
 ): T {
-  const log = options.log ?? DEFAULT_LOG;
+  const config = parseMulticallConfig(options);
   const modifiedProvider = buildProvider();
   const originalProvider = buildProvider();
-  const queue = new MulticallBuffer(options.bufferSizePerBlockId);
+  const queue = new MulticallBuffer(
+    config.maxCallsCount,
+    config.maxCallDataSize
+  );
 
-  const resolveMulticall = (blockTag: BlockTag | undefined) => {
+  const executeCallsFromQueue = (blockTag: BlockTag | undefined) => {
+    const callDataSize = queue.callDataSize(blockTag);
     const callEntries = queue.flush(blockTag);
 
-    log(
-      `resolveMulticall: executing multicall3 request blockTag=${blockTag} callsCount=${callEntries.length}`
+    config.log(
+      `resolveMulticall: executing multicall3 request blockTag=${blockTag} callsCount=${callEntries.length} callDataSize=${callDataSize} [bytes]`
     );
 
-    multicall3(
-      originalProvider,
-      callEntries,
-      blockTag,
-      options.multicallAddress
-    )
+    multicall3(originalProvider, callEntries, blockTag, config.multicallAddress)
       .then((result3s) => {
         for (let i = 0; i < result3s.length; i++) {
           // we should handle fallback specially because the can fail partially
@@ -57,7 +92,9 @@ export function withMulticall<T extends providers.Provider>(
         }
       })
       .catch((e) => {
-        log(`call failed rejecting ${callEntries.length} bounded promises`);
+        config.log(
+          `call failed rejecting ${callEntries.length} bounded promises`
+        );
         // if multicall fails we have to reject all promises bounded to it
         for (let i = 0; i < callEntries.length; i++) {
           callEntries[i].reject(e);
@@ -73,19 +110,22 @@ export function withMulticall<T extends providers.Provider>(
     const resolvedBlockTag = await blockTag;
 
     const { promise, resolve, reject } = createDeferedPromise<string>();
-
-    queue.push(resolvedBlockTag, {
+    const entry = {
       ...multicall3Request,
       blockTag: resolvedBlockTag,
       resolve,
       reject,
-    });
+    };
 
-    if (queue.isFull(resolvedBlockTag)) {
-      log(
-        `queue for blockTag=${resolvedBlockTag} is full calling resolveMulticall`
-      );
-      resolveMulticall(resolvedBlockTag);
+    if (queue.willCallDataSizeBeExceeded(resolvedBlockTag, entry)) {
+      console.log("CALL DATA SIZE EXCEEDED");
+      executeCallsFromQueue(resolvedBlockTag);
+    }
+
+    queue.push(resolvedBlockTag, entry);
+
+    if (queue.isCallsCountFull(resolvedBlockTag)) {
+      executeCallsFromQueue(resolvedBlockTag);
     }
 
     return await promise;
@@ -94,14 +134,14 @@ export function withMulticall<T extends providers.Provider>(
   const resolveBuffersForAllBlocks = () => {
     const flushedCallsByBlockId = queue.pickAll();
     for (const [_, callEntries] of flushedCallsByBlockId) {
-      resolveMulticall(callEntries[0].blockTag);
+      executeCallsFromQueue(callEntries[0].blockTag);
     }
   };
 
-  if (options.autoResolveInterval > 0) {
+  if (config.autoResolveInterval > 0) {
     const interval = setInterval(
       resolveBuffersForAllBlocks,
-      options.autoResolveInterval
+      config.autoResolveInterval
     );
     interval.unref();
   }
