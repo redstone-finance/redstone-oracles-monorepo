b0d23cff514ce85d0675c444c1cc389f612cf3e3
Mike <konopek68@gmail.com>
2024-02-05 13:16:28 +0100

    feat: add timely last update timestamp overide (#1906)

--- packages/utils/src/common/time.ts
+++ packages/utils/src/common/time.ts
@@ -21,3 +21,11 @@ export const timeout = <T>(
 
 export const sleep = (ms: number) =>
   new Promise((resolve, _rejects) => setTimeout(resolve, ms));
+
+export const msToMin = (ms: number) => ms / 60_000;
+
+export const minToMs = (min: number) => min * 60_000;
+
+export const hourToMs = (hours: number) => hours * 3600_000;
+
+export const msToHours = (ms: number) => ms / 3600_000;
