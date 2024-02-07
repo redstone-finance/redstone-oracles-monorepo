350fbcb19e3caccd3200e56101d3ec761908213c
Wojciech Rybakiewicz <98404816+wrybakiewicz@users.noreply.github.com>
2024-02-07 13:07:43 +0100

    Changes from PR #1907

--- packages/utils/src/common/axios-retry.ts
+++ packages/utils/src/common/axios-retry.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { retry } from "./retry";
 
-export async function axiosGetWithRetries<T>(
-  url: string,
-  retryLimit: number,
-  retryDelayMilliseconds?: number
-): Promise<T> {
+export async function axiosGetWithRetries<T>(args: {
+  url: string;
+  retryLimit: number;
+  retryDelayMilliseconds?: number;
+  timeout?: number;
+}): Promise<T> {
   return (await retry({
-    fn: async () => await axios.get(url),
+    fn: async () => await axios.get(args.url, { timeout: args.timeout }),
     fnName: `axios.get`,
-    maxRetries: retryLimit,
-    waitBetweenMs: retryDelayMilliseconds,
+    maxRetries: args.retryLimit,
+    waitBetweenMs: args.retryDelayMilliseconds,
   })()) as T;
 }
