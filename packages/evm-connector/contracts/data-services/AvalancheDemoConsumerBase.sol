50a77d62c0b19e35ce0c26ed7c4573721bc7e8af
Mike <konopek68@gmail.com>
2024-02-07 18:05:36 +0100

    feat: Lower concurrency in lambda core monitoring (#1886)

--- packages/evm-connector/contracts/data-services/AvalancheDemoConsumerBase.sol
+++ packages/evm-connector/contracts/data-services/AvalancheDemoConsumerBase.sol
@@ -18,6 +18,8 @@ contract AvalancheDemoConsumerBase is RedstoneConsumerNumericBase {
   ) public view virtual override returns (uint8) {
     if (signerAddress == 0x3a7d971De367FE15D164CDD952F64205F2D9f10c) {
       return 0;
+    } else if (signerAddress == 0x41ed5321B76C045f5439eCf9e73F96c6c25B1D75) {
+      return 1;
     } else {
       revert SignerNotAuthorised(signerAddress);
     }
