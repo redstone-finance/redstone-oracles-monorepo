c4f9a25f044f887c0133e3acd2dd13490f1b6b82
Alex Suvorov <Suvorovalex1997@gmail.com>
2024-02-12 12:52:05 +0100

    chore: lambda for redstone txs indexing in ts (#1555)
    * chore: boilerplate code with initial structure added
    
    * feat: implemented blockchain service
    
    * feat: implemented powerful RedStoneTxParser
    
    * chore: switched to the new yarn
    
    * chore: made it work
    
    * chore: lambda for on-chain-txs indexing name updated
    
    * feat: implemented relayer manifest exporting with checks
    
    * fix: removed tmp manifest for testing new CI
    
    * chore: swaitched to influx js client library
    
    * chore: indexed ethereum transactions
    
    * chore: improved fields in txs points and added dataTimestamp
    
    * feat: added lambda entrypoint
    
    * chore: fixes after self-review
    
    * chore: updated yarn.lock
    
    * feat: added gas-related and blockNumber fields
    
    * chore: part of fixes after review
    
    * chore: fixes after Mike's review
    
    * chore: fixes after Robert's review
    
    * chore: linter errors fixed
    
    * chore: improved types for objects
    
    * feat: improvements in set-last-indexed-block-for-chain script
    
    * chore: added more manifests to exported object in on-chain-relayer
    
    * chore: few bugfixes
    
    * chore: added execution stopping
    
    * chore: yarn.loc removed
    
    * chore: yarn.lock added from main branch
    
    * chore: yarn.lock updated
    
    * chore: yarn.lock updated once again
    
    * fix: fixed the problem with latest block syncing for ethereum and bnb
    
    * chore: updated exported manifests
    
    * chore: added few dbg logs
    
    * chore: comments updated
    
    * chore: improved exit handling
    
    * chore: implemented custom influx js
    
    * chore: fixed a bug in point strinifying
    
    * chore: few more checks with iflux js
    
    * chore: yarn.lock updated
    
    * chore: removed simple influx js
    
    * chore: fixed exported relayer manifests
    
    * chore: resolved issues with syncpack
    
    * chore: few tweaks in automated graceful timeout
    
    * feat: exit handling improved
    
    * chore: yet another try with graceful stoppping
    
    * Update packages/lambda-redstone-txs-indexer/.eslintrc.js
    
    Co-authored-by: robal <robert@warp.cc>
    
    * Update packages/lambda-redstone-txs-indexer/package.json
    
    Co-authored-by: robal <robert@warp.cc>
    
    * Update packages/lambda-redstone-txs-indexer/package.json
    
    Co-authored-by: robal <robert@warp.cc>
    
    * Update packages/lambda-redstone-txs-indexer/src/BlockchainService.ts
    
    Co-authored-by: robal <robert@warp.cc>
    
    * Update packages/lambda-redstone-txs-indexer/src/index.ts
    
    Co-authored-by: robal <robert@warp.cc>
    
    * Update packages/lambda-redstone-txs-indexer/tsconfig.build.json
    
    Co-authored-by: robal <robert@warp.cc>
    
    * chore: added async keyword
    
    * chore: switched to using MathUtils
    
    * chore: added mantaLayerBank to exported manifests
    
    * chore: switched to using getFromEnv in redstone-txs-syncer lambda
    
    * chore: added schema validation to relayer manifests
    
    * chore: added relayer manifests to tsconfig build in on-chain-relayer
    
    * chore: removed excluding of relayer-manifests
    
    * chore: added on-chain-relayer dependency to lambd for redstone txs syncers
    
    * chore: yarn.lock added and updated zip script
    
    * chore: removed unused z library
    
    ---------
    
    Co-authored-by: robal <robert@warp.cc>

---
+++ packages/on-chain-relayer/test/relayer/exported-manifests.test.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import { expect } from "chai";
+import {
+  OnChainRelayerManifest,
+  OnChainRelayerManifestSchema,
+  manifests,
+} from "../../src/";
+
+const RELAYER_MANIFESTS_FOLDER = "./relayer-manifests";
+
+describe("exported-manifests", () => {
+  const files = fs.readdirSync(RELAYER_MANIFESTS_FOLDER);
+  for (const file of files) {
+    if (path.extname(file) === ".json") {
+      const relayerName = path.basename(file, ".json");
+
+      // We run each test in a loop to separate errors for each manifest
+      it(`Should properly export ${relayerName} manifest`, () => {
+        expect(manifests).to.haveOwnProperty(relayerName);
+        const fileContent = fs.readFileSync(
+          path.join(RELAYER_MANIFESTS_FOLDER, file),
+          "utf8"
+        );
+        const parsedManfiest = JSON.parse(
+          fileContent
+        ) as OnChainRelayerManifest;
+        const exportedManifest = manifests[relayerName];
+        expect(exportedManifest).to.deep.equal(parsedManfiest);
+        OnChainRelayerManifestSchema.parse(exportedManifest);
+      });
+    }
+  }
+});
