eb257b344645eda147da0aede70f04fe1513a61d
Jan Olszak <janekolszak@users.noreply.github.com>
2024-02-21 12:11:00 +0100

    feat: fast oracle node (#2002)

--- packages/sdk/src/data-services-urls.ts
+++ packages/sdk/src/data-services-urls.ts
@@ -16,6 +16,7 @@ export const REDSTONE_DATA_SERVICES_URLS: Partial<Record<string, string[]>> = {
   "redstone-primary-demo": DEV_GWS,
   "redstone-main-demo": DEV_GWS,
   "redstone-rapid-demo": DEV_GWS,
+  "redstone-fast-demo": DEV_GWS,
   "redstone-stocks-demo": DEV_GWS,
   "redstone-avalanche-demo": DEV_GWS,
   "redstone-arbitrum-demo": DEV_GWS,
