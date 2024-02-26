da01d0254a740d4f444b926d745407d539ad142b
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2024-02-26 09:41:27 +0100

    feat: price aggregator based on market depth (#2004)

--- packages/utils/src/ISafeNumber/index.ts
+++ packages/utils/src/ISafeNumber/index.ts
@@ -1,4 +1,5 @@
 export * from "./ISafeNumber";
 export * from "./ISafeNumberFactory";
 export * from "./ISafeNumberMath";
+export * from "./ISafeNumberWeighted";
 export * from "./JsNativeSafeNumber";
