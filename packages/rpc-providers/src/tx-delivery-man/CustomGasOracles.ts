4d21713d84fa806e82dfca7296f9e3b6c796eddf
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-03-11 15:44:11 +0100

    feat: handle fee model of etherlink (#2122)
    * feat: custom gas oracle for etherlink-ghostnet

--- packages/rpc-providers/src/tx-delivery-man/CustomGasOracles.ts
+++ packages/rpc-providers/src/tx-delivery-man/CustomGasOracles.ts
@@ -1,51 +1,10 @@
-import {
-  GasOracleFn,
-  TransactionDeliveryManOpts,
-} from "./TransactionDeliveryMan";
-import { RedstoneCommon } from "@redstone-finance/utils";
-import axios from "axios";
-
-const ONE_GWEI = 1e9;
-const ONE_MICRO_KAVA = 1e12;
-
-const getEthGasRequest = RedstoneCommon.memoize({
-  functionToMemoize: () =>
-    axios.get<{
-      result: { suggestBaseFee: number; FastGasPrice: number };
-    }>(`https://api.etherscan.io/api?module=gastracker&action=gasoracle`),
-  ttl: 5_100,
-});
-
-const getEthFeeFromGasOracle: GasOracleFn = async (
-  opts: TransactionDeliveryManOpts
-) => {
-  const { data } = await getEthGasRequest();
-  const { suggestBaseFee, FastGasPrice } = data.result;
-
-  if (!suggestBaseFee || !FastGasPrice) {
-    throw new Error("Failed to fetch price from oracle");
-  }
-
-  return {
-    maxFeePerGas: Math.round(FastGasPrice * ONE_GWEI),
-    maxPriorityFeePerGas: Math.round(
-      (FastGasPrice - suggestBaseFee) * ONE_GWEI
-    ),
-    gasLimit: opts.gasLimit,
-  };
-};
-
-const kavaGasOracle: GasOracleFn = async (
-  opts: TransactionDeliveryManOpts
-  // eslint-disable-next-line @typescript-eslint/require-await
-) => {
-  return {
-    gasLimit: opts.gasLimit,
-    gasPrice: 0.26 * ONE_MICRO_KAVA,
-  };
-};
+import { GasOracleFn } from "./TransactionDeliveryMan";
+import { getEthFeeFromGasOracle } from "./custom-gas-oracles/ethereum";
+import { kavaGasOracle } from "./custom-gas-oracles/kava";
+import { etherlinkGhostnetGasOracle } from "./custom-gas-oracles/etherlink-ghostnet";
 
 export const CHAIN_ID_TO_GAS_ORACLE = {
   1: getEthFeeFromGasOracle,
   2222: kavaGasOracle,
+  128123: etherlinkGhostnetGasOracle,
 } as Record<number, GasOracleFn | undefined>;
