3d71060f3fa80786d3886b82916e40e176613213
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-03-14 09:53:39 +0100

    feat: verify lambda txs indexer unexpected change of lastindexedblock (#2143)

--- packages/utils/src/common/time.ts
+++ packages/utils/src/common/time.ts
@@ -1,20 +1,25 @@
 export const timeout = <T>(
   prom: Promise<T>,
   timeoutMS: number,
-  customErrorMessage?: string
+  customErrorMessage?: string,
+  timeoutCallback?: (
+    resolve: (value: T | PromiseLike<T>) => void,
+    reject: (reason?: unknown) => void
+  ) => void
 ): Promise<T> => {
   let timer: NodeJS.Timeout;
   return Promise.race<T>([
     prom,
     new Promise(
-      (_r, reject) =>
-        (timer = setTimeout(
-          () =>
-            reject(
-              new Error(customErrorMessage ?? `Timeout error ${timeoutMS} [MS]`)
-            ),
-          timeoutMS
-        ))
+      (resolve, reject) =>
+        (timer = setTimeout(() => {
+          if (timeoutCallback) {
+            return timeoutCallback(resolve, reject);
+          }
+          reject(
+            new Error(customErrorMessage ?? `Timeout error ${timeoutMS} [MS]`)
+          );
+        }, timeoutMS))
     ),
   ]).finally(() => clearTimeout(timer));
 };
