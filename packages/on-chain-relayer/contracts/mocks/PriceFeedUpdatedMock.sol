a102ed06fb2085fac005adae4dcea7e254f81b2a
Mike <konopek68@gmail.com>
2024-03-19 10:51:17 +0100

    feat: ABDK audit fixes (#2134)
    * fix: abdk audit fixes
    
    * feat: audit style fixes

--- packages/on-chain-relayer/contracts/mocks/PriceFeedUpdatedMock.sol
+++ packages/on-chain-relayer/contracts/mocks/PriceFeedUpdatedMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.14;
 
 import {PriceFeedWithRounds} from "../price-feeds/with-rounds/PriceFeedWithRounds.sol";
 import {IRedstoneAdapter} from "../core/IRedstoneAdapter.sol";
