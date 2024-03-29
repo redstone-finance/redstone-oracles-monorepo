872b5e345ad5b1f43e293937e6b24c1b858ec5ee
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2024-03-29 15:14:50 +0100

    feat: add sUSDe_RATE_PROVIDER and merged adapter contract for it (#2246)
    * feat: add sUSDe_RATE_PROVIDER and merged adapter contract for it
    
    * Changes after Kuba review
    
    * Renames
    
    * Remove dividing by 0
    
    * Move base contract to relayer and add tests
    
    * Add more tests
    
    * fix lint
    
    * Extract deviation calculation to lib
    
    * Fix test after adding library
    
    * chore: few tweaks for custom susde adapter (#2269)
    
    * chore: few tweaks
    
    * test: added a test for DeviationLib and zero value
    
    * feat: implemented 18 decimals in getRate function
    
    * chore: changed approach to decimals in the custom susde adapter
    
    * test: added 2 more tests for MergedAdapterWithoutRoundsSusdeRateProviderBase.test.ts
    
    * chore: resolved problem with big numbers in tests without ignoring eslint
    
    * feat: add precision divider
    
    ---------
    
    Co-authored-by: Alex Suvorov <Suvorovalex1997@gmail.com>

---
+++ packages/on-chain-relayer/contracts/libs/DeviationLib.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.14;
+
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+library DeviationLib {
+  function calculateAbsDeviation(
+    uint256 proposedValue,
+    uint256 originalValue,
+    uint256 precision
+  ) external pure returns (uint256) {
+    int256 originalValueAsInt = SafeCast.toInt256(originalValue);
+    return
+      abs(
+        ((SafeCast.toInt256(proposedValue) - originalValueAsInt) *
+          100 *
+          SafeCast.toInt256(precision)) / originalValueAsInt
+      );
+  }
+
+  function abs(int256 x) private pure returns (uint256) {
+    return SafeCast.toUint256(x >= 0 ? x : -x);
+  }
+}
