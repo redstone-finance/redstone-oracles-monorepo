ac1ddd19c0746e3fe82ed67f57bf8967d35484e0
Mike <konopek68@gmail.com>
2024-04-02 18:24:19 +0200

    feat: Erc7412 package (#2195)
    * feat: init erc7412 package
    
    * feat: add erc7412 example and code
    
    * feat: define generate7412CompatibleCall
    
    * feat: adjust tests
    
    * fix: yarn versions
    
    * fix: tests
    
    * fix: yarn
    
    * feat: trigger super.validateOnRead but after stale data check
    
    * fix: yarn.lock
    
    ---------
    
    Co-authored-by: robal <robert@warp.cc>

---
+++ packages/erc7412/contracts/RedstoneERC7412.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import { RedstonePrimaryProdWithoutRoundsERC7412 } from '@redstone-finance/on-chain-relayer/contracts/erc7412/RedstonePrimaryProdWithoutRoundsERC7412.sol';
+
