b5e219586d4f7e334263f7758542f61da506c55a
Mike <konopek68@gmail.com>
2024-04-09 10:27:27 +0200

    feat: Use own defined types instead of generated (#2359)

---
+++ packages/evm-multicall/src/types.ts
@@ -0,0 +1,24 @@
+import { Contract } from "ethers";
+
+export interface Call3 {
+  address: string;
+  allowFailure: boolean;
+  callData: string;
+}
+
+export interface Result {
+  success: boolean;
+  returnData: string;
+}
+
+export interface Multicall3 extends Contract {
+  aggregate3(calls: Call3[]): Promise<Result>;
+}
+
+export interface RedstoneCall3 extends Call3 {
+  gasLimit: number;
+}
+
+export interface RedstoneMulticall3 extends Contract {
+  aggregate3(calls: RedstoneCall3[]): Promise<Result>;
+}
