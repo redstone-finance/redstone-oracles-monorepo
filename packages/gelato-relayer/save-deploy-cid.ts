6b55278acaf3c9e73c770fa858a3a8e4fa7d4108
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-04-11 14:47:48 +0200

    chore: write log in gelato function on exec (#2391)

---
+++ packages/gelato-relayer/save-deploy-cid.ts
@@ -0,0 +1,23 @@
+import * as readline from "readline";
+import * as fs from "node:fs";
+
+const OUTPUT_FILE = "web3-function_redstone.deployed";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+rl.on("line", (line: string) => {
+  const cleanLine = line.replace(/\[[0-9;]*m/g, "");
+  const match = cleanLine.match(/CID: (\S+)/);
+  if (match) {
+    const cid = match[1];
+    fs.writeFileSync(OUTPUT_FILE, cid);
+  }
+});
+
+process.on("exit", () => {
+  fs.appendFileSync(OUTPUT_FILE, "\n");
+});
