d97f3f7fd21d0d9405e254dca708f3dd07f90348
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-04-11 11:33:50 +0200

    chore: move gelato test from goerli to sepolia (#2383)

--- packages/gelato-relayer/test/e2e.test.ts
+++ packages/gelato-relayer/test/e2e.test.ts
@@ -2,6 +2,7 @@ import hre from "hardhat";
 import { expect } from "chai";
 import { before } from "mocha";
 import { Web3FunctionHardhat } from "@gelatonetwork/web3-functions-sdk/hardhat-plugin";
+// @ts-ignore
 import * as args from "./userArgs.json";
 
 import { Web3FunctionResultCallData } from "@gelatonetwork/web3-functions-sdk";
@@ -19,7 +20,7 @@ describe("RedStone Gelato w3f: On-chain Relayer & remote manifest e2e Tests", fu
 
   it("Return canExec: true when update is needed", async () => {
     const userArgs: any = { ...args };
-    const { result } = await redstoneW3f.run({ userArgs });
+    const { result } = await redstoneW3f.run("onRun", { userArgs });
     expect(result.canExec).to.equal(true);
 
     const callData = (
@@ -31,7 +32,7 @@ describe("RedStone Gelato w3f: On-chain Relayer & remote manifest e2e Tests", fu
 
     expect(callData.length).to.equal(1);
     expect(callData[0].to).to.equal(
-      "0x37D7B138e9545af5D944009E94D56A5398EC7CE4"
+      "0x11B714817cBC92D402383cFd3f1037B122dcf69A"
     );
   });
 });
