de2640d8aefc4072243f38ba558d0a13692febf1
Mike <konopek68@gmail.com>
2024-04-17 16:55:45 +0200

    feat: remove redstone-prettier-config from package.json (#2436)

--- packages/on-chain-relayer/src/core/contract-interactions/get-adapter-contract.ts
+++ packages/on-chain-relayer/src/core/contract-interactions/get-adapter-contract.ts
@@ -1,8 +1,8 @@
 import { Contract, Wallet } from "ethers";
 import { RedstoneAdapterBase } from "../../../typechain-types";
 import { config } from "../../config";
-import { getRelayerProvider } from "./get-relayer-provider";
 import { getAbiForAdapter } from "./get-abi-for-adapter";
+import { getRelayerProvider } from "./get-relayer-provider";
 
 export const getAdapterContract = () => {
   const { privateKey, adapterContractAddress } = config();
