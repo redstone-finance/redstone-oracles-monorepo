a0cd3e6999427af42d4454de86be8518af4ceb1a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-04-26 11:50:23 +0200

    chore: remove not working rpc urls (#2547)

---
+++ packages/rpc-providers/test/chain-configs/chain-config-rpc-urls.test.ts
@@ -0,0 +1,35 @@
+import { RedstoneCommon } from "@redstone-finance/utils";
+import chai from "chai";
+import { ethers } from "ethers";
+import { describe, test } from "mocha";
+import { ChainConfigs } from "../../src";
+
+export const RETRY_CONFIG = {
+  maxRetries: 3,
+  waitBetweenMs: 1000,
+  backOff: {
+    backOffBase: 2,
+  },
+  disableLog: true,
+};
+
+describe("Validate chain config rpc urls", () => {
+  for (const [name, config] of Object.entries(ChainConfigs)) {
+    for (const rpcUrl of config.publicRpcUrls) {
+      if (rpcUrl.includes("localhost")) {
+        continue;
+      }
+
+      test(`Test '${name}' rpc url: ${rpcUrl}`, async () => {
+        const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+        const chainId = await RedstoneCommon.retry({
+          fn: async () => (await provider.getNetwork()).chainId,
+          fnName: "provider.getNetwork()",
+          ...RETRY_CONFIG,
+        })();
+
+        chai.expect(chainId, `Wrong chainId`).to.eq(config.chainId);
+      });
+    }
+  }
+});
