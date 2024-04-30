056ce6da893e868f7f561f82c3a32fd94844c357
Mike <konopek68@gmail.com>
2024-04-30 09:40:56 +0200

    feat: Relayer improvments (#2567)

--- packages/utils/src/common/js-utils.ts
+++ packages/utils/src/common/js-utils.ts
@@ -16,3 +16,20 @@ export const toSafeRecord = <V, R extends Record<string | number, V>>(
       return target[p];
     },
   }) as Required<R>;
+
+export type PromisifiedRecord<T> = {
+  [t in keyof T]: Promise<T[t]>;
+};
+
+export const waitForAllRecord = async <T>(
+  s: PromisifiedRecord<T>
+): Promise<T> => {
+  const keys = Object.keys(s) as [keyof T];
+  const promises = keys.map((k) => s[k]);
+  const results = await Promise.all(promises);
+  const result = {} as T;
+  for (let i = 0; i < keys.length; ++i) {
+    result[keys[i]] = results[i];
+  }
+  return result;
+};
