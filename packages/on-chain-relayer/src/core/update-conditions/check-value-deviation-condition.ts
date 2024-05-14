ad389630a4e1396dbd6978ce68416c1de469baa8
robal <robert@warp.cc>
2024-05-14 09:46:40 +0200

    feat: allow for multi-point packages in relayer (#2589)

--- packages/on-chain-relayer/src/core/update-conditions/check-value-deviation-condition.ts
+++ packages/on-chain-relayer/src/core/update-conditions/check-value-deviation-condition.ts
@@ -4,59 +4,86 @@ import {
   ValuesForDataFeeds,
 } from "@redstone-finance/sdk";
 import { MathUtils } from "@redstone-finance/utils";
-import { BigNumber } from "ethers";
-import { formatUnits } from "ethers/lib/utils";
+import { BigNumber, utils } from "ethers";
 import { RelayerConfig } from "../../types";
+import { chooseDataPackagesTimestamp } from "./data-packages-timestamp";
+
+const getDataPointsForDataFeedId = (
+  dataPackages: DataPackagesResponse,
+  dataFeedId: string
+) =>
+  Object.values(dataPackages)
+    .flat()
+    .flatMap((dataPackage) => dataPackage!.dataPackage.dataPoints)
+    .filter((dataPoint) => dataPoint.dataFeedId === dataFeedId);
+
+const verifyDataPackagesAreDisjoint = (dataPackages: DataPackagesResponse) => {
+  const dataPackagesPerDataFeedId: Partial<Record<string, string>> = {};
+  const warnings = [];
+  for (const dataPackage of Object.values(dataPackages).flat()) {
+    for (const dataPoint of dataPackage!.dataPackage.dataPoints) {
+      const dataFeedId = dataPoint.dataFeedId;
+      const dataPackageName = dataPackage!.dataPackage.dataFeedId!;
+
+      if (!dataPackagesPerDataFeedId[dataFeedId]) {
+        dataPackagesPerDataFeedId[dataFeedId] = dataPackageName;
+      } else {
+        warnings.push(
+          `Potential misconfiguration detected! Data feed ${dataFeedId} included in two packages: ${dataPackageName} and ${dataPackagesPerDataFeedId[dataFeedId]}`
+        );
+      }
+    }
+  }
+  return warnings;
+};
 
 export const checkValueDeviationCondition = (
   dataPackages: DataPackagesResponse,
   valuesFromContract: ValuesForDataFeeds,
   config: RelayerConfig
 ) => {
-  const dataFeedsIds = Object.keys(dataPackages);
-
   const logTrace = new ValueDeviationLogTrace();
 
   let maxDeviation = 0;
   let shouldUpdatePrices = false;
-  for (const dataFeedId of dataFeedsIds) {
+  const warnings = verifyDataPackagesAreDisjoint(dataPackages);
+  logTrace.addWarnings(warnings);
+  const timestampMilliseconds = chooseDataPackagesTimestamp(dataPackages);
+  for (const dataFeedId of config.dataFeeds) {
     const valueFromContract =
       valuesFromContract[dataFeedId] ?? BigNumber.from(0);
+    const dataPoints = getDataPointsForDataFeedId(dataPackages, dataFeedId);
+    for (const dataPoint of dataPoints) {
+      const dataPointObj = dataPoint.toObj() as INumericDataPoint;
 
-    for (const { dataPackage } of dataPackages[dataFeedId]!) {
-      for (const dataPoint of dataPackage.dataPoints) {
-        const dataPointObj = dataPoint.toObj() as INumericDataPoint;
+      const valueFromContractAsDecimal = Number(
+        utils.formatUnits(
+          valueFromContract.toString(),
+          dataPointObj.decimals ?? consts.DEFAULT_NUM_VALUE_DECIMALS
+        )
+      );
 
-        const valueFromContractAsDecimal = Number(
-          formatUnits(
-            valueFromContract.toString(),
-            dataPointObj.decimals ?? consts.DEFAULT_NUM_VALUE_DECIMALS
-          )
+      logTrace.addPerDataFeedLog(
+        timestampMilliseconds,
+        valueFromContractAsDecimal,
+        dataPointObj
+      );
+
+      const currentDeviation = calculateDeviation(
+        dataPointObj.value,
+        valueFromContractAsDecimal
+      );
+
+      if (config.priceFeedsDeviationOverrides?.[dataFeedId]) {
+        shouldUpdatePrices ||=
+          currentDeviation >= config.priceFeedsDeviationOverrides[dataFeedId];
+        logTrace.addDeviationInfo(
+          currentDeviation,
+          config.priceFeedsDeviationOverrides[dataFeedId],
+          dataFeedId
         );
-
-        logTrace.addPerDataFeedLog(
-          dataPackage.timestampMilliseconds,
-          valueFromContractAsDecimal,
-          dataPackages[dataFeedId]!.length,
-          dataPointObj
-        );
-
-        const currentDeviation = calculateDeviation(
-          dataPointObj.value,
-          valueFromContractAsDecimal
-        );
-
-        if (config.priceFeedsDeviationOverrides?.[dataFeedId]) {
-          shouldUpdatePrices ||=
-            currentDeviation >= config.priceFeedsDeviationOverrides[dataFeedId];
-          logTrace.addDeviationInfo(
-            currentDeviation,
-            config.priceFeedsDeviationOverrides[dataFeedId],
-            dataFeedId
-          );
-        } else {
-          maxDeviation = Math.max(currentDeviation, maxDeviation);
-        }
+      } else {
+        maxDeviation = Math.max(currentDeviation, maxDeviation);
       }
     }
   }
@@ -76,12 +103,11 @@ export const checkValueDeviationCondition = (
 const calculateDeviation = (
   valueFromFetchedDataPackage: number,
   valueFromContract: number
-) => {
-  return MathUtils.calculateDeviationPercent({
+) =>
+  MathUtils.calculateDeviationPercent({
     deviatedValue: valueFromFetchedDataPackage,
     baseValue: valueFromContract,
   });
-};
 
 class ValueDeviationLogTrace {
   private perDataFeedId: Record<
@@ -90,7 +116,6 @@ class ValueDeviationLogTrace {
         valueFromContract: number;
         valuesFromNode: number[];
         timestamp: number;
-        packagesCount: number;
       }
     | undefined
   > = {};
@@ -99,11 +124,11 @@ class ValueDeviationLogTrace {
     thresholdDeviation: string;
     dataFeedId?: string;
   }[] = [];
+  private warnings: string[] = [];
 
   addPerDataFeedLog(
     timestamp: number,
     valueFromContract: number,
-    packagesCount: number,
     dataPoint: INumericDataPoint
   ) {
     const dataFeedId = dataPoint.dataFeedId;
@@ -112,7 +137,6 @@ class ValueDeviationLogTrace {
       this.perDataFeedId[dataFeedId] = {
         valueFromContract: valueFromContract,
         valuesFromNode: [dataPoint.value],
-        packagesCount,
         timestamp,
       };
     } else {
@@ -132,10 +156,15 @@ class ValueDeviationLogTrace {
     });
   }
 
+  addWarnings(warnings: string[]) {
+    this.warnings.push(...warnings);
+  }
+
   toString(): string {
     return JSON.stringify({
       ...this.perDataFeedId,
       ...this.deviationLogs,
+      ...(this.warnings.length ? { warnings: this.warnings } : {}),
     });
   }
 }
