77358467abebf3eed7bb6356e89cba875aa69f7a
robal <robert@warp.cc>
2024-05-15 11:56:14 +0200

    chore: add build-dev to force build everything, not only application code (#2628)

--- packages/on-chain-relayer/hardhat.config.ts
+++ packages/on-chain-relayer/hardhat.config.ts
@@ -1,10 +1,11 @@
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import "dotenv/config";
 import "hardhat-gas-reporter";
 import { HardhatUserConfig } from "hardhat/config";
+
+import "@nomiclabs/hardhat-waffle";
 
 const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
 
