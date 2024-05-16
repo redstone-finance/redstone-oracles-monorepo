796f3ed2d2559532c8472bdb90c91dff13ab4be5
robal <robert@warp.cc>
2024-05-16 12:49:16 +0200

    Changes from PR #2627

--- packages/protocol/src/data-package/DataPackage.ts
+++ packages/protocol/src/data-package/DataPackage.ts
@@ -19,16 +19,19 @@ import { SignedDataPackage } from "./SignedDataPackage";
 export interface DataPackagePlainObj {
   dataPoints: DataPointPlainObj[];
   timestampMilliseconds: number;
+  dataPackageId?: string; // temporarly we need both to avoid simultaneous updates of whole infrastructure
   dataFeedId?: string;
 }
 
 export class DataPackage extends Serializable {
+  public dataFeedId?: string;
   constructor(
     public readonly dataPoints: DataPoint[],
     public readonly timestampMilliseconds: number,
-    public readonly dataFeedId?: string
+    public readonly dataPackageId?: string
   ) {
     super();
+    this.dataFeedId = this.dataPackageId;
 
     if (dataPoints.length === 0) {
       throw new Error("Can not create a data package with no data points");
@@ -71,7 +74,7 @@ export class DataPackage extends Serializable {
     return new DataPackage(
       dataPoints,
       plainObject.timestampMilliseconds,
-      plainObject.dataFeedId
+      plainObject.dataPackageId
     );
   }
 
