4652a7ce31779d91d33aa72dc71c640b87662d0e
Mike <konopek68@gmail.com>
2024-06-04 11:50:07 +0200

    Fix is node runtime check (#2781)

--- packages/utils/src/common/runtime.ts
+++ packages/utils/src/common/runtime.ts
@@ -1,1 +1,6 @@
-export const isNodeRuntime = () => !!process;
+export const isNodeRuntime = () =>
+  typeof process !== "undefined" &&
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  process.versions != null &&
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  process.versions.node != null;
