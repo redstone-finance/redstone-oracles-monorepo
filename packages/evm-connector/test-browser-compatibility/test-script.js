38fee221f440f0c6cf38a47113db536badb72ae3
Mike <konopek68@gmail.com>
2024-06-04 11:50:07 +0200

    Fix is node runtime check (#2781)

---
+++ packages/evm-connector/test-browser-compatibility/test-script.js
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import { WrapperBuilder } from "../src/index";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://ethereum.publicnode.com"
+);
+
+const contract = new ethers.Contract(
+  "0x76A495b0bFfb53ef3F0E94ef0763e03cE410835C",
+  [
+    "function updateDataFeedsValues(uint256 dataPackagesTimestamp) external",
+    "function getUniqueSignersThreshold() public view returns (uint8)",
+  ],
+  provider
+);
+
+const builder = new WrapperBuilder(contract);
+
+const contractWrapped = builder.usingDataService({
+  dataPackagesIds: ["pufETH/ETH"],
+  dataServiceId: "redstone-primary-prod",
+});
+
+async function populateTx() {
+  const tx = await contractWrapped.populateTransaction.updateDataFeedsValues(0);
+  console.log({ tx });
+
+  document.getElementById("code").textContent = JSON.stringify(tx, null, 4);
+}
+
+populateTx();
