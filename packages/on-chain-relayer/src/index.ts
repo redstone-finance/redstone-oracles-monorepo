54079a5a4e94ade1e6b578a8ecb3c489b7916039
robal <robert@warp.cc>
2024-06-27 22:37:27 +0200

    chore: refactor (#2920)

--- packages/on-chain-relayer/src/index.ts
+++ packages/on-chain-relayer/src/index.ts
@@ -1,4 +1,16 @@
-export { type RedstoneAdapterBase } from "../typechain-types/";
+import { abi as ISortedOraclesAbi } from "../artifacts/contracts/custom-integrations/mento/ISortedOracles.sol/ISortedOracles.json";
+import { abi as MentoAdapterBaseAbi } from "../artifacts/contracts/custom-integrations/mento/MentoAdapterBase.sol/MentoAdapterBase.json";
+import { abi as PriceFeedsAdapterWithRoundsAbi } from "../artifacts/contracts/price-feeds/with-rounds/PriceFeedsAdapterWithRounds.sol/PriceFeedsAdapterWithRounds.json";
+import { abi as PriceFeedWithRoundsAbi } from "../artifacts/contracts/price-feeds/with-rounds/PriceFeedWithRounds.sol/PriceFeedWithRounds.json";
+import manifests from "../relayer-manifests";
+
+export type {
+  ISortedOracles,
+  MentoAdapterBase,
+  PriceFeedsAdapterWithRounds,
+  PriceFeedWithRounds,
+  RedstoneAdapterBase,
+} from "../typechain-types";
 export {
   getIterationArgs,
   type UpdatePricesArgs,
@@ -18,6 +30,10 @@ export {
   type RelayerConfig,
   type UpdateTriggers,
 } from "./types";
-export { manifests };
-
-import manifests from "../relayer-manifests";
+export {
+  ISortedOraclesAbi,
+  manifests,
+  MentoAdapterBaseAbi,
+  PriceFeedsAdapterWithRoundsAbi,
+  PriceFeedWithRoundsAbi,
+};
