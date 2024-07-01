80260db8382d5039e7af37b8f1926a8c939b571e
Maciej Bohusz <93550769+bohmac@users.noreply.github.com>
2024-07-01 12:43:45 +0200

    2830 Added retries to dry run tests when 513 occurs (#2859)
    * chore: add debugging information about http code response to test failures a tak
    
    * chore: add try catch to debug 513 error codr
    
    * test: expanding logs
    
    * test: Add retry logic for requests in test, clean up debug code
    
    * test:add axios-retry 4.4.0
    
    * test: alignment of axios version
    
    * test: Fix typo in log
    
    * test: Update Axios to 1.7.2
    
    * test: Axios version alignment
    
    * test: Axios version alignment v2
    
    * test: Axios version alignment

--- packages/integration/src/framework/fetch-data-packages-from-local-and-prod-cache.ts
+++ packages/integration/src/framework/fetch-data-packages-from-local-and-prod-cache.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 import { DataPackages } from "./compare-data-packages";
 import { GatewayInstance, getCacheServicePort } from "./gateway-manager";
+
+axiosRetry(axios, {
+  retries: 2,
+  retryCondition: (error) => {
+    return axios.isAxiosError(error) && error.response?.status === 513;
+  },
+  retryDelay: (retryCount) => {
+    console.log("513 - Retry attempt: ", retryCount);
+    return retryCount * 500;
+  },
+});
 
 const HISTORICAL_ORACLE_GATEWAY_URL = process.env.HISTORICAL_ORACLE_GATEWAY_URL;
 
@@ -17,13 +29,29 @@ export const fetchDataPackagesFromCaches = async (
       )}/data-packages/historical/mock-data-service/${timestamp}`
     )
   ).data;
-
+  let responseFromProdCache: DataPackages | undefined;
   const prodDataServiceName = manifestFileName.replace("data-services/", "");
-  const responseFromProdCache = (
-    await axios.get<DataPackages>(
-      `${HISTORICAL_ORACLE_GATEWAY_URL}/data-packages/historical/redstone-${prodDataServiceName}-prod/${timestamp}`
-    )
-  ).data;
-
+  try {
+    responseFromProdCache = (
+      await axios.get<DataPackages>(
+        `${HISTORICAL_ORACLE_GATEWAY_URL}/data-packages/historical/redstone-${prodDataServiceName}-prod/${timestamp}`,
+        {
+          validateStatus: function (status) {
+            return status === 200;
+          },
+        }
+      )
+    ).data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Prod cache response code:",
+        error.response?.status,
+        " For timestamp ",
+        timestamp
+      );
+    }
+    throw error;
+  }
   return { responseFromLocalCache, responseFromProdCache, timestamp };
 };
