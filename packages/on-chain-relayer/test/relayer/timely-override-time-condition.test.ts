8bd5e8975401ac7d847c21a467f545d761806f93
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2024-07-22 14:37:50 +0200

    Refactor relayers (#3111)
    * Initial changes
    
    * Refactor cd.
    
    * Catalog files
    
    * Merge config providers and entrypoint
    
    * Update imports
    
    * Update gelato
    
    * Update relayer tests
    
    * Code review changes
    
    * Bug fixes
    
    * Change folder name
    
    * Update imports in tests
    
    * Code review fixes
    
    * Clean up docker
    
    ---------
    
    Co-authored-by: Jakub Jakubowski <jakubjakubowski@Jakubs-MacBook-Air.local>

--- packages/on-chain-relayer/test/relayer/timely-override-time-condition.test.ts
+++ packages/on-chain-relayer/test/relayer/timely-override-time-condition.test.ts
@@ -6,27 +6,45 @@ import { mockEnvVariables } from "../helpers";
 describe("timely override time condition", () => {
   it("should temporary set update interval when time conditions exists", async () => {
     mockEnvVariables({
-      updateConditions: ["value-deviation", "time"],
-      updatePriceInterval: 1_000,
+      updateConditions: {
+        ETH: ["value-deviation", "time"],
+        BTC: ["value-deviation", "time"],
+      },
+      updateTriggers: {
+        ETH: {
+          deviationPercentage: 10,
+          timeSinceLastUpdateInMilliseconds: 1000,
+        },
+        BTC: {
+          deviationPercentage: 10,
+          timeSinceLastUpdateInMilliseconds: 1000,
+        },
+      },
     });
-    expect(config().updatePriceInterval).to.equal(1_000);
-    expect(config().updateConditions).to.deep.equal([
+    expect(
+      config().updateTriggers["ETH"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(1_000);
+    expect(config().updateConditions["ETH"]).to.deep.equal([
       "value-deviation",
       "time",
     ]);
 
     timelyOverrideSinceLastUpdate(1);
 
-    expect(config().updatePriceInterval).to.equal(1);
-    expect(config().updateConditions).to.deep.equal([
+    expect(
+      config().updateTriggers["ETH"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(1);
+    expect(config().updateConditions["ETH"]).to.deep.equal([
       "value-deviation",
       "time",
     ]);
 
     await RedstoneCommon.sleep(2);
 
-    expect(config().updatePriceInterval).to.equal(1_000);
-    expect(config().updateConditions).to.deep.equal([
+    expect(
+      config().updateTriggers["ETH"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(1_000);
+    expect(config().updateConditions["ETH"]).to.deep.equal([
       "value-deviation",
       "time",
     ]);
@@ -34,23 +52,40 @@ describe("timely override time condition", () => {
 
   it("should temporary set update interval when time conditions DOES NOT exists", async () => {
     mockEnvVariables({
-      updateConditions: ["value-deviation"],
-      updatePriceInterval: undefined,
+      updateConditions: {
+        ETH: ["value-deviation", "time"],
+        BTC: ["value-deviation"],
+      },
+      updateTriggers: {
+        ETH: {
+          deviationPercentage: 10,
+          timeSinceLastUpdateInMilliseconds: 1000,
+        },
+        BTC: {
+          deviationPercentage: 10,
+        },
+      },
     });
-    expect(config().updateConditions).to.deep.equal(["value-deviation"]);
-    expect(config().updatePriceInterval).to.equal(undefined);
+    expect(config().updateConditions["BTC"]).to.deep.equal(["value-deviation"]);
+    expect(
+      config().updateTriggers["BTC"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(undefined);
 
     timelyOverrideSinceLastUpdate(1);
 
-    expect(config().updatePriceInterval).to.equal(1);
-    expect(config().updateConditions).to.deep.equal([
+    expect(
+      config().updateTriggers["BTC"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(1);
+    expect(config().updateConditions["BTC"]).to.deep.equal([
       "value-deviation",
       "time",
     ]);
 
     await RedstoneCommon.sleep(2);
 
-    expect(config().updatePriceInterval).to.equal(undefined);
-    expect(config().updateConditions).to.deep.equal(["value-deviation"]);
+    expect(
+      config().updateTriggers["BTC"].timeSinceLastUpdateInMilliseconds
+    ).to.equal(undefined);
+    expect(config().updateConditions["BTC"]).to.deep.equal(["value-deviation"]);
   });
 });
