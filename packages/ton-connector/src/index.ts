2ec8761491c5923f2102a32e561f9d2c1a1e77c2
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-07-22 10:40:57 +0200

    fix: ton connector build settings (#3157)

--- packages/ton-connector/src/index.ts
+++ packages/ton-connector/src/index.ts
@@ -1,6 +1,7 @@
 export { TonPriceFeed } from "../wrappers/TonPriceFeed";
 export { TonPriceManager } from "../wrappers/TonPriceManager";
 export { TonContractConnector } from "./TonContractConnector";
+export * from "./config";
 export { BlueprintTonNetwork } from "./network/BlueprintTonNetwork";
 export { CustomTonNetwork } from "./network/CustomTonNetwork";
 export * from "./network/TonNetwork";
