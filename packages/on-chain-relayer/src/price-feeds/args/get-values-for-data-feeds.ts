e70a6c2aa01965a4adbe19130b52a10d7689fda0
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2024-07-22 14:37:50 +0200

    Refactor relayers (#3111)

--- packages/on-chain-relayer/src/price-feeds/args/get-values-for-data-feeds.ts
+++ packages/on-chain-relayer/src/price-feeds/args/get-values-for-data-feeds.ts
@@ -4,7 +4,7 @@ import { IRedstoneAdapter, MentoAdapterBase } from "../../../typechain-types";
 import { config } from "../../config";
 import { getValuesForMentoDataFeeds } from "../../custom-integrations/mento/mento-utils";
 
-import { getSortedOraclesContractAtAddress } from "./get-sorted-oracles-contract-at-address";
+import { getSortedOraclesContractAtAddress } from "../../custom-integrations/mento/get-sorted-oracles-contract-at-address";
 
 export const getValuesForDataFeeds = async (
   priceFeedsAdapter: IRedstoneAdapter,
--- packages/on-chain-relayer/test/update-conditions/cron-condition.test.ts
+++ packages/on-chain-relayer/test/update-conditions/cron-condition.test.ts
@@ -11,7 +11,11 @@ import {
 describe("cron-condition", () => {
   before(() => {
     mockEnvVariables({
-      cronExpressions: ["0 * * * *"], // every hour at 0th minute
+      updateTriggers: {
+        ETH: {
+          cron: ["0 * * * *"], // every hour at 0th minute
+        },
+      },
     });
   });
 
@@ -23,6 +27,7 @@ describe("cron-condition", () => {
     setCurrentSystemTime("2023-08-16T00:01:00");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T00:00:30");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
@@ -36,6 +41,7 @@ describe("cron-condition", () => {
     setCurrentSystemTime("2023-08-16T00:59:59");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T00:00:30");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
@@ -49,6 +55,7 @@ describe("cron-condition", () => {
     setCurrentSystemTime("2023-08-16T01:00:01");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T00:59:59");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
@@ -60,12 +67,17 @@ describe("cron-condition", () => {
 
   it("should return true if one of multiple cron expressions fulfilled", () => {
     mockEnvVariables({
-      cronExpressions: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+      updateTriggers: {
+        ETH: {
+          cron: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+        },
+      },
     });
 
     setCurrentSystemTime("2023-08-16T01:02:01");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T01:01:30");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
@@ -77,12 +89,17 @@ describe("cron-condition", () => {
 
   it("should return true if two of multiple cron expressions fulfilled", () => {
     mockEnvVariables({
-      cronExpressions: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+      updateTriggers: {
+        ETH: {
+          cron: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+        },
+      },
     });
 
     setCurrentSystemTime("2023-08-16T01:01:01");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T01:00:30");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
@@ -94,12 +111,17 @@ describe("cron-condition", () => {
 
   it("should return false if none of multiple cron expressions fulfilled", () => {
     mockEnvVariables({
-      cronExpressions: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+      updateTriggers: {
+        ETH: {
+          cron: ["0 * * * *", "1 * * * *", "2 * * * *"], // every hour at 0th, 1st and 2nd minute
+        },
+      },
     });
 
     setCurrentSystemTime("2023-08-16T01:05:01");
     const lastUpdateTimestamp = dateStrToMilliseconds("2023-08-16T01:03:30");
     const { shouldUpdatePrices, warningMessage } = cronCondition(
+      "ETH",
       lastUpdateTimestamp,
       config()
     );
