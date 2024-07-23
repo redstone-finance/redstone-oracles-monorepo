ee9578535ba80baa2758d90bc92fbd46e08c9adb
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-07-23 21:24:05 +0200

    feat: upgrade fuel integration to testnet-toolchain (#3127)
    * feat: upgrade fuel-connector for testnet

--- packages/fuel-connector/src/FuelContractConnector.ts
+++ packages/fuel-connector/src/FuelContractConnector.ts
@@ -1,12 +1,19 @@
-import { WalletLocked, WalletUnlocked } from "fuels";
+import { IContractConnector } from "@redstone-finance/sdk";
+import { Account } from "fuels";
 import { FuelConnector } from "./FuelConnector";
 
-export abstract class FuelContractConnector<Adapter> extends FuelConnector {
-  protected constructor(
-    protected wallet: WalletLocked | WalletUnlocked | undefined
-  ) {
+export abstract class FuelContractConnector<Adapter>
+  extends FuelConnector
+  implements IContractConnector<Adapter>
+{
+  protected constructor(protected wallet?: Account) {
     super(wallet?.provider.url);
   }
 
   abstract getAdapter(): Promise<Adapter>;
+
+  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
+  waitForTransaction(_txId: string): Promise<boolean> {
+    return Promise.resolve(true);
+  }
 }
