01f55fe995f4075817e008e42cfddfa5a6bb234f
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-07-23 21:24:05 +0200

    feat: upgrade fuel integration to testnet-toolchain (#3127)
    * feat: upgrade fuel-connector for testnet

--- packages/fuel-connector/test/common/set-test-envs.ts
+++ packages/fuel-connector/test/common/set-test-envs.ts
@@ -1,7 +1,7 @@
 // Local: Run the instance of fuel-core by invoking: fuel-core run --db-type in-memory
 // put here a private key of one of initial/genesis wallets to be found at the top of logs of the running instance.
+// By default --default-signer flag would sign your transactions with the following private-key:
+// 0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c
 
-process.env.GENESIS_SECRET = "";
-process.env.CONTRACT_ID =
-  "0x8fdae6cb766121b5382977e98dc948dbdc95812a3e60c2cf2c0dfff06e877989";
-process.env.PRIVATE_KEY = "";
+process.env.PRIVATE_KEY =
+  "0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c";
