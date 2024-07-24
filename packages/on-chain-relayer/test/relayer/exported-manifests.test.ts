857b68bcce487bc7bee6c5d0a4d39343314a6d5d
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-07-24 14:35:21 +0200

    feat: ci for wallet balances monitoring (#3166)
    * feat: monitoring-manifests-ci
    
    Co-authored-by: robal <robert@warp.cc>

--- packages/on-chain-relayer/test/relayer/exported-manifests.test.ts
+++ packages/on-chain-relayer/test/relayer/exported-manifests.test.ts
@@ -1,30 +1,50 @@
 import { expect } from "chai";
 import fs from "fs";
 import path from "path";
-import { OnChainRelayerManifestSchema, manifests } from "../../src/";
+import { manifests, multiFeedManifests } from "../../src";
+import {
+  AnyOnChainRelayerManifest,
+  AnyOnChainRelayerManifestSchema,
+} from "../../src/schemas";
 
 const RELAYER_MANIFESTS_FOLDER = "./relayer-manifests";
+const MULTI_FEED_RELAYER_MANIFESTS_FOLDER = "./relayer-manifests-multi-feed";
 
 describe("exported-manifests", () => {
-  const files = fs.readdirSync(RELAYER_MANIFESTS_FOLDER);
+  checkExportedManifests(RELAYER_MANIFESTS_FOLDER, manifests);
+});
+
+describe("exported multi-feed-manifests", () => {
+  checkExportedManifests(
+    MULTI_FEED_RELAYER_MANIFESTS_FOLDER,
+    multiFeedManifests
+  );
+});
+
+function checkExportedManifests(
+  directory: string,
+  manifestConfig: { [p: string]: AnyOnChainRelayerManifest }
+) {
+  const files = fs.readdirSync(directory);
+
   for (const file of files) {
     if (path.extname(file) === ".json") {
       const relayerName = path.basename(file, ".json");
 
       // We run each test in a loop to separate errors for each manifest
       it(`Should properly export ${relayerName} manifest`, () => {
-        expect(manifests).to.haveOwnProperty(relayerName);
-        const fileContent = fs.readFileSync(
-          path.join(RELAYER_MANIFESTS_FOLDER, file),
-          "utf8"
-        );
-        const parsedManifest = OnChainRelayerManifestSchema.parse(
+        expect(manifestConfig).to.haveOwnProperty(relayerName);
+        const fileContent = fs.readFileSync(path.join(directory, file), "utf8");
+
+        const parsedManifest = AnyOnChainRelayerManifestSchema.parse(
           JSON.parse(fileContent)
         );
+
         const exportedManifest =
-          manifests[relayerName as keyof typeof manifests];
+          manifestConfig[relayerName as keyof typeof manifestConfig];
+
         expect(exportedManifest).to.deep.equal(parsedManifest);
       });
     }
   }
-});
+}
