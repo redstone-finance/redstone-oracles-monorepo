494a796521027172f8ba5a21420e842b091c2b6b
robal <robert@warp.cc>
2024-07-25 22:25:02 +0200

    update typescript to 5.5.4 and libraries versions (#3198)

--- packages/rpc-providers/src/provider-decorators/provider-decorators.ts
+++ packages/rpc-providers/src/provider-decorators/provider-decorators.ts
@@ -3,6 +3,6 @@ export {
   CallMetricDecorator,
   GetBlockNumberMetricDecorator,
 } from "./MetricDecorator";
+export { MulticallDecorator } from "./multicall/MulticallDecorator";
 export { Treat0xAsErrorDecorator } from "./Treat0xAsErrorDecorator";
 export { TxDumpDecorator } from "./TxsDumpDecorator";
-export { MulticallDecorator } from "./multicall/MulticallDecorator";
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,7 @@
-export * as SafeNumber from "./ISafeNumber";
 export * as RedstoneCommon from "./common";
 export * as RedstoneConstants from "./constants";
 export * as RedstoneCrypto from "./crypto";
+export * as SafeNumber from "./ISafeNumber";
 export * from "./logger";
 export * as MathUtils from "./math"; // to avoid clash with Math global
 export * from "./monitoring";
