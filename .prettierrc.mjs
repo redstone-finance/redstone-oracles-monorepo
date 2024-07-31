0ca3872d083f16defca46ae6b191b252ce95c882
robal <robert@warp.cc>
2024-07-31 11:16:29 +0200

    feat: multi-feed on mainnet (#3228)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,5 +2,5 @@ import companyPrettier from "redstone-prettier-config";
 
 export default {
   ...companyPrettier,
-  plugins: ["prettier-plugin-organize-imports"],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-solidity"],
 };
