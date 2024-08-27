cd1d0030c482723ec27bb178c2b3b83e9fb94fc0
robal <robert@warp.cc>
2024-08-27 08:52:58 +0200

    add solidity-coverage to hardhat.config.ts (#3425)

--- packages/on-chain-relayer/hardhat.config.ts
+++ packages/on-chain-relayer/hardhat.config.ts
@@ -4,6 +4,7 @@ import "@typechain/hardhat";
 import "dotenv/config";
 import "hardhat-gas-reporter";
 import { HardhatUserConfig } from "hardhat/config";
+import "solidity-coverage";
 
 import "@nomiclabs/hardhat-waffle";
 
