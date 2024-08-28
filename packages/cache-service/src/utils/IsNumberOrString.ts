1ebd30741ab818a78c54c63b46a9744a16100614
robal <robert@warp.cc>
2024-08-28 13:36:40 +0200

    chore: linter - loosen this usage rules (#3433)

--- packages/cache-service/src/utils/IsNumberOrString.ts
+++ packages/cache-service/src/utils/IsNumberOrString.ts
@@ -6,12 +6,10 @@ import {
 
 @ValidatorConstraint({ name: "string-or-number", async: false })
 export class IsNumberOrString implements ValidatorConstraintInterface {
-  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
   validate(text: unknown, _args: ValidationArguments) {
     return typeof text === "number" || typeof text === "string";
   }
 
-  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
   defaultMessage(_args: ValidationArguments) {
     return "($value) must be number or string";
   }
