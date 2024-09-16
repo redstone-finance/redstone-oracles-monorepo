88aff3a92d0a9cbc6a37a3cb5e769fab129027c7
robal <robert@warp.cc>
2024-09-16 11:53:36 +0200

    chore: shell scripts linting (#3534)

--- packages/casper-connector/rust/scripts/deploy.sh
+++ packages/casper-connector/rust/scripts/deploy.sh
@@ -2,11 +2,11 @@
 
 JQ=node-jq
 
-result=$(make $1)
+result=$(make "$1")
 execution_results=$(echo "$result" | $JQ 'if has("result") then .result.deploy_hash else "" end')
 if [[ -n $execution_results ]]; then
   echo "${execution_results}" | tee ./scripts/DEPLOY_HASH
 else
   echo "${result}"
-  exit 1;
+  exit 1
 fi
