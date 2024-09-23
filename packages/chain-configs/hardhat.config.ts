171c68063844857ea79d67d65f580da8952acf72
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2024-09-23 14:54:03 +0200

    [RDS-777] Add printing and testing of ssm rpc urls (#3593)

---
+++ packages/chain-configs/hardhat.config.ts
@@ -0,0 +1,12 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+import "solidity-coverage";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  mocha: {
+    timeout: 30_000,
+  },
+};
+
+export default config;
