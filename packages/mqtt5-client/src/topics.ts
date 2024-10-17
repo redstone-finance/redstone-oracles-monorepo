a52b7a9c4d09112d919f6f134740c0f767393bfa
Mike <konopek68@gmail.com>
2024-10-17 16:18:07 +0200

    fix: encoding/decoding in mqtt (#3742)

--- packages/mqtt5-client/src/topics.ts
+++ packages/mqtt5-client/src/topics.ts
@@ -22,3 +22,41 @@ export function decodeDataPackageTopic(encodedTopic: string): DataPackageTopic {
     nodeAddress,
   };
 }
+
+export const encodeTopic = (topic: string) => {
+  const encodedParts = [];
+  const parts = topic.split("/");
+
+  let index = 0;
+  for (const part of parts) {
+    if (part === "+" || part === "#") {
+      encodedParts.push(part);
+    } else if (part.startsWith("$") && index === 0) {
+      encodedParts.push(part);
+    } else {
+      encodedParts.push(encodeURIComponent(part));
+    }
+    index++;
+  }
+
+  return encodedParts.join("/");
+};
+
+export const decodeTopic = (topic: string) => {
+  const parts = [];
+  const encodedParts = topic.split("/");
+
+  let index = 0;
+  for (const encodedPart of encodedParts) {
+    if (encodedPart === "+" || encodedPart === "#") {
+      parts.push(encodedPart);
+    } else if (encodedPart.startsWith("$") && index === 0) {
+      parts.push(encodedPart);
+    } else {
+      parts.push(decodeURIComponent(encodedPart));
+    }
+    index++;
+  }
+
+  return parts.join("/");
+};
