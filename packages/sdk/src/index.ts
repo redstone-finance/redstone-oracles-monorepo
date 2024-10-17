9b8ee44eb91ad1015e4bc25d878af10c8f47667d
Mike <konopek68@gmail.com>
2024-10-17 16:18:07 +0200

    fix: encoding/decoding in mqtt (#3742)

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -73,6 +73,7 @@ export * from "./contracts/prices/IPricesContractAdapter";
 export * from "./contracts/prices/sample-run";
 export * from "./data-feed-values";
 export * from "./data-services-urls";
+export * from "./DataPackageSubscriber";
 export * from "./fetch-data-packages";
 export * from "./oracle-registry";
 export * from "./request-data-packages";
