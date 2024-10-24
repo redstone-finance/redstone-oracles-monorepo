95a1a330094922baed3d49b9ac0d56b7926f0410
robal <robert@warp.cc>
2024-10-24 15:37:40 +0200

    chore: do not run tests in dist directory (#3792)

--- packages/cache-service/jest.config.ts
+++ packages/cache-service/jest.config.ts
@@ -7,6 +7,7 @@ const config: Config.InitialOptions = {
   coverageReporters: ["html"],
   preset: "ts-jest",
   setupFiles: ["<rootDir>/.jest/set-env-vars.ts"],
+  testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
 
 export default config;
