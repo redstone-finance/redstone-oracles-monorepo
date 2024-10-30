dd9c08c24f2270dc32c8630bce933f692f994f79
Wojciech Rybakiewicz <98404816+wrybakiewicz@users.noreply.github.com>
2024-10-30 14:20:49 +0100

    feat: add oev to relayer (#3466)

--- packages/on-chain-relayer/test/relayer/update-prices.test.ts
+++ packages/on-chain-relayer/test/relayer/update-prices.test.ts
@@ -23,12 +23,9 @@ const TEST_PRIVATE_KEY =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 describe("update-prices", () => {
-  before(() => {
-    server.listen();
+  afterEach(() => {
+    server.resetHandlers();
   });
-
-  afterEach(() => server.resetHandlers());
-  after(() => server.close());
 
   it("should update price in price-feeds adapter", async () => {
     mockEnvVariables();
