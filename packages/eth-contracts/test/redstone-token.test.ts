350958a48c2c77154d516575bda655b56ae70137
Alex Suvorov <Suvorovalex1997@gmail.com>
2024-11-02 19:49:12 +0100

    feat: token contract improvements after audit (#3781)
    [Token Generation Event _
    SSC.pdf](https://github.com/user-attachments/files/17479148/Token.Generation.Event._.SSC.pdf)
    
    [CODESPECT_REDSTONE_AUDIT_TOKEN_DRAFT.pdf](https://github.com/user-attachments/files/17479149/CODESPECT_REDSTONE_AUDIT_TOKEN_DRAFT.pdf)

--- packages/eth-contracts/test/redstone-token.test.ts
+++ packages/eth-contracts/test/redstone-token.test.ts
@@ -19,12 +19,21 @@ describe("RedStone token", () => {
     otherAddr = await other.getAddress();
   });
 
+  it("Should not init contract with too high total supply", async () => {
+    const ContractFactory = await ethers.getContractFactory("RedstoneToken");
+    const tooBigSupply = ethers.utils.parseEther("50000000").add(1);
+    await expect(ContractFactory.deploy(tooBigSupply)).to.be.revertedWith(
+      "CanNotMintMoreThanMaxSupply"
+    );
+  });
+
   it("Should properly transfer tokens", async () => {
     expect(await contract.balanceOf(minterAddr)).to.equal(1000);
 
-    const tx = await contract.transfer(otherAddr, 700);
-    await tx.wait();
+    const transferTx = await contract.transfer(otherAddr, 700);
+    await transferTx.wait();
 
+    expect(transferTx).to.emit("Transfer").withArgs(otherAddr, minterAddr, 700);
     expect(await contract.balanceOf(minterAddr)).to.equal(300);
     expect(await contract.balanceOf(otherAddr)).to.equal(700);
   });
@@ -33,7 +42,7 @@ describe("RedStone token", () => {
     contract = contract.connect(other);
 
     await expect(contract.mint(otherAddr, 100)).to.be.revertedWith(
-      "RedstoneToken: minting by an unauthorized address"
+      "OnlyMinterCanMint"
     );
   });
 
@@ -47,7 +56,7 @@ describe("RedStone token", () => {
   });
 
   it("Should mint max supply", async () => {
-    let max = ethers.utils.parseEther("50000000");
+    const max = ethers.utils.parseEther("50000000");
 
     const tx = await contract.mint(minterAddr, max.sub(1000));
     await tx.wait();
@@ -56,28 +65,43 @@ describe("RedStone token", () => {
   });
 
   it("Should not mint more than max supply", async () => {
-    let max = ethers.utils.parseEther("50000000");
+    const max = ethers.utils.parseEther("50000000");
 
     const tx = await contract.mint(minterAddr, max.sub(1000));
     await tx.wait();
 
     await expect(contract.mint(minterAddr, 1)).to.be.revertedWith(
-      "RedstoneToken: cannot mint more than MAX SUPPLY"
+      "CanNotMintMoreThanMaxSupply"
     );
   });
 
-  it("Should not update minter from unauthorized account", async () => {
-    contract = contract.connect(other);
+  it("Should properly update minter ", async () => {
+    const proposalTx = await contract.proposeNewMinter(otherAddr);
+    await proposalTx.wait();
+    expect(proposalTx).to.emit("MinterProposal").withArgs(otherAddr);
+    expect(await contract.minter()).to.equal(minterAddr);
+    expect(await contract.proposedMinter()).to.equal(otherAddr);
 
-    await expect(contract.updateMinter(otherAddr)).to.be.revertedWith(
-      "RedstoneToken: minter update by an unauthorized address"
-    );
-  });
-
-  it("Should update minter ", async () => {
-    const tx = await contract.updateMinter(otherAddr);
-    await tx.wait();
-
+    const acceptRoleTx = await contract.connect(other).acceptMinterRole();
+    await acceptRoleTx.wait();
+    expect(acceptRoleTx).to.emit("MinterUpdate").withArgs(otherAddr);
     expect(await contract.minter()).to.equal(otherAddr);
+    expect(await contract.proposedMinter()).to.equal(
+      ethers.constants.AddressZero
+    );
+  });
+
+  it("Should not propose new minter from an unauthorized account", async () => {
+    await expect(
+      contract.connect(other).proposeNewMinter(otherAddr)
+    ).to.be.revertedWith("OnlyMinterCanProposeNewMinter");
+  });
+
+  it("Should not accept minter role from an unauthorized account", async () => {
+    const proposalTx = await contract.proposeNewMinter(otherAddr);
+    await proposalTx.wait();
+    await expect(contract.acceptMinterRole()).to.be.revertedWith(
+      "OnlyProposedMinterCanAcceptMinterRole"
+    );
   });
 });
