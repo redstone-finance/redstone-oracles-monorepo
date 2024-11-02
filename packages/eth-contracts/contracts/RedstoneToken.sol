8af7c1ecfdb9665ed2d5f2968bab6323ceb813c5
Alex Suvorov <Suvorovalex1997@gmail.com>
2024-11-02 19:49:12 +0100

    feat: token contract improvements after audit (#3781)
    [Token Generation Event _
    SSC.pdf](https://github.com/user-attachments/files/17479148/Token.Generation.Event._.SSC.pdf)
    
    [CODESPECT_REDSTONE_AUDIT_TOKEN_DRAFT.pdf](https://github.com/user-attachments/files/17479149/CODESPECT_REDSTONE_AUDIT_TOKEN_DRAFT.pdf)

--- packages/eth-contracts/contracts/RedstoneToken.sol
+++ packages/eth-contracts/contracts/RedstoneToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -9,26 +9,53 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @dev Standard implementation of ERC20 for Redstone token
  */
 contract RedstoneToken is ERC20 {
-  uint256 constant MAX_SUPPLY = 50_000_000e18;
+  uint256 public constant MAX_SUPPLY = 50_000_000e18;
 
+  error CanNotMintMoreThanMaxSupply();
+  error OnlyMinterCanMint();
+  error OnlyMinterCanProposeNewMinter();
+  error OnlyProposedMinterCanAcceptMinterRole();
+
+  event MinterProposal(address indexed proposedMinter);
   event MinterUpdate(address indexed newMinter);
 
   address public minter;
+  address public proposedMinter;
 
-  constructor(uint256 initialSupply) ERC20("Redstone", "REDSTONE") {
+  constructor(uint256 initialSupply) ERC20("Redstone", "RED") {
+    enforceMaxSupplyLimit(initialSupply);
     _mint(msg.sender, initialSupply);
     minter = msg.sender;
   }
 
   function mint(address account, uint256 amount) external {
-    require(msg.sender == minter, "RedstoneToken: minting by an unauthorized address");
+    if (msg.sender != minter) {
+      revert OnlyMinterCanMint();
+    }
+    enforceMaxSupplyLimit(totalSupply() + amount);
     _mint(account, amount);
-    require(totalSupply() <= MAX_SUPPLY, "RedstoneToken: cannot mint more than MAX SUPPLY");
   }
 
-  function updateMinter(address newMinter) external {
-    require(msg.sender == minter, "RedstoneToken: minter update by an unauthorized address");
-    minter = newMinter;
-    emit MinterUpdate(newMinter);
+  function proposeNewMinter(address newProposedMinter) external {
+    if (msg.sender != minter) {
+      revert OnlyMinterCanProposeNewMinter();
+    }
+    proposedMinter = newProposedMinter;
+    emit MinterProposal(newProposedMinter);
+  }
+
+  function acceptMinterRole() external {
+    if (msg.sender != proposedMinter) {
+      revert OnlyProposedMinterCanAcceptMinterRole();
+    }
+    minter = proposedMinter;
+    proposedMinter = address(0);
+    emit MinterUpdate(minter);
+  }
+
+  function enforceMaxSupplyLimit(uint256 totalSupply) internal pure {
+    if (totalSupply > MAX_SUPPLY) {
+      revert CanNotMintMoreThanMaxSupply();
+    }
   }
 }
