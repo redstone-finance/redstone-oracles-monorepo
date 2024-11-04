6ad793bc561e055ad108e69eb233bd5ac38349c3
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-11-04 08:47:55 +0100

    [RDS-1172] feat: add MultiFeedInfluxContractFacade (#3834)
    Co-authored-by: robal <robert@warp.cc>

---
+++ packages/on-chain-relayer/src/facade/MultiFeedInfluxContractFacade.ts
@@ -0,0 +1,26 @@
+import {
+  MultiFeedAdapterWithoutRounds,
+  MultiFeedEvmContractFacade,
+} from "../index";
+import { RelayerConfig } from "../types";
+import { RelayerDataInfluxService } from "./RelayerDataInfluxService";
+
+const RELAYER_DATA_BUCKET = "dry-run-relayer-data";
+
+export class MultiFeedInfluxContractFacade extends MultiFeedEvmContractFacade {
+  constructor(
+    contract: MultiFeedAdapterWithoutRounds,
+    relayerConfig: RelayerConfig
+  ) {
+    const { influxUrl, influxToken } = relayerConfig;
+
+    const influxService = new RelayerDataInfluxService({
+      url: influxUrl!,
+      token: influxToken!,
+      bucketName: RELAYER_DATA_BUCKET,
+      orgName: "redstone",
+    });
+
+    super(contract, (args) => influxService.updatePriceValues(args));
+  }
+}
