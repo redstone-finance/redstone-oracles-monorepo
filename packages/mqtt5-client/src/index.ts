7769cf877125584b0ffd6ed6a50848c31ea7a1cb
Michal_Konopka <konopek68@gmail.com>
2024-11-07 09:38:00 +0100

    fix: mqtt check monitoring types (#3888)

--- packages/mqtt5-client/src/index.ts
+++ packages/mqtt5-client/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./DataPackageSubscriber";
 export * from "./Mqtt5Client";
+export * from "./MultiPubSubClient";
 export * from "./RateLimitsCircuitBreaker";
 export * from "./SerializerDeserializer";
 export * as MqttTopics from "./topics";
