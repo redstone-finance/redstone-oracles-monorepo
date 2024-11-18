6c6710c86d4ded7f22b919c3d2437fd8019e4b32
robal <robert@warp.cc>
2024-11-18 08:58:53 +0100

    chore: enable mqtt lint in CI (#3962)

--- packages/mqtt5-client/src/index.ts
+++ packages/mqtt5-client/src/index.ts
@@ -3,5 +3,4 @@ export * from "./Mqtt5Client";
 export * from "./MultiPubSubClient";
 export * from "./RateLimitsCircuitBreaker";
 export * from "./SerializerDeserializer";
-export * from "./MultiPubSubClient";
 export * as MqttTopics from "./topics";
