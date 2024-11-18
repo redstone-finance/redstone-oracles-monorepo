a929f596f6158f517722ebe89d1c384a26409bf9
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-11-18 14:44:02 +0100

    [RDS-1268] chore: added skipping update when the updated package is newer than t… (#3958)

--- packages/on-chain-relayer/test/update-conditions/value-deviation-condition-fallback.test.ts
+++ packages/on-chain-relayer/test/update-conditions/value-deviation-condition-fallback.test.ts
@@ -3,6 +3,7 @@ import { mockEnvVariables } from "../helpers";
 import {
   HISTORICAL_DATA_POINTS,
   performFallbackValueDeviationConditionTest,
+  performSkipFrequentUpdatesCheck,
   VERY_SMALL_DATA_POINTS,
 } from "./perform-fallback-value-deviation-condition-test";
 
@@ -65,49 +66,6 @@ describe("value-deviation-condition fallback mode tests", () => {
     );
   });
 
-  it("should return false if not enough time passed since last update", async () => {
-    mockEnvVariables({
-      fallbackOffsetInMilliseconds: 60_000,
-      historicalPackagesGateways: ["X"],
-      fallbackSkipDeviationBasedFrequentUpdates: true,
-    });
-    const { shouldUpdatePrices, warningMessage } =
-      await performFallbackValueDeviationConditionTest(
-        1230.99,
-        13011.68,
-        HISTORICAL_DATA_POINTS
-      );
-
-    expect(shouldUpdatePrices).to.be.false;
-    expect(warningMessage).to.match(
-      /Deviation in fallback mode:.*Update skipped: less than.*Value has deviated enough to be.*Historical Value has deviated enough to be/
-    );
-  });
-
-  it("should return true if skip frequent updates enabled and enough time passed since last update", async () => {
-    mockEnvVariables({
-      fallbackOffsetInMilliseconds: 60_000,
-      historicalPackagesGateways: ["X"],
-      fallbackSkipDeviationBasedFrequentUpdates: true,
-    });
-    const { shouldUpdatePrices, warningMessage } =
-      await performFallbackValueDeviationConditionTest(
-        1230.99,
-        13011.68,
-        HISTORICAL_DATA_POINTS,
-        Date.now() - 2 * 60 * 1000
-      );
-
-    expect(shouldUpdatePrices).to.be.true;
-    expect(warningMessage).to.match(
-      /Deviation in fallback mode: Value has deviated enough to be/
-    );
-    expect(warningMessage).to.match(
-      /Historical Value has deviated enough to be/
-    );
-    expect(warningMessage).not.to.match(/Update skipped: less than/);
-  });
-
   it("should return false if both latest and older values diff lower than expected", async () => {
     const { shouldUpdatePrices, warningMessage } =
       await performFallbackValueDeviationConditionTest(
@@ -123,5 +81,58 @@ describe("value-deviation-condition fallback mode tests", () => {
     expect(warningMessage).not.to.match(
       /Historical Value has not deviated enough to be/
     );
+  });
+
+  it("should return false if not enough time passed since last update but the updated package is not newer than the historical one", async () => {
+    const { shouldUpdatePrices, warningMessage } =
+      await performSkipFrequentUpdatesCheck(true, false);
+
+    expect(shouldUpdatePrices).to.be.false;
+
+    expect(warningMessage).to.match(
+      /Deviation in fallback mode:.*Update skipped: less than.*milliseconds passed since last update.*Value has deviated enough to be.*Historical Value has deviated enough to be/
+    );
+    expect(warningMessage).not.to.match(/last updated package timestamp/);
+  });
+
+  it("should return false if not enough time passed since last update and the updated package is newer than the historical one", async () => {
+    const { shouldUpdatePrices, warningMessage } =
+      await performSkipFrequentUpdatesCheck(true, true);
+
+    expect(shouldUpdatePrices).to.be.false;
+    expect(warningMessage).to.match(
+      /Deviation in fallback mode:.*Update skipped: less than.*milliseconds passed since last update.*; last updated package timestamp.*is newer that the historical one.*Value has deviated enough to be.*Historical Value has deviated enough to be/
+    );
+  });
+
+  it("should return false if enough time passed since last update but the updated package is newer than the historical one", async () => {
+    const { shouldUpdatePrices, warningMessage } =
+      await performSkipFrequentUpdatesCheck(false, true);
+
+    expect(shouldUpdatePrices).to.be.false;
+    expect(warningMessage).to.match(
+      /Deviation in fallback mode:.*Update skipped: last updated package timestamp.*is newer that the historical one.*Value has deviated enough to be.*Historical Value has deviated enough to be/
+    );
+    expect(warningMessage).not.to.match(
+      /milliseconds passed since last update/
+    );
+  });
+
+  it("should return true if skip frequent updates enabled and enough time passed since last update and the updated package is not newer than the historical one", async () => {
+    const { shouldUpdatePrices, warningMessage } =
+      await performSkipFrequentUpdatesCheck(false, false);
+
+    expect(shouldUpdatePrices).to.be.true;
+    expect(warningMessage).to.match(
+      /Deviation in fallback mode: Value has deviated enough to be/
+    );
+    expect(warningMessage).to.match(
+      /Historical Value has deviated enough to be/
+    );
+    expect(warningMessage).not.to.match(/Update skipped:/);
+    expect(warningMessage).not.to.match(
+      /milliseconds passed since last update/
+    );
+    expect(warningMessage).not.to.match(/last updated package timestamp/);
   });
 });
