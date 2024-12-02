41c256f285676b596c2d343ae5f456ca980a5c6d
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-12-02 11:50:45 +0100

    [RDS-477] update radix connector for clutterfish (#4070)

--- packages/radix-connector/src/index.ts
+++ packages/radix-connector/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./contracts/price_adapter/PriceAdapterRadixContractAdapter";
 export * from "./contracts/price_adapter/PriceAdapterRadixContractConnector";
+export * from "./contracts/price_adapter/PriceAdapterRadixContractDeployer";
 export * from "./radix/RadixClient";
