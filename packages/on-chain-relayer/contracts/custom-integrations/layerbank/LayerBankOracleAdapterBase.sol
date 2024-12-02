9a5ed493195402c71e5d148500fa4c2a470fc318
Alex Suvorov <Suvorovalex1997@gmail.com>
2024-12-02 15:53:51 +0100

    chore: increased min required version of solidity to 0.8.17 (#4022)
    Fix for issue 7.2 after the Talfao audit
    
    [CODESPECT_REDSTONE_AUDIT_ORACLES_DRAFT.pdf](https://github.com/user-attachments/files/17893684/CODESPECT_REDSTONE_AUDIT_ORACLES_DRAFT.pdf)

--- packages/on-chain-relayer/contracts/custom-integrations/layerbank/LayerBankOracleAdapterBase.sol
+++ packages/on-chain-relayer/contracts/custom-integrations/layerbank/LayerBankOracleAdapterBase.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.17;
 
 import {ILToken} from "./ILToken.sol";
 import {IPriceCalculator} from "./IPriceCalculator.sol";
