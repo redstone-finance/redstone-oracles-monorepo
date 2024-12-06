d7b7b7ec492f98d85936b091e98165b9092712c6
jakkorzeniewski <jakub.korzeniewski@redstone.finance>
2024-12-06 09:58:50 +0100

    [RDS-1263] Allow sending error logs with specified severity (#4061)

--- packages/internal-utils/src/aws/error-logger.ts
+++ packages/internal-utils/src/aws/error-logger.ts
@@ -2,6 +2,11 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 import { RedstoneCommon } from "@redstone-finance/utils";
 import z from "zod";
 
+const errorSeverities = ["critical", "error", "warning", "info"] as const;
+
+export const ErrorSeveritiesEnum = z.enum(errorSeverities);
+export type ErrorSeverity = z.infer<typeof ErrorSeveritiesEnum>;
+
 const AWS_REGION = RedstoneCommon.getFromEnv(
   "AWS_REGION",
   z.string().default("eu-west-1")
@@ -19,9 +24,13 @@ function invokeLambda(functionName: string, payload: unknown) {
   return lambdaClient.send(command);
 }
 
-export async function sendErrorLog(functionName: string, errorLog: string) {
+export async function sendErrorLog(
+  functionName: string,
+  errorLog: string,
+  severity?: ErrorSeverity
+) {
   try {
-    await invokeLambda(functionName, { message: errorLog });
+    await invokeLambda(functionName, { message: errorLog, severity });
   } catch (e) {
     throw new Error(
       `Error sending error log: ${RedstoneCommon.stringifyError(e)}`
