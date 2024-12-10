6f1d89b534c5394e16ac71a48f3f9e8e759b1c38
Jan Olszak <janekolszak@users.noreply.github.com>
2024-12-10 11:50:04 +0100

    [RDS-1289] feat: add external provider teb-demo-1 to monitoring and sdk (#4172)

--- packages/sdk/src/data-services-urls.ts
+++ packages/sdk/src/data-services-urls.ts
@@ -26,6 +26,7 @@ export const REDSTONE_DATA_SERVICES_URLS: Partial<Record<string, string[]>> = {
   "mock-data-service-tests": ["http://valid-cache.com"],
   "redstone-external-demo-1": STAGING_GWS,
   "kudasaijp-demo-1": STAGING_GWS,
+  "teb-demo-1": STAGING_GWS,
 };
 
 export const resolveDataServiceUrls = (dataServiceId: string): string[] => {
