5e12afed04c02b0f5a95c7e1d30298d054622776
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-12-13 16:15:59 +0100

    [RDS-1520] radix stage2 structure changes (#4216)
    Structural changes only

--- packages/radix-connector/scripts/sample_deploy.ts
+++ packages/radix-connector/scripts/sample_deploy.ts
@@ -1,25 +1,27 @@
 import { RedstoneCommon } from "@redstone-finance/utils";
-import fs from "fs";
 import { readFileSync } from "node:fs";
 import { RadixPackageDeployer } from "./RadixPackageDeployer";
-import { getFilename, NETWORK, PRIVATE_KEY } from "./constants";
+import {
+  getContractFilename,
+  NETWORK,
+  PRIVATE_KEY,
+  saveAddress,
+} from "./constants";
 
 async function deploy(contractName: string) {
   const client = new RadixPackageDeployer(PRIVATE_KEY, NETWORK.id);
 
   const wasm = readFileSync(
-    getFilename(`${contractName}.wasm`, `${contractName}/artifacts`)
+    getContractFilename(`${contractName}.wasm`, `${contractName}/artifacts`)
   );
   const rpd = readFileSync(
-    getFilename(`${contractName}.rpd`, `${contractName}/artifacts`)
+    getContractFilename(`${contractName}.rpd`, `${contractName}/artifacts`)
   );
   const feeLock = 120;
 
   const packageId = await client.deployPackage(wasm, rpd, feeLock);
-  await fs.promises.writeFile(
-    getFilename(`package.${NETWORK.name}.addr`, contractName),
-    packageId
-  );
+
+  await saveAddress("component", contractName, packageId);
 }
 
 void deploy(RedstoneCommon.getFromEnv("CONTRACT_NAME"));
