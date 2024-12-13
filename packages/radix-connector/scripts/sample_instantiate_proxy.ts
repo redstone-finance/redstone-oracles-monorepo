0a1030fcffe94203bfa2567e87f9f208b6be8b8a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-12-13 16:15:59 +0100

    [RDS-1520] radix stage2 structure changes (#4216)
    Structural changes only

--- packages/radix-connector/scripts/sample_instantiate_proxy.ts
+++ packages/radix-connector/scripts/sample_instantiate_proxy.ts
@@ -1,15 +1,14 @@
 import { RedstoneCommon } from "@redstone-finance/utils";
-import fs from "fs";
-import { RadixClient } from "../../src";
-import { ProxyRadixContractDeployer } from "../../src/contracts/proxy/ProxyRadixContractDeployer";
-import { NonFungibleGlobalIdInput } from "../../src/radix/utils";
+import { RadixClient } from "../src";
+import { ProxyRadixContractDeployer } from "../src/contracts/proxy/ProxyRadixContractDeployer";
+import { NonFungibleGlobalIdInput } from "../src/radix/utils";
 import {
-  getFilename,
   loadAddress,
   NETWORK,
   PRICE_FEED_NAME,
   PRIVATE_KEY,
   PROXY_NAME,
+  saveAddress,
 } from "./constants";
 
 const OWNER_BADGE: () => NonFungibleGlobalIdInput = () => ({
@@ -27,23 +26,20 @@ async function instantiate() {
 
   const connector = new ProxyRadixContractDeployer(
     client,
-    await loadAddress(`package.${NETWORK.name}.addr`, PROXY_NAME),
+    await loadAddress(`package`, PROXY_NAME),
     OWNER_BADGE(),
     MAN_BADGE(),
-    await loadAddress(`component.${NETWORK.name}.addr`, PRICE_FEED_NAME)
+    await loadAddress(`component`, PRICE_FEED_NAME)
   );
 
   const componentId = await connector.getComponentId();
   console.log(componentId);
 
-  await fs.promises.writeFile(
-    getFilename(`component.${NETWORK.name}.addr`, PROXY_NAME),
-    componentId
-  );
+  await saveAddress(`component`, PROXY_NAME, componentId);
 
   const adapter = await connector.getAdapter();
   await adapter.setContractGlobalAddress(
-    await loadAddress(`component.${NETWORK.name}.addr`, PRICE_FEED_NAME),
+    await loadAddress(`component`, PRICE_FEED_NAME),
     MAN_BADGE()
   );
 }
