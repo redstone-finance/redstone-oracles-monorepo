373e0b46ae0705d3de3991ff17b6ec87589b82a2
piotrostr <piotr.ostrowski@aol.com>
2024-12-18 10:04:18 +0100

    sui connector (#4062)

---
+++ packages/sui-connector/src/SuiContractConnector.ts
@@ -0,0 +1,33 @@
+import { SuiClient } from "@mysten/sui/client";
+import { Keypair } from "@mysten/sui/cryptography";
+import type {
+  IContractConnector,
+  IPricesContractAdapter,
+} from "@redstone-finance/sdk";
+import { SuiPricesContractAdapter } from "./SuiPricesContractAdapter";
+import { SuiConfig } from "./config";
+
+export class SuiContractConnector
+  implements IContractConnector<IPricesContractAdapter>
+{
+  constructor(
+    private readonly client: SuiClient,
+    private readonly config: SuiConfig,
+    private readonly keypair: Keypair
+  ) {}
+
+  getAdapter(): Promise<SuiPricesContractAdapter> {
+    return Promise.resolve(
+      new SuiPricesContractAdapter(this.client, this.config, this.keypair)
+    );
+  }
+
+  async getBlockNumber(): Promise<number> {
+    return Number(await this.client.getLatestCheckpointSequenceNumber());
+  }
+
+  async waitForTransaction(txId: string): Promise<boolean> {
+    const tx = await this.client.waitForTransaction({ digest: txId });
+    return tx.digest === txId;
+  }
+}
