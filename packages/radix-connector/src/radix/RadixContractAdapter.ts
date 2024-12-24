e3904150f85610a1fee2646d54aee7f774ae137a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-12-24 14:01:23 +0100

    Changes from PR #4306

--- packages/radix-connector/src/radix/RadixContractAdapter.ts
+++ packages/radix-connector/src/radix/RadixContractAdapter.ts
@@ -1,8 +1,11 @@
 import { RadixClient } from "./RadixClient";
 
+export type ReadMode = "ReadFromStorage" | "CallReadMethod";
+
 export abstract class RadixContractAdapter {
   constructor(
     protected client: RadixClient,
-    protected componentId: string
+    protected componentId: string,
+    public readMode: ReadMode = "ReadFromStorage"
   ) {}
 }
