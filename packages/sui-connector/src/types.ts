6e660b136d7fa0a327b0c3caedf9acc67df7b29a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-01-14 12:32:32 +0100

    [RDS-1752] sui on-chain relayer (#4389)
    Co-authored-by: Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>

--- packages/sui-connector/src/types.ts
+++ packages/sui-connector/src/types.ts
@@ -1,37 +1,53 @@
 import { z } from "zod";
 
-export const PriceAdapterDataContent = z.object({
-  fields: z.object({
-    prices: z.object({
-      fields: z.object({
-        id: z.object({
-          id: z.string(),
-        }),
+function flattenFields<T>(data: { fields: T }): T {
+  return data.fields;
+}
+
+const ConfigContent = z
+  .object({
+    fields: z.object({
+      signer_count_threshold: z.number(),
+    }),
+  })
+  .transform(flattenFields);
+
+const PricesContent = z
+  .object({
+    fields: z.object({
+      id: z.object({
+        id: z.string(),
       }),
     }),
-    config: z.object({
-      fields: z.object({
-        signer_count_threshold: z.number(),
-      }),
+  })
+  .transform(flattenFields);
+
+export const PriceAdapterDataContent = z
+  .object({
+    fields: z.object({
+      prices: PricesContent,
+      config: ConfigContent,
     }),
-  }),
+  })
+  .transform(flattenFields);
+
+const PriceData = z.object({
+  feed_id: z.array(z.number()),
+  timestamp: z.string(),
+  value: z.string(),
+  write_timestamp: z.string(),
 });
 
-export const PriceDataContent = z.object({
-  fields: z.object({
-    value: z.object({
-      fields: z.object({
-        feed_id: z.array(z.number()),
-        timestamp: z.string(),
-        value: z.string(),
-        write_timestamp: z.string(),
-      }),
-    }),
-  }),
-});
+const ValueContent = z
+  .object({
+    fields: PriceData,
+  })
+  .transform(flattenFields);
 
-export const ClockDataContent = z.object({
-  fields: z.object({
-    timestamp_ms: z.string(),
-  }),
-});
+export const PriceDataContent = z
+  .object({
+    fields: z.object({
+      value: ValueContent,
+    }),
+  })
+  .transform(flattenFields);
---
+++ packages/sui-connector/test/SuiPricesContractAdapter.spec.ts
@@ -0,0 +1,167 @@
+import { SuiClient } from "@mysten/sui/client";
+import { ContractParamsProvider } from "@redstone-finance/sdk";
+import { RedstoneCommon } from "@redstone-finance/utils";
+import dotenv from "dotenv";
+import {
+  makeSuiClient,
+  makeSuiKeypair,
+  readSuiConfig,
+  SuiNetworkSchema,
+  SuiPricesContractAdapter,
+} from "../src";
+
+const DATA_SERVICE_ID = "redstone-primary-prod";
+const WRITE_TEST_TIMEOUT = 20 * 1_000; // 20 secs
+
+describe("SuiPricesContractAdapter", () => {
+  let adapter: SuiPricesContractAdapter;
+  let contractParamsProvider: ContractParamsProvider;
+  let suiClient: SuiClient;
+
+  beforeAll(() => {
+    dotenv.config();
+    const network = RedstoneCommon.getFromEnv("NETWORK", SuiNetworkSchema);
+    const config = readSuiConfig(network);
+    const client = makeSuiClient(network);
+    const keypair = makeSuiKeypair();
+    adapter = new SuiPricesContractAdapter(client, config, keypair);
+    suiClient = makeSuiClient(network);
+  });
+
+  beforeEach(() => {
+    contractParamsProvider = new ContractParamsProvider({
+      dataServiceId: DATA_SERVICE_ID,
+      dataPackagesIds: ["LBTC"],
+      uniqueSignersCount: 3,
+    });
+  });
+
+  describe("getUniqueSignerThreshold", () => {
+    it("should return the unique signer threshold", async () => {
+      const result = await adapter.getUniqueSignerThreshold();
+      expect(result).toBeGreaterThan(0);
+      expect(result).toBeLessThan(8);
+    });
+  });
+
+  describe.skip("getPricesFromPayload", () => {});
+
+  describe.skip("readTimestampFromContract", () => {});
+
+  describe("writePricesFromPayloadToContract", () => {
+    it(
+      "should write prices from payload to contract",
+      async () => {
+        const result = await adapter.writePricesFromPayloadToContract(
+          contractParamsProvider
+        );
+        expect(typeof result).toBe("string");
+        expect(result.length).toBeGreaterThanOrEqual(0);
+        const receipt = await suiClient.waitForTransaction({ digest: result });
+        expect(receipt.digest).toBe(result);
+      },
+      WRITE_TEST_TIMEOUT
+    );
+
+    it(
+      "should write prices with multiple feed IDs",
+      async () => {
+        contractParamsProvider = new ContractParamsProvider({
+          dataServiceId: DATA_SERVICE_ID,
+          dataPackagesIds: ["ETH", "BTC"],
+          uniqueSignersCount: 3,
+        });
+
+        const result = await adapter.writePricesFromPayloadToContract(
+          contractParamsProvider
+        );
+        expect(typeof result).toBe("string");
+        expect(result.length).toBeGreaterThanOrEqual(0);
+        const receipt = await suiClient.waitForTransaction({ digest: result });
+        expect(receipt.digest).toBe(result);
+      },
+      WRITE_TEST_TIMEOUT
+    );
+
+    it(
+      "should write prices for a lot of feeds",
+      async () => {
+        contractParamsProvider = new ContractParamsProvider({
+          dataServiceId: DATA_SERVICE_ID,
+          dataPackagesIds: [
+            "USDT",
+            "LINK",
+            "AVAX",
+            "USDC",
+            "EUROC",
+            "DAI",
+            "EUR",
+            "CRV",
+            "BNB",
+            "wstETH",
+            "ezETH",
+            "pzETH",
+            "ETH+",
+            "XVS",
+            "SolvBTC",
+            "STONE",
+            "WBTC",
+            "bsdETH",
+            "AERO",
+            "eUSD",
+          ],
+          uniqueSignersCount: 3,
+        });
+
+        const result = await adapter.writePricesFromPayloadToContract(
+          contractParamsProvider
+        );
+
+        expect(typeof result).toBe("string");
+        expect(result.length).toBeGreaterThanOrEqual(0);
+        const receipt = await suiClient.waitForTransaction({ digest: result });
+        expect(receipt.digest).toBe(result);
+      },
+      WRITE_TEST_TIMEOUT
+    );
+  });
+
+  describe("readLatestUpdateBlockTimestamp", () => {
+    it("should read the latest update block timestamp", async () => {
+      const result = await adapter.readLatestUpdateBlockTimestamp("ETH");
+      expect(result).toBeGreaterThanOrEqual(0);
+      const fullYear = new Date(result).getFullYear();
+      expect(fullYear).toBeGreaterThan(2024);
+    });
+  });
+
+  describe("readPricesFromContract", () => {
+    it("should read prices from contract", async () => {
+      const result = await adapter.readPricesFromContract(
+        contractParamsProvider
+      );
+      expect(result.length).toBeGreaterThan(0);
+      expect(typeof result[0] === "bigint");
+    });
+
+    it("should read prices with multiple feed IDs", async () => {
+      contractParamsProvider = new ContractParamsProvider({
+        dataServiceId: DATA_SERVICE_ID,
+        dataPackagesIds: ["LBTC", "ETH", "BTC"],
+        uniqueSignersCount: 3,
+      });
+
+      const result = await adapter.readPricesFromContract(
+        contractParamsProvider
+      );
+
+      expect(result.length).toBe(3);
+      result.forEach((price) => {
+        expect(typeof price === "bigint");
+      });
+
+      // this would fail if ETH flipped BTC
+      expect(result[1] < result[2]).toBeTruthy();
+    });
+  });
+});
