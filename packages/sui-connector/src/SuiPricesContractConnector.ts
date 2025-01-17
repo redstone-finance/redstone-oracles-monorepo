36ff7db5212e0ffccb288e091766342c95c296c9
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-01-17 10:24:06 +0100

    feat: create sui monitoring (#4449)

--- packages/sui-connector/src/SuiPricesContractConnector.ts
+++ packages/sui-connector/src/SuiPricesContractConnector.ts
@@ -1,19 +1,23 @@
-import { SuiClient } from "@mysten/sui/client";
+import { SuiClient, SuiTransactionBlockResponse } from "@mysten/sui/client";
 import { Keypair } from "@mysten/sui/cryptography";
+import { MIST_PER_SUI } from "@mysten/sui/utils";
 import type {
   IContractConnector,
   IPricesContractAdapter,
 } from "@redstone-finance/sdk";
+import { loggerFactory } from "@redstone-finance/utils";
 import { SuiPricesContractAdapter } from "./SuiPricesContractAdapter";
 import { SuiConfig } from "./config";
 
 export class SuiPricesContractConnector
   implements IContractConnector<IPricesContractAdapter>
 {
+  private readonly logger = loggerFactory("sui-prices-contract-connector");
+
   constructor(
     private readonly client: SuiClient,
     private readonly config: SuiConfig,
-    private readonly keypair: Keypair
+    private readonly keypair?: Keypair
   ) {}
 
   getAdapter(): Promise<SuiPricesContractAdapter> {
@@ -27,7 +31,30 @@ export class SuiPricesContractConnector
   }
 
   async waitForTransaction(txId: string): Promise<boolean> {
-    const tx = await this.client.waitForTransaction({ digest: txId });
-    return tx.digest === txId;
+    await this.client.waitForTransaction({ digest: txId });
+    const response = await this.client.getTransactionBlock({
+      digest: txId,
+      options: {
+        showEffects: true,
+        showBalanceChanges: true,
+      },
+    });
+    const status = response.effects!.status.status;
+    const success = status == "success";
+    const cost = SuiPricesContractConnector.getCost(response);
+
+    this.logger.log(
+      `Transaction ${txId} finished, status: ${status.toUpperCase()}${success ? "" : ` (${response.effects!.status.error})`}, cost: ${cost} SUI, timestamp: ${response.timestampMs}`,
+      response.errors
+    );
+
+    return success;
+  }
+
+  private static getCost(response: SuiTransactionBlockResponse) {
+    return (
+      Number(BigInt(response.balanceChanges![0].amount.replace("-", ""))) /
+      Number(MIST_PER_SUI)
+    );
   }
 }
