a4effa13c19bf8ab0e6f3dc64b9aee27299097b2
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-01-27 15:01:09 +0100

    [RDS-1870]: Solana make config const in the contract (#4514)

--- packages/solana-connector/solana/tests/redstone-sol.ts
+++ packages/solana-connector/solana/tests/redstone-sol.ts
@@ -8,7 +8,6 @@ import {
   makePriceSeed,
   makeFeedIdBytes,
 } from "./util";
-import { PRIMARY_SIGNERS } from "./signers";
 
 describe("redstone-sol", () => {
   const provider = anchor.AnchorProvider.env();
@@ -31,9 +30,6 @@ describe("redstone-sol", () => {
   ];
 
   let pdas: { [id: string] : anchor.web3.PublicKey } = {};
-
-  let configAccount: anchor.web3.PublicKey;
-
   const systemProgram = anchor.web3.SystemProgram.programId;
 
   before(async () => {
@@ -43,31 +39,6 @@ describe("redstone-sol", () => {
         program.programId
       )[0];
     }
-
-    configAccount = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("config")],
-      program.programId
-    )[0];
-  });
-
-  it("initializes correctly", async () => {
-    await program.methods
-      .initialize(
-        PRIMARY_SIGNERS,
-        [],
-        3, // signer_count_threshold
-        new anchor.BN(15 * 60 * 1000), // max_timestamp_delay_ms (15 minutes)
-        new anchor.BN(3 * 60 * 1000), // max_timestamp_ahead_ms (3 minutes)
-        new anchor.BN(1_000), // 1 second
-      )
-      .accountsStrict({
-        owner: anchor.getProvider().publicKey,
-        systemProgram,
-        configAccount,
-      })
-      .rpc();
-
-    console.log("Config initialized at:", configAccount.toString());
   });
 
   async function testFeedIdPush(feedId: string) {
@@ -80,7 +51,6 @@ describe("redstone-sol", () => {
         .accountsStrict({
           user: provider.wallet.publicKey,
           priceAccount,
-          configAccount,
           systemProgram,
         })
         .rpc({ skipPreflight: false });
@@ -96,90 +66,5 @@ describe("redstone-sol", () => {
     });
   }
 
-  // feedIds.forEach(testFeedIdPush);
   feedIds.forEach(testFeedIdPush);
-
-  describe("Config updates", () => {
-    it("Owner can update the config", async () => {
-      const newSigners = PRIMARY_SIGNERS.slice(0, 3);
-      const newThreshold = 2;
-      const newMaxDelay = new anchor.BN(20 * 60 * 1000); // 20 minutes
-      const newMaxAhead = new anchor.BN(5 * 60 * 1000); // 5 minutes
-
-      await program.methods
-        .updateConfig(newSigners, null, newThreshold, newMaxDelay, newMaxAhead, null)
-        .accountsStrict({
-          owner: provider.wallet.publicKey,
-          configAccount,
-        })
-        .rpc();
-
-      const updatedConfig = await program.account.configAccount.fetch(
-        configAccount
-      );
-
-      expect(updatedConfig.signers).to.deep.equal(newSigners);
-      expect(updatedConfig.signerCountThreshold).to.equal(newThreshold);
-      expect(updatedConfig.maxTimestampDelayMs.toString()).to.equal(
-        newMaxDelay.toString()
-      );
-      expect(updatedConfig.maxTimestampAheadMs.toString()).to.equal(
-        newMaxAhead.toString()
-      );
-    });
-
-    it("Non-owner cannot update the config", async () => {
-      const nonOwnerWallet = anchor.web3.Keypair.generate();
-      await provider.connection.requestAirdrop(
-        nonOwnerWallet.publicKey,
-        1000000000
-      );
-
-      try {
-        await program.methods
-          .updateConfig(null, null, 4, null, null, null)
-          .accountsStrict({
-            owner: nonOwnerWallet.publicKey,
-            configAccount,
-          })
-          .signers([nonOwnerWallet])
-          .rpc();
-        expect.fail("Expected error but transaction succeeded");
-      } catch (error) {
-        expect((error as ProgramError).toString()).to.include(
-          "A has one constraint was violated"
-        );
-      }
-    });
-
-    it("Partial update of config is possible", async () => {
-      const originalConfig = await program.account.configAccount.fetch(
-        configAccount
-      );
-      const newMaxDelay = new anchor.BN(25 * 60 * 1000); // 25 minutes
-
-      await program.methods
-        .updateConfig(null, null, null, newMaxDelay, null, null)
-        .accountsStrict({
-          owner: provider.wallet.publicKey,
-          configAccount,
-        })
-        .rpc();
-
-      const updatedConfig = await program.account.configAccount.fetch(
-        configAccount
-      );
-
-      expect(updatedConfig.signers).to.deep.equal(originalConfig.signers);
-      expect(updatedConfig.signerCountThreshold).to.equal(
-        originalConfig.signerCountThreshold
-      );
-      expect(updatedConfig.maxTimestampDelayMs.toNumber()).to.equal(
-        newMaxDelay.toNumber()
-      );
-      expect(updatedConfig.maxTimestampAheadMs.toNumber()).to.equal(
-        originalConfig.maxTimestampAheadMs.toNumber()
-      );
-    });
-  });
 });
