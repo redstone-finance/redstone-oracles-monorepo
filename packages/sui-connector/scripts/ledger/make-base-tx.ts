4875bfe8801f23020dd2c9a85806e072b2034e20
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-01-29 15:18:51 +0100

    Changes from PR #4541

---
+++ packages/sui-connector/scripts/ledger/make-base-tx.ts
@@ -0,0 +1,42 @@
+import { Transaction } from "@mysten/sui/transactions";
+import { SUI_FRAMEWORK_ADDRESS } from "@mysten/sui/utils";
+import { RedstoneCommon } from "@redstone-finance/utils";
+import assert from "node:assert";
+import {
+  DEFAULT_GAS_BUDGET,
+  makeSuiClient,
+  SuiNetworkName,
+  SuiNetworkSchema,
+} from "../../src";
+
+async function fetchCoinObjects(
+  network: SuiNetworkName,
+  senderAddress: string
+) {
+  const coins = await makeSuiClient(network).getCoins({ owner: senderAddress });
+  assert(!coins.hasNextPage, "Too many coins");
+
+  return coins.data
+    .filter((data) => data.coinType === `${SUI_FRAMEWORK_ADDRESS}::sui::SUI`)
+    .map((data) => ({
+      objectId: data.coinObjectId,
+      version: data.version,
+      digest: data.digest,
+    }));
+}
+
+export async function makeBaseTx(
+  creator: (tx: Transaction, network: SuiNetworkName) => void,
+  senderAddress: string
+) {
+  const network = RedstoneCommon.getFromEnv("NETWORK", SuiNetworkSchema);
+
+  const tx = new Transaction();
+  tx.setSender(senderAddress);
+  tx.setGasBudget(DEFAULT_GAS_BUDGET);
+  tx.setGasPayment(await fetchCoinObjects(network, senderAddress));
+
+  creator(tx, network);
+
+  return { network, tx };
+}
