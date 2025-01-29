15015972b37baa0f3a7287f0821a9e78b6ecef1a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-01-29 15:18:51 +0100

    Lukasz2891/sui multi sig upgrade (#4541)

--- packages/sui-connector/scripts/ledger/generate-transaction-data.ts
+++ packages/sui-connector/scripts/ledger/generate-transaction-data.ts
@@ -1,22 +1,22 @@
 import { toBase64 } from "@mysten/bcs";
 import { Transaction } from "@mysten/sui/transactions";
-import { RedstoneCommon } from "@redstone-finance/utils";
 import "dotenv/config";
-import { makeSuiClient, SuiNetworkName, SuiNetworkSchema } from "../../src";
+import { makeSuiClient, SuiNetworkName } from "../../src";
+import { makeBaseTx } from "./make-base-tx";
 
-const MULTI_SIG_ADDRESS =
+export const MULTI_SIG_ADDRESS =
   "0x2fb6aa8a4bdedb65c0979747e954cd92ede02ecd8d401b1df4be9f9d81c4b8b1";
 
 export async function generateTransactionData(
   creator: (tx: Transaction, network: SuiNetworkName) => void,
   multiSigAddress = MULTI_SIG_ADDRESS
 ) {
-  const network = RedstoneCommon.getFromEnv("NETWORK", SuiNetworkSchema);
+  const { network, tx } = await makeBaseTx(creator, multiSigAddress);
 
-  const tx = new Transaction();
-  tx.setSender(multiSigAddress);
+  const encodedTransaction = toBase64(
+    await tx.build({ client: makeSuiClient(network) })
+  );
+  console.log(encodedTransaction);
 
-  creator(tx, network);
-
-  console.log(toBase64(await tx.build({ client: makeSuiClient(network) })));
+  return encodedTransaction;
 }
