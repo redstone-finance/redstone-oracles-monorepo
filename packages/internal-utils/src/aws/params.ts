89f054a6d6b3658f879c440de464e4b42e47b230
Jan Olszak <janekolszak@users.noreply.github.com>
2025-01-29 12:58:25 +0100

    feat: fix and clean DEX price shift estimation lambda (#4568)

--- packages/internal-utils/src/aws/params.ts
+++ packages/internal-utils/src/aws/params.ts
@@ -42,3 +42,31 @@ export async function getS3ConfigurationValue() {
 
   return await getS3ParameterValue(bucketName, objectKey);
 }
+
+// Get ENV variables that end with "_ARN"
+// Try to get the value from SSM and set it in the ENV, but rename the key to remove "_ARN"
+export const secretsToEnv = async (): Promise<void> => {
+  const promises = Object.entries(process.env)
+    .filter(([key]) => key.endsWith("_ARN"))
+    .map(async ([key, value]) => {
+      const envVariable = key.replace("_ARN", "");
+      if (envVariable === "" || !value || value === "") {
+        return await Promise.resolve();
+      }
+
+      const secret = await getSSMParameterValue(value);
+      if (secret) {
+        process.env[envVariable] = secret;
+      }
+    });
+
+  if (promises.length === 0) {
+    return await Promise.resolve();
+  }
+
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    console.error("Error while fetching secrets", error);
+  }
+};
