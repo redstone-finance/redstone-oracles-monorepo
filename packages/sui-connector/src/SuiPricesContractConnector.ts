9079bdf8815fbd9bf4ba86b2e56a8030cbb38a73
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-02-06 16:42:21 +0100

    [RDS-1865] sui gas model and balance equivocation (#4640)

--- packages/sui-connector/src/SuiPricesContractConnector.ts
+++ packages/sui-connector/src/SuiPricesContractConnector.ts
@@ -4,6 +4,7 @@ import type { IContractConnector } from "@redstone-finance/sdk";
 import { SuiConfig } from "./config";
 import { SuiContractConnector } from "./SuiContractConnector";
 import { SuiPricesContractAdapter } from "./SuiPricesContractAdapter";
+import { SuiTxDeliveryMan } from "./SuiTxDeliveryMan";
 
 export class SuiPricesContractConnector
   extends SuiContractConnector<SuiPricesContractAdapter>
@@ -12,14 +13,27 @@ export class SuiPricesContractConnector
   constructor(
     client: SuiClient,
     private readonly config: SuiConfig,
-    private readonly keypair?: Keypair
+    private readonly keypair?: Keypair,
+    private readonly txDeliveryMan?: SuiTxDeliveryMan
   ) {
     super(client);
   }
 
   override getAdapter(): Promise<SuiPricesContractAdapter> {
     return Promise.resolve(
-      new SuiPricesContractAdapter(this.client, this.config, this.keypair)
+      new SuiPricesContractAdapter(
+        this.client,
+        this.config,
+        this.keypair,
+        this.getTxDeliveryMan()
+      )
+    );
+  }
+
+  protected getTxDeliveryMan() {
+    return (
+      this.txDeliveryMan ??
+      SuiContractConnector.getCachedDeliveryMan(this.client, this.keypair!)
     );
   }
 }
