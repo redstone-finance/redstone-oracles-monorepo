9b1a8e0eb64be0426686e0158532a9219264edd1
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-02-10 10:44:13 +0100

    chore: add redstone mulitcall and rpc to berachain (#4680)

--- packages/chain-configs/test/rpc-urls/public-rpc-urls.test.ts
+++ packages/chain-configs/test/rpc-urls/public-rpc-urls.test.ts
@@ -2,12 +2,7 @@ import { RpcUrlsPerChain } from "../../scripts/read-ssm-rpc-urls";
 import { getLocalChainConfigs } from "../../src";
 import { validateNetworkRpcUrls } from "./common";
 
-const CHAINS_TO_SKIP_RPC_CHECK = [
-  "Monad Devnet",
-  "Hemi Network",
-  "Ink",
-  "Berachain",
-];
+const CHAINS_TO_SKIP_RPC_CHECK = ["Monad Devnet", "Hemi Network", "Ink"];
 
 const validatePublicRpcUrls = () => {
   const chainConfigs = getLocalChainConfigs();
