8653485d38a4c5fb5d6f750427b5b4f45ce410a0
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-02-13 13:12:41 +0100

    [RDS-2144]: Monitoring for movement (#4755)

--- packages/movement-connector/src/MovementPricesContractConnector.ts
+++ packages/movement-connector/src/MovementPricesContractConnector.ts
@@ -1,34 +1,20 @@
-import {
-  Account,
-  AccountAddress,
-  Aptos,
-  TransactionResponse,
-} from "@aptos-labs/ts-sdk";
-import type {
-  IContractConnector,
-  IPricesContractAdapter,
-} from "@redstone-finance/sdk";
-import { loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
+import { Account, AccountAddress, Aptos } from "@aptos-labs/ts-sdk";
+import { MovementContractConnector } from "./MovementContractConnector";
 import { MovementPricesContractAdapter } from "./MovementPricesContractAdapter";
 import { MovementViewContractAdapter } from "./MovementViewContractAdapter";
 import { MovementWriteContractAdapter } from "./MovementWriteContractAdapter";
 import { IMovementContractAdapter } from "./types";
-import { txCost } from "./utils";
 
-export const TIMEOUT_IN_SEC = 20;
-
-export class MovementPricesContractConnector
-  implements IContractConnector<IPricesContractAdapter>
-{
-  private readonly logger = loggerFactory("movement-prices-contract-connector");
+export class MovementPricesContractConnector extends MovementContractConnector<MovementPricesContractAdapter> {
   private readonly packageObjectAddress: AccountAddress;
   private readonly priceAdapterObjectAddress: AccountAddress;
 
   constructor(
-    private readonly client: Aptos,
+    client: Aptos,
     args: { packageObjectAddress: string; priceAdapterObjectAddress: string },
     private readonly account?: Account
   ) {
+    super(client);
     this.packageObjectAddress = AccountAddress.fromString(
       args.packageObjectAddress
     );
@@ -37,7 +23,7 @@ export class MovementPricesContractConnector
     );
   }
 
-  getAdapter(): Promise<MovementPricesContractAdapter> {
+  override getAdapter(): Promise<MovementPricesContractAdapter> {
     const adapter: IMovementContractAdapter = {
       writer: this.account
         ? new MovementWriteContractAdapter(
@@ -55,38 +41,5 @@ export class MovementPricesContractConnector
     };
 
     return Promise.resolve(new MovementPricesContractAdapter(adapter));
-  }
-
-  async getBlockNumber(): Promise<number> {
-    return Number((await this.client.getLedgerInfo()).block_height);
-  }
-
-  async waitForTransaction(txId: string): Promise<boolean> {
-    try {
-      const committedTx = await this.client.waitForTransaction({
-        transactionHash: txId,
-        options: {
-          timeoutSecs: TIMEOUT_IN_SEC,
-        },
-      });
-      if (committedTx.success) {
-        this.processCommittedTransactionCost(committedTx);
-
-        return true;
-      }
-    } catch (exception) {
-      this.logger.error(
-        `Failed to validate transaction state: ${RedstoneCommon.stringifyError(exception)}`
-      );
-    }
-
-    return false;
-  }
-
-  private processCommittedTransactionCost(tx: TransactionResponse) {
-    const cost = txCost(tx);
-    this.logger.log(
-      `Transaction ${tx.hash} finished, status: COMMITTED, cost: ${cost} MOVE.`
-    );
   }
 }
