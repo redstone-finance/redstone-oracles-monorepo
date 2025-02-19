f549a1bd27cf38bd3ae478f1e6ff2515817b1dbb
robal <robert@warp.cc>
2025-02-19 09:54:03 +0100

    chore: simplify typing (#4824)

--- packages/utils/src/common/batch-promises.ts
+++ packages/utils/src/common/batch-promises.ts
@@ -6,9 +6,9 @@ export async function batchPromises<T>(
   msBetweenBatches: number,
   promises: (() => Promise<T>)[],
   failOnError = false
-): Promise<T[]> {
-  const results: T[] = [];
-  const errors: Error[] = [];
+): Promise<Awaited<T>[]> {
+  const results = [];
+  const errors = [];
   for (let i = 0; i < promises.length; i += batchSize) {
     if (i !== 0) {
       await sleep(msBetweenBatches);
@@ -17,7 +17,7 @@ export async function batchPromises<T>(
     const batchResults = await Promise.allSettled(batch.map((f) => f()));
     const filteredResults = batchResults.filter(
       (result) => result.status === "fulfilled"
-    ) as PromiseFulfilledResult<T>[];
+    );
     results.push(...filteredResults.map((result) => result.value));
     const filteredErrors = batchResults.filter(
       (result) => result.status === "rejected"
