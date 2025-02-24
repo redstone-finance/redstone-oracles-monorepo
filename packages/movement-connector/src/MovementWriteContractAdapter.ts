a911b99ac6a0ab67e2f8e7c9ff47427c1115a527
Bartosz Lenart <bartosz.lenart@redstone.finance>
2025-02-24 11:28:48 +0100

    [RDS-2333] fis:Rewrite to be sequential. (#4912)
    [RDS-2333](https://redstone-team.atlassian.net/browse/RDS-2333)
    
    
    [RDS-2333]:
    https://redstone-team.atlassian.net/browse/RDS-2333?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: bartossh <bartosz.lenart@redstone.com>

--- packages/movement-connector/src/MovementWriteContractAdapter.ts
+++ packages/movement-connector/src/MovementWriteContractAdapter.ts
@@ -5,9 +5,8 @@ import {
   InputGenerateTransactionPayloadData,
   MoveFunctionId,
   MoveVector,
-  TransactionWorkerEventsEnum,
 } from "@aptos-labs/ts-sdk";
-import { loggerFactory } from "@redstone-finance/utils";
+import { loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
 import { TRANSACTION_DEFAULT_CONFIG } from "./config";
 import { MovementOptionsContractUtil } from "./MovementOptionsContractUtil";
 import { IMovementWriteContractAdapter, TransactionConfig } from "./types";
@@ -27,72 +26,80 @@ export class MovementWriteContractAdapter
     private readonly config: TransactionConfig = TRANSACTION_DEFAULT_CONFIG
   ) {}
 
-  async writePricesBatch(
+  async writePrices(
     payloads: { payload: string; feedId: string }[]
   ): Promise<string> {
-    const data = [];
+    let hash;
 
     for (const { payload, feedId } of payloads) {
-      data.push({
-        function:
-          `${this.priceAdapterPackageAddress.toString()}::price_adapter::write_price` as MoveFunctionId,
-        functionArguments: [
-          this.priceAdapterObjectAddress.toString(),
-          makeFeedId(feedId),
-          makePayload(payload),
-        ],
-      });
+      try {
+        hash = await this.retrySendTransaction({
+          function:
+            `${this.priceAdapterPackageAddress.toString()}::price_adapter::write_price` as MoveFunctionId,
+          functionArguments: [
+            this.priceAdapterObjectAddress.toString(),
+            makeFeedId(feedId),
+            makePayload(payload),
+          ],
+        });
+      } catch (e) {
+        this.logger.error(
+          `Write price for feed: ${feedId} failed with message: ${RedstoneCommon.stringifyError(e)}`
+        );
+      }
     }
 
-    await this.sendBatchTransactions(data);
-    return await this.waitForNSent(data.length);
+    if (!hash) {
+      throw new Error("No transaction committed with success.");
+    }
+
+    return hash;
   }
 
-  private async sendBatchTransactions(
-    data: InputGenerateTransactionPayloadData[]
+  private async retrySendTransaction(
+    data: InputGenerateTransactionPayloadData
   ) {
     for (let i = 0; i < this.config.maxTxSendAttempts; i++) {
       try {
-        return await this.trySendBatchTransactions(data, i);
+        return await this.trySendTransaction(data, i);
       } catch {
-        this.logger.info(`Retrying ${i + 1} time to batch send transactions.`);
+        this.logger.info(`Retrying ${i + 1} time to send transaction.`);
       }
     }
+    throw new Error(`${this.config.maxTxSendAttempts} attempts unsuccessful.`);
   }
 
-  private async trySendBatchTransactions(
-    data: InputGenerateTransactionPayloadData[],
+  private async trySendTransaction(
+    data: InputGenerateTransactionPayloadData,
     iteration: number
   ) {
     const options = await this.optionsProvider.prepareTransactionOptions(
       this.config.writePriceOctasTxGasBudget,
       iteration
     );
-    this.client.transaction.batch.removeAllListeners();
-    this.logger.debug(`Sending ${data.length} transactions`);
-    this.client.transaction.batch.forSingleAccount({
-      sender: this.account,
+
+    const transaction = await this.client.transaction.build.simple({
+      sender: this.account.accountAddress,
       data,
       options,
     });
-  }
 
-  private async waitForNSent(n: number): Promise<string> {
-    return await new Promise((resolve) => {
-      let sent = 0;
-      this.client.transaction.batch.on(
-        TransactionWorkerEventsEnum.TransactionSent,
-        (data) => {
-          sent += 1;
-          this.logger.debug(
-            `Sent transaction ${data.transactionHash}, msg: ${data.message}. ${sent}/${n}`
-          );
-          if (sent === n) {
-            resolve(data.transactionHash);
-          }
-        }
-      );
-    });
+    const pendingTransaction =
+      await this.client.transaction.signAndSubmitTransaction({
+        transaction,
+        signer: this.account,
+      });
+
+    const candidateTransaction =
+      await this.client.transaction.waitForTransaction({
+        transactionHash: pendingTransaction.hash,
+      });
+
+    if (!candidateTransaction.success) {
+      throw new Error(`Transaction ${candidateTransaction.hash} failed.`);
+    }
+
+    return candidateTransaction.hash;
   }
 }
 
