fe161960afe4811539bbd0786061650828a18020
robal <robert@warp.cc>
2025-02-26 15:29:48 +0100

    chore: update libraries with vulnerabilities reported by dependabot (#4937)

--- packages/on-chain-relayer/contracts/price-feeds/interfaces/IPriceFeed.sol
+++ packages/on-chain-relayer/contracts/price-feeds/interfaces/IPriceFeed.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {IPriceFeedLegacy} from "./IPriceFeedLegacy.sol";
 
 /**
