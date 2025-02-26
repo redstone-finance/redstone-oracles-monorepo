0dea2f456dd26f37340747d0922c9801e479ef1f
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-02-26 10:25:44 +0100

    [RDS-2321] Movement multi executor (#4871)

--- packages/movement-connector/src/price_adapter/MovementPriceAdapterContractViewer.ts
+++ packages/movement-connector/src/price_adapter/MovementPriceAdapterContractViewer.ts
@@ -1,8 +1,16 @@
 import { AccountAddress, Aptos, MoveVector } from "@aptos-labs/ts-sdk";
 import { hexlify } from "@ethersproject/bytes";
 import { ContractData } from "@redstone-finance/sdk";
-import { loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
+import {
+  loggerFactory,
+  MultiExecutor,
+  RedstoneCommon,
+} from "@redstone-finance/utils";
 import { BigNumber } from "ethers";
+import {
+  ALL_EXECUTIONS_TIMEOUT_MS,
+  SINGLE_EXECUTION_TIMEOUT_MS,
+} from "../AptosClientBuilder";
 import { MovementContractViewer } from "../MovementContractViewer";
 import { IMovementPriceAdapterViewer, PriceDataSchema } from "../types";
 import { makeFeedIdBytes } from "../utils";
@@ -21,6 +29,33 @@ export class MovementPriceAdapterContractViewer
     private readonly priceAdapterObjectAddress: string
   ) {
     super(client, "price_adapter", packageAddress);
+  }
+
+  static createMultiReader(
+    client: Aptos,
+    priceAdapterPackageAddress: string,
+    priceAdapterObjectAddress: string
+  ) {
+    const clients =
+      "__instances" in client ? (client.__instances as Aptos[]) : [client];
+
+    return MultiExecutor.create(
+      clients.map(
+        (client) =>
+          new MovementPriceAdapterContractViewer(
+            client,
+            priceAdapterPackageAddress,
+            priceAdapterObjectAddress
+          )
+      ),
+      {},
+      {
+        ...MultiExecutor.DEFAULT_CONFIG,
+        defaultMode: MultiExecutor.ExecutionMode.AGREEMENT,
+        singleExecutionTimeoutMs: SINGLE_EXECUTION_TIMEOUT_MS,
+        allExecutionsTimeoutMs: ALL_EXECUTIONS_TIMEOUT_MS,
+      }
+    );
   }
 
   async viewUniqueSignerThreshold(): Promise<number> {
