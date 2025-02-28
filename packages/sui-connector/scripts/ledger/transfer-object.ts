1caf4d48b564c27a91578e98add5a9c2afac517c
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-02-28 15:55:12 +0100

    [RDS-2420] feat: sui redeploy contract infra on mainnet  (#4977)

--- packages/sui-connector/scripts/ledger/transfer-object.ts
+++ packages/sui-connector/scripts/ledger/transfer-object.ts
@@ -1,9 +1,9 @@
 import { generateTransactionData } from "./generate-transaction-data";
 
 const OBJECT_ID_TO_TRANSFER =
-  "0x1afe086edb3e945c2b2767aa8430b5c2e190b9108fb2820d8a2ad2acfcb1ff1c";
+  "0xe6d8c0181883f4892154c8702743c4107933202680be21b2f1a7c8bf0488f38f";
 const DESTINATION_ADDRESS =
-  "0x1be84a182296d18fd3bc8fcffc24b54db4af09c49d4e734395a8b8e16971e19f";
+  "0xab1bf56ae7e13625027ee67e028deda37669b9f77f65465db2286c87a6166bcd";
 
 void generateTransactionData((tx) =>
   tx.transferObjects([OBJECT_ID_TO_TRANSFER], DESTINATION_ADDRESS)
