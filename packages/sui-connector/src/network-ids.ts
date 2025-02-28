5cc72970f98e99e1225298d2f2965aa6e498faad
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-02-28 15:55:12 +0100

    [RDS-2420] feat: sui redeploy contract infra on mainnet  (#4977)

--- packages/sui-connector/src/network-ids.ts
+++ packages/sui-connector/src/network-ids.ts
@@ -1,5 +1,5 @@
+import _ from "lodash";
 import { SuiNetworkName } from "./config";
-
 /// A numeric result of invoking SuiClient.getChainIdentifier method
 /// same as for `sui_getChainIdentifier` API method
 export const SUI_NETWORK_IDS: { [p: number]: SuiNetworkName } = {
@@ -10,4 +10,8 @@ export const SUI_NETWORK_IDS: { [p: number]: SuiNetworkName } = {
 
 export function getSuiNetworkName(chainId: number): SuiNetworkName {
   return SUI_NETWORK_IDS[chainId] ?? "localnet";
+}
+
+export function getSuiChainId(network: SuiNetworkName) {
+  return Number(_.findKey(SUI_NETWORK_IDS, (v) => v === network)!);
 }
