ed53477d48174778052f435b328b3e07b835f50c
jakkorzeniewski <jakub.korzeniewski@redstone.finance>
2025-03-05 14:16:12 +0100

    Hotfix: Revert SSM Secret changes (#5047)
    - This reverts commit 4e19e39e662b960be601473781cb50b20afef2a7.
    - Changes broke a few lambdas, reverting for the time of investigation

--- packages/internal-utils/src/aws/params.ts
+++ packages/internal-utils/src/aws/params.ts
@@ -1,156 +1,21 @@
 import { S3 } from "@aws-sdk/client-s3";
-import {
-  GetParameterCommand,
-  GetParametersCommand,
-  SSMClient,
-} from "@aws-sdk/client-ssm";
-import { RedstoneCommon } from "@redstone-finance/utils";
-import { ZodType, ZodTypeDef } from "zod";
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 import { AWS_REGION } from "./aws";
-import { getNonExpiredDataFromSSMCache, saveDataInSSMCache } from "./ssm-cache";
 
 const s3 = new S3();
 const ssmClient = new SSMClient({ region: AWS_REGION });
-
-const getClient = (region?: string) =>
-  region ? new SSMClient({ region }) : ssmClient;
 
 export async function getSSMParameterValue(
   parameterName: string,
   region?: string
 ) {
-  const currentTime = Date.now();
-  const cachedData = getNonExpiredDataFromSSMCache(
-    { parameterName, region },
-    currentTime
-  );
-
-  if (cachedData) {
-    console.log(`Using cached SSM value for ${parameterName}`);
-    return cachedData.value;
-  }
-
-  const client = getClient(region);
+  const client = region ? new SSMClient({ region }) : ssmClient;
   const command = new GetParameterCommand({
     Name: parameterName,
     WithDecryption: true,
   });
 
-  const parameterValue = (await client.send(command)).Parameter?.Value;
-  saveDataInSSMCache({ parameterName, region }, parameterValue, currentTime);
-
-  return parameterValue;
-}
-
-export async function getSSMParametersValues(
-  parameterNames: string[],
-  region?: string
-) {
-  const currentTime = Date.now();
-  const result: Record<string, string | undefined> = {};
-
-  const parametersToFetch = [];
-  for (const parameterName of parameterNames) {
-    const cachedData = getNonExpiredDataFromSSMCache(
-      { parameterName, region },
-      currentTime
-    );
-
-    if (cachedData) {
-      console.log(`Using cached SSM value for ${parameterName}`);
-      result[parameterName] = cachedData.value;
-    } else {
-      parametersToFetch.push(parameterName);
-    }
-  }
-
-  if (parametersToFetch.length === 0) {
-    // All data was cached, nothing to fetch
-    return result;
-  }
-
-  const client = getClient(region);
-  const command = new GetParametersCommand({
-    Names: parametersToFetch,
-    WithDecryption: true,
-  });
-
-  const parameters = (await client.send(command)).Parameters;
-  if (parameters === undefined) {
-    console.warn(
-      "Failed to fetch parameters from SSM, returning only cached ones"
-    );
-    console.warn("Failed parameters:", parametersToFetch);
-    return result;
-  }
-
-  for (const parameter of parameters) {
-    if (parameter.Name) {
-      result[parameter.Name] = parameter.Value;
-      saveDataInSSMCache(
-        { parameterName: parameter.Name, region },
-        parameter.Value,
-        currentTime
-      );
-    }
-  }
-
-  return result;
-}
-
-export async function getSSMParamWithEnvFallback<T = string>(
-  parameterName: string | undefined,
-  envVariable: string,
-  schema?: ZodType<T, ZodTypeDef, T | undefined>,
-  region?: string
-) {
-  if (!parameterName) {
-    console.log(
-      `SSM Path was not provided, defaulting to value from ENV ${envVariable}`
-    );
-    return RedstoneCommon.getFromEnv(envVariable, schema);
-  }
-
-  try {
-    const parameter = await getSSMParameterValue(parameterName, region);
-    if (parameter) {
-      return parameter;
-    }
-
-    console.warn(`Failed to fetch value of ${parameterName} from SSM`);
-  } catch (e) {
-    console.warn(
-      `An error occured when fetching SSM Parameter Value from ${parameterName}`
-    );
-    RedstoneCommon.stringifyError(e);
-  }
-
-  console.warn(`Defaulting to value from ENV ${envVariable}`);
-  return RedstoneCommon.getFromEnv(envVariable, schema);
-}
-
-export async function getRequiredSSMParametersValues(
-  parameterNames: string[],
-  region?: string
-): Promise<Record<string, string>> {
-  const parameters = await getSSMParametersValues(parameterNames, region);
-
-  RedstoneCommon.assert(
-    parameters,
-    `Failed to retrieve parameters data from GetParameters command`
-  );
-
-  const result: Record<string, string> = {};
-
-  for (const [parameterName, value] of Object.entries(parameters)) {
-    RedstoneCommon.assert(
-      value,
-      `Failed to fetch value of ${parameterName} from SSM`
-    );
-    result[parameterName] = value;
-  }
-
-  return result;
+  return (await client.send(command)).Parameter?.Value;
 }
 
 export async function getS3ParameterValue(
