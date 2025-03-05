53a0bed9430abe1d1ee7cc9d7ceb3e76f3489a9d
jakkorzeniewski <jakub.korzeniewski@redstone.finance>
2025-03-05 12:52:58 +0100

    [RDS-907] Don't provide lambda secrets directly in env variables (#4641)
    - Changed env variables that read values from SSM directly in terraform
    to ARNs, added fetching the secret values in lambdas
    - Change improves security as there are no plain-text secrets in
    environment variables
    - Need to verify that secrets are being correctly accessed by all lambdas
    after changes

--- packages/chain-configs/src/get-chain-configs.ts
+++ packages/chain-configs/src/get-chain-configs.ts
@@ -36,7 +36,7 @@ const fetchChainConfigsWithCache = RedstoneCommon.memoize({
 });
 
 export async function fetchChainConfigs(): Promise<ChainConfigs> {
-  const config = getRemoteMonitoringManifestConfigFromEnv();
+  const config = await getRemoteMonitoringManifestConfigFromEnv();
 
   if (config.shouldUseLocal) {
     return getLocalChainConfigs();
