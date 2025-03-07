5b17ca65216308107f67d0dba9a3a7a621885289
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-03-07 09:59:07 +0100

    [RDS-2419]: Rm radix price adapter (#5058)

--- packages/radix-connector/scrypto/contracts/multi_feed_price_adapter/tests/tests/mod.rs
+++
@@ -1,6 +0,0 @@
-redstone_testing::test_price_adapter_multi_feed_specific_impl!(
-    crate::env::test_env::MultiFeedPriceAdapterTestEnv,
-    env,
-    crate::env::sim_env::MultiFeedPriceAdapterSimEnv,
-    sim
-);
