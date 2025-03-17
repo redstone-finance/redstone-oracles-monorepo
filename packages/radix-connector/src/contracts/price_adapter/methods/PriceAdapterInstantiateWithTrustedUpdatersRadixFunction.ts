71498110650f8e0bbf64cb20b0f23867567223e8
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-03-17 10:01:57 +0100

    [RDS-2560] feat: add trusted updaters param (#5141)

---
+++ packages/radix-connector/src/contracts/price_adapter/methods/PriceAdapterInstantiateWithTrustedUpdatersRadixFunction.ts
@@ -0,0 +1,34 @@
+import { array, str, Value, ValueKind } from "@radixdlt/radix-engine-toolkit";
+import { DEFAULT_INSTANTIATE_XRD_FEE } from "../../../radix/constants";
+import { PriceAdapterInstantiateRadixFunction } from "./PriceAdapterInstantiateRadixFunction";
+
+export class PriceAdapterInstantiateWithTrustedUpdatersRadixFunction extends PriceAdapterInstantiateRadixFunction {
+  constructor(
+    packageId: string,
+    signerCountThreshold: number,
+    signers: string[],
+    private trustedUpdaters: string[],
+    packageName = "PriceAdapter",
+    fee = DEFAULT_INSTANTIATE_XRD_FEE
+  ) {
+    super(
+      packageId,
+      signerCountThreshold,
+      signers,
+      packageName,
+      fee,
+      "instantiate_with_trusted_updaters"
+    );
+  }
+
+  override getParams(): Value[] {
+    return [
+      ...super.getParams(),
+      array(ValueKind.String, ...this.trustedUpdaters.map(str)),
+    ];
+  }
+
+  override interpret(value: unknown[]) {
+    return super.interpret(value[0]);
+  }
+}
