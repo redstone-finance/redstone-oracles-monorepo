6a357fa622aa454bdb0606be4371ef0d2ef7a731
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-03-18 13:54:33 +0100

    [RDS-2592] radix enable fallback&agreement mechanism (#5162)

--- packages/utils/src/common/misc.ts
+++ packages/utils/src/common/misc.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export function roundToSignificantDigits(num: number, number = 2): number {
   if (num === 0) return 0;
 
@@ -20,13 +22,54 @@ export function getS(value: number, s = "s") {
 }
 
 export function stringify<R>(result: R) {
-  try {
-    return JSON.stringify(result);
-  } catch (e) {
-    return result !== undefined &&
-      result !== null &&
-      typeof result.toString === "function"
-      ? result.toString()
-      : "[Unable to stringify value]";
+  if (typeof result === "string") {
+    return result;
   }
+
+  try {
+    if (
+      result !== undefined &&
+      result !== null &&
+      (typeof result === "number" ||
+        typeof result === "boolean" ||
+        typeof result === "bigint" ||
+        typeof result === "symbol")
+    ) {
+      return String(result);
+    }
+
+    return unescapeString(JSON.stringify(result));
+  } catch (e) {
+    if (
+      result !== undefined &&
+      result !== null &&
+      typeof result.toString === "function" &&
+      result.toString !== Object.prototype.toString
+    ) {
+      return unescapeString(result.toString());
+    }
+
+    if (result !== undefined && result !== null && typeof result === "object") {
+      try {
+        const properties = Object.entries(result).map(([key, value]) => {
+          if (typeof value === "object" && value !== null) {
+            value = stringify(value);
+          } else if (typeof value === "function") {
+            value = "[Function]";
+          }
+          return `${key}: ${value}`;
+        });
+
+        return `{${properties.join(", ")}}`;
+      } catch {
+        return "[Complex Object]";
+      }
+    }
+
+    return "[Unable to stringify value]";
+  }
+}
+
+function unescapeString(s: string) {
+  return _.unescape(s).replace(/\\"/g, '"').replace(/\\\\/g, "\\");
 }
