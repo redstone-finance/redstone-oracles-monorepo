55ec1f7b91cb29de1583a0c501f333db2cb28ba2
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-03-20 17:37:19 +0100

    feat: radix upgrade log messages (#5210)

--- packages/radix-connector/src/radix/RadixClientConfig.ts
+++ packages/radix-connector/src/radix/RadixClientConfig.ts
@@ -4,6 +4,7 @@ export const DEFAULT_TRANSACTION_XRD_FEE = 1;
 export const DEFAULT_INSTANTIATE_XRD_FEE = 100;
 export const MAX_TIP_PERCENTAGE = 65_535; // https://docs.radixdlt.com/docs/transaction-limits
 export const ALLOWED_FORWARD_EPOCH_COUNT = 100;
+export const TX_WAIT_POLL_DELAY_MS = 500;
 
 export interface RadixClientConfig {
   maxTxSendAttempts: number;
