3cb885e827f6ff6ee7f2761fe8396bfe2118a1ef
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-03-20 18:23:37 +0100

    [RDS-2591]: Multisig access rule creation for proxy. (#5203)

--- packages/radix-connector/src/contracts/proxy/ProxyRadixContractAdapter.ts
+++ packages/radix-connector/src/contracts/proxy/ProxyRadixContractAdapter.ts
@@ -1,18 +1,10 @@
 import { RadixContractAdapter } from "../../radix/RadixContractAdapter";
-import { NonFungibleGlobalIdInput } from "../../radix/utils";
 import { SetGlobalContractAddressInvocationMethod } from "./methods/SetGlobalContractAddressInvocationMethod";
 
 export class ProxyFeedRadixContractAdapter extends RadixContractAdapter {
-  public async setContractGlobalAddress(
-    address: string,
-    proofBadge: NonFungibleGlobalIdInput
-  ) {
+  public async setContractGlobalAddress(address: string) {
     await this.client.call(
-      new SetGlobalContractAddressInvocationMethod(
-        this.componentId,
-        address,
-        proofBadge
-      )
+      new SetGlobalContractAddressInvocationMethod(this.componentId, address)
     );
   }
 }
