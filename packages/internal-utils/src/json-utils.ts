71c599f5c8c9d2c35da1eda5281129743b383de4
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-03-24 21:56:22 +0100

    refactor: use one function for reading and saving json files (#5255)
    - we had the same functions for reading json files in multiple places
    - moved reading and saving json files it to internal-utils and exporting
    it from there

---
+++ packages/internal-utils/src/json-utils.ts
@@ -0,0 +1,20 @@
+import { readFileSync, writeFileSync } from "fs";
+
+export const readJsonFile = <T = unknown>(path: string): T => {
+  const content = readFileSync(path, "utf-8");
+  try {
+    return JSON.parse(content) as T;
+  } catch (e) {
+    throw new Error(`File "${path}" does not contain a valid JSON`);
+  }
+};
+
+export const saveJsonFile = (filePath: string, data: unknown) => {
+  try {
+    const jsonData = JSON.stringify(data, null, 2);
+    writeFileSync(filePath, jsonData, "utf-8");
+    console.log(`File was saved successfully: ${filePath}`);
+  } catch (error) {
+    console.error(`Error writing file to disk: ${JSON.stringify(error)}`);
+  }
+};
