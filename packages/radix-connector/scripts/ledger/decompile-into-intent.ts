bba41115d3984615feb90d473f2bf3fe14c5c4a7
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-03-25 08:56:53 +0100

    [RDS-2631]: Add scripts for ledger multisignature (#5242)
    - Added scripts for radix multisig signing
    - Needed to be able to governance proxy contract

---
+++ packages/radix-connector/scripts/ledger/decompile-into-intent.ts
@@ -0,0 +1,18 @@
+import { RadixEngineToolkit } from "@radixdlt/radix-engine-toolkit";
+import { arrayify } from "ethers/lib/utils";
+import { COMPILED_TRANSACTION } from "./config";
+
+export async function decompileIntent(compiledIntent: string) {
+  return await RadixEngineToolkit.Intent.decompile(
+    arrayify(compiledIntent),
+    "String"
+  );
+}
+
+async function main(compiledIntent: string) {
+  const intent = await decompileIntent(compiledIntent);
+
+  console.log(intent);
+}
+
+void main(COMPILED_TRANSACTION);
