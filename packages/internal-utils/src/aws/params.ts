a6c67264996641e14b0e966ebb8eae60c59d7afa
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-03-25 17:49:01 +0100

    feat: prepare relayer for manifest from S3 (#5263)
    - unified S3 interaction by moving read and write functions to
    `internal-utils`
    - added fetching manifest for relayer from S3 by setting two env
    variables `REMOTE_MANIFEST_S3_BUCKET_NAME` and
    `REMOTE_MANIFEST_S3_BUCKET_KEY`
    - tested on dev with manifest from bucket

--- packages/internal-utils/src/aws/params.ts
+++ packages/internal-utils/src/aws/params.ts
@@ -1,14 +1,13 @@
-import { S3 } from "@aws-sdk/client-s3";
 import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 import { AWS_REGION } from "./aws";
+import { readS3Object } from "./s3";
 
-const s3 = new S3();
 const ssmClient = new SSMClient({ region: AWS_REGION });
 
-export async function getSSMParameterValue(
+export const getSSMParameterValue = async (
   parameterName: string,
   region?: string
-) {
+) => {
   const client = region ? new SSMClient({ region }) : ssmClient;
   const command = new GetParameterCommand({
     Name: parameterName,
@@ -16,32 +15,16 @@ export async function getSSMParameterValue(
   });
 
   return (await client.send(command)).Parameter?.Value;
-}
+};
 
-export async function getS3ParameterValue(
-  bucketName: string,
-  objectKey: string
-) {
-  const params = {
-    Bucket: bucketName,
-    Key: objectKey,
-  };
-
-  const data = await s3.getObject(params);
-
-  return await data.Body?.transformToString("utf-8");
-}
-
-export async function getS3ConfigurationValue() {
+export const getS3ConfigurationValue = async <T>() => {
   const bucketName = process.env.CONFIGURATION_S3_BUCKET;
   const objectKey = process.env.CONFIGURATION_S3_OBJECT;
-
   if (!bucketName || !objectKey) {
     throw new Error("S3 configuration not set");
   }
-
-  return await getS3ParameterValue(bucketName, objectKey);
-}
+  return await readS3Object<T>(bucketName, objectKey);
+};
 
 // Get ENV variables that end with "_ARN"
 // Try to get the value from SSM and set it in the ENV, but rename the key to remove "_ARN"
