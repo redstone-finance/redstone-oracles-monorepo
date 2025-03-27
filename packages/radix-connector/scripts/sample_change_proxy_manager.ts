36b3521df5af870b03c03a60291e3f4f8c81ca40
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-03-27 18:35:03 +0100

    [RDS-2679]: Make proxy manager role updatable by itself (#5316)
    - Manager role is updatable by itself
    - So proxy manager rule can be transfer to different multisig(or any
    custom accessrule)

---
+++ packages/radix-connector/scripts/sample_change_proxy_manager.ts
@@ -0,0 +1,27 @@
+import { RedstoneCommon } from "@redstone-finance/utils";
+import "dotenv/config";
+import { z } from "zod";
+import { ProxyRadixContractConnector } from "../src";
+import { makeMultisigAccessRule } from "../src/radix/utils";
+import { FEED_ID, loadAddress, makeRadixClient, PROXY_NAME } from "./constants";
+
+async function changeProxyManager() {
+  const networkId = RedstoneCommon.getFromEnv("NETWORK_ID", z.number());
+  const client = makeRadixClient(networkId);
+
+  const connector = new ProxyRadixContractConnector(
+    client,
+    await loadAddress(`component`, PROXY_NAME, FEED_ID)
+  );
+
+  const adapter = await connector.getAdapter();
+  await adapter.changeManagerAccessRule(
+    await makeMultisigAccessRule(
+      1,
+      [client.getNotarySigner()!.publicKeyHex()],
+      networkId
+    )
+  );
+}
+
+void changeProxyManager();
