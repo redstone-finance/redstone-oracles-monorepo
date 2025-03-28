7d0e512ee8c2fcf7452fec660304d6729d047c86
just_ppe <84311757+ppedziwiatr@users.noreply.github.com>
2025-03-28 09:45:51 +0100

    [RDS-2401] Node updates when only configuration changes - part 2 - dex configs

---
+++ packages/internal-utils/src/Terminator.ts
@@ -0,0 +1,34 @@
+import { loggerFactory } from "@redstone-finance/utils";
+
+// TODO: deduplicate
+const logger = loggerFactory("Terminator");
+
+export enum ExitCodes {
+  ManifestConfigError = 1,
+  NodeRemoteConfigError = 2,
+  RestartConfigExitCode = 100,
+}
+
+export function terminateWithManifestConfigError(errorDetails: string): never {
+  logger.error(
+    `Manifest configuration error: ${errorDetails}.\n` +
+      `Terminating with exit code ${ExitCodes.ManifestConfigError}`
+  );
+
+  process.exit(ExitCodes.ManifestConfigError);
+}
+
+export function terminateWithUpdateConfigExitCode(): never {
+  process.exit(ExitCodes.RestartConfigExitCode);
+}
+
+export function terminateWithNodeRemoteConfigError(
+  errorDetails: string
+): never {
+  logger.error(
+    `Node configuration error: ${errorDetails}.\n` +
+      `Terminating with exit code ${ExitCodes.NodeRemoteConfigError}`
+  );
+
+  process.exit(ExitCodes.NodeRemoteConfigError);
+}
