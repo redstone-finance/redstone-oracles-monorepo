fb47555832e06a870ea38c28bc8541839b82a4d7
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-03-31 09:54:59 +0200

    [RDS-2692]: Add tx for transfering proxy manager role (#5355)

---
+++ packages/radix-connector/scripts/ledger/transfer-multisig-tx.ts
@@ -0,0 +1,41 @@
+import { RadixEngineToolkit } from "@radixdlt/radix-engine-toolkit";
+import { RedstoneCommon } from "@redstone-finance/utils";
+import "dotenv/config";
+import { hexlify } from "ethers/lib/utils";
+import { z } from "zod";
+import { RadixClient } from "../../src";
+import { SetRolaRadixInvocation } from "../../src/contracts/proxy/methods/ChangeManagerRole";
+import { makeMultisigAccessRule } from "../../src/radix/utils";
+import {
+  FEED_ID,
+  loadAddress,
+  makeRadixClient,
+  PROXY_NAME,
+} from "../constants";
+
+const THRESHOLD = 2;
+const MULTI_SIG_PUBLIC_KEYS = [""]; // ed public keys hex without 0x prefix
+
+export async function getTx(client: RadixClient, networkId: number) {
+  const componentId = await loadAddress(`component`, PROXY_NAME, FEED_ID);
+  const invocation = new SetRolaRadixInvocation(
+    componentId,
+    "proxy_man_auth",
+    await makeMultisigAccessRule(THRESHOLD, MULTI_SIG_PUBLIC_KEYS, networkId)
+  );
+
+  return invocation.getDedicatedTransaction(await client.getAccountAddress());
+}
+
+async function transferMultiSig() {
+  const networkId = RedstoneCommon.getFromEnv("NETWORK_ID", z.number());
+  const client = makeRadixClient(networkId);
+  const tx = await getTx(client, networkId);
+
+  const intent = await client.compileTransactionToIntent(tx);
+  console.log(await RadixEngineToolkit.Intent.decompile(intent, "String"));
+
+  console.log(hexlify(intent));
+}
+
+void transferMultiSig();
