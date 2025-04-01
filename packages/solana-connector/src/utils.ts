78b408cd55107f85411b1fec3ef0fe5bb19e1cdf
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-04-01 22:51:01 +0200

    [RDS-2693] solana make it working again (#5368)

--- packages/solana-connector/src/utils.ts
+++ packages/solana-connector/src/utils.ts
@@ -1,3 +1,11 @@
 export function hexToU8Array(hex: string): Uint8Array {
   return Uint8Array.from(Buffer.from(hex, "hex"));
 }
+
+export const makeFeedIdBytes = (feedId: string) => {
+  return Buffer.from(feedId.padEnd(32, "\0"));
+};
+
+export const makePriceSeed = () => {
+  return Buffer.from("price".padEnd(32, "\0"));
+};
