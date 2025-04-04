df54e31f43f80d39dc6345b742a869484775c09e
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-04 11:11:27 +0200

    [RDS-2759]: Radix index also failed transactions (#5433)

---
+++ packages/radix-connector/src/radix/types.ts
@@ -0,0 +1,4 @@
+import { StreamTransactionsRequestTransactionStatusFilterEnum } from "@radixdlt/babylon-gateway-api-sdk";
+
+export type TransactionStatusFilter =
+  StreamTransactionsRequestTransactionStatusFilterEnum;
