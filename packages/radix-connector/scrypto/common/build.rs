234f948f7f05f2977785f54eb82fece346751c86
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-04 11:10:50 +0200

    [RDS-2772]: Radix audit fixes (#5436)

--- packages/radix-connector/scrypto/common/build.rs
+++ packages/radix-connector/scrypto/common/build.rs
@@ -1,12 +1,10 @@
 fn main() {
-    if let Ok(value) = std::env::var("FORCE_REAL_NETWORK_TEST") {
+    if let Ok(value) = std::env::var("MOCK_TIME") {
         if value == "true" {
-            println!("cargo:rustc-cfg=feature=\"real_network_test\"");
+            println!("cargo:rustc-cfg=feature=\"mock-time\"");
         }
     }
 
-    println!("cargo:rerun-if-env-changed=FORCE_REAL_NETWORK_TEST");
-    println!("cargo:rerun-if-feature-changed=test_sim_env");
-    println!("cargo:rerun-if-feature-changed=real_network");
-    println!("cargo:rerun-if-feature-changed=real_network_test");
+    println!("cargo:rerun-if-env-changed=MOCK_TIME");
+    println!("cargo:rerun-if-feature-changed=mock-time");
 }
