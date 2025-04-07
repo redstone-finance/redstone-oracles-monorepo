3f7332c7f2dfecd236671afbdd0e4be76a3de57a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-04-07 16:29:48 +0200

    [RDS-2799] radix re deploy production (#5465)

---
+++ packages/radix-connector/deployments/radixMultiFeed/common/src/decimals.rs
@@ -0,0 +1,36 @@
+use crate::price_adapter_error::PriceAdapterError;
+pub use redstone::network::as_str::AsAsciiStr;
+use redstone::{network::error::Error, Value};
+use scrypto::math::Decimal;
+
+pub const REDSTONE_DECIMALS_EXP: u8 = 8;
+pub const REDSTONE_DECIMALS: u64 = 10u64.pow(REDSTONE_DECIMALS_EXP as u32);
+
+pub trait ToRedStoneDecimal<D> {
+    fn to_redstone_decimal(self) -> Result<D, Error>;
+}
+
+impl ToRedStoneDecimal<Decimal> for Value {
+    fn to_redstone_decimal(self) -> Result<Decimal, Error> {
+        (0, self).to_redstone_decimal()
+    }
+}
+
+impl ToRedStoneDecimal<Decimal> for (usize, Value) {
+    fn to_redstone_decimal(self) -> Result<Decimal, Error> {
+        let value = Decimal::try_from(self.1)
+            .map_err(|_| PriceAdapterError::DecimalOverflow(self.1, self.0))?;
+
+        Ok(value / REDSTONE_DECIMALS)
+    }
+}
+
+pub trait ToRedStoneDecimals<D> {
+    fn to_redstone_decimals(self) -> Result<Vec<D>, Error>;
+}
+
+impl<T: ToRedStoneDecimal<D>, D, It: Iterator<Item = T>> ToRedStoneDecimals<D> for It {
+    fn to_redstone_decimals(self) -> Result<Vec<D>, Error> {
+        self.map(|value| value.to_redstone_decimal()).collect()
+    }
+}
