2f5468dd30700b5eb4d541a067a3b7df55eaff41
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2025-04-08 16:03:09 +0200

    Make top-up script non-evm compatibel (#5309)

--- packages/chain-orchestrator/src/blockchain-service/get-non-evm-blockchain-service.ts
+++ packages/chain-orchestrator/src/blockchain-service/get-non-evm-blockchain-service.ts
@@ -3,6 +3,7 @@ import { AptosClientBuilder } from "@redstone-finance/movement-connector";
 import { RadixClientBuilder } from "@redstone-finance/radix-connector";
 import { SolanaConnectionBuilder } from "@redstone-finance/solana-connector";
 import { SuiClientBuilder } from "@redstone-finance/sui-connector";
+import { RedstoneCommon } from "@redstone-finance/utils";
 import { MovementBlockchainService } from "./MovementBlockchainService";
 import { RadixBlockchainService } from "./RadixBlockchainService";
 import { SolanaBlockchainService } from "./SolanaBlockchainService";
@@ -11,7 +12,8 @@ import { SuiBlockchainService } from "./SuiBlockchainService";
 export function getNonEvmBlockchainService(
   rpcUrls: string[],
   chainType: NonEvmChainType,
-  chainId: number
+  chainId: number,
+  privateKey?: RedstoneCommon.PrivateKey
 ) {
   switch (chainType) {
     case "sui": {
@@ -32,6 +34,7 @@ export function getNonEvmBlockchainService(
       const radixClient = new RadixClientBuilder()
         .withNetworkId(chainId)
         .withRpcUrls(rpcUrls)
+        .withPrivateKey(privateKey)
         .build();
       return new RadixBlockchainService(radixClient);
     }
