19eb0bcbe305b55bb02ec948463dcf0767b7e7b6
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2025-04-08 16:09:20 +0200

    Add Solana Indexer (#5467)

--- packages/chain-orchestrator/src/blockchain-service/SolanaBlockchainService.ts
+++ packages/chain-orchestrator/src/blockchain-service/SolanaBlockchainService.ts
@@ -1,6 +1,6 @@
 import { SolanaContractConnector } from "@redstone-finance/solana-connector";
 import { loggerFactory } from "@redstone-finance/utils";
-import { Connection } from "@solana/web3.js";
+import { ConfirmedSignatureInfo, Connection, PublicKey } from "@solana/web3.js";
 import { NonEvmBlockchainService } from "./NonEvmBlockchainService";
 
 export class SolanaBlockchainService extends NonEvmBlockchainService {
@@ -23,5 +23,28 @@ export class SolanaBlockchainService extends NonEvmBlockchainService {
     });
 
     return block;
+  }
+
+  async getTransactions(fromSlot: number, toSlot: number, addresses: string[]) {
+    const allSignatures: ConfirmedSignatureInfo[] = [];
+    for (const address of addresses) {
+      allSignatures.push(
+        ...(await this.connection.getSignaturesForAddress(
+          new PublicKey(address)
+        ))
+      );
+    }
+
+    const filteredSignatures = allSignatures.filter(
+      (sig) => sig.slot >= fromSlot && sig.slot <= toSlot
+    );
+
+    const transactionDetailsPromises = filteredSignatures.map((signature) =>
+      this.connection.getTransaction(signature.signature, {
+        maxSupportedTransactionVersion: 0,
+      })
+    );
+
+    return await Promise.all(transactionDetailsPromises);
   }
 }
