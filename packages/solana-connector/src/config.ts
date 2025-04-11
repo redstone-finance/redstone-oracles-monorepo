777acd29d964918c84a382f0754f65eb73f096fd
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-11 09:30:28 +0200

    [RDS-2830]: Solana pass gas parameters (#5522)

--- packages/solana-connector/src/config.ts
+++ packages/solana-connector/src/config.ts
@@ -1,6 +1,7 @@
+// default config results in maximum fee of 0.02 Sol
 export const DEFAULT_SOLANA_CONFIG: SolanaConfig = {
   maxComputeUnits: 200_000,
-  maxPricePerComputeUnit: 10_000,
+  maxPricePerComputeUnit: 100_000_000,
   gasMultiplier: 5.5,
   maxTxAttempts: 5,
 };
@@ -10,4 +11,25 @@ export interface SolanaConfig {
   maxPricePerComputeUnit: number;
   gasMultiplier: number;
   maxTxAttempts: number;
+}
+
+export function createSolanaConfig(args: {
+  gasLimit?: number;
+  gasMultiplier?: number;
+  maxTxSendAttempts?: number;
+}) {
+  const gasMultiplier =
+    args.gasMultiplier ?? DEFAULT_SOLANA_CONFIG.gasMultiplier;
+  const maxTxAttempts =
+    args.maxTxSendAttempts ?? DEFAULT_SOLANA_CONFIG.maxTxAttempts;
+  const maxPricePerComputeUnit = args.gasLimit
+    ? Math.floor(args.gasLimit / DEFAULT_SOLANA_CONFIG.maxComputeUnits)
+    : DEFAULT_SOLANA_CONFIG.maxPricePerComputeUnit;
+
+  return {
+    ...DEFAULT_SOLANA_CONFIG,
+    gasMultiplier,
+    maxPricePerComputeUnit,
+    maxTxAttempts,
+  };
 }
