d77e11111817315f4e591b5aa291bde3fda94f88
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-15 14:36:23 +0200

    [RDS-2833]: Test solana adapter (#5545)

--- packages/solana-connector/src/index.ts
+++ packages/solana-connector/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./config";
 export * from "./price_adapter/AnchorReadonlyProvider";
+export * from "./price_adapter/PriceAdapterContract";
 export * from "./price_adapter/SolanaPricesContractAdapter";
 export * from "./SolanaConnectionBuilder";
 export * from "./SolanaContractConnector";
