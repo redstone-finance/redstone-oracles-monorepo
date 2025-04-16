7b430987aa0eb2bb359e39174e4992f835d07ed0
just_ppe <84311757+ppedziwiatr@users.noreply.github.com>
2025-04-16 12:05:18 +0200

    Revert "[RDS-1041] Prevent leaking secrets in logs (#5399)" (#5585)

--- packages/utils/src/logger/index.ts
+++ packages/utils/src/logger/index.ts
@@ -23,14 +23,12 @@ export const loggerFactory = (moduleName: string): RedstoneLogger => {
       ? new JSONReporter()
       : new FancyReporter();
 
-    const logger = consola
+    return consola
       .create({
         reporters: [mainReporter],
         level: getLogLevel(),
       })
       .withTag(moduleName);
-
-    return createSanitizedLogger(logger);
   } else {
     return console;
   }
@@ -42,50 +40,3 @@ export const getLogLevel = () => {
     z.nativeEnum(LogLevel).default(DEFAULT_LOG_LEVEL)
   );
 };
-
-export function createSanitizedLogger(logger: RedstoneLogger): RedstoneLogger {
-  const methods = ["log", "info", "warn", "error", "debug"] as const;
-  const sanitizedLogger = { ...logger } as RedstoneLogger;
-  methods.forEach((method) => {
-    if (typeof logger[method] === "function") {
-      const original = logger[method].bind(logger);
-      sanitizedLogger[method] = (...args: unknown[]) => {
-        const sanitizedArgs = args.map((arg) => sanitizeValue(arg));
-        original.apply(logger, sanitizedArgs);
-      };
-    }
-  });
-  return sanitizedLogger;
-}
-
-export function sanitizeValue(value: unknown): unknown {
-  if (typeof value === "string") {
-    return sanitizeLogMessage(value);
-  } else if (Array.isArray(value)) {
-    return value.map((item) => sanitizeValue(item));
-  } else if (value !== null && typeof value === "object") {
-    const result: Record<string, unknown> = {};
-    for (const [key, val] of Object.entries(value)) {
-      result[key] = sanitizeValue(val);
-    }
-    return result;
-  }
-  return value;
-}
-
-export function sanitizeLogMessage(message: string): string {
-  // Regex to find HTTP, HTTPS, and WSS URLs in a log message
-  const urlRegex = /(https?|wss):\/\/[^\s]+/g;
-
-  return message.replace(urlRegex, (match) => {
-    try {
-      const parsedUrl = new URL(match);
-      const protocol = parsedUrl.protocol;
-      const host = parsedUrl.hostname;
-      const lastFour = match.slice(-4);
-      return `${protocol}//${host}/...${lastFour}`;
-    } catch (err) {
-      return match;
-    }
-  });
-}
