70394ce5b25527aad7223c10ba8a7b719436e76c
Przemysław Maranowski <przemek.maranowski@gmail.com>
2025-04-17 14:34:55 +0200

    Fix recursion in log sanitization (#5584)

--- packages/utils/src/logger/index.ts
+++ packages/utils/src/logger/index.ts
@@ -10,6 +10,7 @@ import { isNodeRuntime } from "../common/runtime";
 
 const DEFAULT_ENABLE_JSON_LOGS = true;
 const DEFAULT_LOG_LEVEL = LogLevel.Info;
+const MAX_DEPTH = 5;
 
 export type RedstoneLogger = Consola | Console;
 
@@ -40,3 +41,70 @@ export const getLogLevel = () => {
     z.nativeEnum(LogLevel).default(DEFAULT_LOG_LEVEL)
   );
 };
+
+export function createSanitizedLogger(logger: RedstoneLogger): RedstoneLogger {
+  const methods = ["log", "info", "warn", "error", "debug"] as const;
+  const sanitizedLogger = { ...logger } as RedstoneLogger;
+  methods.forEach((method) => {
+    if (typeof logger[method] === "function") {
+      const original = logger[method].bind(logger);
+      sanitizedLogger[method] = (...args: unknown[]) => {
+        const sanitizedArgs = args.map((arg) => sanitizeValue(arg));
+        original.apply(logger, sanitizedArgs);
+      };
+    }
+  });
+  return sanitizedLogger;
+}
+
+function sanitize(
+  val: unknown,
+  seen: WeakSet<object>,
+  depth: number = 0
+): unknown {
+  if (depth >= MAX_DEPTH) {
+    return "[Max Depth Reached]";
+  }
+  if (typeof val === "string") {
+    return sanitizeLogMessage(val);
+  } else if (Array.isArray(val)) {
+    if (seen.has(val)) {
+      return "[Circular]";
+    }
+    seen.add(val);
+    return val.map((item) => sanitize(item, seen, depth + 1));
+  } else if (val !== null && typeof val === "object") {
+    if (seen.has(val)) {
+      return "[Circular]";
+    }
+    seen.add(val);
+    const result: Record<string, unknown> = {};
+    for (const [key, item] of Object.entries(val)) {
+      result[key] = sanitize(item, seen, depth + 1);
+    }
+    return result;
+  }
+  return val;
+}
+
+export function sanitizeValue<T>(value: T): T {
+  const seen = new WeakSet();
+  return sanitize(value, seen) as T;
+}
+
+export function sanitizeLogMessage(message: string): string {
+  // Regex to find HTTP, HTTPS, and WSS URLs in a log message
+  const urlRegex = /(https?|wss):\/\/[^\s]+/g;
+
+  return message.replace(urlRegex, (match) => {
+    try {
+      const parsedUrl = new URL(match);
+      const protocol = parsedUrl.protocol;
+      const host = parsedUrl.hostname;
+      const lastFour = match.slice(-4);
+      return `${protocol}//${host}/...${lastFour}`;
+    } catch (err) {
+      return match;
+    }
+  });
+}
