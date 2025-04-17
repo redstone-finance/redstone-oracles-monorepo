e91d3abb0160c51b93f7f912c48076fbd18f21d9
robal <robert@warp.cc>
2025-04-17 15:26:10 +0200

    feat: RDS-2663 configurable region for AWS components, use AWS_REGION env for default region (#5600)

---
+++ packages/internal-utils/src/aws/aws-clients.ts
@@ -0,0 +1,28 @@
+import { CloudWatch, CloudWatchClient } from "@aws-sdk/client-cloudwatch";
+import { ECSClient } from "@aws-sdk/client-ecs";
+import { LambdaClient } from "@aws-sdk/client-lambda";
+import { S3 } from "@aws-sdk/client-s3";
+import { SSMClient } from "@aws-sdk/client-ssm";
+import { regionalCache } from "./region";
+
+export const getCloudwatch = regionalCache(
+  (region: string) => new CloudWatch({ region })
+);
+
+export const getCloudWatchClient = regionalCache(
+  (region: string) => new CloudWatchClient({ region })
+);
+
+export const getEcsClient = regionalCache(
+  (region: string) => new ECSClient({ region })
+);
+
+export const getLambdaClient = regionalCache(
+  (region: string) => new LambdaClient({ region })
+);
+
+export const getS3 = regionalCache((region: string) => new S3({ region }));
+
+export const getSsmClient = regionalCache(
+  (region: string) => new SSMClient({ region })
+);
