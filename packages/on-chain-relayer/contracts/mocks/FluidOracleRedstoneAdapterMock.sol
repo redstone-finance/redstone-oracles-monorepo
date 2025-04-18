9476cdd1e978f886bb5dfcb7db3bbebd8513baa4
Michał Szynwelski <szynek@warp.cc>
2025-04-18 11:41:32 +0200

    [RDS-2746] audit recommendation hal-08: unused imports and constants (#5599)
    Audit recommendation:
    (HAL-08) Unused imports and constants
    
    For now only enabled rules for unused imports and vars

--- packages/on-chain-relayer/contracts/mocks/FluidOracleRedstoneAdapterMock.sol
+++ packages/on-chain-relayer/contracts/mocks/FluidOracleRedstoneAdapterMock.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.17;
 
 import {EthUsdcRedstoneAdapterForFluidOracle} from "../custom-integrations/fluid/EthUsdcRedstoneAdapterForFluidOracle.sol";
 import {AuthorisedMockSignersBase} from "@redstone-finance/evm-connector/contracts/mocks/AuthorisedMockSignersBase.sol";
-import {PriceFeedsAdapterWithoutRounds} from "../price-feeds/without-rounds/PriceFeedsAdapterWithoutRounds.sol";
 
 contract FluidOracleRedstoneAdapterMock is EthUsdcRedstoneAdapterForFluidOracle,AuthorisedMockSignersBase {
   function getUniqueSignersThreshold() public view virtual override returns (uint8) {
