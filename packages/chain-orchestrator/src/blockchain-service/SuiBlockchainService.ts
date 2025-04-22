f59acbb82375b4d6eed8edece54fc6e5ed8a2b3d
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2025-04-22 11:27:45 +0200

    Add sui and solana support to top-up lambda (#5586)

--- packages/chain-orchestrator/src/blockchain-service/SuiBlockchainService.ts
+++ packages/chain-orchestrator/src/blockchain-service/SuiBlockchainService.ts
@@ -1,4 +1,5 @@
 import { SuiClient } from "@mysten/sui/client";
+import { Keypair } from "@mysten/sui/cryptography";
 import { SuiContractConnector } from "@redstone-finance/sui-connector";
 import { RedstoneCommon } from "@redstone-finance/utils";
 import { NonEvmBlockchainService } from "./NonEvmBlockchainService";
@@ -12,8 +13,11 @@ const RETRY_CONFIG: Omit<RedstoneCommon.RetryConfig, "fn"> = {
 };
 
 export class SuiBlockchainService extends NonEvmBlockchainService {
-  constructor(private client: SuiClient) {
-    super(new SuiContractConnector(client));
+  constructor(
+    private client: SuiClient,
+    keypair?: Keypair
+  ) {
+    super(new SuiContractConnector(client, keypair));
   }
 
   async getTimeForBlock(block: number): Promise<Date> {
