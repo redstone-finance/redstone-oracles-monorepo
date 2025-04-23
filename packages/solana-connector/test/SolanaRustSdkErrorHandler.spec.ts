25a3ecd090f5028b077d1fb04f545c63384a0838
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-23 13:39:38 +0200

    [RDS-2963]: Skipable rust-sdk errors (#5652)

---
+++ packages/solana-connector/test/SolanaRustSdkErrorHandler.spec.ts
@@ -0,0 +1,50 @@
+import {
+  SkippableRustSdkError,
+  SolanaRustSdkErrroHandler,
+} from "../src/SolanaRustSdkErrorHandler";
+
+function makeInstructionError(code: number) {
+  return {
+    InstructionError: [
+      0,
+      {
+        Custom: code,
+      },
+    ],
+  };
+}
+
+function makeBadInstructionError() {
+  return {
+    InstructionErrorer: [
+      0,
+      {
+        Customer: 10,
+      },
+    ],
+  };
+}
+
+describe("SolanaRustSdkErrorHandler tests", () => {
+  it("Handles unknown errors", () => {
+    const unknownErrors = [0, 1, 2, 3, 4].map(makeInstructionError);
+
+    for (const unknownError of unknownErrors) {
+      expect(SolanaRustSdkErrroHandler.canSkipError(unknownError)).toBeFalsy();
+    }
+  });
+  it("Handles known errors", () => {
+    const unknownErrors = Object.values(SkippableRustSdkError)
+      .filter((error) => typeof error === "number")
+      .map(makeInstructionError);
+
+    for (const unknownError of unknownErrors) {
+      expect(SolanaRustSdkErrroHandler.canSkipError(unknownError)).toBeTruthy();
+    }
+  });
+  it("Handles badly formatted error", () => {
+    expect(
+      SolanaRustSdkErrroHandler.canSkipError(makeBadInstructionError())
+    ).toBeFalsy();
+  });
+});
