d946004d959a0a60c67890c99aaf9b9a72b71119
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-04-23 14:53:25 +0200

    feat: don't use public rpcs urls in node (#5650)

--- packages/internal-utils/src/Terminator.ts
+++ packages/internal-utils/src/Terminator.ts
@@ -6,6 +6,7 @@ const logger = loggerFactory("Terminator");
 export enum ExitCodes {
   ManifestConfigError = 1,
   NodeRemoteConfigError = 2,
+  RpcsConfigError = 3,
   RestartConfigExitCode = 100,
 }
 
@@ -31,4 +32,13 @@ export function terminateWithNodeRemoteConfigError(
   );
 
   process.exit(ExitCodes.NodeRemoteConfigError);
+}
+
+export function terminateWithRpcsConfigError(errorDetails: string): never {
+  logger.error(
+    `RPCs URLs configuration error: ${errorDetails}.\n` +
+      `Terminating with exit code ${ExitCodes.RpcsConfigError}`
+  );
+
+  process.exit(ExitCodes.RpcsConfigError);
 }
