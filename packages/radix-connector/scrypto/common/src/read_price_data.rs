974fc417678833acd95cf39ec34d62a813062bef
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-04-28 09:32:22 +0200

    [RDS- 3041]: Radix change hashmap yo keyvalue store (#5715)

--- packages/radix-connector/scrypto/common/src/read_price_data.rs
+++ packages/radix-connector/scrypto/common/src/read_price_data.rs
@@ -1,26 +1,27 @@
 use crate::price_adapter_error::PriceAdapterError;
 use core::fmt::Debug;
 use redstone::{network::error::Error, FeedId};
-use scrypto::prelude::HashMap;
+use scrypto::data::scrypto::{ScryptoDecode, ScryptoDescribe, ScryptoEncode};
+use scrypto::prelude::KeyValueStore;
 
-pub fn read_price_data<'a, T: Debug>(
-    prices: &'a HashMap<FeedId, T>,
+pub fn read_price_data<T: Clone + Debug + ScryptoEncode + ScryptoDecode + ScryptoDescribe>(
+    prices: &KeyValueStore<FeedId, T>,
     feed_id: &FeedId,
     index: usize,
-) -> Result<&'a T, Error> {
+) -> Result<T, Error> {
     match prices.get(feed_id) {
-        Some(price) => Ok(price),
+        Some(price) => Ok(price.clone()),
         _ => Err(PriceAdapterError::MissingDataFeedValue(index, *feed_id).into()),
     }
 }
 
-pub fn read_prices_data<T: Debug + Clone>(
-    prices: &HashMap<FeedId, T>,
+pub fn read_prices_data<T: Debug + Clone + ScryptoEncode + ScryptoDecode + ScryptoDescribe>(
+    prices: &KeyValueStore<FeedId, T>,
     feed_ids: &[FeedId],
 ) -> Result<Vec<T>, Error> {
     feed_ids
         .iter()
         .enumerate()
-        .map(|(index, feed_id)| read_price_data(prices, feed_id, index).cloned())
+        .map(|(index, feed_id)| read_price_data(prices, feed_id, index))
         .collect()
 }
