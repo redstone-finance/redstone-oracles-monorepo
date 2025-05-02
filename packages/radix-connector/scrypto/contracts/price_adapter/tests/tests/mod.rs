0d322175dcf01a5523af0c4e2f59048e5030748a
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-05-02 14:32:55 +0200

    [RDS-2974]: radix rust contract tests (#5778)

--- packages/radix-connector/scrypto/contracts/price_adapter/tests/tests/mod.rs
+++ packages/radix-connector/scrypto/contracts/price_adapter/tests/tests/mod.rs
@@ -1,7 +1,2 @@
-#[cfg(feature = "mock-time")]
-redstone_testing::test_price_adapter_multi_feed_specific_impl!(
-    crate::env::test_env::PriceAdapterTestEnv,
-    env,
-    crate::env::sim_env::PriceAdapterSimEnv,
-    sim
-);
+mod sim_tests;
+mod test_env_tests;
