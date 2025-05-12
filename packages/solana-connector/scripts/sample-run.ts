a2ab5193c83f748490e8f1de20d9f973a34ae211
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-05-12 11:26:06 +0200

    [RDS-3068] solana simultaneuos fetching data fix (#5905)

--- packages/solana-connector/scripts/sample-run.ts
+++ packages/solana-connector/scripts/sample-run.ts
@@ -1,25 +1,18 @@
 import {
-  fetchChainConfigs,
-  getChainConfigByChainId,
-} from "@redstone-finance/chain-configs";
-import {
   ContractParamsProvider,
   getSignersForDataServiceId,
   sampleRun,
 } from "@redstone-finance/sdk";
-import { RedstoneCommon } from "@redstone-finance/utils";
 import "dotenv/config";
 import { hexlify } from "ethers/lib/utils";
-import _ from "lodash";
-import z from "zod";
 import {
-  CLUSTER_NAMES,
   readCluster,
   SolanaConnectionBuilder,
   SolanaContractConnector,
 } from "../src";
 import { readProgramAddress } from "./consts";
-import { readKeypair, readUrl } from "./utils";
+import { getRpcUrls } from "./get-rpc-urls";
+import { readKeypair } from "./utils";
 
 async function main() {
   const keypair = readKeypair();
@@ -41,23 +34,6 @@ async function main() {
   );
 
   await sampleRun(paramsProvider, solanaContractConnector);
-}
-
-async function getRpcUrls() {
-  if (
-    RedstoneCommon.getFromEnv(
-      "OVERRIDE_URLS_BY_ENV",
-      z.boolean().default(false)
-    )
-  ) {
-    return [readUrl()];
-  }
-
-  return getChainConfigByChainId(
-    await fetchChainConfigs(),
-    Number(_.findKey(CLUSTER_NAMES, (c) => c === readCluster())!),
-    "solana"
-  ).publicRpcUrls;
 }
 
 void main();
---
+++ packages/solana-connector/scripts/write-simultaneously.ts
@@ -0,0 +1,67 @@
+import {
+  ContractParamsProvider,
+  DataPackagesRequestParams,
+  DataPackagesResponseCache,
+  getSignersForDataServiceId,
+} from "@redstone-finance/sdk";
+import { RedstoneCommon } from "@redstone-finance/utils";
+import { hexlify } from "ethers/lib/utils";
+import {
+  readCluster,
+  SolanaConnectionBuilder,
+  SolanaContractConnector,
+} from "../src";
+import { readProgramAddress } from "./consts";
+import { getRpcUrls } from "./get-rpc-urls";
+import { readKeypair } from "./utils";
+
+async function prepareParamsProviderWithData(
+  requestParams: DataPackagesRequestParams
+) {
+  const cache = new DataPackagesResponseCache();
+  const paramsProvider = new ContractParamsProvider(requestParams, cache);
+  const data = await paramsProvider.requestDataPackages();
+  cache.update(data, requestParams);
+
+  return paramsProvider;
+}
+
+export async function writeSimultaneously() {
+  const keypair = readKeypair();
+  console.log("Public key:", hexlify(keypair.publicKey.toBytes()));
+  const rpcUrls = await getRpcUrls();
+  const connection = new SolanaConnectionBuilder().withRpcUrls(rpcUrls).build();
+
+  const requestParams: DataPackagesRequestParams = {
+    dataPackagesIds: ["ETH"],
+    dataServiceId: "redstone-primary-prod",
+    uniqueSignersCount: 3,
+    authorizedSigners: getSignersForDataServiceId("redstone-primary-prod"),
+    enableEnhancedLogs: true,
+  };
+
+  const solanaContractConnector = new SolanaContractConnector(
+    connection,
+    readProgramAddress(readCluster()),
+    keypair
+  );
+
+  const adapter = await solanaContractConnector.getAdapter();
+
+  const paramsProvider = await prepareParamsProviderWithData(requestParams);
+  await RedstoneCommon.sleep(10000);
+  const paramsProvider2 = await prepareParamsProviderWithData(requestParams);
+  await RedstoneCommon.sleep(3000);
+
+  await Promise.allSettled(
+    [paramsProvider2, paramsProvider].map((paramsProvider) =>
+      adapter.writePricesFromPayloadToContract(paramsProvider)
+    )
+  );
+}
+
+async function main() {
+  await writeSimultaneously();
+}
+
+void main();
