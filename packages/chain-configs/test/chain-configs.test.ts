b17be3f51c0d3f05b86c83d2a111ef772c69e51e
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-05-13 12:47:26 +0200

    feat: remove BBUSD and BBTC (#5914)

--- packages/chain-configs/test/chain-configs.test.ts
+++ packages/chain-configs/test/chain-configs.test.ts
@@ -21,7 +21,6 @@ const CHAINS_TO_SKIP_MULTICALL_ADDRESS_CHECK = [
   "zkSync",
   "zkLink",
   "Haven1 Testnet",
-  "BounceBit Mainnet",
   "TAC Turin",
   "Corn Maizenet", // first few create transactions failed leading to different address
   "Polygon Mainnet",
