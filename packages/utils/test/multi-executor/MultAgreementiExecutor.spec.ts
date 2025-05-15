792819c2ea4331aced7fd1c5214575b73a01bf76
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-05-15 09:47:56 +0200

    fix: multi-agreement executor resolves when the array is empty (#5963)

--- packages/utils/test/multi-executor/MultAgreementiExecutor.spec.ts
+++ packages/utils/test/multi-executor/MultAgreementiExecutor.spec.ts
@@ -52,7 +52,7 @@ describe("MultiAgreementExecutor", () => {
       [CLIENTS[0], CLIENTS[1], new ArrayMockClient([], 2, 3 * EXEC_TIME, true)],
       [
         new ArrayMockClient(["A", "B"], 0, EXEC_TIME * 2),
-        new ArrayMockClient([undefined, "B", "C"], 2, EXEC_TIME),
+        new ArrayMockClient([undefined, "B", "C"], 1, EXEC_TIME),
         new ArrayMockClient(["A", undefined, "C"], 2, 3 * EXEC_TIME),
       ],
     ]) {
@@ -73,12 +73,12 @@ describe("MultiAgreementExecutor", () => {
     for (const instances of [
       [
         new ArrayMockClient(["A"], 0, EXEC_TIME * 2),
-        new ArrayMockClient([undefined, "B", undefined], 2, EXEC_TIME),
+        new ArrayMockClient([undefined, "B", undefined], 1, EXEC_TIME),
         new ArrayMockClient([undefined, undefined, "C"], 2, 3 * EXEC_TIME),
       ],
       [
         new ArrayMockClient(UNDEF, 0, EXEC_TIME * 2),
-        new ArrayMockClient(UNDEF, 2, EXEC_TIME),
+        new ArrayMockClient(UNDEF, 1, EXEC_TIME),
         new ArrayMockClient(UNDEF, 2, 3 * EXEC_TIME),
       ],
     ]) {
@@ -97,7 +97,7 @@ describe("MultiAgreementExecutor", () => {
     for (const instances of [
       [
         new ArrayMockClient([], 0, EXEC_TIME * 2),
-        new ArrayMockClient([], 2, EXEC_TIME),
+        new ArrayMockClient([], 1, EXEC_TIME),
         new ArrayMockClient([], 2, 3 * EXEC_TIME),
       ],
     ]) {
@@ -116,23 +116,35 @@ describe("MultiAgreementExecutor", () => {
     for (const instances of [
       [
         new ArrayMockClient(ABC, 0, EXEC_TIME * 2),
-        new ArrayMockClient(["D", "B", undefined], 2, EXEC_TIME),
+        new ArrayMockClient(["D", "B", undefined], 1, EXEC_TIME),
         new ArrayMockClient(["E", undefined, "C"], 2, 3 * EXEC_TIME),
       ],
       [
         new ArrayMockClient([], 0, EXEC_TIME * 2),
-        new ArrayMockClient(["D", "B", "C"], 2, EXEC_TIME),
+        new ArrayMockClient(["D", "B", "C"], 1, EXEC_TIME),
         new ArrayMockClient(["E", "B", "C"], 2, 3 * EXEC_TIME),
       ],
       [
         new ArrayMockClient([], 0, EXEC_TIME),
-        new ArrayMockClient(["D", "B", "C"], 2, 2 * EXEC_TIME),
+        new ArrayMockClient(["D", "B", "C"], 1, 2 * EXEC_TIME),
         new ArrayMockClient(["E", "B", "C"], 2, 3 * EXEC_TIME),
       ],
       [
         new ArrayMockClient(ABC, 0, EXEC_TIME * 2),
-        new ArrayMockClient(["D", "B", "C"], 2, EXEC_TIME, true),
+        new ArrayMockClient(["D", "B", "C"], 1, EXEC_TIME, true),
         new ArrayMockClient(["E", "B", "C"], 2, 3 * EXEC_TIME),
+      ],
+      [
+        new ArrayMockClient(ABC, 0, EXEC_TIME * 2),
+        new ArrayMockClient([], 1, 1.5 * EXEC_TIME, true),
+        new ArrayMockClient([], 2, EXEC_TIME, true),
+        new ArrayMockClient(["E", "B", "C"], 3, 3 * EXEC_TIME),
+      ],
+      [
+        new ArrayMockClient(ABC, 0, EXEC_TIME * 2),
+        new ArrayMockClient([], 1, EXEC_TIME, true),
+        new ArrayMockClient([], 2, 1.5 * EXEC_TIME),
+        new ArrayMockClient(["E", "B", "C"], 3, 3 * EXEC_TIME),
       ],
     ]) {
       const sut = makeSut(instances, config, {
@@ -152,13 +164,74 @@ describe("MultiAgreementExecutor", () => {
   it("MultiAgreement should throw for unagreed values", async () => {
     const instances = [
       new ArrayMockClient(ABC, 0, EXEC_TIME * 2),
-      new ArrayMockClient(["D", "B", undefined], 2, EXEC_TIME),
+      new ArrayMockClient(["D", "B", undefined], 1, EXEC_TIME),
       new ArrayMockClient(["E", undefined, "C"], 2, 3 * EXEC_TIME),
     ];
     const sut = makeSut(instances, config);
 
     await expect(sut.someArrayFunction()).rejects.toThrowError(
       "Agreement failed: got max 1 equal result, needed at least 2"
+    );
+  });
+
+  it("MultiAgreement should throw when all results failed", async () => {
+    const instances = [
+      new ArrayMockClient([], 0, EXEC_TIME * 2, true),
+      new ArrayMockClient([], 1, EXEC_TIME, true),
+      new ArrayMockClient([], 2, 3 * EXEC_TIME, true),
+    ];
+    const sut = makeSut(instances, config);
+
+    await expect(sut.someArrayFunction()).rejects.toThrowError(
+      "MultiAgreement failed: got 0 successful results, needed at least 2"
+    );
+  });
+
+  it("MultiAgreement should throw when all results failed when shouldResolveUnagreedToUndefined is set to true", async () => {
+    const instances = [
+      new ArrayMockClient([], 0, EXEC_TIME * 2, true),
+      new ArrayMockClient([], 1, EXEC_TIME, true),
+      new ArrayMockClient([], 2, 3 * EXEC_TIME, true),
+    ];
+    const sut = makeSut(instances, config, {
+      ...DEFAULT_CONFIG,
+      multiAgreementShouldResolveUnagreedToUndefined: true,
+    });
+
+    await expect(sut.someArrayFunction()).rejects.toThrowError(
+      "MultiAgreement failed: got 0 successful results, needed at least 2"
+    );
+  });
+
+  it("MultiAgreement should throw when too less successful results", async () => {
+    const instances = [
+      new ArrayMockClient([], 0, EXEC_TIME * 2, true),
+      new ArrayMockClient([], 1, EXEC_TIME, true),
+      new ArrayMockClient(ABC, 2, 3 * EXEC_TIME),
+    ];
+    const sut = makeSut(instances, config, {
+      ...DEFAULT_CONFIG,
+      multiAgreementShouldResolveUnagreedToUndefined: true,
+    });
+
+    await expect(sut.someArrayFunction()).rejects.toThrowError(
+      "MultiAgreement failed: got 1 successful result, needed at least 2"
+    );
+  });
+
+  it("MultiAgreement should throw when too less successful results when shouldResolveUnagreedToUndefined is set to true", async () => {
+    const instances = [
+      new ArrayMockClient([], 0, EXEC_TIME * 2, true),
+      new ArrayMockClient([], 1, EXEC_TIME, true),
+      new ArrayMockClient(ABC, 2, 3 * EXEC_TIME),
+    ];
+    const sut = makeSut(instances, config, {
+      ...DEFAULT_CONFIG,
+      multiAgreementShouldResolveUnagreedToUndefined: true,
+    });
+
+    await expect(sut.someArrayFunction()).rejects.toThrowError(
+      "MultiAgreement failed: got 1 successful result, needed at least 2"
     );
   });
 });
