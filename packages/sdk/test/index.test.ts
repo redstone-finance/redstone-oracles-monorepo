d79722e5dcf21dfc5544c42fa17038fcbea087dd
robal <robert@warp.cc>
2025-05-15 09:55:43 +0200

    chore: remove "rapid" and "stocks" data services (#5915)

--- packages/sdk/test/index.test.ts
+++ packages/sdk/test/index.test.ts
@@ -4,6 +4,6 @@ describe("oracle registry state", () => {
   test("Should properly get oracle registry state", async () => {
     const state = await getOracleRegistryState();
     expect(Object.keys(state.dataServices).length).toBeGreaterThanOrEqual(9);
-    expect(state.dataServices).toHaveProperty("redstone-stocks-demo");
+    expect(state.dataServices).toHaveProperty("redstone-primary-prod");
   });
 });
