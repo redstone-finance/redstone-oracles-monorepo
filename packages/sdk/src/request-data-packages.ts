fb54aaefcd40aadf1e2e314362cfbd50222c2cff
robal <robert@warp.cc>
2025-05-16 16:56:42 +0200

    chore: remove garbage from @redstone-finance publilc packages (#5949)

--- packages/sdk/src/request-data-packages.ts
+++ packages/sdk/src/request-data-packages.ts
@@ -411,6 +411,7 @@ function sendRequestToGateway(url: string, pathComponents: string[]) {
     .concat(pathComponents)
     .join("/");
 
+  console.log(`ROBAL - request ${sanitizedUrl}`);
   return axios.get<Record<string, SignedDataPackagePlainObj[]>>(sanitizedUrl, {
     timeout: GET_REQUEST_TIMEOUT,
   });
