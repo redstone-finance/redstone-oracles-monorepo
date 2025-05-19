0c8d1c22e482e43a11cce98b0a1bf5279d7e7739
just_ppe <84311757+ppedziwiatr@users.noreply.github.com>
2025-05-19 12:45:23 +0200

    feat: enable healtcheck in relayer (#5941)
    https://redstone-team.atlassian.net/browse/RDS-3027
    
    ---------
    
    Co-authored-by: robal <robert@warp.cc>

--- packages/on-chain-relayer/src/main.ts
+++ packages/on-chain-relayer/src/main.ts
@@ -1,3 +1,7 @@
+import "dotenv/config";
+// note: this line is intentional, otherwise linter changes the order of imports
+// dotenv/config MUST be a very first import.
+
 import { runRelayer } from "./run-relayer";
 
 void runRelayer();
