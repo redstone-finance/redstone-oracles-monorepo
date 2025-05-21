23fc83d8fb9624d2194c17f6c4acad1d7cd6d2cb
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-05-21 11:48:50 +0200

    chore: Fix style (#6040)

--- packages/solana-connector/scripts/ledger/execute-config-tx.ts
+++ packages/solana-connector/scripts/ledger/execute-config-tx.ts
@@ -9,13 +9,13 @@ import { LEDGER_ACCOUNT, SQUAD_ADDRESS } from "./config";
 import { makeSolana } from "./ledger-utils";
 import { SquadsMultisig } from "./multi-sig-utils";
 
-async function execute_config_tx(squadAddress: PublicKey) {
+async function executeConfigTx(squadAddress: PublicKey) {
   const connection = makeConnection();
   const solanaLedger = await makeSolana(LEDGER_ACCOUNT);
   const squadUtils = new SquadsMultisig(squadAddress, connection);
   const publicKey = (await solanaLedger.getPublicKey()).ed;
 
-  const ix = await squadUtils.execute_config(publicKey, undefined);
+  const ix = await squadUtils.executeConfig(publicKey, undefined);
 
   const tx = new VersionedTransaction(
     new TransactionMessage({
@@ -29,4 +29,4 @@ async function execute_config_tx(squadAddress: PublicKey) {
   console.log(await connection.sendTransaction(tx));
 }
 
-void execute_config_tx(SQUAD_ADDRESS);
+void executeConfigTx(SQUAD_ADDRESS);
