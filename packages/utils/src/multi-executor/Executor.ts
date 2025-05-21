093ddfb6cb296eb145715808d05ea80b62c4bf4d
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-05-21 10:06:29 +0200

    feat: improve-multi-executor-logs (#6030)

--- packages/utils/src/multi-executor/Executor.ts
+++ packages/utils/src/multi-executor/Executor.ts
@@ -1,26 +1,52 @@
-import { timeoutOrResult } from "../common";
+import { stringify, stringifyError, timeoutOrResult } from "../common";
 import { loggerFactory } from "../logger";
 
 export type AsyncFn<R> = () => Promise<R>;
 
-export abstract class Executor<R> {
-  protected readonly logger = loggerFactory("executor");
+export type FnBox<R> = {
+  fn: AsyncFn<R>;
+  index: number;
+  name: string;
+  description?: string;
+};
 
-  static getPromises<R>(functions: AsyncFn<R>[], timeoutMs?: number) {
-    return functions.map((func) => timeoutOrResult(func(), timeoutMs));
+const logger = loggerFactory("executor");
+
+export abstract class Executor<R> {
+  protected readonly logger = logger;
+
+  static getPromises<R>(functions: FnBox<R>[], timeoutMs?: number) {
+    return functions.map((func) => this.execFn(func, timeoutMs));
   }
 
-  abstract execute(functions: AsyncFn<R>[]): Promise<R>;
+  static async execFn<R>(func: FnBox<R>, timeoutMs?: number) {
+    const date = Date.now();
+    const prefix = (result: string) =>
+      `[${func.name}] Promise #${func.index} ${result} in ${Date.now() - date} [ms]`;
+    const suffix = func.description ? ` (${func.description})` : "";
+
+    try {
+      const result = await timeoutOrResult(func.fn(), timeoutMs, "timed out");
+      logger.debug(`${prefix("returns")}: ${stringify(result)}${suffix}`);
+      return result;
+    } catch (error) {
+      logger.warn(
+        `${prefix("failed")}: ${stringifyError(error)}${suffix}`,
+        error
+      );
+
+      throw error;
+    }
+  }
+
+  abstract execute(functions: FnBox<R>[]): Promise<R>;
 }
 
 /** TODO: Possible extensions
- * implement instance names
  * implement config overrides for methods
  * implement quorumRatio/Number as functions
- * improve logging
- * improve error logging
  * add rpc curated list
  * change getModes to return single element (max or sth)
- * RateExecutor can inherit from ParallelExecutor
+ * RaceExecutor can inherit from ParallelExecutor
  * Promise batching
  */
