974a19c4c8733a698d71063cd01b25008cb9ff24
Asia <ziola.jm@gmail.com>
2025-05-21 15:45:15 +0200

    feat: celo - migration from baklava testnet to alfajores testnet (#6029)

--- packages/on-chain-relayer/scripts/master-manual-script/relayer-name-to-manual-key.ts
+++ packages/on-chain-relayer/scripts/master-manual-script/relayer-name-to-manual-key.ts
@@ -15,8 +15,7 @@ export const relayerNameToManualKeyArn: Record<string, string> = {
   etherlinkGhostnetTezosXtzEthBtc:
     "/prod/on-chain-relayer/etherlink-ghostnet/tezos/xtzethbtc/manual/private_key",
   mantaLayerBank: "/prod/on-chain-relayer/manta/layerbank/manual/private-key",
-  mentoBaklavaMultisig:
-    "/prod/on-chain-relayer/baklava/mento/testnet_private_key",
+  mentoAlfajoresMultisig: "/prod/on-chain-relayer/alfajores/mento/private-key",
   mentoCeloMainnet: "/prod/on-chain-relayer/mento/celo/private-key",
   merlinMerl:
     "/prod/on-chain-relayer/merlin/multi-price-feed/manual/private-key",
