19056e27b4cd28feabc119770b12b06ce27d5c6f
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-05-27 14:45:23 +0200

    chore: remove member (#6095)

---
+++ packages/solana-connector/scripts/ledger/remove-multi-sig-members.ts
@@ -0,0 +1,43 @@
+import {
+  PublicKey,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
+import "dotenv/config";
+import { parseTransaction } from "../tx-checks/check-tx";
+import { makeConnection } from "../utils";
+import { LEDGER_ACCOUNT, SQUAD_ADDRESS } from "./config";
+import { makeSolana } from "./ledger-utils";
+import { SquadsMultisig } from "./multi-sig-utils";
+
+const OLD_MEMBERS = [
+  new PublicKey("Fuq9VAhDahfaXodDne3zjmzYWZ7L92tmWSF7gPHVVXi1"),
+];
+const NEW_THRESHOLD = 2;
+
+async function removeMember(
+  squadAddress: PublicKey,
+  members: PublicKey[],
+  newThreshold: number
+) {
+  const connection = makeConnection();
+  const solanaLedger = await makeSolana(LEDGER_ACCOUNT);
+  const squadUtils = new SquadsMultisig(squadAddress, connection);
+  const publicKey = (await solanaLedger.getPublicKey()).ed;
+
+  const ix = await squadUtils.removeMembers(publicKey, members, newThreshold);
+
+  const tx = new VersionedTransaction(
+    new TransactionMessage({
+      payerKey: publicKey,
+      recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+      instructions: [ix],
+    }).compileToV0Message()
+  );
+
+  await parseTransaction(tx, connection, squadUtils);
+  await solanaLedger.signTransaction(tx);
+  console.log(await connection.sendTransaction(tx));
+}
+
+void removeMember(SQUAD_ADDRESS, OLD_MEMBERS, NEW_THRESHOLD);
