7e874974f634362fdc74c50768761ae731f021d6
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-05-30 20:05:40 +0200

    chore: solana chore (#6151)

--- packages/solana-connector/src/gas-oracles/AggressiveSolanaGasOracle.ts
+++ packages/solana-connector/src/gas-oracles/AggressiveSolanaGasOracle.ts
@@ -1,15 +1,11 @@
 import { loggerFactory } from "@redstone-finance/utils";
-import { Connection } from "@solana/web3.js";
 import { SolanaConfig } from "../config";
 import { ISolanaGasOracle } from "./ISolanaGasOracle";
 
 export class AggressiveSolanaGasOracle implements ISolanaGasOracle {
   private readonly logger = loggerFactory("solana-gas-oracle");
 
-  constructor(
-    private readonly connection: Connection,
-    private readonly config: SolanaConfig
-  ) {}
+  constructor(private readonly config: SolanaConfig) {}
 
   getPriorityFeePerUnit(iterationIndex: number): Promise<number> {
     const fee = Math.ceil(
