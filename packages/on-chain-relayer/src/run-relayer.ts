ba134c1725479c01b583a2a3e9d0d22a24b31023
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-06-02 11:19:14 +0200

    [RDS-3227] solana split relayer for independent iterations for feeds (#6117)
    * Allows to run a parallel relayer processing for the predefined subsets
    of feeds.
    * When no `feedsSplit` and `splitAllFeeds` env values are defined, the
    relayer runs one processing mechanism as previously.
    * Why the `splitAllFeeds` flag is not enough? 
    * Because I can imagine the big number of feeds of which some subset is
    more lazy.
    * Why the definition is not in manifest?
    * We want to introduce the behaviour for the main (solana) relayer only
    * It works best with mqtt, where also reduces the delay of waiting for
    other feeds
    * It could not work properly when an aggressive nonce agreement process
    is required
    * It has common quarantined list for all processes for solana/sui/radix
    * It doesn't synchronize or optimize other value-fetching mechanisms
    (like reading values from contract or agreeing the block number), the
    iterations are also independent.
    * Unit tests for splitting mechanism are written by ClaudeAI 4 OPUS -
    and this time I like them

--- packages/on-chain-relayer/src/run-relayer.ts
+++ packages/on-chain-relayer/src/run-relayer.ts
@@ -1,6 +1,12 @@
 import { RedstoneHealthcheck } from "@redstone-finance/healthcheck";
-import { loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
+import {
+  loggerFactory,
+  RedstoneCommon,
+  RedstoneLogger,
+} from "@redstone-finance/utils";
 import { config, ConsciouslyInvoked } from "./config/config";
+import { RelayerConfig } from "./config/RelayerConfig";
+import { splitRelayerConfig } from "./config/split-relayer-config";
 import { timelyOverrideSinceLastUpdate } from "./config/timely-override-since-last-update";
 import { AsyncTaskRunner } from "./runner/AsyncTaskRunner";
 import { MqttRunner } from "./runner/MqttRunner";
@@ -18,6 +24,17 @@ export const runRelayer = async () => {
     )}`
   );
 
+  const configs = splitRelayerConfig(relayerConfig);
+  if (configs[0] !== relayerConfig) {
+    logger.log(
+      `Splitting relayer config into ${configs.length} configs: [${configs.map((config) => `[${config.dataFeeds.toString()}]`).toString()}]`
+    );
+  }
+
+  configs.forEach((config) => run(config, logger));
+};
+
+function run(relayerConfig: RelayerConfig, logger: RedstoneLogger) {
   if (relayerConfig.temporaryUpdatePriceInterval !== -1) {
     timelyOverrideSinceLastUpdate(
       relayerConfig,
@@ -33,10 +50,9 @@ export const runRelayer = async () => {
         `Unhandled Rejection at: ${RedstoneCommon.stringifyError(reason)}`
     );
   });
-
   if (relayerConfig.runWithMqtt) {
     void MqttRunner.run(relayerConfig);
   } else {
     void AsyncTaskRunner.run(relayerConfig, logger);
   }
-};
+}
