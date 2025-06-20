60340b49a7dd1e997a155a7d63edc4a9e792682f
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-06-20 11:06:28 +0200

    [RDS-3350] sui change on chain configuration (#6294)

--- packages/sui-connector/scripts/ledger/transfer-object.ts
+++ packages/sui-connector/scripts/ledger/transfer-object.ts
@@ -3,7 +3,7 @@ import { generateTransactionData } from "./generate-transaction-data";
 const OBJECT_ID_TO_TRANSFER =
   "0xe6d8c0181883f4892154c8702743c4107933202680be21b2f1a7c8bf0488f38f";
 const DESTINATION_ADDRESS =
-  "0xab1bf56ae7e13625027ee67e028deda37669b9f77f65465db2286c87a6166bcd";
+  "0xf692064687942408782cbd4ca33bee081852a75c1985082daac1cf27c563bfc9";
 
 void generateTransactionData((tx) =>
   tx.transferObjects([OBJECT_ID_TO_TRANSFER], DESTINATION_ADDRESS)
