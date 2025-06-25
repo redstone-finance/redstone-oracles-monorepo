e5a914a7d5bbe108592dc2b343272b7ddc388b4e
just_ppe <84311757+ppedziwiatr@users.noreply.github.com>
2025-06-25 15:45:45 +0200

    chore: increase iteration period in monitoring (#6371)
    300s is not enough.
    proof:
    https://eu-west-2.console.aws.amazon.com/cloudwatch/home?region=eu-west-2#logsV2:logs-insights$3FqueryDetail$3D~(end~0~start~-43200~timeType~'RELATIVE~tz~'UTC~unit~'seconds~editorString~'filter*20*40logStream*20*3d*20*27dev-dev-monitoring*2fdev-dev-monitoring-container*2f87aef7bcd93545e58aad2cd73d1ea7d2*27*0a*20*7c*20fields*20*40timestamp*2c*20*40message*0a*20*7c*20filter*20*40message*20like*20*27Running*20dev*20tests*27*0a*20*7c*20sort*20*40timestamp*20desc*0a*0a~queryId~'3bc86ab6-f571-49a7-b8e3-d0b43b37ef41~source~(~'dev-dev-monitoring-log-group)~lang~'CWLI)
    
    during ~5minutes (18:32-18:37) iteration was run only once (and haven't
    completed in this time). As a result, the iteration healthcheck was
    reporting unhealthy status and the task is restarted.

--- packages/healthcheck/src/monitor/IterationsHealthCheck.ts
+++ packages/healthcheck/src/monitor/IterationsHealthCheck.ts
@@ -10,78 +10,55 @@ import {
 const logger = loggerFactory("IterationHealthCheck");
 
 /**
- * Health check implementation that monitors how many iteration events
- * have occurred within a defined period to determine system health.
+ * Health check implementation that monitors when the last iteration
+ * has occurred.
  *
  * This health check is useful for ensuring that a background process
  * or loop is running with expected frequency and consistency.
  *
  * Environment variables used (must be set):
- * - `HEALTHCHECK_ITERATION_PERIOD_S`: Period (in seconds) during which iterations are counted.
- * - `HEALTHCHECK_ITERATION_MIN_REQUIRED_ITERATIONS`: Minimum number of iterations required within the period.
- * - `HEALTHCHECK_ITERATION_START_PERIOD_S`: (Optional) Startup grace period (in seconds); defaults to 30s.
+ * - `HEALTHCHECK_ITERATION_PERIOD_S`: Period (in seconds) during which iteration should occur.
  */
 export class IterationsHealthCheck implements HealthCheck {
-  // latest iteration times at the beginning
-  private readonly iterationsDescTimes: number[] = [];
   private readonly params = Object.freeze({
     periodInS: RedstoneCommon.getFromEnv(
       "HEALTHCHECK_ITERATION_PERIOD_S",
       z.number() // no sane default
     ),
-    minRequiredIterations: RedstoneCommon.getFromEnv(
-      "HEALTHCHECK_ITERATION_MIN_REQUIRED_ITERATIONS",
-      z.number() // no sane default
-    ),
-    startPeriodInS: RedstoneCommon.getFromEnv(
-      "HEALTHCHECK_ITERATION_START_PERIOD_S",
-      z.number().default(30)
-    ),
   });
 
   private readonly startDate = new Date();
+  private lastIterationDate: Date | null = null;
 
   check(fireDate: Date): Promise<HealthCheckResult> {
-    if (
-      IterationsHealthCheck.secondsBetween(this.startDate, fireDate) <=
-      this.params.startPeriodInS
-    ) {
-      logger.debug("Iteration check within startup period");
+    const { periodInS } = this.params;
+    const referenceDate = this.lastIterationDate ?? this.startDate;
+    const elapsed = IterationsHealthCheck.secondsBetween(
+      referenceDate,
+      fireDate
+    );
+    const isStartup = this.lastIterationDate == null;
+
+    if (elapsed <= periodInS) {
+      if (isStartup) {
+        logger.debug("Iteration check within startup period");
+      } else {
+        logger.info(
+          `Last iteration (${this.lastIterationDate?.toISOString()}) within ${periodInS}s`
+        );
+      }
       return healthy();
     }
 
-    const checkPeriodStartMs =
-      fireDate.getTime() - this.params.periodInS * 1000;
-    const pruned = this.pruneOlderThan(checkPeriodStartMs);
-    logger.debug(`Pruned ${pruned} oldest iteration entries`);
-    const iterationsWithinPeriod = this.iterationsDescTimes.length;
-
-    const msg = `${iterationsWithinPeriod} iterations registered within last ${this.params.periodInS}s, required: ${this.params.minRequiredIterations}`;
-    if (iterationsWithinPeriod >= this.params.minRequiredIterations) {
-      logger.info(msg);
-      return healthy();
-    } else {
-      return unhealthy([msg]);
-    }
+    const msg = isStartup
+      ? "Still no iteration registered"
+      : `Last iteration (${this.lastIterationDate?.toISOString()}) NOT within ${periodInS}s`;
+    return unhealthy([msg]);
   }
 
   registerIteration() {
     logger.info("Registering iteration");
-    this.iterationsDescTimes.unshift(Date.now());
-  }
-
-  private pruneOlderThan(oldestAllowedIterationTime: number): number {
-    let removed = 0;
-    while (this.iterationsDescTimes.length > 0) {
-      const currentOldestTime = this.iterationsDescTimes.at(-1)!;
-      if (currentOldestTime < oldestAllowedIterationTime) {
-        this.iterationsDescTimes.pop();
-        removed++;
-      } else {
-        break;
-      }
-    }
-    return removed;
+    this.lastIterationDate = new Date();
   }
 
   private static secondsBetween(earlier: Date, later: Date): number {
