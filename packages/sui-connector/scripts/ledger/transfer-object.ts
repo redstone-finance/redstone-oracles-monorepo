db0d15652f365b092780cea8ed86bc30fe4171dc
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-06-25 10:26:35 +0200

    [RDS-3423] sui solana update multisig setup (#6366)

--- packages/sui-connector/scripts/ledger/transfer-object.ts
+++ packages/sui-connector/scripts/ledger/transfer-object.ts
@@ -3,7 +3,7 @@ import { generateTransactionData } from "./generate-transaction-data";
 const OBJECT_ID_TO_TRANSFER =
   "0xe6d8c0181883f4892154c8702743c4107933202680be21b2f1a7c8bf0488f38f";
 const DESTINATION_ADDRESS =
-  "0xf692064687942408782cbd4ca33bee081852a75c1985082daac1cf27c563bfc9";
+  "0x267476be4ee60b4f93682cd41660d18cac33ef11d4cc50bf1241b707549e4e83";
 
 void generateTransactionData((tx) =>
   tx.transferObjects([OBJECT_ID_TO_TRANSFER], DESTINATION_ADDRESS)
