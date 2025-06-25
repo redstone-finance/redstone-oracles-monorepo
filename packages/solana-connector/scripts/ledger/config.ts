6456fcda999dc98f2dd71c2bc562955b79a6e74b
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-06-25 10:26:35 +0200

    [RDS-3423] sui solana update multisig setup (#6366)

--- packages/solana-connector/scripts/ledger/config.ts
+++ packages/solana-connector/scripts/ledger/config.ts
@@ -3,11 +3,13 @@ import { makeConnection } from "../utils";
 import { SquadsMultisig } from "./multi-sig-utils";
 
 export const LEDGER_ACCOUNT = 3;
-export const THRESHOLD = 2;
+export const THRESHOLD = 3;
 export const MULTI_SIG_PUBLIC_KEYS = [
   "b3cf8140d542beba4ac7b492afbc427a5441e643ff6631c9423ba7892dc2cf96",
   "c31f0e40ec3c164388a270bf0790f8d3da165d53436019663c8d643051edaab2",
-  "dd8c421c0c5753deea9865e51d6c1c52e9fe28265cc2a06bc841b3a6c48058a2",
+  "97b6920a47beae087f4f1e7470ec14ac134a373a47bb2dfecfa1aac3efebe105",
+  "c1cd8221a4166dc33b845563a4ada000860379d36b3f72e6635017fd0bfcee0e",
+  "6613f8d286f38da095d7b4e7fd932aee14f5649feed780c28f043ab8bf4324cf",
 ];
 
 export const SQUAD_ADDRESS = new PublicKey(
