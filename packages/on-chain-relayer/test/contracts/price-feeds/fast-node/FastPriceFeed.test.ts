31d2b885a85fc65bf301cc6d69429e7e56e38bab
Michał Szynwelski <szynek@redstone.finance>
2025-07-08 22:42:38 +0200

    Contracts for perun (#6161)
    A new adapter and price feed for Perun implementing the existing
    interfaces.
    
    In the adapter, we assume that:
    - price updates can be made by 5 hardcoded updaters,
    - updates may occur frequently, including multiple times within the same
    block,
    - we store price timestamps and block timestamps in microseconds,
    - we support the concept of rounds; rounds are calculated with each
    price update for a given feed,
    - the price for a round is calculated according to the following
    algorithm: we take the 3 most recent prices out of 5, calculate their
    median, and if the newest price does not deviate from the median by more
    than 1 percent, it is returned; otherwise, the median is returned.
    
    The contract is optimized for gas efficiency. Alternatives I tested
    include:
    - other aggregation algorithms (the best one turned out to be the one
    minimizing the number of comparisons),
    - different methods of storing/reading data (using store/load in
    assembly).
    
    ---------
    
    Co-authored-by: Hatskier <Suvorovalex1997@gmail.com>

---
+++ packages/on-chain-relayer/test/contracts/price-feeds/fast-node/FastPriceFeed.test.ts
@@ -0,0 +1,71 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import {
+  FastMultiFeedAdapter,
+  FastPriceFeedMock,
+} from "../../../../typechain-types";
+import { deployAdapter, updateByAllNodes } from "./FastMultiFeedAdapter.test";
+
+const DATA_FEED_ID = ethers.utils.formatBytes32String("ETH");
+
+async function deployFastPriceFeed(
+  adapter: FastMultiFeedAdapter
+): Promise<FastPriceFeedMock> {
+  const factory = await ethers.getContractFactory("FastPriceFeedMock");
+  const feed = await factory.deploy(adapter.address, DATA_FEED_ID);
+  await feed.deployed();
+  return feed;
+}
+
+describe("FastPriceFeed", function () {
+  let adapter: FastMultiFeedAdapter;
+  let feed: FastPriceFeedMock;
+
+  beforeEach(async () => {
+    adapter = await deployAdapter();
+    feed = await deployFastPriceFeed(adapter);
+  });
+
+  it("should return latestAnswer as int256 price", async () => {
+    const prices = [1230, 1232, 1233, 1234, 1235];
+    await updateByAllNodes(adapter, prices);
+    const answer = await feed.latestAnswer();
+    expect(answer).to.equal(ethers.utils.parseUnits("1235", 8));
+  });
+
+  it("should return correct round data", async () => {
+    const prices = [5675, 5676, 5677, 5678, 5679];
+    const { blockTimestamps } = await updateByAllNodes(adapter, prices);
+    const roundId = await feed.latestRound();
+    const data = await feed.getRoundData(roundId);
+
+    expect(data[0]).to.equal(roundId); // roundId
+    expect(data[1]).to.equal(ethers.utils.parseUnits("5679", 8)); // answer
+    expect(data[2].toNumber()).to.equal(blockTimestamps[4] * 1_000_000); // blockTimestamp
+    expect(data[3].toNumber()).to.equal(blockTimestamps[4] * 1_000_000); // blockTimestamp
+    expect(data[4]).to.equal(roundId); // answeredInRound
+  });
+
+  it("should revert if getRoundData is called with invalid round", async () => {
+    await expect(feed.getRoundData(1)).to.be.revertedWithCustomError(
+      adapter,
+      "RoundIdTooHigh"
+    );
+  });
+
+  it("should return latestRoundData correctly", async () => {
+    const prices = [10002, 10001, 10000, 9999, 9998];
+    const { blockTimestamps } = await updateByAllNodes(adapter, prices);
+    const data = await feed.latestRoundData();
+    expect(data[0]).to.equal(1); // roundId
+    expect(data[1]).to.equal(ethers.utils.parseUnits("9998", 8));
+    expect(data[2].toNumber()).to.equal(blockTimestamps[4] * 1_000_000); // blockTimestamp
+    expect(data[3].toNumber()).to.equal(blockTimestamps[4] * 1_000_000); // blockTimestamp
+    expect(data[0]).to.equal(1); // answeredInRound
+  });
+
+  it("should return correct decimals and version", async () => {
+    expect(await feed.decimals()).to.equal(8);
+    expect(await feed.version()).to.equal(1);
+  });
+});
