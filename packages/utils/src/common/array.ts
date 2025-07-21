8564fa4d70029032dbcf8ae5c9d4f774c9e3f86e
Hatskier <Suvorovalex1997@gmail.com>
2025-07-21 15:33:11 +0200

    feat: more checks in status lambda (#6424)

--- packages/utils/src/common/array.ts
+++ packages/utils/src/common/array.ts
@@ -1,3 +1,10 @@
 export const removeDuplicates = <T>(arr: T[]): T[] => {
   return Array.from(new Set(arr));
 };
+
+export const getFirstAndOnly = <T>(arr: T[]): T => {
+  if (arr.length !== 1) {
+    throw new Error(`The array length must be 1. Real length: ${arr.length}`);
+  }
+  return arr[0];
+};
