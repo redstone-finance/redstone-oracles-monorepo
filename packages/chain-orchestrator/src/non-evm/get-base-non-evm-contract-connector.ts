7d8d75f77393d208071908a894d9e5d027aa053a
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-07-21 20:43:48 +0200

    [RDS-3767]: Add aptos type (#6633)

--- packages/chain-orchestrator/src/non-evm/get-base-non-evm-contract-connector.ts
+++ packages/chain-orchestrator/src/non-evm/get-base-non-evm-contract-connector.ts
@@ -1,5 +1,6 @@
 import {
   AptosClientBuilder,
+  MovementClientBuilder,
   MovementContractConnector,
 } from "@redstone-finance/movement-connector";
 import {
@@ -34,6 +35,13 @@ export function getBaseNonEvmContractConnector(
           .build()
       );
     case "movement":
+      return new MovementContractConnector(
+        new MovementClientBuilder()
+          .withNetworkId(networkId)
+          .withRpcUrls(rpcUrls)
+          .build()
+      );
+    case "aptos":
       return new MovementContractConnector(
         new AptosClientBuilder()
           .withNetworkId(networkId)
