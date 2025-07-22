1b5c5c0c629274d95a03bb36342a2ef2e0331ebf
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-07-22 14:33:15 +0200

    [RDS-3673] & [RDS-3675]: stellar contract connector and adapter (#6628)

---
+++ packages/stellar-connector/scripts/sample-deploy.ts
@@ -0,0 +1,19 @@
+import { ContractDeployer } from "../src";
+import { makeKeypair, makeServer, saveAdapterId } from "./utils";
+
+const WASM_FILEPATH =
+  "./stellar/target/wasm32v1-none/release/redstone_adapter.wasm";
+
+async function sampleDeploy() {
+  const server = makeServer();
+  const keypair = makeKeypair();
+
+  const deployer = new ContractDeployer(server, keypair, WASM_FILEPATH);
+
+  const deployResult = await deployer.deploy();
+
+  console.log(`🚀 contract deployed at: ${deployResult.contractId}`);
+  saveAdapterId(deployResult.contractId);
+}
+
+void sampleDeploy();
