76292b1035c77d52d5b135c5dfae8066fcdf5530
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-07-31 10:59:27 +0200

    Deploy stellar contract inside tests

--- packages/stellar-connector/test/StellarContractConnector.spec.ts
+++ packages/stellar-connector/test/StellarContractConnector.spec.ts
@@ -1,30 +1,36 @@
 import { RedstoneCommon } from "@redstone-finance/utils";
 import { Keypair, rpc } from "@stellar/stellar-sdk";
 import { z } from "zod";
-import { loadAdapterId } from "../scripts/utils";
+import { wasmFilePath } from "../scripts/utils";
 import {
+  ContractDeployer,
   StellarContractAdapter,
   StellarPriceAdapterContractConnector,
 } from "../src";
 
+const DEPLOY_CONTRACT_TIMEOUT_MS = 20 * 1_000;
+
 describe("StellarContractConnector", () => {
   let connector: StellarPriceAdapterContractConnector;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     const rpcUrl = RedstoneCommon.getFromEnv("RPC_URL", z.string().url());
     const server = new rpc.Server(rpcUrl, { allowHttp: true });
 
     const privateKey = RedstoneCommon.getFromEnv("PRIVATE_KEY", z.string());
     const keypair = Keypair.fromSecret(privateKey);
 
-    const adapterId = loadAdapterId();
+    const deployer = new ContractDeployer(server, keypair);
+    const { contractId: adapterId } = await deployer.deploy(
+      wasmFilePath("redstone_adapter")
+    );
 
     connector = new StellarPriceAdapterContractConnector(
       server,
       keypair,
       adapterId
     );
-  });
+  }, DEPLOY_CONTRACT_TIMEOUT_MS);
 
   describe("getBlockNumber", () => {
     it("should get block number", async () => {
