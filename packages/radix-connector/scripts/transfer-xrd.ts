05aa76b5bfdc7b194012832cf5683a5a42f90b54
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2025-08-01 08:48:53 +0200

    fix: transfer radix script (#6748)

--- packages/radix-connector/scripts/transfer-xrd.ts
+++ packages/radix-connector/scripts/transfer-xrd.ts
@@ -1,12 +1,12 @@
 import { RadixEngineToolkit } from "@radixdlt/radix-engine-toolkit";
 import { TransferXRDRadixMethod } from "../src/methods/TransferXRDRadixMethod";
-import { makeRadixClient, NETWORK, PRIVATE_KEY } from "./constants";
+import { makeRadixClient, NETWORK, NETWORK_ID, PRIVATE_KEY } from "./constants";
 
 async function transfer(toAddress: string, amount: number) {
   if (!PRIVATE_KEY) {
     throw new Error("No PRIVATE_KEY is defined in the .env file");
   }
-  const client = makeRadixClient();
+  const client = makeRadixClient(NETWORK_ID);
 
   const addressBook = await RadixEngineToolkit.Utils.knownAddresses(NETWORK.id);
   const fromAddress = await client.getAccountAddress();
@@ -23,7 +23,7 @@ async function transfer(toAddress: string, amount: number) {
 
 async function main() {
   await transfer(
-    "account_rdx1687dkg4a7v7qmhrad347kt5zrpsmpnqzcafuq9lzzau98l357hqqr7",
+    "account_rdx16xrpmhnzphjdpcd326wz42wdxyf28t9j2e4axn6t8p4au2vrgrdtxp",
     100
   );
 }
