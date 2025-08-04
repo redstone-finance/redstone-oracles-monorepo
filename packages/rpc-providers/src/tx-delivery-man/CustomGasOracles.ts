a724f1f179018d216bf1a10e43c7198e69ce971c
Michal_Konopka <konopek68@gmail.com>
2025-08-04 16:37:37 +0200

    feat: remove custom gas oracle for ethereum (#6784)

--- packages/rpc-providers/src/tx-delivery-man/CustomGasOracles.ts
+++ packages/rpc-providers/src/tx-delivery-man/CustomGasOracles.ts
@@ -1,13 +1,11 @@
 import { GasOracleFn } from "./TxDelivery";
 import { blockscoutStatsGasOracle } from "./custom-gas-oracles/blockscout-stats-gas-oracle";
-import { getEthFeeFromGasOracle } from "./custom-gas-oracles/ethereum";
 import { kavaGasOracle } from "./custom-gas-oracles/kava";
 import { merlinGasOracle } from "./custom-gas-oracles/merlin";
 
 const ETHERLINK_GHOSTNET_BLOCKSCOUT_EXPLORER_BASE_URL = `https://testnet.explorer.etherlink.com`;
 
 export const CHAIN_ID_TO_GAS_ORACLE = {
-  1: getEthFeeFromGasOracle,
   2222: kavaGasOracle,
   128123: blockscoutStatsGasOracle(
     ETHERLINK_GHOSTNET_BLOCKSCOUT_EXPLORER_BASE_URL
