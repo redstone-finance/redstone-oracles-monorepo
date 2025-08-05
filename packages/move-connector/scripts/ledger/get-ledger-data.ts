78c8fb21b294b3eb44f4f1e890d826d9f53b075d
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-08-05 10:11:56 +0200

    [RDS-3923] Ledger scripts aptos/movement (#6761)

--- packages/move-connector/scripts/ledger/get-ledger-data.ts
+++ packages/move-connector/scripts/ledger/get-ledger-data.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+import { LEDGER_ACCOUNT_ID } from "./const";
 import { getLedgerData, makeAptosLedger } from "./ledger-utils";
 
 export const main = async (accountId = 0) => {
@@ -9,4 +11,4 @@ export const main = async (accountId = 0) => {
 // // Connect the ledger
 // // Run the Sui application on it or rerun it when the ledger has gone into sleep mode
 // // WARN ON THE account ID (Default set to 0, as the first account)
-main().catch((err) => console.log(err));
+main(LEDGER_ACCOUNT_ID).catch((err) => console.log(err));
