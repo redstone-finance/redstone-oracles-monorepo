182d3ec62f883abba23e1e224c4d094beb9ebb8d
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-08-05 10:11:56 +0200

    [RDS-3923] Ledger scripts aptos/movement (#6761)

--- packages/move-connector/scripts/ledger/execute-as-ledger.ts
+++ packages/move-connector/scripts/ledger/execute-as-ledger.ts
@@ -1,6 +1,5 @@
 import { AccountAddress, Aptos, SimpleTransaction } from "@aptos-labs/ts-sdk";
 import { handleTxAsLedger, makeAptos } from "../utils";
-import { LEDGER_ACCOUNT_ID } from "./const";
 import { getLedgerData, makeAptosLedger } from "./ledger-utils";
 
 export async function executeAsLedger(
@@ -8,14 +7,12 @@ export async function executeAsLedger(
     aptos: Aptos,
     signerAddress: AccountAddress
   ) => Promise<SimpleTransaction>,
-  accountId = LEDGER_ACCOUNT_ID
+  accountId: number
 ) {
   const aptosLedger = await makeAptosLedger();
   const aptos = makeAptos();
   const data = await getLedgerData(aptosLedger, accountId);
-  await aptos.getAccountInfo({
-    accountAddress: AccountAddress.from(data.address),
-  });
+
   const tx = await txCreator(aptos, AccountAddress.from(data.address));
 
   return await handleTxAsLedger(aptos, aptosLedger, tx, accountId);
