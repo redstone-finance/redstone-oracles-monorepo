06d98a89c296b532995f410a658912ecb76149dc
Hatskier <Suvorovalex1997@gmail.com>
2025-08-06 18:55:31 +0200

    chore: chain-agnostic oracle tests configuration (#6756)
    - What has changed (don't be specific about files)? **Added
    chain-agnostic-tests package, added an ability to create empty data
    packages and data packages with duplicated data feeds in SDK to improve
    test coverage, added toOrdinal function in utils**
    - Why was the change done? - **to kick of implementation of
    chain-agnostic tests on non-EVM and EVM chains (to ensure our oracles
    work in the same way on all chains)**
    - Potential risks or what should be double checked before release? -
    **Please review the SDK changes. If you have any ideas what types of
    tests should be added - please comment, and I will add them in the
    following PRs**

---
+++ packages/chain-agnostic-oracle-tests/src/pull/corruptedPayloadTestCases.ts
@@ -0,0 +1,26 @@
+import { basicPullModelTestConfig } from "../common";
+
+const REDSTONE_MARKER_HEX = "000002ed57011e0000";
+
+// Assertion just in case someone tries to change the basic test config
+if (!basicPullModelTestConfig.expectedSuccess) {
+  throw new Error(`Basic test should describe successful path`);
+}
+
+export const corruptedPayloadTestCases = {
+  "Should work with valid payload": basicPullModelTestConfig,
+  "No redstone marker in the end should revert": {
+    ...basicPullModelTestConfig,
+    expectedSuccess: false,
+    payloadGenerator: (timestamp: number) => {
+      const originalPayload =
+        basicPullModelTestConfig.payloadGenerator(timestamp);
+      if (!originalPayload.endsWith(REDSTONE_MARKER_HEX)) {
+        throw new Error(
+          `Expected to get a payload with redstone marker in the end. Got: ${originalPayload}`
+        );
+      }
+      return originalPayload.slice(0, REDSTONE_MARKER_HEX.length);
+    },
+  },
+};
