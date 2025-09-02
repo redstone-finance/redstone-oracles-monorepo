8eb38acc35d7af0bbda9b26d88a4a941a756303d
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-09-02 11:09:45 +0200

    [RDS-4261] aptos use aptos libs instead of movement (#7076)

--- packages/move-connector/scripts/get-env.ts
+++ packages/move-connector/scripts/get-env.ts
@@ -1,11 +1,18 @@
 import { RedstoneCommon } from "@redstone-finance/utils";
 import { z } from "zod";
-
+import { AptosAndMovementNetwork } from "../src/network-ids";
 import { MoveNetworkSchema } from "./config";
 import { ContractNameEnum } from "./contract-name-enum";
 
 export function getEnvNetwork() {
   return RedstoneCommon.getFromEnv("NETWORK", MoveNetworkSchema);
+}
+
+export function getEnvNetworkEnum() {
+  return RedstoneCommon.getFromEnv(
+    "NETWORK",
+    z.nativeEnum(AptosAndMovementNetwork)
+  );
 }
 
 export function getEnvContractName() {
