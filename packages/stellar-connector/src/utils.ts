365b0562638c6850ec52bc918736bc8f2b4f0588
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-09-03 10:01:48 +0200

    [RDS-4263] feat: use hex keys in stellar (#7101)

--- packages/stellar-connector/src/utils.ts
+++ packages/stellar-connector/src/utils.ts
@@ -2,12 +2,23 @@ import { RedstoneCommon } from "@redstone-finance/utils";
 import { Keypair } from "@stellar/stellar-sdk";
 import { z } from "zod";
 
+const STELLAR_SECRET_KEY_PREFIX = "S";
+
 export function makeKeypair(privateKey = readPrivateKey()) {
   try {
-    return Keypair.fromSecret(privateKey);
+    if (privateKey.startsWith(STELLAR_SECRET_KEY_PREFIX)) {
+      return Keypair.fromSecret(privateKey);
+    }
+
+    return Keypair.fromRawEd25519Seed(
+      Buffer.from(
+        privateKey.startsWith("0x") ? privateKey.substring(2) : privateKey,
+        "hex"
+      )
+    );
   } catch (e) {
     throw new Error(
-      `Wrong secret key format: ${RedstoneCommon.stringifyError(e)});`
+      `Wrong secret key format: ${RedstoneCommon.stringifyError(e)}`
     );
   }
 }
