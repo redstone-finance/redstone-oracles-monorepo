bb4c2d1fab55b16fc0a0082d07d85a85484e317a
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-09-09 14:57:51 +0200

    Changes from PR #7164

--- packages/stellar-connector/src/adapters/StellarContractConnector.ts
+++ packages/stellar-connector/src/adapters/StellarContractConnector.ts
@@ -1,10 +1,15 @@
 import { IContractConnector } from "@redstone-finance/sdk";
+import { Keypair } from "@stellar/stellar-sdk";
 import { StellarRpcClient } from "../stellar/StellarRpcClient";
+import { StellarSigner } from "../stellar/StellarSigner";
 
 export class StellarContractConnector<Adapter>
   implements IContractConnector<Adapter>
 {
-  constructor(private readonly rpcClient: StellarRpcClient) {}
+  constructor(
+    private readonly rpcClient: StellarRpcClient,
+    private readonly keypair?: Keypair
+  ) {}
 
   getAdapter(): Promise<Adapter> {
     throw new Error("Must be implemented in a subclass");
@@ -28,5 +33,16 @@ export class StellarContractConnector<Adapter>
     const balance = await this.rpcClient.getAccountBalance(address);
 
     return balance * (10n ** 18n / 10n ** 7n);
+  }
+
+  async transfer(toAddress: string, amountInXlm: number) {
+    if (!this.keypair) {
+      throw new Error("Keypair is missing");
+    }
+    await this.rpcClient.transferXlm(
+      new StellarSigner(this.keypair),
+      toAddress,
+      amountInXlm
+    );
   }
 }
