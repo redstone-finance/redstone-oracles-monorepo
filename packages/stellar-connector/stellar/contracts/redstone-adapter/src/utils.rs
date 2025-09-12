08f9f1fa6286536212f2cf7dbec554c7de11386b
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-09-12 14:08:15 +0200

    chore: stellar bump sdk and staleness check (#7210)

--- packages/stellar-connector/stellar/contracts/redstone-adapter/src/utils.rs
+++ packages/stellar-connector/stellar/contracts/redstone-adapter/src/utils.rs
@@ -1,5 +1,7 @@
-use redstone::FeedId;
+use redstone::{FeedId, TimestampMillis};
 use soroban_sdk::{Env, String};
+
+const MS_IN_SEC: u64 = 1_000;
 
 pub fn feed_to_string(env: &Env, feed: FeedId) -> String {
     let feed_bytes = feed.to_array();
@@ -7,4 +9,8 @@ pub fn feed_to_string(env: &Env, feed: FeedId) -> String {
     let non_zero = feed_bytes.split(|byte| *byte == 0).next().unwrap();
 
     String::from_bytes(env, non_zero)
+}
+
+pub fn now(env: &Env) -> TimestampMillis {
+    TimestampMillis::from_millis(env.ledger().timestamp() * MS_IN_SEC)
 }
