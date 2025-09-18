f671e4490184aa51056b75c5dc65f7e77ff9dc21
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-09-18 10:35:44 +0200

    Bump BASE_FEE to 1000 in txDeliveryMan (#7285)

--- packages/stellar-connector/src/stellar/StellarTxDeliveryManConfig.ts
+++ packages/stellar-connector/src/stellar/StellarTxDeliveryManConfig.ts
@@ -2,6 +2,7 @@ const STROOPS_IN_XLM = 10_000_000;
 const LEDGER_TIME_MS = 5_000;
 
 const DEFAULT_GAS_LIMIT_STROOPS = 1 * STROOPS_IN_XLM;
+const DEFAULT_GAS_BASE_STROOPS = 1000;
 const DEFAULT_GAS_MULTIPLIER = 1.4;
 const DEFAULT_MAX_TX_SEND_ATTEMPTS = 5;
 
@@ -9,6 +10,7 @@ export const WAIT_BETWEEN_MS = LEDGER_TIME_MS + 1_000;
 
 export interface StellarTxDeliveryManConfig {
   gasLimit: number;
+  gasBase: number;
   gasMultiplier: number;
   maxTxSendAttempts: number;
   expectedTxDeliveryTimeInMS?: number;
@@ -17,6 +19,7 @@ export interface StellarTxDeliveryManConfig {
 export function configFromPartial(config?: Partial<StellarTxDeliveryManConfig>) {
   return {
     gasLimit: config?.gasLimit ?? DEFAULT_GAS_LIMIT_STROOPS,
+    gasBase: config?.gasBase ?? DEFAULT_GAS_BASE_STROOPS,
     gasMultiplier: config?.gasMultiplier ?? DEFAULT_GAS_MULTIPLIER,
     maxTxSendAttempts: config?.maxTxSendAttempts ?? DEFAULT_MAX_TX_SEND_ATTEMPTS,
     expectedTxDeliveryTimeInMS: config?.expectedTxDeliveryTimeInMS,
