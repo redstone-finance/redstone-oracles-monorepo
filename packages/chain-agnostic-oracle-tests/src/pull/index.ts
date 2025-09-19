dad8820b5b64d2c7c015b048c2b1417f69ede19d
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-09-19 11:00:57 +0200

    chore: additional tests (#7294)

--- packages/chain-agnostic-oracle-tests/src/pull/index.ts
+++ packages/chain-agnostic-oracle-tests/src/pull/index.ts
@@ -1,12 +1,13 @@
 import { Nested, PullModelTestCase } from "../types";
 import { corruptedPayloadTestCases } from "./corruptedPayloadTestCases";
-import { dataPackagesTestCases } from "./dataPackagesTestCases";
+import { dataPackagesTestCases, nonEvmAdditionalTests } from "./dataPackagesTestCases";
 import { dataTimestampTestCases } from "./dataTimestampTestCases";
 import { signaturesTestCases } from "./signaturesTestCases";
 
 const pullModelTestCases: Record<string, Nested<PullModelTestCase>> = {
   "Data timestamp": dataTimestampTestCases,
   "Data packages": dataPackagesTestCases,
+  "Data packages non evm": nonEvmAdditionalTests,
   "Signatures tests": signaturesTestCases,
   "Corrupted payload": corruptedPayloadTestCases,
 };
