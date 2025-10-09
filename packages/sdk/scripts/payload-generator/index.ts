6c8eed05fac77214f99d8606d8c034b0b39cfb7f
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-10-09 13:49:09 +0200

    chore: canton-connector 3.4.0 target 2.1 (#7561)

--- packages/sdk/scripts/payload-generator/index.ts
+++ packages/sdk/scripts/payload-generator/index.ts
@@ -1,4 +1,12 @@
-import { DataServiceIds, getSignersForDataServiceId, requestRedstonePayload } from "../../src";
+import * as fs from "node:fs";
+import path from "node:path";
+import {
+  convertDataPackagesResponse,
+  DataServiceIds,
+  getSignersForDataServiceId,
+  requestDataPackages,
+  requestRedstonePayload,
+} from "../../src";
 
 const DATA_SERVICE_ID = "redstone-primary-prod";
 const DATA_FEEDS = ["BTC", "ETH"];
@@ -6,14 +14,30 @@ const UNIQUE_SIGNER_COUNT = 3;
 
 const scriptArgs = process.argv.slice(2);
 
-requestRedstonePayload(
-  {
+async function requestPayloads(format: string, filenamePrefix?: string) {
+  const reqParams = {
     dataPackagesIds: DATA_FEEDS,
     dataServiceId: DATA_SERVICE_ID,
     uniqueSignersCount: UNIQUE_SIGNER_COUNT,
     authorizedSigners: getSignersForDataServiceId(DATA_SERVICE_ID as DataServiceIds),
-  },
-  scriptArgs[0]
-)
-  .then((value) => console.log(value))
-  .catch((e) => console.error(e));
+  };
+
+  if (format === "all" && filenamePrefix) {
+    const signedDataPackagesResponse = await requestDataPackages(reqParams);
+
+    for (format of ["hex", "bytes", "json"]) {
+      fs.writeFileSync(
+        path.join(__dirname, `${filenamePrefix}.${format}`),
+        convertDataPackagesResponse(signedDataPackagesResponse, format)
+      );
+    }
+  } else {
+    console.log(await requestRedstonePayload(reqParams, format));
+  }
+}
+
+async function main() {
+  await requestPayloads(scriptArgs[0], scriptArgs[1]);
+}
+
+void main();
