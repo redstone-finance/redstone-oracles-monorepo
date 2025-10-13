462482f522269dfb2167857437ec0579cd8c7766
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-10-13 11:30:51 +0200

    Audyt fixes #4, #5 and #7 (#7525)

--- packages/stellar-connector/stellar/common/src/lib.rs
+++ packages/stellar-connector/stellar/common/src/lib.rs
@@ -6,7 +6,7 @@ pub mod upgradable;
 use soroban_sdk::{
     contracttype,
     xdr::{ScErrorCode, ScErrorType},
-    Error, U256,
+    ConversionError, Error, InvokeError, U256,
 };
 
 #[derive(Debug, Clone)]
@@ -24,3 +24,18 @@ pub const CONTRACT_TTL_EXTEND_TO_LEDGERS: u32 = CONTRACT_TTL_SECS * 3 / 10;
 
 pub const MISSING_STORAGE_ENTRY: Error =
     Error::from_type_and_code(ScErrorType::Storage, ScErrorCode::MissingValue);
+
+pub fn flatten_call_result<T>(
+    result: Result<Result<T, ConversionError>, Result<Error, InvokeError>>,
+) -> Result<T, Error> {
+    match result {
+        Ok(Ok(v)) => Ok(v),
+        Ok(Err(e)) => Err(e.into()),
+        Err(Ok(e)) => Err(e),
+        Err(Err(InvokeError::Abort)) => Err(Error::from_type_and_code(
+            ScErrorType::Contract,
+            ScErrorCode::InternalError,
+        )),
+        Err(Err(InvokeError::Contract(code))) => Err(Error::from_contract_error(code)),
+    }
+}
