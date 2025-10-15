39fdc14664acaaec79c4e67c8a9f130ae95369f8
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-10-15 16:06:20 +0200

    [stellar] 2step ownership (#7654)

---
+++ packages/stellar-connector/scripts/ledger/cancel-ownership-transfer-tx.ts
@@ -0,0 +1,13 @@
+import { loadContractId } from "../utils";
+import { MULTISIG_ADDRESS } from "./consts";
+import { printTx } from "./print-tx";
+
+const FEE_STROOPS = 1000;
+
+async function cancelOwnershipTransferTx(contractId = loadContractId()) {
+  await printTx(contractId, (adapter) =>
+    adapter.cancelOwnershipTransferTx(MULTISIG_ADDRESS, FEE_STROOPS)
+  );
+}
+
+void cancelOwnershipTransferTx();
