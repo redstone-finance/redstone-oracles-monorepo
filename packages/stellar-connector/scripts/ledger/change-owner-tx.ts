de66149c40bd129320dbbb1dd65759b8884efe1a
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-10-15 16:06:20 +0200

    [stellar] 2step ownership (#7654)

--- packages/stellar-connector/scripts/ledger/change-owner-tx.ts
+++ packages/stellar-connector/scripts/ledger/change-owner-tx.ts
@@ -1,22 +1,16 @@
-import { Contract } from "@stellar/stellar-sdk";
-import { makeKeypair, StellarClientBuilder, StellarContractAdapter } from "../../src";
-import { loadContractId, readNetwork, readUrl } from "../utils";
+import { makeKeypair } from "../../src";
+import { loadContractId } from "../utils";
 import { MULTISIG_ADDRESS } from "./consts";
+import { printTx } from "./print-tx";
 
 const FEE_STROOPS = 1000;
 
 async function changeOwnerTx(contractId = loadContractId()) {
   const keypair = makeKeypair();
 
-  const client = new StellarClientBuilder()
-    .withStellarNetwork(readNetwork())
-    .withRpcUrl(readUrl())
-    .build();
-
-  const adapter = new StellarContractAdapter(client, new Contract(contractId));
-  const tx = await adapter.changeOwnerTx(MULTISIG_ADDRESS, keypair.publicKey(), FEE_STROOPS);
-
-  console.log(tx.toEnvelope().toXDR("hex"));
+  await printTx(contractId, (adapter) =>
+    adapter.changeOwnerTx(MULTISIG_ADDRESS, keypair.publicKey(), FEE_STROOPS)
+  );
 }
 
 void changeOwnerTx();
