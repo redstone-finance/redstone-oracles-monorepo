1487132bd822cd1a99f8eb1bcf325267377126df
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-10-17 08:58:26 +0200

    Fixes for ledger scripts (#7685)

--- packages/stellar-connector/scripts/ledger/cancel-ownership-transfer-tx.ts
+++ packages/stellar-connector/scripts/ledger/cancel-ownership-transfer-tx.ts
@@ -2,7 +2,7 @@ import { loadContractId } from "../utils";
 import { MULTISIG_ADDRESS } from "./consts";
 import { printTx } from "./print-tx";
 
-const FEE_STROOPS = 1000;
+const FEE_STROOPS = "1000";
 
 async function cancelOwnershipTransferTx(contractId = loadContractId()) {
   await printTx(contractId, (adapter) =>
--- packages/stellar-connector/scripts/ledger/sample-upgrade-tx.ts
+++ packages/stellar-connector/scripts/ledger/sample-upgrade-tx.ts
@@ -2,7 +2,7 @@ import { getSampleUpgradeTx } from "../sample-upgrade";
 import { loadContractId } from "../utils";
 import { MULTISIG_ADDRESS } from "./consts";
 
-const FEE_STROOPS = 1000;
+const FEE_STROOPS = "1000";
 
 async function sampleUpgrade(contractId = loadContractId()) {
   const { adapter, wasmHash } = await getSampleUpgradeTx(contractId);
