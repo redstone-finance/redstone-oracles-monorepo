28b56438d4e9dc030f33236e1ca568dab72e3502
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-10-27 12:02:28 +0100

    [RDS-4628]: unify tx delivery man - stellar (#7785)

--- packages/stellar-connector/src/stellar/StellarContractDeployer.ts
+++ packages/stellar-connector/src/stellar/StellarContractDeployer.ts
@@ -1,12 +1,12 @@
 import { Address, Operation } from "@stellar/stellar-sdk";
 import { readFileSync } from "fs";
 import { StellarClient } from "./StellarClient";
-import { StellarTxDeliveryMan } from "./StellarTxDeliveryMan";
+import { StellarOperationSender } from "./StellarOperationSender";
 
 export class StellarContractDeployer {
   constructor(
     private readonly client: StellarClient,
-    private readonly txDeliveryMan: StellarTxDeliveryMan
+    private readonly operationSender: StellarOperationSender
   ) {}
 
   async deploy(wasmPath: string) {
@@ -22,19 +22,19 @@ export class StellarContractDeployer {
   async upload(wasmPath: string) {
     const wasmBuffer = readFileSync(wasmPath);
 
-    const hash = await this.txDeliveryMan.sendTransaction(() => {
-      return Operation.uploadContractWasm({ wasm: wasmBuffer });
-    });
+    const hash = await this.operationSender.sendTransaction(
+      Operation.uploadContractWasm({ wasm: wasmBuffer })
+    );
 
     return (await this.client.getTransaction(hash, (returnValue) => returnValue.bytes())).value!;
   }
 
   async createContract(wasmHash: Buffer) {
-    const address = Address.fromString(await this.txDeliveryMan.getPublicKey());
+    const address = Address.fromString(await this.operationSender.getPublicKey());
 
-    const hash = await this.txDeliveryMan.sendTransaction(() => {
-      return Operation.createCustomContract({ wasmHash, address });
-    });
+    const hash = await this.operationSender.sendTransaction(
+      Operation.createCustomContract({ wasmHash, address })
+    );
 
     return (await this.client.getTransaction(hash, Address.fromScVal)).value!;
   }
