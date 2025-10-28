ab478d48fcd104eedc29d2a68080c188b70f0303
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2025-10-28 12:36:11 +0100

    Deploy new monad bolt multi-feed (#7853)

--- packages/evm-adapters/contracts/price-feeds/data-services/MultiFeedAdapterWithoutRoundsPrimaryDemo.sol
+++ packages/evm-adapters/contracts/price-feeds/data-services/MultiFeedAdapterWithoutRoundsPrimaryDemo.sol
@@ -6,7 +6,7 @@ import {MultiFeedAdapterWithoutRounds} from "../without-rounds/MultiFeedAdapterW
 
 abstract contract MultiFeedAdapterWithoutRoundsPrimaryDemo is MultiFeedAdapterWithoutRounds {
   function getUniqueSignersThreshold() public view virtual override returns (uint8) {
-    return 1;
+    return 2;
   }
 
   function getAuthorisedSignerIndex(
