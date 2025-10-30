7568a7c9b08b620e8408d9fac5d0fac83634e7b1
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-10-30 10:06:20 +0100

    Update stellar Multisig info (#7926)

--- packages/stellar-connector/scripts/consts.ts
+++ packages/stellar-connector/scripts/consts.ts
@@ -2,4 +2,4 @@ export const FEEDS = ["ETH", "BTC"];
 export const PRICE_FEED_WASM_HASH =
   "3e464b6dda26a8d5b0bf13d2cb7781a6142bf2c2e4b64edcba093911fa16df5c";
 export const PRICE_ADAPTER_WASM_HASH =
-  "9f77e8f327b59abb78fa9f8e42f81260d7077b247b7f7c748484f06409f933b1";
+  "0fed8de986bc052d26eedb88e0f32760c7276a0a147b0f34ded0179147e4aca7";
