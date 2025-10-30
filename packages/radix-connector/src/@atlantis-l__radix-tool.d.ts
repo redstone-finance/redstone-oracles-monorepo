781affe72d8bdfaf6148438ee49b7eaee6e86b1d
robal <robert@warp.cc>
2025-10-30 14:48:57 +0100

    chore: change module property in tsconfig to more strict nodenext (#7845)

---
+++ packages/radix-connector/src/@atlantis-l__radix-tool.d.ts
@@ -0,0 +1,12 @@
+declare module "@atlantis-l/radix-tool" {
+  export * from "@atlantis-l/radix-tool/dist/models/index.d.ts";
+  export * from "@atlantis-l/radix-tool/dist/tools/index.d.ts";
+}
+declare module "@atlantis-l/radix-tool/dist/models/index.d.ts" {
+  export * from "@atlantis-l/radix-tool/dist/models/ownership.d.ts";
+  export * from "@atlantis-l/radix-tool/dist/models/result.d.ts";
+  export * from "@atlantis-l/radix-tool/dist/models/wallet.d.ts";
+}
+declare module "@atlantis-l/radix-tool/dist/tools/index.d.ts" {
+  export * from "@atlantis-l/radix-tool/dist/tools/package-deployer.d.ts";
+}
