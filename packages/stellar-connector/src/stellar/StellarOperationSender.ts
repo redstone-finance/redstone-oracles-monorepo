24e4a891f8fcdcaf534028e7ab80a3722cf22dd4
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-11-10 10:44:18 +0100

    chore: move result to utils  (#7921)

--- packages/stellar-connector/src/stellar/StellarOperationSender.ts
+++ packages/stellar-connector/src/stellar/StellarOperationSender.ts
@@ -1,11 +1,10 @@
 import {
   ContractUpdater,
-  ok,
   TxDeliveryMan,
   TxDeliveryManUpdateStatus,
-  unwrapSuccess,
 } from "@redstone-finance/multichain-kit";
 import { ContractParamsProvider } from "@redstone-finance/sdk";
+import { FP, RedstoneCommon } from "@redstone-finance/utils";
 import { Operation, xdr } from "@stellar/stellar-sdk";
 import { StellarClient } from "./StellarClient";
 import { StellarSigner } from "./StellarSigner";
@@ -45,13 +44,20 @@ export class StellarOperationSender {
   }
 
   async sendTransaction(operation: StellarOperation): Promise<string> {
-    const result = await this.txDeliveryMan.submit(async () => {
-      const txResult = await this.executor.executeOperation(operation);
+    const operationResult = await FP.tryCallAsyncStringifyError(() =>
+      this.txDeliveryMan.submit(async () => {
+        const txResult = await this.executor.executeOperation(operation);
 
-      return ok({ transactionHash: txResult.hash });
-    });
+        return FP.ok({ transactionHash: txResult.hash });
+      })
+    );
 
-    return unwrapSuccess(result).transactionHash;
+    const operationResultFlatten = FP.flatten(
+      FP.mapErr(operationResult, (err) => [RedstoneCommon.stringify(err)])
+    );
+    const hashResult = FP.map(operationResultFlatten, (r) => r.transactionHash);
+
+    return FP.unwrapSuccess(hashResult);
   }
 
   async getPublicKey() {
