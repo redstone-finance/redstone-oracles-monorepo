c4f2da6d0cd8b28c78764639f7e12db5b5f8eb8c
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2025-11-25 12:34:13 +0100

    Changes from PR #8373

---
+++ packages/utils/src/operation-queue/SetOperationQueue.ts
@@ -0,0 +1,26 @@
+import { isSubsetOf, stringify } from "../common";
+import { OperationQueue, QueueEntry } from "./OperationQueue";
+
+export class SetOperationQueue extends OperationQueue<Set<string>> {
+  protected override conforms(oldId: Set<string>, newId: Set<string>, strict = false) {
+    return isSubsetOf(newId, oldId) && (!strict || isSubsetOf(oldId, newId));
+  }
+
+  override purge(replacedOperationIndex: number, id: Set<string>) {
+    for (let i = this.queue.length - 1; i > replacedOperationIndex; i--) {
+      const idToRemove = this.queue[i].id;
+      if (this.conforms(idToRemove, id)) {
+        this.queue.splice(i, 1);
+        this.logger.debug(`Removed operation #${i} [${stringify(idToRemove)} from the queue`);
+      }
+    }
+  }
+
+  protected override expandQueueEntry(entry: QueueEntry<Set<string>>, now: number = Date.now()) {
+    return {
+      ...super.expandQueueEntry(entry, now),
+      keys: Array.from(entry.id.keys()),
+      size: entry.id.size,
+    };
+  }
+}
