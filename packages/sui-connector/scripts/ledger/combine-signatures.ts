dd9e8ebb04a422979e6c25d9c905504e9979e73a
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-11-28 09:11:59 +0100

    chore: ... sig order (#8177)

--- packages/sui-connector/scripts/ledger/combine-signatures.ts
+++ packages/sui-connector/scripts/ledger/combine-signatures.ts
@@ -24,4 +24,8 @@ function combineSignatures(pkHexes: string[], signatures: string[]) {
   return multiSigAddress;
 }
 
+// Remember that the signatures order should match order of multi-sig-pk-hexes.
+// For example:
+// MULTI_SIG_PK_HEXES = [pk_0, ..., pk_5] and we have signature from pk_2, pk_4 and pk_5.
+// The order of signatures here should be ["sig_2", "sig_4", "sig_5"].
 combineSignatures(MULTI_SIG_PK_HEXES, [""]);
