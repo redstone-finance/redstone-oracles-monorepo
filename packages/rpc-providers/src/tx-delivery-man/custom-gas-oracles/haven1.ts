e0c016c010eb1cde384893981268d1fd5f8ca8d8
Cezary <35804520+cehali@users.noreply.github.com>
2025-12-01 14:50:56 +0100

    chore: remove haven1 (#8556)

--- packages/rpc-providers/src/tx-delivery-man/custom-gas-oracles/haven1.ts
+++
@@ -1,8 +0,0 @@
-// This chain has no gas fees, gasPrice = 0 for every transaction
-
-import type { GasOracleFn } from "../common";
-
-export const haven1GasOracle: GasOracleFn = () =>
-  Promise.resolve({
-    gasPrice: 0,
-  });
