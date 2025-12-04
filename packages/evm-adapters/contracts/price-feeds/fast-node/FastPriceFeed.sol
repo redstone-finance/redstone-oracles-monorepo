548d4232af6f2e3034108f9c679c4fef90730943
Michał Szynwelski <szynek@redstone.finance>
2025-12-04 18:44:00 +0100

    feat: megaETH - mini block timestamp (#8589)

--- packages/evm-adapters/contracts/price-feeds/fast-node/FastPriceFeed.sol
+++ packages/evm-adapters/contracts/price-feeds/fast-node/FastPriceFeed.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.17;
+
 import {IPriceFeed} from "../interfaces/IPriceFeed.sol";
 import {IFastMultiFeedAdapter} from "./IFastMultiFeedAdapter.sol";
 
 /// Price feed for FastMultiFeedAdapter
 abstract contract FastPriceFeed is IPriceFeed {
+  uint256 internal constant INT256_MAX = uint256(type(int256).max);
+  uint256 internal constant UINT80_MAX = uint256(type(uint80).max);
+
+  error UnsafeUint256ToInt256Conversion(uint256 value);
+  error UnsafeUint256ToUint80Conversion(uint256 value);
+
   // Returns the price feed adapter contract used by this feed
   function getPriceFeedAdapter() public view virtual returns (IFastMultiFeedAdapter);
   // Returns the data feed identifier used by this feed
@@ -16,12 +23,24 @@ abstract contract FastPriceFeed is IPriceFeed {
 
   // Returns the latest round id available from the adapter
   function latestRound() public view override returns (uint80) {
-    return uint80(getPriceFeedAdapter().getLatestRoundId(getDataFeedId()));
+    uint256 latestRoundUint256 = getPriceFeedAdapter().getLatestRoundId(getDataFeedId());
+
+    if (latestRoundUint256 > UINT80_MAX) {
+      revert UnsafeUint256ToUint80Conversion(latestRoundUint256);
+    }
+
+    return uint80(latestRoundUint256);
   }
 
   // Returns the latest price answer from the adapter
   function latestAnswer() public view override returns (int256) {
-    return int256(getPriceFeedAdapter().getValueForDataFeed(getDataFeedId()));
+    uint256 uintAnswer = getPriceFeedAdapter().getValueForDataFeed(getDataFeedId());
+
+    if (uintAnswer > INT256_MAX) {
+      revert UnsafeUint256ToInt256Conversion(uintAnswer);
+    }
+
+    return int256(uintAnswer);
   }
 
   // Returns detailed data for a specific round
@@ -29,7 +48,7 @@ abstract contract FastPriceFeed is IPriceFeed {
     uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound
   ) {
     IFastMultiFeedAdapter.PriceData memory data = getPriceFeedAdapter().getRoundData(getDataFeedId(), _roundId);
-    return (_roundId, int256(uint256(data.price)), uint256(data.priceTimestamp), uint256(data.priceTimestamp), _roundId);
+    return (_roundId, int256(uint256(data.price)), data.blockTimestamp, data.blockTimestamp, _roundId);
   }
 
   // Returns detailed data for the latest round
