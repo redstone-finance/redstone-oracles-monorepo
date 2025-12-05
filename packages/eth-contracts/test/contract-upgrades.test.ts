eac8338a23a259824b2b6b6315001929c3e52f8f
robal <robert@warp.cc>
2025-12-05 12:02:43 +0100

    chore: more lint! (#8645)

--- packages/eth-contracts/test/contract-upgrades.test.ts
+++ packages/eth-contracts/test/contract-upgrades.test.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { Contract } from "ethers";
