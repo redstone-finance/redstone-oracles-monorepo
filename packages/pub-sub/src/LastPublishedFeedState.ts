960442b36e7ff9a53b45724ea4a06c9edf4a9e88
Dawid Sowa <dawid.sowa@redstone.finance>
2025-12-08 15:10:16 +0100

    refactor: rename mqtt5-client to pub-sub package and `MultiPubSubClient` → `PooledMqttClient` (#8656)

---
+++ packages/pub-sub/src/LastPublishedFeedState.ts
@@ -0,0 +1,43 @@
+import { SignedDataPackage } from "@redstone-finance/protocol";
+import { PackageResponse } from "./common";
+
+export class LastPublishedFeedState {
+  private lastPublishPerFeed: Map<string, number>;
+
+  constructor(dataPackageIds: string[], initTimestamp: number) {
+    this.lastPublishPerFeed = new Map(dataPackageIds.map((id) => [id, initTimestamp]));
+  }
+
+  isNewerThanLastPublished(dataPackageId: string, timestamp: number) {
+    return timestamp > this.lastPublishPerFeed.get(dataPackageId)!;
+  }
+
+  isAnyFeedNotPublishedIn(maxDelay: number) {
+    const now = Date.now();
+    return [...this.lastPublishPerFeed.values()].some((timestamp) => now - timestamp > maxDelay);
+  }
+
+  getLastPublishTime(dataPackageId: string) {
+    return this.lastPublishPerFeed.get(dataPackageId)!;
+  }
+
+  update(dataPackageIds: string[], newTimestamp: number) {
+    for (const dataPackageId of dataPackageIds) {
+      this.lastPublishPerFeed.set(dataPackageId, newTimestamp);
+    }
+  }
+
+  filterOutNotNewerPackages<P extends SignedDataPackage>(dataPackagesResponse: PackageResponse<P>) {
+    return Object.fromEntries(
+      Object.entries(dataPackagesResponse).filter(([dataPackageId, packages]) =>
+        this.isNewerThanLastPublished(dataPackageId, packages![0].dataPackage.timestampMilliseconds)
+      )
+    );
+  }
+
+  toString() {
+    return [...this.lastPublishPerFeed.entries()]
+      .map(([dataPackageId, timestamp]) => `${dataPackageId}:${timestamp}`)
+      .join(" ");
+  }
+}
