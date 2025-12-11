b769ca21156a60e0d2d1d9eec6f176ee0a4265c9
kostekIV <27210860+kostekIV@users.noreply.github.com>
2025-12-11 10:52:56 +0100

    [RDS-4628]: solana tx delivery man (#8737)

--- packages/solana-connector/src/client/SolanaContractUpdater.ts
+++ packages/solana-connector/src/client/SolanaContractUpdater.ts
@@ -1,99 +1,111 @@
-import { loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
+import {
+  ContractUpdateContext,
+  ContractUpdater,
+  MultiTxDeliveryMan,
+  TxDeliveryMan,
+} from "@redstone-finance/multichain-kit";
+import { ContractParamsProvider } from "@redstone-finance/sdk";
+import { FP, loggerFactory, RedstoneCommon } from "@redstone-finance/utils";
 import {
   ComputeBudgetProgram,
   Keypair,
   TransactionInstruction,
   TransactionMessage,
-  TransactionSignature,
   VersionedTransaction,
 } from "@solana/web3.js";
 import { SolanaConfig } from "../config";
 import { AggressiveSolanaGasOracle } from "../gas-oracles/AggressiveSolanaGasOracle";
 import { ISolanaGasOracle } from "../gas-oracles/ISolanaGasOracle";
 import { RegularSolanaGasOracle } from "../gas-oracles/RegularSolanaGasOracle";
+import { PriceAdapterContract } from "../price_adapter/PriceAdapterContract";
 import { SolanaRustSdkErrroHandler } from "../price_adapter/SolanaRustSdkErrorHandler";
-import { SOLANA_SLOT_TIME_INTERVAL_MS, SolanaClient } from "./SolanaClient";
 import { getRecentBlockhash } from "./get-recent-blockhash";
+import { SOLANA_SLOT_TIME_INTERVAL_MS, SolanaClient } from "./SolanaClient";
 
-export type TransactionInstructionsCreator = (inputs: string[]) => Promise<
-  {
-    instruction: TransactionInstruction;
-    id: string;
-  }[]
->;
+const MAX_FEED_COUNT_IN_UPDATE = 1;
 
-export class SolanaTxDeliveryMan {
-  private readonly logger = loggerFactory("solana-tx-delivery-man");
+export class SolanaContractUpdater implements ContractUpdater {
+  private readonly logger = loggerFactory("solana-contract-updater");
   private readonly gasOracle: ISolanaGasOracle;
+  private readonly txDeliveryMan: TxDeliveryMan;
 
   constructor(
     private readonly client: SolanaClient,
     private readonly config: SolanaConfig,
-    private readonly keypair: Keypair
+    private readonly keypair: Keypair,
+    private readonly contract: PriceAdapterContract
   ) {
     this.gasOracle = config.useAggressiveGasOracle
       ? new AggressiveSolanaGasOracle(config)
       : new RegularSolanaGasOracle(client, config);
+
+    this.txDeliveryMan = new MultiTxDeliveryMan(
+      {
+        expectedTxDeliveryTimeInMs: config.expectedTxDeliveryTimeMs,
+        maxTxSendAttempts: config.maxTxAttempts,
+        batchSizePerRequestParams: () => MAX_FEED_COUNT_IN_UPDATE,
+      },
+      "solana-contract-tx-delivery-man"
+    );
   }
 
   public getPublicKey() {
     return this.keypair.publicKey;
   }
 
-  async sendTransactions(txIds: string[], ixCreator: TransactionInstructionsCreator) {
-    return await this.sendTransactionsWithRetry(txIds, ixCreator);
+  async writePrices(paramsProvider: ContractParamsProvider) {
+    return await this.txDeliveryMan.updateContract(this, paramsProvider);
   }
 
-  private async sendTransactionsWithRetry(
-    txIds: string[],
-    creator: TransactionInstructionsCreator,
-    iterationIndex = 0,
-    errors: unknown[] = []
+  async update(
+    paramsProvider: ContractParamsProvider,
+    context: ContractUpdateContext,
+    attempt: number
   ) {
-    if (iterationIndex >= this.config.maxTxAttempts) {
-      throw new Error(
-        `Max attempts reached: ${this.config.maxTxAttempts}. ${RedstoneCommon.stringifyError(new AggregateError(errors))}`
-      );
-    }
+    const feedCount = paramsProvider.getDataFeedIds().length;
 
-    this.logger.info(
-      `Sending transaction${RedstoneCommon.getS(txIds.length)} for [${txIds.toString()}]${iterationIndex ? `; Attempt #${iterationIndex + 1}/${this.config.maxTxAttempts}` : ""}`
+    if (feedCount !== MAX_FEED_COUNT_IN_UPDATE) {
+      return FP.err(`solana updater expects single feed in update call, got ${feedCount}`);
+    }
+    const feedId = paramsProvider.getDataFeedIds()[0];
+    const payload = await FP.tryAwait(
+      paramsProvider.getPayloadHex(false, {
+        withUnsignedMetadata: true,
+        metadataTimestamp: context.updateStartTimeMs,
+        componentName: "solana-connector",
+      })
     );
 
-    const failedTxs: string[] = [];
-    const successfulTxs: TransactionSignature[] = [];
-
-    const sendAndConfirmWrapper = async (id: string, instruction: TransactionInstruction) => {
-      try {
-        const signature = await this.sendAndConfirm(id, instruction, iterationIndex);
-        successfulTxs.push(signature);
-      } catch (err) {
-        errors.push(err);
-        failedTxs.push(id);
-        this.logger.error(`Failed transaction for ${id}, ${RedstoneCommon.stringifyError(err)}`);
-      }
-    };
-
-    const transactionsToSend = await creator(txIds);
-    const promises = transactionsToSend.map(({ id, instruction }) =>
-      sendAndConfirmWrapper(id, instruction)
+    const ix = await FP.mapAsyncStringifyError(payload, (payload) =>
+      this.contract.writePriceTx(this.keypair.publicKey, feedId, payload)
     );
-    await Promise.allSettled(promises);
+    const transactionHash = await FP.mapAsyncStringifyError(ix, (ix) =>
+      this.sendAndConfirm(feedId, ix, attempt)
+    );
 
-    if (failedTxs.length !== 0) {
-      const txSignatures = await this.sendTransactionsWithRetry(
-        failedTxs,
-        creator,
-        iterationIndex + 1,
-        errors
-      );
-      successfulTxs.push(...txSignatures);
-    }
-
-    return successfulTxs;
+    return FP.mapStringifyError(transactionHash, (transactionHash) => ({ transactionHash }));
   }
 
-  private async wrapWithGas(ix: TransactionInstruction, iteration = 0) {
+  private async sendAndConfirm(
+    id: string,
+    instruction: TransactionInstruction,
+    iterationIndex = 0
+  ) {
+    const message = await this.wrapWithGas(instruction, iterationIndex);
+    const tx = new VersionedTransaction(message.compileToV0Message());
+
+    tx.sign([this.keypair]);
+
+    const signature = await this.client.sendTransaction(tx, {
+      skipPreflight: true,
+    });
+
+    await this.waitForTransaction(signature, id);
+
+    return signature;
+  }
+
+  private async wrapWithGas(ix: TransactionInstruction, iteration: number) {
     const computeUnits = this.config.maxComputeUnits;
     const writableKeys = ix.keys.filter((meta) => meta.isWritable).map((meta) => meta.pubkey);
     const priorityFeeUnitPriceCostInMicroLamports = await this.gasOracle.getPriorityFeePerUnit(
@@ -124,25 +136,6 @@ export class SolanaTxDeliveryMan {
       recentBlockhash,
       instructions,
     });
-  }
-
-  private async sendAndConfirm(
-    id: string,
-    instruction: TransactionInstruction,
-    iterationIndex = 0
-  ) {
-    const message = await this.wrapWithGas(instruction, iterationIndex);
-    const tx = new VersionedTransaction(message.compileToV0Message());
-
-    tx.sign([this.keypair]);
-
-    const signature = await this.client.sendTransaction(tx, {
-      skipPreflight: true,
-    });
-
-    await this.waitForTransaction(signature, id);
-
-    return signature;
   }
 
   private async waitForTransaction(txSignature: string, id: string) {
