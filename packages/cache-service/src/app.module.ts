e7498f9fb831b9c151c739c686ce8d7d358d7b91
just_ppe <84311757+ppedziwiatr@users.noreply.github.com>
2025-12-13 16:49:43 +0100

    chore: remove streamr (#8823)

--- packages/cache-service/src/app.module.ts
+++ packages/cache-service/src/app.module.ts
@@ -1,30 +1,18 @@
 import { Module, Provider } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ScheduleModule } from "@nestjs/schedule";
 import mongoose from "mongoose";
 import { LoggerModule } from "nestjs-pino";
 import { AppController } from "./app.controller";
 import { MongoBroadcaster } from "./broadcasters/mongo-broadcaster";
-import { StreamrBroadcaster } from "./broadcasters/streamr-broadcaster";
 import config from "./config";
 import { DataFeedsMetadataController } from "./data-feeds-metadata/data-feeds-metadata.controller";
 import { DataPackagesControllerV1 } from "./data-packages/data-packages.controller.v1";
 import { DataPackagesControllerV2 } from "./data-packages/data-packages.controller.v2";
 import { DataPackagesService } from "./data-packages/data-packages.service";
 import { OracleRegistryStateController } from "./oracle-registry-state/oracle-registry-state.controller";
-import { StreamrListenerService } from "./streamr-listener/streamr-listener.service";
 
 const providers: Provider[] = [DataPackagesService];
 const imports = [LoggerModule.forRoot()];
-
-if (config.enableStreamrListening) {
-  providers.push(StreamrListenerService);
-  imports.push(ScheduleModule.forRoot());
-}
-
-if (config.streamrPrivateKey) {
-  providers.push(StreamrBroadcaster);
-}
 
 if (config.mongoDbUrl) {
   providers.push(MongoBroadcaster);
