77b77daae45837e55683fd6331834304a9543053
robal <robert@warp.cc>
2025-12-16 15:54:44 +0100

    chore: verify minimal required node version (#8810)

--- packages/utils/src/common/runtime.ts
+++ packages/utils/src/common/runtime.ts
@@ -1,4 +1,34 @@
-export const isNodeRuntime = () =>
-  typeof process !== "undefined" &&
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- add reason here, please
-  process.versions?.node !== undefined;
+// Node
+
+import { isDefined } from "./objects";
+
+type MaybeNode = { process?: { versions: { node: string }; env: Record<string, string> } };
+const nodeGlobalThis = globalThis as unknown as MaybeNode;
+
+export const getNodeVersion = (): string | undefined => nodeGlobalThis.process?.versions.node;
+export const isNodeRuntime = () => isDefined(getNodeVersion());
+
+// Deno
+
+type MaybeDeno = {
+  Deno?: {
+    version: { deno: string };
+    env: { toObject: () => Record<string, string> };
+  };
+};
+const denoGlobalThis = globalThis as unknown as MaybeDeno;
+
+export const getDenoVersion = (): string | undefined => denoGlobalThis.Deno?.version.deno;
+export const isDenoRuntime = () => isDefined(getDenoVersion());
+
+// Env
+
+export const getEnv = (): Record<string, string> => {
+  if (isNodeRuntime()) {
+    return nodeGlobalThis.process!.env;
+  }
+  if (isDenoRuntime()) {
+    return denoGlobalThis.Deno!.env.toObject();
+  }
+  return {};
+};
