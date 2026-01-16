35da3b1854553749fb38f3dc5bdd83e704ab5cd6
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-01-16 14:45:27 +0100

    [RDS-5526] featured app in canton (#9148)

--- packages/canton-connector/scripts/sample-run.ts
+++ packages/canton-connector/scripts/sample-run.ts
@@ -5,7 +5,7 @@ import {
 } from "@redstone-finance/sdk";
 import { CantonClient } from "../src";
 import { PricesCantonContractConnector } from "../src/adapters/PricesCantonContractConnector";
-import { getJsonApiUrl, keycloakTokenProvider, readPartySuffix } from "./utils";
+import { getJsonApiUrl, keycloakTokenProvider, readNetwork, readPartySuffix } from "./utils";
 
 const VIEWER_PARTY_ID = `RedStoneOracleViewer`;
 const UPDATER_PARTY_ID = `RedStoneOracleUpdater`;
@@ -15,7 +15,12 @@ async function main(adapterId = ADAPTER_ID) {
   const tokenProvider = () => keycloakTokenProvider();
   const [client, updateClient] = [VIEWER_PARTY_ID, UPDATER_PARTY_ID].map(
     (partyId) =>
-      new CantonClient(`${partyId}::${readPartySuffix()}`, getJsonApiUrl(), tokenProvider)
+      new CantonClient(
+        `${partyId}::${readPartySuffix()}`,
+        getJsonApiUrl(),
+        tokenProvider,
+        readNetwork()
+      )
   );
 
   const connector = new PricesCantonContractConnector(client, updateClient, adapterId);
---
+++ packages/canton-connector/src/adapters/CoreFeaturedCantonContractAdapter.ts
@@ -0,0 +1,37 @@
+import { ContractParamsProvider } from "@redstone-finance/sdk";
+import { CantonClient } from "../CantonClient";
+import { DamlTuple2 } from "../utils";
+import { CoreCantonContractAdapter } from "./CoreCantonContractAdapter";
+
+export const DEFS_KEY_CORE_FEATURED = "core-featured";
+export const DEFS_KEY_FEATURED_APP_RIGHT = "featured-app-right";
+
+export const ICORE_FEATURED_TEMPLATE_NAME = `IRedStoneCoreFeatured:IRedStoneCoreFeatured`;
+const GET_PRICES_FEATURED_CHOICE = "GetPricesFeatured";
+
+export class CoreFeaturedCantonContractAdapter extends CoreCantonContractAdapter {
+  constructor(
+    client: CantonClient,
+    adapterId = client.Defs[DEFS_KEY_CORE_FEATURED].coreId,
+    interfaceId = client.Defs.featuredInterfaceId,
+    templateName = ICORE_FEATURED_TEMPLATE_NAME
+  ) {
+    super(client, adapterId, interfaceId, templateName);
+  }
+
+  override async getPricesFromPayload(paramsProvider: ContractParamsProvider) {
+    const result: DamlTuple2<string[]> = await this.exerciseChoice(
+      GET_PRICES_FEATURED_CHOICE,
+      {
+        ...(await CoreCantonContractAdapter.getPayloadArguments(paramsProvider)),
+        featuredCid: this.client.Defs[DEFS_KEY_FEATURED_APP_RIGHT].contractId,
+        caller: this.client.partyId,
+      },
+      true,
+      this.client,
+      [this.client.Defs[DEFS_KEY_FEATURED_APP_RIGHT]]
+    );
+
+    return result._1.map(CoreCantonContractAdapter.convertDecimalValue);
+  }
+}
