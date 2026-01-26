1afb334c6b2e033c4a947cacf15bea2ba18cba28
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-01-26 18:20:21 +0100

    chore: unique signer count canton (#9373)

--- packages/canton-connector/scripts/sample-run.ts
+++ packages/canton-connector/scripts/sample-run.ts
@@ -5,21 +5,23 @@ import {
 } from "@redstone-finance/sdk";
 import { CantonClient } from "../src";
 import { PricesCantonContractConnector } from "../src/adapters/PricesCantonContractConnector";
-import { getJsonApiUrl, keycloakTokenProvider, readNetwork, readPartySuffix } from "./utils";
+import { getJsonApiUrl, getTokenProvider, readNetwork, readPartySuffix, readUserId } from "./utils";
 
 const VIEWER_PARTY_ID = `RedStoneOracleViewer`;
 const UPDATER_PARTY_ID = `RedStoneOracleUpdater`;
-const ADAPTER_ID = "RedStoneAdapter-040";
+const ADAPTER_ID = "RedStoneAdapter-743";
 
 async function main(adapterId = ADAPTER_ID) {
-  const tokenProvider = () => keycloakTokenProvider();
+  const tokenProvider = getTokenProvider();
+
   const [client, updateClient] = [VIEWER_PARTY_ID, UPDATER_PARTY_ID].map(
     (partyId) =>
       new CantonClient(
         `${partyId}::${readPartySuffix()}`,
         getJsonApiUrl(),
         tokenProvider,
-        readNetwork()
+        readNetwork(),
+        readUserId()
       )
   );
 
