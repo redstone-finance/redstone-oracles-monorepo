486634954c567f067ff8fa2687d7ef01e49a7e0f
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-01-26 18:20:21 +0100

    chore: unique signer count canton (#9373)

--- packages/canton-connector/scripts/utils.ts
+++ packages/canton-connector/scripts/utils.ts
@@ -6,6 +6,8 @@ import path from "node:path";
 import { z } from "zod";
 import { CantonNetwork, CantonNetworks } from "../src";
 
+const TOKEN_PROVIDER_TYPE = z.enum(["keycloak", "file", "none"]);
+
 export function readParticipantUrl() {
   return RedstoneCommon.getFromEnv("PARTICIPANT", z.url());
 }
@@ -14,12 +16,37 @@ export function readNetwork(): CantonNetwork {
   return RedstoneCommon.getFromEnv("NETWORK", z.enum(CantonNetworks).default("devnet"));
 }
 
+export function readUserId() {
+  return RedstoneCommon.getFromEnv("USER_ID", z.string().optional());
+}
+
 export function readPartySuffix() {
   return RedstoneCommon.getFromEnv("PARTY_SUFFIX");
 }
 
-export function getJsonApiUrl(participantUrl = readParticipantUrl()) {
-  return `${participantUrl}/jsonapi`;
+export function readApiPath() {
+  return RedstoneCommon.getFromEnv("API_PATH", z.string().default("/jsonapi"));
+}
+
+export function getJsonApiUrl(participantUrl = readParticipantUrl(), path = readApiPath()) {
+  return `${participantUrl}${path}`;
+}
+
+export function readProviderType() {
+  return RedstoneCommon.getFromEnv("TOKEN_PROVIDER_TYPE", TOKEN_PROVIDER_TYPE.default("keycloak"));
+}
+
+export function getTokenProvider() {
+  const providerType = readProviderType();
+
+  switch (providerType) {
+    case "keycloak":
+      return () => keycloakTokenProvider();
+    case "file":
+      return () => fileTokenProvider();
+    case "none":
+      return undefined;
+  }
 }
 
 export async function fileTokenProvider(
