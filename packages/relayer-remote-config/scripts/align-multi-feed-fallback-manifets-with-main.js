396b3683ea4985e09a3b6a54173070b3e7e780b6
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2026-02-05 14:50:37 +0100

    Deploy new price feeds (#9513)

--- packages/relayer-remote-config/scripts/align-multi-feed-fallback-manifets-with-main.js
+++ packages/relayer-remote-config/scripts/align-multi-feed-fallback-manifets-with-main.js
@@ -3,11 +3,21 @@ const fs = require("fs");
 const MAIN_MANIFESTS_PATH = "../main/relayer-manifests-multi-feed";
 const FALLBACK_MANIFESTS_PATH = "../fallback/relayer-manifests-multi-feed";
 
+const NON_EVM_MAIN_MANIFESTS_PATH = "../main/relayer-manifests-non-evm";
+const NON_EVM_FALLBACK_MANIFESTS_PATH = "../fallback/relayer-manifests-non-evm";
+
 (() => {
   const mainManifestPaths = fs.readdirSync(MAIN_MANIFESTS_PATH, "utf8");
 
   for (const manifestPath of mainManifestPaths) {
     const manifest = fs.readFileSync(`${MAIN_MANIFESTS_PATH}/${manifestPath}`);
     fs.writeFileSync(`${FALLBACK_MANIFESTS_PATH}/${manifestPath}`, manifest);
+  }
+
+  const nonEvmMainManifestPaths = fs.readdirSync(NON_EVM_MAIN_MANIFESTS_PATH, "utf8");
+
+  for (const manifestPath of nonEvmMainManifestPaths) {
+    const manifest = fs.readFileSync(`${NON_EVM_MAIN_MANIFESTS_PATH}/${manifestPath}`);
+    fs.writeFileSync(`${NON_EVM_FALLBACK_MANIFESTS_PATH}/${manifestPath}`, manifest);
   }
 })();
