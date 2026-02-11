0d9b97fb0ea582ce6741a7e92157869696b25200
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-02-11 12:09:55 +0100

    chore: sui issue (#9701)

--- packages/sui-connector/src/SuiCoinProvider.ts
+++ packages/sui-connector/src/SuiCoinProvider.ts
@@ -1,28 +1,32 @@
-import { SuiClient } from "@mysten/sui/client";
+import { SuiClient, SuiObjectChange } from "@mysten/sui/client";
 import { Keypair } from "@mysten/sui/cryptography";
 import { Transaction } from "@mysten/sui/transactions";
+import { SUI_TYPE_ARG } from "@mysten/sui/utils";
 import { loggerFactory, MultiExecutor, RedstoneCommon } from "@redstone-finance/utils";
-import { SuiReader } from "./SuiReader";
+import _ from "lodash";
 
 const MERGE_COINS_BATCH_SIZE = 1;
 const MERGE_COINS_MS_BETWEEN_BATCHES = 0;
+const COIN_TRANSFER_TX_LOOKUP_COUNT_LIMIT = 100;
+const FETCH_COINS_PAGE_COUNT_LIMIT = 10;
+
+const RETRY_CONFIG: Omit<RedstoneCommon.RetryConfig, "fn"> = {
+  maxRetries: 3,
+  waitBetweenMs: 500,
+};
 
 export class SuiCoinProvider {
   protected readonly logger = loggerFactory("sui-coin-provider");
 
-  constructor(
-    private readonly client: SuiClient,
-    private readonly keypair: Keypair,
-    private readonly suiReader: SuiReader
-  ) {}
+  constructor(private readonly client: SuiClient) {}
 
-  async getSourceCoins(minimumBalance: bigint) {
+  async getSourceCoins(minimumBalance: bigint, keypair: Keypair) {
     let sourceCoins = undefined;
     try {
-      const coin = await this.suiReader.getLatestReceivedCoin(
-        this.keypair.getPublicKey().toSuiAddress(),
+      const coin = await this.getLatestReceivedCoin(
+        keypair.getPublicKey().toSuiAddress(),
         minimumBalance,
-        (coinsToPurge) => this.purge(coinsToPurge)
+        (coinsToPurge) => this.purge(coinsToPurge, keypair)
       );
 
       if (coin) {
@@ -39,7 +43,7 @@ export class SuiCoinProvider {
     return sourceCoins;
   }
 
-  private async purge(coinsToPurge: string[][]) {
+  private async purge(coinsToPurge: string[][], keypair: Keypair) {
     if (!coinsToPurge.length) {
       return;
     }
@@ -51,11 +55,11 @@ export class SuiCoinProvider {
     await RedstoneCommon.batchPromises(
       MERGE_COINS_BATCH_SIZE,
       MERGE_COINS_MS_BETWEEN_BATCHES,
-      coinsToPurge.map((coins) => () => this.mergeCoins(coins))
+      coinsToPurge.map((coins) => () => this.mergeCoins(coins, keypair))
     );
   }
 
-  private async mergeCoins(coinIds: string[]) {
+  private async mergeCoins(coinIds: string[], keypair: Keypair) {
     if (coinIds.length > 1) {
       const client = MultiExecutor.createForSubInstances(this.client, (client) => client, {
         signAndExecuteTransaction: MultiExecutor.ExecutionMode.FALLBACK,
@@ -65,17 +69,124 @@ export class SuiCoinProvider {
 
       try {
         const tx = new Transaction();
+        const [firstCoinId, ...remainingCoinIds] = coinIds;
         tx.mergeCoins(
-          tx.gas,
-          coinIds.map((id) => tx.object(id))
+          tx.object(firstCoinId),
+          remainingCoinIds.map((id) => tx.object(id))
         );
 
-        await client.signAndExecuteTransaction({ transaction: tx, signer: this.keypair });
+        await client.signAndExecuteTransaction({ transaction: tx, signer: keypair });
       } catch (e) {
         this.logger.warn(RedstoneCommon.stringifyError(e));
       }
     } else {
       this.logger.log(`Not purging - not enough coins to purge in set`);
     }
+  }
+
+  private async getLatestReceivedCoin(
+    address: string,
+    minBalance: bigint,
+    purgeFun: (address: string[][]) => Promise<void>,
+    coinType = SUI_TYPE_ARG,
+    txLimitCount = COIN_TRANSFER_TX_LOOKUP_COUNT_LIMIT
+  ) {
+    const txs = await this.client.queryTransactionBlocks({
+      filter: { ToAddress: address },
+      options: { showObjectChanges: true },
+      order: "descending",
+      limit: txLimitCount,
+    });
+
+    const { coins, coinsToMerge } = await this.getCoinsWithSufficientBalance(
+      address,
+      minBalance,
+      coinType
+    );
+
+    await purgeFun(coinsToMerge.map((coins) => coins.map((coin) => coin.coinObjectId)));
+
+    if (coins.length === 0) {
+      throw new Error(
+        `Did not find any coin with sufficient balance for account: ${address}, balance: ${minBalance}`
+      );
+    }
+
+    for (const tx of txs.data) {
+      const receivedCoin = tx.objectChanges?.find((change) =>
+        SuiCoinProvider.isReceivedCoin(change, coinType, address)
+      );
+
+      if (!receivedCoin) {
+        continue;
+      }
+
+      const coin = coins.find((c) => c.coinObjectId === receivedCoin.objectId);
+
+      if (!coin) {
+        continue;
+      }
+
+      if (BigInt(coin.balance) >= minBalance) {
+        return receivedCoin.objectId;
+      }
+    }
+
+    return undefined;
+  }
+
+  private async getCoinsWithSufficientBalance(
+    address: string,
+    minBalance: bigint,
+    coinType: string
+  ) {
+    const coins = [];
+    const coinsToMerge = [];
+    let cursor: string | null | undefined = null;
+    let page = 0;
+
+    do {
+      this.logger.info(`Fetching coins of ${address}, page #${page}`);
+      const { data, nextCursor, hasNextPage } = await RedstoneCommon.retry({
+        fn: () =>
+          this.client.getCoins({
+            owner: address,
+            coinType,
+            cursor,
+          }),
+        ...RETRY_CONFIG,
+      })();
+
+      const [sufficient, insufficient] = _.partition(data, (c) => BigInt(c.balance) > minBalance);
+
+      coins.push(...sufficient);
+      if (insufficient.length) {
+        coinsToMerge.push(insufficient);
+      }
+      cursor = hasNextPage ? nextCursor : null;
+      page += 1;
+    } while (cursor && (page < FETCH_COINS_PAGE_COUNT_LIMIT || !coins.length));
+
+    return { coins, coinsToMerge };
+  }
+
+  private static isReceivedCoin(
+    change: SuiObjectChange,
+    coinType: string,
+    address: string
+  ): change is Extract<SuiObjectChange, { type: "created" | "mutated" }> {
+    if (change.type !== "created" && change.type !== "mutated") {
+      return false;
+    }
+
+    if (!change.objectType.includes(coinType)) {
+      return false;
+    }
+
+    if (typeof change.owner !== "object" || !("AddressOwner" in change.owner)) {
+      return false;
+    }
+
+    return change.owner.AddressOwner === address;
   }
 }
