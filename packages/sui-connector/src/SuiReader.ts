7f8969df75c2b7859e74c863f0887d1998f2004c
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-02-11 12:09:55 +0100

    chore: sui issue (#9701)

--- packages/sui-connector/src/SuiReader.ts
+++ packages/sui-connector/src/SuiReader.ts
@@ -1,9 +1,5 @@
-import { PaginatedTransactionResponse, SuiClient, SuiObjectChange } from "@mysten/sui/client";
-import { SUI_TYPE_ARG } from "@mysten/sui/utils";
+import { PaginatedTransactionResponse, SuiClient } from "@mysten/sui/client";
 import { RedstoneCommon } from "@redstone-finance/utils";
-import _ from "lodash";
-
-const TX_LOOKUP_COUNT_LIMIT = 100;
 
 export class SuiReader {
   constructor(private readonly client: SuiClient) {}
@@ -65,84 +61,6 @@ export class SuiReader {
     }));
   }
 
-  async getLatestReceivedCoin(
-    address: string,
-    minBalance: bigint,
-    purgeFun: (address: string[][]) => Promise<void>,
-    coinType = SUI_TYPE_ARG,
-    txLimitCount = TX_LOOKUP_COUNT_LIMIT
-  ) {
-    const txs = await this.client.queryTransactionBlocks({
-      filter: { ToAddress: address },
-      options: { showObjectChanges: true },
-      order: "descending",
-      limit: txLimitCount,
-    });
-
-    const { coins, coinsToMerge } = await this.getCoinsWithSufficientBalance(
-      address,
-      minBalance,
-      coinType
-    );
-
-    await purgeFun(coinsToMerge.map((coins) => coins.map((coin) => coin.coinObjectId)));
-
-    if (coins.length === 0) {
-      throw new Error(
-        `Did not find any coin with sufficient balance for account: ${address}, balance: ${minBalance}`
-      );
-    }
-
-    for (const tx of txs.data) {
-      const receivedCoin = tx.objectChanges?.find((change) =>
-        SuiReader.isReceivedCoin(change, coinType, address)
-      );
-
-      if (!receivedCoin) {
-        continue;
-      }
-
-      const coin = coins.find((c) => c.coinObjectId === receivedCoin.objectId);
-
-      if (!coin) {
-        continue;
-      }
-
-      if (BigInt(coin.balance) >= minBalance) {
-        return receivedCoin.objectId;
-      }
-    }
-
-    return undefined;
-  }
-
-  async getCoinsWithSufficientBalance(address: string, minBalance: bigint, coinType: string) {
-    const maxPagesToFetch = 10;
-    const coins = [];
-    const coinsToMerge = [];
-    let cursor = null;
-    let page = 0;
-
-    do {
-      const { data, nextCursor, hasNextPage } = await this.client.getCoins({
-        owner: address,
-        coinType,
-        cursor,
-      });
-
-      const [sufficient, insufficient] = _.partition(data, (c) => BigInt(c.balance) > minBalance);
-
-      coins.push(...sufficient);
-      if (insufficient.length) {
-        coinsToMerge.push(insufficient);
-      }
-      cursor = hasNextPage ? nextCursor : null;
-      page += 1;
-    } while (cursor && (page <= maxPagesToFetch || !coins.length));
-
-    return { coins, coinsToMerge };
-  }
-
   private async findLatestVersionAtCheckpoint(objectId: string, checkpointNumber: number) {
     await RedstoneCommon.waitForBlockNumber(
       () => this.client.getLatestCheckpointSequenceNumber().then(Number),
@@ -191,25 +109,5 @@ export class SuiReader {
     }
 
     return undefined;
-  }
-
-  private static isReceivedCoin(
-    change: SuiObjectChange,
-    coinType: string,
-    address: string
-  ): change is Extract<SuiObjectChange, { type: "created" | "mutated" }> {
-    if (change.type !== "created" && change.type !== "mutated") {
-      return false;
-    }
-
-    if (!change.objectType.includes(coinType)) {
-      return false;
-    }
-
-    if (typeof change.owner !== "object" || !("AddressOwner" in change.owner)) {
-      return false;
-    }
-
-    return change.owner.AddressOwner === address;
   }
 }
