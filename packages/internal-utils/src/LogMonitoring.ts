304911a1c00e17c28c8130ad7062f4731656b456
Michal_Konopka <konopek68@gmail.com>
2026-02-11 17:09:30 +0100

    MultiBrokerMedianAggregator (#9370)

--- packages/internal-utils/src/LogMonitoring.ts
+++ packages/internal-utils/src/LogMonitoring.ts
@@ -25,6 +25,7 @@ export enum LogMonitoringType {
   HIP3_NOT_ENOUGH_SAMPLES_FOR_EMA = "HIP3_NOT_ENOUGH_SAMPLES_FOR_EMA",
   HIP3_FAILED_TO_FETCH_DEX_META = "HIP3_FAILED_TO_FETCH_DEX_META",
   HIP3_MAX_DEVIATION_BETWEEN_UPDATES_CROSSED = "HIP3_MAX_DEVIATION_BETWEEN_UPDATES_CROSSED",
+  HIP3_STALE_REBASE_FACTOR = "HIP3_STALE_REBASE_FACTOR",
   // when fallback is activated but we get nonce duplicated error this means that fallback and main
   // when fallback is activated, but we get nonce duplicated error this means that fallback and main
   // in parallel are trying to deliver data, this SHOULD happen only once when main was down and later
