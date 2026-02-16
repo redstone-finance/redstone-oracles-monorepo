c4c8df9e70ea0a6d31ea9bb627c8445a4225116d
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-02-16 15:35:38 +0100

    chore: batch stellar ttls (#9777)

--- packages/utils/src/common/batch-promises.ts
+++ packages/utils/src/common/batch-promises.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { assertThenReturnOrFail } from "./errors";
 import { sleep } from "./time";
 
@@ -7,19 +8,39 @@ export async function batchPromises<T>(
   promises: (() => Promise<T>)[],
   failOnError = false
 ): Promise<Awaited<T>[]> {
+  const { results, errors } = await batchPromisesAndSplitSettlements(
+    promises,
+    batchSize,
+    msBetweenBatches
+  );
+
+  return assertThenReturnOrFail(results, errors, "batch operation failed", failOnError);
+}
+
+export async function batchPromisesAndSplitSettlements<T>(
+  promises: (() => Promise<T>)[],
+  batchSize: number,
+  msBetweenBatches: number
+): Promise<{ results: Awaited<T>[]; errors: Error[] }> {
   const results = [];
   const errors = [];
   for (let i = 0; i < promises.length; i += batchSize) {
-    if (i !== 0) {
+    if (i !== 0 && msBetweenBatches) {
       await sleep(msBetweenBatches);
     }
     const batch = promises.slice(i, i + batchSize);
     const batchResults = await Promise.allSettled(batch.map((f) => f()));
-    const filteredResults = batchResults.filter((result) => result.status === "fulfilled");
+    const [filteredResults, filteredErrors] = _.partition(
+      batchResults,
+      (result) => result.status === "fulfilled"
+    );
     results.push(...filteredResults.map((result) => result.value));
-    const filteredErrors = batchResults.filter((result) => result.status === "rejected");
     errors.push(...filteredErrors.map((r) => r.reason as Error));
   }
 
-  return assertThenReturnOrFail(results, errors, "batch operation failed", failOnError);
+  return { results, errors };
+}
+
+export async function splitAllSettled<T>(promises: (() => Promise<T>)[]) {
+  return await batchPromisesAndSplitSettlements(promises, promises.length, 0);
 }
