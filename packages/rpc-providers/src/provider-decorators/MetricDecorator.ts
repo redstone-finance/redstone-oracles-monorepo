89935d24b4122e567603faae7c849c39fe3aac12
Jak0bJJ <93732026+Jak0bJJ@users.noreply.github.com>
2026-02-25 12:30:12 +0100

    Add send metric decorator for relayers telemetry (#9053)

--- packages/rpc-providers/src/provider-decorators/MetricDecorator.ts
+++ packages/rpc-providers/src/provider-decorators/MetricDecorator.ts
@@ -1,9 +1,11 @@
 import { BlockTag, TransactionRequest } from "@ethersproject/abstract-provider";
 import { TelemetryPoint } from "@redstone-finance/internal-utils";
 import { sanitizeLogMessage } from "@redstone-finance/utils";
-import { providers } from "ethers";
+import { BigNumber, providers } from "ethers";
 import { Deferrable } from "ethers/lib/utils";
 import { getProviderNetworkInfo, ReportMetricFn } from "../common";
+
+type FeeHistoryResponse = { reward: string[] };
 
 export function CallMetricDecorator(
   factory: () => providers.Provider,
@@ -14,22 +16,17 @@ export function CallMetricDecorator(
     const { chainId, url } = getProviderNetworkInfo(provider);
 
     const oldCall = provider.call.bind(provider);
-    provider.call = async (transaction: Deferrable<TransactionRequest>, blockTag?: BlockTag) => {
-      const start = performance.now();
-      let isFailure = false;
-      try {
-        const result = await oldCall(transaction, blockTag);
-        isFailure = result === "0x";
-        return result;
-      } catch (e) {
-        isFailure = true;
-        throw e;
-      } finally {
-        const end = performance.now();
-        const point = createTelemetryPoint("call", chainId, url, isFailure, end - start);
-        reportMetric(point);
-      }
-    };
+    provider.call = async (transaction: Deferrable<TransactionRequest>, blockTag?: BlockTag) =>
+      await timeMethod(
+        () => oldCall(transaction, blockTag),
+        reportMetric,
+        (duration, isFailure, result) => {
+          isFailure = isFailure || result === "0x";
+          const point = createTelemetryPoint("call", chainId, url, isFailure, duration);
+          return point;
+        }
+      );
+
     return provider;
   };
 
@@ -42,32 +39,117 @@ export function GetBlockNumberMetricDecorator(
 ) {
   const newFactory = () => {
     const provider = factory();
-
     const { chainId, url } = getProviderNetworkInfo(provider);
-    const oldGetBlockNumber = provider.getBlockNumber.bind(provider);
-    provider.getBlockNumber = async () => {
-      const start = performance.now();
-      let isFailure = false;
-      let blockNumber = -1;
 
-      try {
-        const result = await oldGetBlockNumber();
-        blockNumber = result;
-        return result;
-      } catch (e) {
-        isFailure = true;
-        throw e;
-      } finally {
-        const end = performance.now();
-        const point = createTelemetryPoint("getBlockNumber", chainId, url, isFailure, end - start);
-        point.floatField("blockNumber", blockNumber);
-        reportMetric(point);
-      }
-    };
+    const oldGetBlockNumber = provider.getBlockNumber.bind(provider);
+    provider.getBlockNumber = async () =>
+      await timeMethod(
+        () => oldGetBlockNumber(),
+        reportMetric,
+        (duration, isFailure, result) => {
+          const point = createTelemetryPoint("blockNumber", chainId, url, isFailure, duration);
+          point.floatField("blockNumber", isFailure ? 0 : Number(result));
+          return point;
+        }
+      );
+
     return provider;
   };
 
   return newFactory;
+}
+
+const methodsToCollectTelemetryFrom = [
+  "eth_estimateGas",
+  "eth_blockNumber",
+  "eth_maxPriorityFeePerGas",
+  "eth_call",
+  "eth_feeHistory",
+];
+
+export function SendMetricDecorator(
+  factory: () => providers.Provider,
+  reportMetric: ReportMetricFn
+) {
+  const newFactory = () => {
+    const provider = factory();
+    const { chainId, url } = getProviderNetworkInfo(provider);
+    // send doesn't exist in providers.Provider only in providers that inherit from it (eg. JsonRpcProvider)
+    if ("send" in provider && typeof provider.send === "function") {
+      const oldSend = provider.send.bind(provider) as (
+        method: string,
+        params: Array<unknown> | Record<string, unknown>
+      ) => Promise<unknown>;
+      provider.send = async (method: string, params: unknown[]) =>
+        !methodsToCollectTelemetryFrom.includes(method)
+          ? await oldSend(method, params)
+          : await timeMethod(
+              () => oldSend(method, params),
+              reportMetric,
+              (duration, isFailure, result) => {
+                const point = createTelemetryPoint(
+                  method.replace("eth_", ""),
+                  chainId,
+                  url,
+                  isFailure,
+                  duration
+                );
+                if (
+                  method === "eth_estimateGas" ||
+                  method === "eth_blockNumber" ||
+                  method === "eth_maxPriorityFeePerGas"
+                ) {
+                  point.floatField(
+                    method.replace("eth_", ""),
+                    isFailure ? 0 : parseInt((result as BigNumber).toString())
+                  );
+                } else if (method === "eth_call") {
+                  point.tag(
+                    "isFailure",
+                    isFailure ? String(true) : ((result as string) === "0x").toString()
+                  );
+                } else if (method === "eth_feeHistory") {
+                  if (!isFailure && (result as FeeHistoryResponse).reward.length > 0) {
+                    const rewardsPerBlockForPercentile = (result as FeeHistoryResponse).reward
+                      .flat()
+                      .map((hex: string) => parseInt(hex, 16));
+                    const maxRewardsPerBlockForPercentile = Math.max(
+                      ...rewardsPerBlockForPercentile
+                    );
+                    point.floatField(
+                      "maxRewardsPerBlockForPercentile",
+                      maxRewardsPerBlockForPercentile
+                    );
+                  }
+                }
+                return point;
+              }
+            );
+    }
+    return provider;
+  };
+  return newFactory;
+}
+
+async function timeMethod<T>(
+  fn: () => Promise<T>,
+  reportMetric: ReportMetricFn,
+  pointCreator: (duration: number, isFailure: boolean, result: unknown) => TelemetryPoint
+): Promise<T> {
+  const start = performance.now();
+  let result: T | undefined;
+  let isFailure = false;
+
+  try {
+    result = await fn();
+    return result;
+  } catch (e) {
+    isFailure = true;
+    throw e;
+  } finally {
+    const duration = performance.now() - start;
+    reportMetric(pointCreator(duration, isFailure, result));
+  }
 }
 
 function createTelemetryPoint(
