6579a734e05c8b4d7265fa48b39037ed8e74b463
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-02-26 13:47:44 +0100

    chore: bump sui to v2 (#9800)

--- packages/sui-connector/scripts/get-rpc-urls.ts
+++ packages/sui-connector/scripts/get-rpc-urls.ts
@@ -1,7 +1,7 @@
 import { fetchChainConfigs, getChainConfigByNetworkId } from "@redstone-finance/chain-configs";
 import { constructNetworkId, RedstoneCommon } from "@redstone-finance/utils";
 import z from "zod";
-import { getSuiChainId, SuiNetworkName } from "../src";
+import { getSuiChainId, GRAPHQL_URLS, SuiNetworkName } from "../src";
 
 export async function getRpcUrls(network: SuiNetworkName) {
   const rpcUrls = RedstoneCommon.getFromEnv("RPC_URLS", z.array(z.url()).optional());
@@ -13,4 +13,10 @@ export async function getRpcUrls(network: SuiNetworkName) {
       constructNetworkId(getSuiChainId(network), "sui")
     ).publicRpcUrls
   );
+}
+
+export function getGraphQLUrls(network: SuiNetworkName) {
+  const urls = RedstoneCommon.getFromEnv("GRAPHQL_URLS", z.array(z.url()).optional());
+
+  return urls ?? [GRAPHQL_URLS[network]];
 }
