b34b81c76b88ae5b4715f005ee1af3c5c8ca03ae
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-03-26 19:57:24 +0100

    chore: contract updater for canton (#10278)

---
+++ packages/canton-connector/src/CantonContractUpdater.ts
@@ -0,0 +1,63 @@
+import {
+  ContractUpdateContext,
+  ContractUpdater,
+  ContractUpdateStatus,
+} from "@redstone-finance/multichain-kit";
+import { ContractParamsProvider } from "@redstone-finance/sdk";
+import { FP } from "@redstone-finance/utils";
+import { CantonClient } from "./CantonClient";
+import { ActiveContractData } from "./utils";
+
+export interface CantonChoiceExerciser {
+  exerciseWriteChoice<Res, Arg extends object>(
+    updateClient: CantonClient,
+    argument: Arg
+  ): Promise<Res>;
+  onError(): void;
+}
+
+export class CantonContractUpdater implements ContractUpdater {
+  constructor(
+    private readonly exerciser: CantonChoiceExerciser,
+    private readonly updateClient: CantonClient
+  ) {}
+
+  async update(
+    paramsProvider: ContractParamsProvider,
+    context: ContractUpdateContext
+  ): Promise<ContractUpdateStatus> {
+    const txResult = await FP.tryCallAsyncStringifyError(
+      async () =>
+        await this.exerciser.exerciseWriteChoice<ActiveContractData | string, object>(
+          this.updateClient,
+          await CantonContractUpdater.getPayloadArguments(paramsProvider, context)
+        )
+    );
+
+    if (FP.isErr(txResult)) {
+      this.exerciser.onError();
+    }
+
+    return FP.mapStringifyError(txResult, (result) => ({
+      transactionHash: typeof result === "string" ? result : result.contractId,
+    }));
+  }
+
+  static async getPayloadArguments(
+    paramsProvider: ContractParamsProvider,
+    context: ContractUpdateContext
+  ) {
+    return {
+      feedIds: paramsProvider.getArrayifiedFeedIds(),
+      payloadHex: await paramsProvider.getPayloadHex(false, {
+        withUnsignedMetadata: true,
+        metadataTimestamp: context.updateStartTimeMs,
+        componentName: "canton-connector",
+      }),
+    };
+  }
+
+  getSignerAddress() {
+    return this.updateClient.partyId;
+  }
+}
