6eb5a2085489b2e347becf508393a5326b01e025
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-04-02 14:31:17 +0200

    chore: remove gelato package (#10450)

--- packages/on-chain-relayer-common/index.ts
+++ packages/on-chain-relayer-common/index.ts
@@ -1,5 +1,5 @@
 // do NOT use any node.js specific imports in this package (like os, fs, path, etc.),
-// as it is being used by the gelato-relayer, which does not accept such dependencies
+// as it is being used by the goldsky-relayer, which does not accept such dependencies
 export * from "./src/adapter-type-utils";
 export * from "./src/get-relayer-manifest-feeds-with-addresses";
 export * as ManifestFetching from "./src/manifest-fetching";
