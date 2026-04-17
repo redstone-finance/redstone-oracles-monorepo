ae8684df36738deb178053faa2dcd5a6fe21f7af
robal <robert@redstone.finance>
2026-04-17 17:21:11 +0200

    Changes from PR #10719

--- packages/move-connector/jest.config.ts
+++ packages/move-connector/jest.config.ts
@@ -1,14 +1,1 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s"],
-  coverageDirectory: "<rootDir>/coverage",
-  coverageReporters: ["html"],
-  preset: "ts-jest",
-  setupFiles: ["./jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
-};
-
-export default config;
+export { default } from "@redstone-finance/jest-config";
