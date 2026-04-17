080126d8e715253679410601cf5a5eff901da7df
robal <robert@redstone.finance>
2026-04-17 17:21:11 +0200

    fix: jest-config package (#10719)

--- packages/ton-connector/jest.config.ts
+++ packages/ton-connector/jest.config.ts
@@ -1,13 +1,9 @@
 import type { Config } from "@jest/types";
+import redstoneConfig from "@redstone-finance/jest-config";
 
 const config: Config.InitialOptions = {
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s"],
-  coverageDirectory: "<rootDir>/coverage",
-  coverageReporters: ["html"],
-  preset: "ts-jest",
+  ...redstoneConfig,
   setupFilesAfterEnv: ["./jest.setup-after-env.js"],
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
 
 export default config;
