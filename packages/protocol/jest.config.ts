e0562a708d01205b973345044fbd206578427b27
robal <robert@redstone.finance>
2026-04-17 17:21:11 +0200

    fix: jest-config package (#10719)

--- packages/protocol/jest.config.ts
+++ packages/protocol/jest.config.ts
@@ -1,8 +1,1 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s"],
-  coverageDirectory: "<rootDir>/coverage",
-  coverageReporters: ["html"],
-  preset: "ts-jest",
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
-};
+export { default } from "@redstone-finance/jest-config";
--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -1,12 +1,1 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s"],
-  coverageDirectory: "<rootDir>/coverage",
-  coverageReporters: ["html"],
-  preset: "ts-jest",
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
-};
-
-export default config;
+export { default } from "@redstone-finance/jest-config";
