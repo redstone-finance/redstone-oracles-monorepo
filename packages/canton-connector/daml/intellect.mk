0105c86215e98230338c6a4eb82cec493d52f8bb
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-04-20 18:38:33 +0200

    feat: canton traffic weighted factory (#10753)

--- packages/canton-connector/daml/intellect.mk
+++ packages/canton-connector/daml/intellect.mk
@@ -3,22 +3,22 @@ sinclude ../.env
 CANTON_API=$(PARTICIPANT)$(API_PATH)
 PACKAGE_IDS=package-ids.json
 
-ADAPTER_NAME=RedStoneAdapter-v16-0.4.0
+ADAPTER_NAME=RedStoneAdapter-v17-0.4.0
 ADAPTER_TEMPLATE_ID=$(shell jq -r '.templates.adapter' $(PACKAGE_IDS))
 
-CORE_NAME=RedStoneCore-v16-0.4.0
+CORE_NAME=RedStoneCore-v17-0.4.0
 CORE_TEMPLATE_ID=$(shell jq -r '.templates.core' $(PACKAGE_IDS))
 CORE_CLIENT_TEMPLATE_ID=$(shell jq -r '.templates.core_client' $(PACKAGE_IDS))
 
-FACTORY_NAME=RedStonePricePillFactory-v16-0.4.0
+FACTORY_NAME=RedStonePricePillFactory-v17-0.4.0
 FACTORY_TEMPLATE_ID=$(shell jq -r '.templates.factory' $(PACKAGE_IDS))
 
-FACTORY_ID=0084b099b9bfae802b94d95a40d31df83e768b44f82b2301e30a519df8cb99f0c7ca121220f1e3ec9b32be23d97115807831744ec5776151365c082b28dc0162efcf12208a
+FACTORY_ID=00dcf463a3ebbc003a95f06cb738fc4c70abdc760fa52d34da0beed3a2896559dcca12122055635c0eb3cf22584577261b921fc33afdd6ef5304301c39b903876f2c4b1af3
 
-REWARD_FACTORY_NAME=RedStoneRewardFactory-v16-0.4.0
+REWARD_FACTORY_NAME=RedStoneRewardFactory-v17-0.4.0
 REWARD_FACTORY_TEMPLATE_ID=$(shell jq -r '.templates.reward_factory' $(PACKAGE_IDS))
 
-REWARD_FACTORY_ID=00c4f0617f731ea035905db5ae18ffec76ca26820369e2ea08259775c604d97edbca121220093dadb7bdc6acb801fffa5624b87e0c7e5de926a6da4eb19c18066bc2817c0f
+REWARD_FACTORY_ID=0089c0c6c1fc974986d0f25f7189bf16ecc2daa2a3f55584e9522d60a5198415b9ca121220b3c2a04dfb026ba762ea444c6cffa631ac5ec72f9648eae1ab8c52f94abedd3a
 
 IADAPTER_TEMPLATE_ID=$(shell jq -r '.interfaces.adapter' $(PACKAGE_IDS))
 ICORE_TEMPLATE_ID=$(shell jq -r '.interfaces.core' $(PACKAGE_IDS))
@@ -77,7 +77,7 @@ list-parties: get-token
 	  "$(CANTON_API)/v2/parties" | jq '.'
 
 deploy-core: get-token
-	curl -X POST -H "Authorization: Bearer $(TOKEN)" \
+	@curl -X POST -H "Authorization: Bearer $(TOKEN)" \
 	  -H "Content-Type: application/json" \
 	  "$(CANTON_API)/v2/commands/submit-and-wait-for-transaction-tree" \
 	  -d '{ \
@@ -313,3 +313,7 @@ get-active-contracts: get-token
 			}, \
 			"activeAtOffset": '"$$LEDGER_END"' \
 		}' | jq -r '.'
+
+xxx:
+	curl -s -H "Authorization: Bearer $(TOKEN)" \
+	  "$(CANTON_API)/v2/packages" | jq '.packageIds[] | select(endswith("794a7d"))'
