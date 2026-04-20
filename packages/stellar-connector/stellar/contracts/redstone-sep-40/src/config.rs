8f75d402cca0cb44a9ae5429ce23e990482d7c34
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-04-20 14:12:00 +0200

    sep-40: resolution = heartbeat (#10747)

--- packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
+++ packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
@@ -3,3 +3,4 @@ use core::time::Duration;
 pub const ADAPTER_ADDRESS: &str = "CB3FOBVQAAMVTCAQEEB5LQNDO3PBTDMEJVF2UFXAYDGRIIP6Y3UCUCGA";
 pub const DECIMALS: u32 = 8;
 pub const ONE_SEC: Duration = Duration::from_secs(1);
+pub const RESOLUTION: u32 = Duration::from_hours(24).as_secs() as u32;
