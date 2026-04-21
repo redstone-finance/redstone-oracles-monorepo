7b70cf83430e1c53b514223474a26e0a5cb2bb6f
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-04-21 17:09:23 +0200

    Changes from PR #10793

--- packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
+++ packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
@@ -1,6 +1,6 @@
 use core::time::Duration;
 
-pub const ADAPTER_ADDRESS: &str = "CB3FOBVQAAMVTCAQEEB5LQNDO3PBTDMEJVF2UFXAYDGRIIP6Y3UCUCGA";
+pub const ADAPTER_ADDRESS: &str = "CCZMRM4ZDZTM4Q6NJWZAXWDMDMT54KK6OHC5XMJS4275VTIIALGV3KLS";
 pub const DECIMALS: u32 = 8;
 pub const ONE_SEC: Duration = Duration::from_secs(1);
 pub const RESOLUTION: u32 = Duration::from_hours(24).as_secs() as u32;
