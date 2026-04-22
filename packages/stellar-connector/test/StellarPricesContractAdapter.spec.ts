911dfd8f63cc966b42ea80a29133743b88d28710
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-04-22 16:05:34 +0200

    chore: update adapter in rr tests, reenable ts tests (#10800)

--- packages/stellar-connector/test/StellarPricesContractAdapter.spec.ts
+++ packages/stellar-connector/test/StellarPricesContractAdapter.spec.ts
@@ -17,7 +17,7 @@ const WRITE_TEST_TIMEOUT_MS = 20 * 1_000;
 
 jest.setTimeout(30000);
 
-describe.skip("StellarPricesContractAdapter", () => {
+describe("StellarPricesContractAdapter", () => {
   let adapter: StellarWriteContractAdapter;
   let client: StellarClient;
   let contractOps: StellarContractOps;
