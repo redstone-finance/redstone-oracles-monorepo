abf3b8f5ca4304b74906929d7d5b7e18293217cf
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-04-24 14:45:34 +0200

    chore: new sep-40 (#10853)

--- packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
+++ packages/stellar-connector/stellar/contracts/redstone-sep-40/src/config.rs
@@ -1,6 +1,6 @@
 use core::time::Duration;
 
-pub const ADAPTER_ADDRESS: &str = "CB3FOBVQAAMVTCAQEEB5LQNDO3PBTDMEJVF2UFXAYDGRIIP6Y3UCUCGA";
+pub const ADAPTER_ADDRESS: &str = "CBIHT4HVRIT5OMVLSXZ44J2ZAXYBDDGOSCN3LTN2DOC6SWHDS5IP6BK3";
 pub const DECIMALS: u32 = 8;
 pub const ONE_SEC: Duration = Duration::from_secs(1);
 pub const RESOLUTION: u32 = Duration::from_hours(24).as_secs() as u32;
