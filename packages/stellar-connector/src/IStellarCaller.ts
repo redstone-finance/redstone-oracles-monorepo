3ab4d023b8a5c233fa70cf10dd4e6d7952459b2f
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-04-30 15:08:13 +0200

    Changes from PR #10935

--- packages/stellar-connector/src/IStellarCaller.ts
+++ packages/stellar-connector/src/IStellarCaller.ts
@@ -1,6 +1,16 @@
 import { Contract, xdr } from "@stellar/stellar-sdk";
 
+export interface IStellarCallerDelegate {
+  waitForBlockNumber(blockNumber?: number): Promise<void>;
+  simulateInvocation<T>(
+    invocation: StellarInvocation,
+    blockNumber?: number,
+    transform?: (retVal: unknown) => T
+  ): Promise<T>;
+}
+
 export interface IStellarCaller {
+  delegateClient?: WeakRef<IStellarCallerDelegate>;
   call<T>(
     invocation: StellarInvocation,
     blockNumber?: number,
