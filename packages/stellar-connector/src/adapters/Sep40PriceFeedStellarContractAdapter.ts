ac63dcc0b8cbaeabb28fbb43ef0df9d2318faf35
kostekIV <27210860+kostekIV@users.noreply.github.com>
2026-05-13 17:43:37 +0200

    chore: Sep40 deployment (#11134)

--- packages/stellar-connector/src/adapters/Sep40PriceFeedStellarContractAdapter.ts
+++ packages/stellar-connector/src/adapters/Sep40PriceFeedStellarContractAdapter.ts
@@ -1,11 +1,13 @@
 import { PriceFeedAdapter } from "@redstone-finance/multichain-kit";
 import { Contract } from "@stellar/stellar-sdk";
+import { Sep40Asset } from "../sep-40-types";
 import { StellarClient } from "../stellar/StellarClient";
 import { Sep40ContractReader } from "./Sep40ContractReader";
 
 export class Sep40PriceFeedStellarContractAdapter implements PriceFeedAdapter {
   protected readonly contract: Contract;
   private readonly reader: Sep40ContractReader;
+  private asset?: Sep40Asset;
 
   constructor(
     client: StellarClient,
@@ -29,7 +31,9 @@ export class Sep40PriceFeedStellarContractAdapter implements PriceFeedAdapter {
   }
 
   async getPriceAndTimestamp(blockNumber?: number) {
-    const data = await this.reader.readLatestData(this.feedId, blockNumber);
+    this.asset ??= await this.reader.feedToAsset(this.feedId);
+
+    const data = await this.reader.readLatestData(this.asset, blockNumber);
     if (!data) {
       throw new Error(`Couldn't find latest data for ${this.feedId}`);
     }
@@ -38,7 +42,9 @@ export class Sep40PriceFeedStellarContractAdapter implements PriceFeedAdapter {
   }
 
   async getRoundData(roundId: bigint, blockNumber?: number) {
-    const data = await this.reader.readRoundData(this.feedId, roundId, blockNumber);
+    this.asset ??= await this.reader.feedToAsset(this.feedId);
+
+    const data = await this.reader.readRoundData(this.asset, roundId, blockNumber);
     if (!data) {
       throw new Error(`Couldn't find round data for ${this.feedId} in round ${roundId}`);
     }
