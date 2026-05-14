36d7f4a3507aada4a1b95f2a5dead848048a1520
robal <robert@redstone.finance>
2026-05-14 08:48:56 +0200

    chore: unify @aws-sdk/* versions and deduplicate other packages versions (#11157)

--- packages/casper-connector/src/casper/casper-blake2b.ts
+++ packages/casper-connector/src/casper/casper-blake2b.ts
@@ -1,4 +1,4 @@
-import { blake2b } from "@noble/hashes/blake2b";
+import { blake2b } from "@noble/hashes/blake2";
 import { arrayify, hexlify } from "ethers/lib/utils";
 
 export function casperBlake2b(dataHex: string, withPrefix = false) {
