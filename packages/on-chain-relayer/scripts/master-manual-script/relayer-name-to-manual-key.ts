3dc1be9a4271c5fa393ba71c69bd294f4136d8e9
Michał Szynwelski <szynek@redstone.finance>
2026-06-04 16:47:56 +0200

    remove mento celo (#11528)

--- packages/on-chain-relayer/scripts/master-manual-script/relayer-name-to-manual-key.ts
+++ packages/on-chain-relayer/scripts/master-manual-script/relayer-name-to-manual-key.ts
@@ -2,6 +2,5 @@ export const relayerNameToManualKeyArn: Record<string, string> = {
   b2MultiPriceFeed: "/prod/on-chain-relayer/b2/multi-price-feed/manual/private_key",
   ethereumEtherfiWeeth: "/prod/on-chain-relayer/ethereum/etherfi/weeth/manual/private_key",
   ethereumPufStaking: "/prod/on-chain-relayer/ethereum/puf/staking/manual/private-key",
-  mentoCeloMainnet: "/prod/on-chain-relayer/mento/celo/private-key",
   arbitrumOneMultiFeed: "/prod/on-chain-relayer/arbitrum/multi-feed/manual/private-key",
 };
