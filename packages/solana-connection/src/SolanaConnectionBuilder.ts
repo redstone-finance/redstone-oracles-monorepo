2029e787c313522db8c133a317609b03d1fb9d1c
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2026-09-01 23:13:50 +0200

    Changes from PR #12795

--- packages/solana-connection/src/SolanaConnectionBuilder.ts
+++ packages/solana-connection/src/SolanaConnectionBuilder.ts
@@ -1,15 +1,24 @@
 import { ChainTypeEnum, MultiExecutor } from "@redstone-finance/utils";
-import { Cluster, Commitment, Connection } from "@solana/web3.js";
+import { Cluster, Commitment, Connection, ConnectionConfig } from "@solana/web3.js";
 import { connectToCluster } from "./cluster";
 import { getSolanaChainId, getSolanaCluster } from "./network-ids";
 import { RedStoneConnection } from "./RedStoneConnection";
 import { API_TYPE_JITO, SolanaApi } from "./SolanaApi";
 import { SolanaRpcOpNormalizer } from "./SolanaRpcOpNormalizer";
 
+type HttpAgent = Exclude<ConnectionConfig["httpAgent"], false | undefined>;
+
 export class SolanaConnectionBuilder extends MultiExecutor.ClientBuilder<Connection> {
   protected override chainType = ChainTypeEnum.enum.solana;
   protected override telemetryOpNormalizer = new SolanaRpcOpNormalizer();
   private shouldUseRedStoneConnection = false;
+  private httpAgent?: HttpAgent;
+
+  withHttpAgent(httpAgent?: HttpAgent) {
+    this.httpAgent = httpAgent;
+
+    return this;
+  }
 
   withCluster(cluster: Cluster) {
     return this.withChainId(getSolanaChainId(cluster));
@@ -45,6 +54,7 @@ export class SolanaConnectionBuilder extends MultiExecutor.ClientBuilder<Connect
     const commitmentOrConfig = {
       commitment: "confirmed" as Commitment,
       disableRetryOnRateLimit: true,
+      ...(this.httpAgent && { httpAgent: this.httpAgent }),
     };
 
     if (this.shouldUseRedStoneConnection) {
